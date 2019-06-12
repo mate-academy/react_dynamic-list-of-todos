a1968c898c30c1f9fb476f336b1fb6b1eae264b7
AlexHladun <gefest.gl@gmail.com>
2019-06-12 14:29:39 +0300

    Solution

---
+++ list-of-todos/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList.jsx';
+
+function App() {
+  return (
+    <table className="App">
+      <TodoList />
+    </table>
+  );
+}
+
+export default App;
