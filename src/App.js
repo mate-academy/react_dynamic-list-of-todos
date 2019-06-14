7da0b30ce64664527fb1cc708d1d74473eba730e
ronyshchenko <ronyshchenko@gmail.com>
2019-06-14 16:13:17 +0300

    two_commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import { TodoList } from './components/TodoList';
+import { Search } from './components/Search';
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       todos: null,
       users: null,
       isTODOSLoaded: false,
       isUsersLoaded: false,
-      loadingCount: 0
+      loadingCount: 0,
+      todosWithUsers: null,
+      filteredTodosWithUsers: null,
+      searchBy: ''
     };
   }
-  
+
   loadData = () => {
     this.makeRequest(`${BASE_URL}/todos`, this.handleTODOsLoad);
     this.makeRequest(`${BASE_URL}/users`, this.handleUsersLoad);
@@ -24,65 +28,91 @@ export class App extends Component {
 
   handleTODOsLoad = xhr => () => {
     this.setState(prevState => ({
-        todos: JSON.parse(xhr.responseText),
-        isTODOSLoaded: true,
-        loadingCount: --prevState.loadingCount
-      }));
+      todos: JSON.parse(xhr.responseText),
+      isTODOSLoaded: true,
+      loadingCount: prevState.loadingCount - 1,
+    }));
   };
-  
+
   handleUsersLoad = xhr => () => {
-    this.setState(prevState => ({
+    this.setState(
+      prevState => ({
         users: JSON.parse(xhr.responseText),
         isUsersLoaded: true,
-        loadingCount: --prevState.loadingCount
-      }));
+        loadingCount: prevState.loadingCount - 1,
+      }),
+      this.checkData
+    );
   };
- 
+
   makeRequest(url, cb) {
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', url);
     xhr.addEventListener('load', cb(xhr));
     xhr.send();
-    this.setState(prevState => ({ loadingCount: ++prevState.loadingCount }));
+    this.setState(prevState => ({ loadingCount: prevState.loadingCount + 1 }));
   }
-  
+
+  search = (event) => {
+    const searchBy = event.target.value;
+
+    this.setState(prevState => {
+      const filteredTodosWithUsers = prevState.todosWithUsers.filter(todo => todo.title.includes(searchBy));
+      return {
+        searchBy,
+        filteredTodosWithUsers,
+      };
+    });
+  }
+
+  checkData() {
+    const { isTODOSLoaded, isUsersLoaded, users, todos } = this.state;
+    if (isTODOSLoaded && isUsersLoaded) {
+      const usersMap = users.reduce((acc, user) => ({ ...acc, [user.id]: user }), {});
+      const todosWithUsers = todos.map(todo => ({ ...todo, user: usersMap[todo.userId] }));
+      this.setState({
+        filteredTodosWithUsers: todosWithUsers,
+        todosWithUsers,
+      });
+    }
+
+  }
+
   render() {
     const {
       isTODOSLoaded,
       isUsersLoaded,
-      todos,
-      users,
-      loadingCount
+      loadingCount,
+      filteredTodosWithUsers,
+      searchBy
     } = this.state;
-    
-    const isLoaded = isTODOSLoaded && isUsersLoaded;
+
+    const isLoaded = isTODOSLoaded && isUsersLoaded && filteredTodosWithUsers !== null;
     const isLoading = loadingCount !== 0;
 
-    const button =(
-    <button 
+    const button = (
+      <button
         type="button"
         disabled={isLoaded}
         onClick={this.loadData}
-        >
-          {isLoading ? 'Loading...' : 'Load data'}
-        </button>
-      );
-    
+      >
+        {isLoading ? 'Loading...' : 'Load data'}
+      </button>
+    );
+
     if (!isLoaded) {
       return button;
-        
-    } else {
-      const usersMap = users.reduce((acc, user) => ({...acc, [user.id]: user}), {});
-      const todosWithUsers = todos.map(todo => ({...todo, user: usersMap[todo.userId]}));
-      return (
-      <>
-      {button}  
-      <TodoList todos={todosWithUsers} />);
-      </>
-      );  
     }
-}
+
+    return (
+      <>
+        <Search search={this.search} value={searchBy} />
+        {button}
+        <TodoList todos={filteredTodosWithUsers} />
+      </>
+    );
+  }
 }
 
 export default App;
