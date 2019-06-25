f07e6c6f4104a919c4101ade9a3c6c513b4b162c
StasGostev <spaceomen@gmail.com>
2019-06-25 12:01:44 +0300

    'Fixed'

--- src/components/User.js
+++ src/components/User.js
@@ -1,15 +1,6 @@
-import React from 'react';
+import React from "react";
 
-// class User extends React.Component {
-//     render() {
-//         return (
-//             <td>{this.props.name}</td>
-//         )
-//     }
-// }
 function User(props) {
-    return (
-        <td>{props.name}</td>
-    );
+  return <td>{props.name}</td>;
 }
 export default User;
