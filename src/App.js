62804c786ab07cf9064393d9ae54e4ed3161e997
artem-meleshko <timonlondon@hotmail.com>
2019-07-11 14:26:59 +0300

    Fixes according to comments

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import { getTodos, getUsers } from './getDataFromServer';
 
 import './App.css';
 
-const getSortedTodos = (todos, key) => {
-  let callback = (typeof todos[0][key] === 'string')
-    ? (todoA, todoB) => todoA[key].localeCompare(todoB[key])
-    : (todoA, todoB) => todoA[key] - todoB[key];
+const getSortedTodos = (todos, key, direction) => {
+  let callback;
+
+  if (typeof todos[0][key] === 'string') {
+    if (direction) {
+      callback = (todoA, todoB) => todoB[key].localeCompare(todoA[key]);
+    } else {
+      callback = (todoA, todoB) => todoA[key].localeCompare(todoB[key]);
+    }
+  } else {
+    if (direction) {
+      callback = (todoA, todoB) => todoB[key] - todoA[key];
+    } else {
+      callback = (todoA, todoB) => todoA[key] - todoB[key];
+    }
+  }
 
   if (key === 'user') {
-    callback = (todoA, todoB) => todoA.user.name.localeCompare(todoB.user.name);
+    if (direction) {
+      callback = (todoA, todoB) => todoA.user.name.localeCompare(todoB.user.name);
+    } else {
+      callback = (todoA, todoB) => todoB.user.name.localeCompare(todoA.user.name);
+    }
   }
 
   return todos.sort(callback);
@@ -23,13 +39,15 @@ class App extends React.Component {
     isLoaded: false,
     isLoading: false,
     sortField: 'id',
+    sortDirection: false,
   };
 
   sortBy = (sortField) => {
-    this.setState({
-      todos: getSortedTodos(this.state.todos, sortField),
+    this.setState(prevState => ({
+      visibleTodos: getSortedTodos(this.state.todos, sortField, this.state.sortDirection),
       sortField: sortField,
-    });
+      sortDirection: !prevState.sortDirection,
+    }));
   };
 
   handleClick = async() => {
@@ -48,6 +66,7 @@ class App extends React.Component {
     setTimeout(() => {
       this.setState({
         todos: todosWithUser,
+        visibleTodos: todosWithUser,
         isLoaded: true,
         isLoading: false,
       });
@@ -55,23 +74,30 @@ class App extends React.Component {
   };
 
   render() {
-    const { todos, sortField } = this.state;
+    const { visibleTodos, sortField } = this.state;
 
     return (
       <section className="table__blur">
         {this.state.isLoaded ? (
           <div className="table__blur">
-            <h1>Todo List</h1>
+
             <h2 className="table__name">Todo List was sorted by {sortField}</h2>
 
-            <button onClick={() => this.sortBy('completed')}>Completed</button>
-            <button onClick={() => this.sortBy('id')}>ID</button>
-            <button onClick={() => this.sortBy('user')}>User</button>
-            <button onClick={() => this.sortBy('title')}>Title</button>
+            <table className="table__blur">
+              <thead>
+                <tr>
+                  <th onClick={() => this.sortBy('id')}>ID <span className="table__arrow">↓↑</span></th>
+                  <th onClick={() => this.sortBy('completed')}>Completed <span className="table__arrow">↓↑</span></th>
+                  <th onClick={() => this.sortBy('title')}>Title <span className="table__arrow">↓↑</span></th>
+                  <th onClick={() => this.sortBy('user')}>User <span className="table__arrow">↓↑</span></th>
+                </tr>
+              </thead>
 
-            <TodoList
-              items={this.state.todos}
-            />
+              <TodoList
+                items={visibleTodos}
+              />
+
+            </table>
           </div>
         ) : (
           <div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -5,21 +5,11 @@ import './App.css';
 import TodoItem from './TodoItem';
 
 const TodoList = ({ items }) => (
-  <table className="table__blur">
-    <thead>
-      <tr>
-        <th>ID <span className="table__arrow">↓↑</span></th>
-        <th>Completed <span className="table__arrow">↓↑</span></th>
-        <th>Title <span className="table__arrow">↓↑</span></th>
-        <th>User <span className="table__arrow">↓↑</span></th>
-      </tr>
-    </thead>
-    <tbody>
-      {items.map(item => (
-        <TodoItem data={item} key={item.id} />
-      ))}
-    </tbody>
-  </table>
+  <tbody>
+    {items.map(item => (
+      <TodoItem data={item} key={item.id} />
+    ))}
+  </tbody>
 );
 
 TodoList.propTypes = {
