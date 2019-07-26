487e449b95a2de93a99123239ff7026d49b38f34
Igori4 <igorpilyak@gmail.com>
2019-07-26 10:29:13 +0300

    removed unnesessary component ListItems

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,58 +1,90 @@
 import React from 'react'
 import { getTodos, getUsers } from './todoService'
-import ListItems from './ListItems'
+// import ListItems from './ListItems'
 import {
-    completedSort,
-    titleSort,
-    nameSort,
+  completedSort,
+  titleSort,
+  nameSort,
 } from './api'
 
 class ToDo extends React.Component {
-    state = {
-        todos: [],
-        load: false,
+  state = {
+    todos: [],
+    load: false,
+  }
+  async componentDidMount() {
+    const [todos, users] = await Promise.all([getTodos(), getUsers()])
+
+    let items = this.getTodoWithUser(users, todos);
+
+    this.setState({
+      todos: items,
+      load: true,
+    })
+  }
+
+  sortTodos = (sortField) => {
+    let copyTodos = this.state.todos;
+
+    if (sortField === 'completed') {
+      completedSort(copyTodos, sortField)
     }
-    async componentDidMount() {
-        const [todos, users] = await Promise.all([getTodos(), getUsers()])
-
-        let items = this.getTodoWithUser(users, todos);
-
-        this.setState({
-            todos: items,
-            load: true,
-        })
-    }
-
-    sortTodos = (sortField) => {
-        let copyTodos = this.state.todos;
-
-        if (sortField === 'completed') {
-            completedSort(copyTodos, sortField)
-        }
-        if (sortField === 'title') {
-            titleSort(copyTodos, sortField)
-        } else {
-            nameSort(copyTodos, sortField)
-        }
-
-        this.setState({
-            todos: copyTodos
-        })
+    if (sortField === 'title') {
+      titleSort(copyTodos, sortField)
+    } else {
+      nameSort(copyTodos, sortField)
     }
 
-    getTodoWithUser(users, todos) {
-        return todos.map(todo => ({
-            ...todo,
-            user: users.find(user => user.id === todo.userId)
-        }))
-    }
+    this.setState({
+      todos: copyTodos
+    })
+  }
 
-    render() {
-        let { todos } = this.state;
-        return (
-            this.state.load ? <ListItems sortTodos={this.sortTodos} todos={todos} /> : '...Loading'
-        )
-    }
+  getTodoWithUser(users, todos) {
+    return todos.map(todo => ({
+      ...todo,
+      user: users.find(user => user.id === todo.userId)
+    }))
+  }
+
+  render() {
+    let { todos } = this.state;
+    return (
+      this.state.load ? <table className="table">
+        <thead>
+          <tr>
+            <th onClick={() => {
+              this.sortTodos('completed')
+            }}>Done</th>
+            <th onClick={() => {
+              this.sortTodos('title')
+            }}>Todo</th>
+            <th onClick={() => {
+              this.sortTodos('user')
+            }}>User</th>
+          </tr>
+        </thead>
+        <tbody >
+          {todos.map(todo => {
+            return <tr key={todo.id}>
+              <td>
+                <input type='checkbox' checked={todo.completed} onChange={() => []} />
+              </td>
+
+              <td>
+                {todo.title}
+              </td>
+
+              <td>
+                {todo.user.name}
+              </td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+        : '...Loading'
+    )
+  }
 }
 
 export default ToDo
