2fb09d9a861779c9adff73ff4c4e8740e288f9d7
vladyslav223 <vladyslav223@gmail.com>
2019-09-23 21:47:41 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
 import React from 'react';
+import cx from 'classnames';
 import './App.css';
+import TodoList from './components/TodoList/TodoList';
 
-import todos from './api/todos';
-import users from './api/users';
+class App extends React.Component{
+  state = {
+    active:0,
+    todosWithUsers:[],
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dynamic list of todos</h1>
-    </div>
-  );
+  }
+
+  loadDatas = () => {
+       Promise.all([
+        fetch('https://jsonplaceholder.typicode.com/users').then(users => users.json()),
+        fetch('https://jsonplaceholder.typicode.com/todos').then(todos => todos.json()),
+    ])
+    .then(usersAndTodos => {
+      this.setState({
+        active:1,
+        todosWithUsers: usersAndTodos[1].map(todo => ({
+          ...todo,
+          user: usersAndTodos[0].find(user => user.id === todo.userId),
+        }))
+      })
+    })
+  }
+
+  sortAZ = () => {
+    this.setState( prevState => ({
+      todosWithUsers: [...prevState.todosWithUsers].sort()
+    }))
+  }
+
+  render () {
+    const { todosWithUsers, active } = this.state;
+    return (
+      <>
+        <div className={active === 1 ? 'button-back down' : 'button-back' }>Loading...</div>
+        <button
+          onClick={this.loadDatas}
+          className="button-start"
+          type="button"
+        >Load</button>
+        <button
+        type="button"
+         onClick={this.sortAZ}
+         className="sort"
+         >sort A-Z</button>
+        <TodoList todos = {todosWithUsers} />
+      </>
+      )
+  }
 }
+
+
+
+
+
+
+// function getTodosWithUsers(todosParam, usersParam) {
+//   return todosParam.map(todo => (
+//     {
+//       ...todo,
+//       user: usersParam.find(user => user.id === todo.userId),
+//     }
+//   ));
+//   }
+// const preparedTodos = getTodosWithUsers(todos, users);
 
 export default App;
