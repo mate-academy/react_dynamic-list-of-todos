a4144d2ba91b473fa72d41234704b1d757da8974
yevhen-sahach <antitop04@gmail.com>
2019-09-23 19:03:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import TodoList from './components/TodoList/TodoList';
 
-import todos from './api/todos';
-import users from './api/users';
+class App extends Component {
+  state ={
+    todos: [],
+    isLoading: false,
+    isLoaded: false,
+    isSorted: false,
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dynamic list of todos</h1>
-    </div>
-  );
+  getTodos = () => {
+    this.setState({
+      isLoading: true,
+    });
+
+    Promise.all([
+      fetch('https://jsonplaceholder.typicode.com/todos'),
+      fetch('https://jsonplaceholder.typicode.com/users'),
+    ])
+      .then(([todos, users]) => Promise.all([todos.json(), users.json()]))
+      .then(([todos, users]) => {
+        const usersMap = users
+          .reduce((acc, currentUser) => ({
+            ...acc,
+            [currentUser.id]: currentUser.name,
+          }), {});
+
+        const preparedTodos = todos.map(todo => ({
+          ...todo,
+          user: usersMap[todo.userId],
+        }));
+
+        this.setState({
+          todos: preparedTodos,
+          isLoaded: true,
+        });
+      });
+  }
+
+  sort = () => {
+    this.setState(({ isSorted }) => ({
+      isSorted: !isSorted,
+    }));
+  }
+
+  render() {
+    const {
+      todos,
+      isLoading,
+      isLoaded,
+      isSorted,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">Dynamic list of todos</h1>
+        {(isLoaded && (
+          <>
+            <button
+              type="button"
+              onClick={this.sort}
+              className="mb-3"
+            >
+              Sort
+            </button>
+            <TodoList todos={isSorted
+              ? [...todos].sort(a => (a.completed ? 1 : -1))
+              : todos}
+            />
+          </>
+        ))
+          || (isLoading && (
+            <button type="button">
+              loading...
+            </button>
+          ))
+          || (
+            <button type="button" onClick={this.getTodos}>
+              Get todos
+            </button>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
