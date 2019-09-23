0647f23665506a5b0e4279ae374a81081cc807b3
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-23 16:19:45 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from './components/Button/Button';
+import TodoList from './components/TodoList/TodoList';
+
 import './App.css';
 
-import todos from './api/todos';
-import users from './api/users';
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dynamic list of todos</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [],
+    users: [],
+    notShown: true,
+  };
+
+  componentDidMount() {
+    Promise.all([
+      fetch(TODOS_URL),
+      fetch(USERS_URL),
+    ])
+      .then(([resTodos, resUsers]) => Promise.all(
+        [resTodos.json(), resUsers.json()]
+      ))
+      .then(([dataTodos, dataUsers]) => this.setState({
+        todos: dataTodos,
+        users: dataUsers,
+      }));
+  }
+
+  getTodosWithUsers = (todosList, usersList) => todosList.map(todo => ({
+    ...todo,
+    user: usersList.find(user => user.id === todo.userId),
+  }))
+
+  handleShow = () => {
+    this.setState({
+      notShown: false,
+    });
+  };
+
+  render() {
+    const { todos, users, notShown } = this.state;
+    console.log('todos', todos);
+    console.log('users', users);
+    const preparedTodos = this.getTodosWithUsers(todos, users);
+
+    return (
+      <div className="App">
+        <h1>Dynamic list of todos</h1>
+        <div className="container">
+          {notShown
+            ? (
+              <Button
+                className="btn--start"
+                text="Start"
+                onClick={this.handleShow}
+              />
+            )
+            : (
+              <div className="content">
+                <p>
+                  <span>Todos: </span>
+                  {todos.length}
+                </p>
+
+                <p>
+                  <span>Users: </span>
+                  {users.length}
+                </p>
+                <TodoList todos={preparedTodos} />
+              </div>
+            )
+          }
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import User from '../User/User';
+import { TodoItemProps } from '../../constants/proptypes';
+
+import './TodoItem.css';
+
+const TodoItem = ({ todo }) => {
+  const {
+    title, completed = false, user, id,
+  } = todo;
+
+  const classNames = !completed
+    ? 'todo-list-item'
+    : 'todo-list-item todo-list-item--done';
+
+  return (
+    <span className={classNames}>
+      <span className="todo-list-item__id">{id}</span>
+      <User user={user} />
+      <span className="todo-list-item__name">{title}</span>
+    </span>
+  );
+};
+
+TodoItem.propTypes = TodoItemProps;
+
+export default TodoItem;
