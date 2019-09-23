f4c217ebe0a0b0ebbee857f2cd747b7c032c55e9
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-23 21:11:16 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
 import React from 'react';
+
+import TodoList from './components/TodoList/TodoList';
 import './App.css';
 
-import todos from './api/todos';
-import users from './api/users';
+const todosFromApi = 'https://jsonplaceholder.typicode.com/todos';
+const usersFromApi = 'https://jsonplaceholder.typicode.com/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dynamic list of todos</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [],
+    users: [],
+    isLoading: false,
+  };
+
+  getTodosWithUsers = (todos, users) => todos.map(item => ({
+    ...item,
+    user: users.find(user => user.id === item.userId),
+  }));
+
+  loadData = () => {
+    this.setState({
+      isLoading: true,
+    });
+
+    Promise.all([
+      fetch(todosFromApi),
+      fetch(usersFromApi),
+    ])
+      .then(([todos, users]) => Promise.all([
+        todos.json(),
+        users.json(),
+      ]))
+      .then(([dataTodos, dataUsers]) => this.setState({
+        todos: [...dataTodos],
+        users: [...dataUsers],
+        isLoading: false,
+      }));
+  };
+
+  render() {
+    const { users, todos, isLoading } = this.state;
+
+    const todosWithUsers = this.getTodosWithUsers(todos, users);
+
+    if (isLoading) {
+      return <p className="loading">Loading...</p>;
+    }
+
+    if (todosWithUsers.length === 0) {
+      return (
+        <div className="container">
+          <h1 className="todos-title">Dynamic List of Todos</h1>
+          <p className="no-todos-list">No todos yet</p>
+          <button
+            type="button"
+            onClick={this.loadData}
+            className="info-load"
+          >
+            Load
+          </button>
+        </div>
+      );
+    }
+
+    return <TodoList todos={todosWithUsers} />;
+  }
 }
 
 export default App;
