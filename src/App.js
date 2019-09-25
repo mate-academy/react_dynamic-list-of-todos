9c9dc9d2cd32ca654df9c6372fdb892fffdf5477
KseniiaMazan <ksusamazan@gmail.com>
2019-09-25 13:20:05 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,106 @@
 import React from 'react';
 import './App.css';
 
-import todos from './api/todos';
-import users from './api/users';
+import getTodosFromServer from './api/apiTodos';
+import getUsersFromServer from './api/apiUsers';
+import TodoList from './Components/TodoList/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dynamic list of todos</h1>
-    </div>
-  );
+function getTodosWithUsers(todos, users) {
+  return todos.map(todo => (
+    {
+      ...todo,
+      user: users.find(user => user.id === todo.userId),
+    }
+  ));
+}
+
+class App extends React.Component {
+  state = {
+    todos: [],
+    isLoading: false,
+    originalTodos: [],
+  }
+
+  loadAllTodos = () => {
+    this.setState({ isLoading: true });
+
+    Promise.all([getTodosFromServer(), getUsersFromServer()])
+      .then(([todos, users]) => {
+        this.setState({
+          todos: getTodosWithUsers(todos, users),
+          originalTodos: getTodosWithUsers(todos, users),
+          isLoading: false,
+        });
+      });
+  }
+
+  handleSortTodos = () => {
+    this.setState(prevState => ({
+      todos: [...prevState.todos].sort((a, b) => {
+        if (a.title > b.title) {
+          return 1;
+        }
+        if (a.title < b.title) {
+          return -1;
+        }
+        return 0;
+      }),
+    }));
+  }
+
+  handleResetTodos = () => {
+    this.setState(prevState => ({
+      todos: [...prevState.originalTodos],
+    }));
+  }
+
+  render() {
+    const { todos, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p className="loading-text">TODOs are loading now...</p>;
+    }
+
+    if (todos.length === 0) {
+      return (
+        <button
+          onClick={this.loadAllTodos}
+          type="button"
+          className="data-button"
+        >
+        Load all todos
+        </button>
+      );
+    }
+
+    return (
+      <section className="section-wrapper">
+        <div className="app">
+          <h1 className="main-title">Static list of todos</h1>
+
+          <p className="title">
+            <span>TODOs: </span>
+            {todos.length}
+          </p>
+          <button
+            type="button"
+            onClick={this.handleSortTodos}
+            className="sort-button"
+          >
+            Press to sort by title
+          </button>
+          <button
+            type="button"
+            className="reset-button"
+            onClick={this.handleResetTodos}
+          >
+            Press to reset
+          </button>
+          <TodoList todos={todos} />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
