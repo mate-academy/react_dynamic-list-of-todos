bd9be848cf4d3de016bcf7560c77ab7017d5ceac
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-09-25 09:08:04 +0300

    draft

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TodoListProps } from '../PropTypes/PropTypes';
+import TodoItem from '../TodoItem/TodoItem';
+import './TodoList.css';
+
+const TodoList = ( { todos }) => {
+  return (<div className="todos-list">
+    {todos.map(todo => (<TodoItem todo={todo} key={todo.id} />))}
+  </div>);
+};
+
+TodoList.propTypes = TodoListProps;
+
+export default TodoList;
