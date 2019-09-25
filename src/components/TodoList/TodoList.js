11ed0da382a8c52f9269e6c93aabb26e7686440f
Volodsher <volodsher@gmail.com>
2019-09-25 10:52:11 +0300

    fixed

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 import './TodoList.css';
 import { TodoItem } from '../TodoItem/TodoItem';
 
-// eslint-disable-next-line import/prefer-default-export
 export const TodoList = (props) => {
-  // eslint-disable-next-line react/prop-types
   const { todos } = props;
   console.log(typeof todos);
 
   return (
     <ul>
-      {/* eslint-disable-next-line react/prop-types */}
       {todos.map(todo => (
         <TodoItem key={todo.id} {...todo} />
       ))}
@@ -19,20 +16,14 @@ export const TodoList = (props) => {
   );
 };
 
-// const shape = PropTypes.shape({
-//   id: PropTypes.number.isRequired,
-//   userId: PropTypes.number.isRequired,
-//   title: PropTypes.string.isRequired,
-//   completed: PropTypes.bool.isRequired,
-//   user: PropTypes.object.isRequired,
-// });
+const shape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  userId: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+});
 
-// TodoItem.propTypes = {
-//   todos: PropTypes.arrayOf(shape).isRequired,
-// }.isRequaired;
-
-TodoItem.propTypes = {
-  todos: PropTypes.object.isRequired,
-}.isRequaired;
-
-export default TodoList;
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(shape).isRequired,
+};
