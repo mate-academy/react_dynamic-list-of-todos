610b9d22ec65895949cd8a9ebeaba270cfde4fdb
Viktor Lukovets <nuerio63@gmail.com>
2019-11-05 12:11:53 +0200

    Solution

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
+async function loadData(url) {
+  return fetch(url).then(response => response.json());
+}
+
 const getTodosWithUsers = async () => {
-  const todos = await fetch(TODOS_URL)
-    .then(response => response.json());
-  const users = await fetch(USERS_URL)
-    .then(response => response.json());
+  const todos = await loadData(TODOS_URL);
+  const users = await loadData(USERS_URL);
 
   return todos.map(todo => (
     {
