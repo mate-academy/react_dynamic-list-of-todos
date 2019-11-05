b06fcd85f180bba3c80d3b4de339b0949e10cf77
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-11-05 12:30:09 +0200

    Update SortingButtons.js

--- src/components/sortingButtons/SortingButtons.js
+++ src/components/sortingButtons/SortingButtons.js
@@ -4,14 +4,33 @@ function SortingButtons({ sorting }) {
   return (
     <div className="btn-group col-sm-8 px-0 mx-auto" role="group" aria-label="Basic example">
       <button
-      type="button"
-      className="btn btn-secondary" onClick={() => sorting('Sort by title')}>Sort by title</button>
-      <button type="button"
-      className="btn btn-secondary" onClick={() => sorting('Sort by username')}>Sort by username</button>
-      <button type="button"
-      className="btn btn-secondary" onClick={() => sorting('Sort by status')}>Sort by status</button>
-      <button type="button"
-      className="btn btn-secondary" onClick={() => sorting('Show default list')}>Show default list</button>
+        type="button"
+        className="btn btn-secondary" 
+        onClick={() => sorting('Sort by title')}
+      >
+        Sort by title
+      </button>
+      <button 
+        type="button"
+        className="btn btn-secondary" 
+        onClick={() => sorting('Sort by username')}
+      >
+        Sort by username
+      </button>
+      <button 
+        type="button"
+        className="btn btn-secondary" 
+        onClick={() => sorting('Sort by status')}
+      >
+        Sort by status
+      </button>
+      <button 
+        type="button"
+        className="btn btn-secondary" 
+        onClick={() => sorting('Show default list')}
+      >
+        Show default list
+      </button>
     </div>
   );
 }
