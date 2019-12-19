b1bfa9985239d837b411029957bed2c8ae476986
Denys <denys.cheporniuk@gmail.com>
2019-12-19 15:43:54 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './App.css';
 
-import todos from './api/todos';
-import users from './api/users';
+import { todos } from './api/todos';
+import { users } from './api/users';
 
-function App() {
+import TodoList from './TodoList';
+
+const App = () => {
+  const getTodos = async() => {
+    const todosData = await todos();
+
+    return todosData;
+  };
+
+  const getUser = async() => {
+    const usersData = await users();
+
+    return usersData;
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of todos</h1>
+      <TodoList
+        getTodos={getTodos}
+        getUsers={getUser}
+      />
     </div>
   );
-}
+};
 
 export default App;
