81c323fd77121dbf39a8ffdb591f1906956b7ab8
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-19 20:12:13 +0200

    first ci

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getData } from './api/fetch';
+import TodoList from './TodoList';
 import './App.css';
 
-import todos from './api/todos';
-import users from './api/users';
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
-function App() {
+const App = () => {
+  const [combinedData, setCombinedData] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+
+  const loadData = async() => {
+    setLoading(true);
+
+    try {
+      const todosData = await getData(TODOS_URL);
+      const usersData = await getData(USERS_URL);
+
+      const allData = todosData.map(todo => ({
+        ...todo,
+        user: usersData.find(user => user.id === todo.userId),
+      }));
+
+      setCombinedData(allData);
+      setLoading(false);
+      setLoaded(true);
+    } catch (e) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of todos</h1>
+
+      {isLoaded ? (
+        <TodoList list={combinedData} />
+      ) : (
+        <>
+          {isError ? (
+            <h2>Error occured!!!</h2>
+          ) : (
+            <h2>No TodoList yet!</h2>
+          )}
+
+          <button
+            className="load-btn"
+            type="button"
+            onClick={loadData}
+          >
+            {isLoading ? 'Loading...' : 'Load TodoList'}
+          </button>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+const HEADERS = {
+  id: 'Id',
+  title: 'Description',
+  completed: 'Is completed?',
+  user: 'Responsible person',
+};
+
+const TodoList = ({ list }) => {
+  const [todos, sortTodos] = useState(list);
+  const [isSorted, setSorted] = useState(false);
+  const [activeField, setActiveField] = useState('id');
+
+  const sortList = (field) => {
+    let callback;
+
+    switch (typeof list[0][field]) {
+      case 'string':
+        callback = (a, b) => a[field].localeCompare(b[field]);
+        break;
+      case 'object':
+        callback = (a, b) => a[field].name.localeCompare(b[field].name);
+        break;
+      case 'boolean':
+        callback = (a, b) => b[field] - a[field];
+        break;
+      default:
+        callback = (a, b) => a[field] - b[field];
+    }
+
+    if (!isSorted || activeField !== field) {
+      sortTodos(todos.sort(callback));
+      setSorted(true);
+      setActiveField(field);
+    } else {
+      sortTodos(todos.reverse());
+      setSorted(false);
+    }
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(HEADERS).map(header => (
+            <th
+              onClick={() => sortList(header)}
+              key={header}
+            >
+              {HEADERS[header]}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map(todo => <TodoItem key={todo.id} item={todo} />)}
+      </tbody>
+    </table>
+  );
+};
+
+TodoList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.object
+  ).isRequired,
+};
+
+export default TodoList;
