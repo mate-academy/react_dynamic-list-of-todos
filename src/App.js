c1a9cdcf8da68078cd2759178d3c6c90895a5f9a
vvxforce <vvxforce@gmail.com>
2019-12-23 11:29:45 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
-const todosURL = 'https://jsonplaceholder.typicode.com/todos';
-const getTodosFromServer = () => {
-  return fetch(todosURL)
-    .then(response => response.json());
+const API_URL = 'https://jsonplaceholder.typicode.com';
+const delay = () => new Promise((resolve) => {
+  setTimeout(resolve, 500);
+});
+const getTodos = () => {
+  console.log('Loading todos');
+  return delay()
+    .then(() => fetch(`${API_URL}/todos`))
+    .then(response => response.ok ? response.json() : [])
+    .catch(() => []);
 };
-const usersURL = 'https://jsonplaceholder.typicode.com/users';
-const getUsersFromServer = () => {
-  return fetch(usersURL)
-    .then(response => response.json());
+const getUsers = () => {
+  console.log('Loading users');
+  return delay()
+    .then(() => fetch(`${API_URL}/users`))
+    .then(response => {
+      console.log('Users are loaded');
+      return response.ok ? response.json() : []
+    })
+    .catch(() => []);
 };
-
-const App = () => {
-  state = {
-    data: [],
-    isLoading: false,
-    hasError: false,
-  }
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setLoaing] = useState(false);
+  const loadData = async() => {
+    setLoaing(true);
+    const [
+      todosFromServer,
+      usersFromServer
+    ] = await Promise.all([
+      getTodos(),
+      getUsers()
+    ]);
+    setTodos(todosFromServer.map(todo => ({
+      ...todo,
+      user: usersFromServer.find(user => todo.userId === user.id)
+    })));
+  };
   return (
-    <div>
-      <h1>List of todos</h1>
+    <div className="App">
+      <h1>Dynamic list of todos</h1>
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              {todo.completed ? '✅' : '❌'}
+              {todo.user.name}
+              {' - '}
+              {todo.title}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <button type="button" onClick={loadData}>
+          {isLoading ? 'Loading...' : 'Load'}
+        </button>
+      )}
     </div>
   );
 }
-
 export default App;
