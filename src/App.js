d554dd8f1a3f3dc39a19ca54c5024f5859cbe34f
vvxforce <vvxforce@gmail.com>
2019-12-24 10:43:36 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
+import TodoItem from './TodoItem'
 const API_URL = 'https://jsonplaceholder.typicode.com';
 const delay = () => new Promise((resolve) => {
   setTimeout(resolve, 500);
 });
+
 const getTodos = () => {
   console.log('Loading todos');
   return delay()
@@ -11,6 +13,7 @@ const getTodos = () => {
     .then(response => response.ok ? response.json() : [])
     .catch(() => []);
 };
+
 const getUsers = () => {
   console.log('Loading users');
   return delay()
@@ -21,10 +24,11 @@ const getUsers = () => {
     })
     .catch(() => []);
 };
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [isLoading, setLoaing] = useState(false);
-  const loadData = async() => {
+  const loadData = async () => {
     setLoaing(true);
     const [
       todosFromServer,
@@ -38,26 +42,56 @@ function App() {
       user: usersFromServer.find(user => todo.userId === user.id)
     })));
   };
+
+  const sortTodosById = () => setTodos([...todos]
+    .sort((a, b) => a.id - b.id));
+  const sortTodosByTitle = () => setTodos([...todos]
+    .sort((a, b) => a.title.localeCompare(b.title)));
+  const sortTodosByCompleted = () => setTodos([...todos]
+    .sort((a, b) => b.completed.toString().localeCompare(
+      a.completed.toString())));
+  const sortTodosByName = () => setTodos([...todos]
+    .sort((a, b) => a.user.name.localeCompare(b.user.name)));
+
   return (
     <div className="App">
       <h1>Dynamic list of todos</h1>
       {todos.length > 0 ? (
-        <ul>
-          {todos.map(todo => (
-            <li key={todo.id}>
-              {todo.completed ? '✅' : '❌'}
-              {todo.user.name}
-              {' - '}
-              {todo.title}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <button type="button" onClick={loadData}>
-          {isLoading ? 'Loading...' : 'Load'}
+        <table>
+          <tr>
+            <th>
+              <button type="button" onClick={sortTodosById}>
+                Sort by Id
         </button>
-      )}
+            </th>
+            <th>
+              <button type="button" onClick={sortTodosByTitle}>
+                Sort by Title
+        </button>
+            </th>
+            <th>
+              <button type="button" onClick={sortTodosByCompleted}>
+                Sort if is completed
+        </button>
+            </th>
+            <th>
+              <button type="button" onClick={sortTodosByName}>
+                Sort by Name
+        </button>
+            </th>
+          </tr>
+          {todos.map(todo => (
+
+            <TodoItem todo={todo} />
+          ))}
+        </table>
+      ) : (
+          <button type="button" onClick={loadData}>
+            {isLoading ? 'Loading...' : 'Load'}
+          </button>
+        )}
     </div>
   );
 }
+
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+
+
+
+TodoList.propTypes = { getTodosArr: PropTypes.func.isRequired };
+
+export default TodoList;
