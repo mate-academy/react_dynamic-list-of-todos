24f33ae8b8e8ce383f0ad0cc5fb18b53d13e6351
mgrinko <glukym@gmail.com>
2020-01-22 17:02:15 +0200

    Updates

---
+++ precache-manifest.6e0876ae5b1869821a9488ffafaf7234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ab0577154eec416e2040c40376c26c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab45a7457719ff5ac3fd",
+    "url": "./static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "f0a3ed543f7be60bccfa",
+    "url": "./static/js/2.746a7615.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.746a7615.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab45a7457719ff5ac3fd",
+    "url": "./static/js/main.42018c63.chunk.js"
+  },
+  {
+    "revision": "1b6846f02281f161bf4f",
+    "url": "./static/js/runtime-main.ed5ba67f.js"
+  }
+]);
