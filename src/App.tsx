ac2c0dc93bc8192c506681ddc2bb591225054516
Natalia Lypovyk <cardinnata@gmail.com>
2020-03-04 09:30:48 +0200

    Dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { useState, FC } from 'react';
 import './App.css';
+import { TodosList } from './components/TodosList/TodosList';
+import { getUsers, getTodos } from './api';
 
-const App = () => (
-  <h1>Dynamic list of TODOs</h1>
-);
+const App: FC<{}> = () => {
+  const [isLoaded, setLoaded] = useState<boolean>(false);
+  const [todos, setTodos] = useState<TodoWithUser[]>([]);
+
+  const loadTodos = async () => {
+    setLoaded(true);
+    const [todosLoaded, users] = await Promise.all([getTodos(), getUsers()]);
+
+    const todosWithUsers = todosLoaded.map(todo => ({
+      ...todo,
+      user: users.find(user => user.id === todo.userId),
+    }));
+
+    setTodos(todosWithUsers);
+  };
+
+  const sortByTitle = () => {
+    setTodos([...todos].sort((a, b) => a.title.localeCompare(b.title)));
+  };
+
+  const sortByName = () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    setTodos([...todos].sort((a, b) => a.user!.name.localeCompare(b.user!.name)));
+  };
+
+  const sortByStatus = () => {
+    setTodos([...todos].sort((a, b) => Number(b.completed) - Number(a.completed)));
+  };
+
+  if (todos.length === 0) {
+    return (
+      <>
+        <button
+          type="button"
+          className="btn-load"
+          onClick={loadTodos}
+          disabled={isLoaded}
+        >
+          Load
+        </button>
+        <p className="text">{isLoaded ? 'Loading...' : ''}</p>
+      </>
+    );
+  }
+
+  return (
+    <TodosList
+      todos={todos}
+      onSortTask={sortByTitle}
+      onSortName={sortByName}
+      onSortStatus={sortByStatus}
+    />
+  );
+};
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,37 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface TodoWithUser extends Todo {
+  user: User | undefined;
+}
