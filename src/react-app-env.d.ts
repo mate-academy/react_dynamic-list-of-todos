7a0ed2f267819b8081271d5631beea4a2877430e
Arimazz <bodyaprymyak@icloud.com>
2020-03-04 23:19:55 +0200

    MORE INTERFACES FOR GOD OF INTERFACES

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -14,21 +14,27 @@ interface User {
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
+  address: Address;
   phone: string;
   website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+  company: Company;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
 }
