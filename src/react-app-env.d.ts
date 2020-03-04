34d1e0d73643e2a19ca97f92c88dc26a54b7b090
Senya Volkov <formobileprima@gmail.com>
2020-03-04 13:08:32 +0200

    fixed interface extand

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -32,32 +32,6 @@ interface Todo {
   completed: boolean;
 }
 
-interface TodoWithUser {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-  user: {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    address: {
-      street: string;
-      suite: string;
-      city: string;
-      zipcode: string;
-      geo: {
-        lat: string;
-        lng: string;
-      };
-    };
-    phone: string;
-    website: string;
-    company: {
-      name: string;
-      catchPhrase: string;
-      bs: string;
-    };
-  };
+interface TodoWithUser extends Todo{
+  user: User;
 }
