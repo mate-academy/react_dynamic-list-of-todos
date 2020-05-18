e5435ab3620a22fcd4fbe4a30e8f54efe18b7f3e
euxos <eugen.ostrozhinsky@gmail.com>
2020-05-18 09:56:12 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Dynamic list of TODOs</h1>
-);
+import { getUsers, getTodos, Todo } from './helpers/api';
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleLoadClick = async () => {
+    const todosFromServer = await getTodos();
+    const usersFromServer = await getUsers();
+
+    const todosWithUsers = todosFromServer.map(todo => ({
+      ...todo,
+      user: usersFromServer.find(user => user.id === todo.userId),
+    }));
+
+    setTodos(todosWithUsers);
+  }
+
+  return (
+    <div>
+      <h1>List</h1>
+
+      <button type="button" onClick={handleLoadClick}>
+        Load
+      </button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>
+            <input type="checkbox" checked={todo.completed} disabled />
+            {todo.title + ' '} 
+            ({todo.user? todo.user.name : 'Unknown'})
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default App;
---
+++ src/helpers/api.ts
@@ -0,0 +1,22 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+
+export interface User {
+  id: number;
+  name: string;
+}
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user?: User;
+}
+
+const getAll = <T>(url: string): Promise<T[]> => {
+  return fetch(API_URL + url)
+    .then(response => response.json());
+}
+
+export const getTodos = () => getAll<Todo>('/todos');
+export const getUsers= () => getAll<User>('/users');
