fdde7fe7141344f16098589ee83ade3e9c7b34d6
aleksey-10 <alexcare1337@gmail.com>
2020-07-03 18:53:20 +0300

    solution

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 export const loadData = async (name: string) => {
-  const response = await fetch(`/api/${name}.json`);
+  const response = await fetch(`https://mate-academy.github.io/react_dynamic-list-of-todos/api/${name}.json`);
   const data = await response.json();
 
   return data;
