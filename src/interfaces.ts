a73ffbba87001517b9e4c3faf3c464c25f0d4545
German <germandolia@gmail.com>
2020-07-15 22:13:18 +0300

    fix interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,23 +5,11 @@ export interface Todo {
   completed: boolean;
 }
 
-export interface Geo {
-  lat: string;
-  lng: string;
-}
-
-export interface Сompany {
-  name: string;
-  catchPhrase: string;
-  bs: string;
-}
-
 export interface Address {
   street: string;
   suite: string;
   city: string;
   zipcode: string;
-  geo: Geo;
 }
 
 export interface User {
@@ -32,7 +20,6 @@ export interface User {
   address: Address;
   phone: string;
   website: string;
-  company: Сompany;
 }
 
 export interface TodosWithUsers {
