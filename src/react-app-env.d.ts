519a4fe4886c2e9cceae443523c1302282613af6
Glib Rulev <velzepooz@gmail.com>
2020-07-18 16:52:10 +0300

    Solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,12 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+// interface PrepearedTodos {
+  // id: number;
+  // userId: number;
+  // title: string;
+  // completed: string;
+  // createdAt: string;
+  // updatedAt: string;
+  // user?: User;
+// }
