54d377df78ab633d5be71f1450910a26d723b926
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-23 20:49:47 +0300

    mockup of App classy element

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Dynamic list of TODOs</h1>
-);
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface State {
+  setLoading: boolean;
+  setLoaded: boolean;
+  todos: Todo[];
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    setLoading: false,
+    setLoaded: false,
+    todos: [],
+  };
+
+  render() {
+    const {
+      setLoading,
+      setLoaded,
+      todos,
+    } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of TODOs</h1>
+        {
+          (!setLoaded)
+            ? (
+              <button type="button">
+                {setLoading ? 'Loading...' : 'Load'}
+              </button>
+            ) : (
+              <div>
+                here will be some list of
+                {todos}
+              </div>
+            )
+        }
+      </>
+    );
+  }
+}
 
 export default App;
