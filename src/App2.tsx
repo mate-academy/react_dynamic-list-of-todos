cdc5577c876f7696daab3bd10ed82f2ccdf6608b
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-29 13:57:50 +0300

    start refactoring to Hooks

---
+++ src/App2.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component<{}, {}> {
+  state={};
+
+  render() {
+    return (
+      <span>work in progress</span>
+    );
+  }
+}
+
+export default App;
