fb07fedb7eeaf68459736b91cd82365ff9378cf1
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-29 21:37:29 +0300

    api refactored to using async / await

--- src/api.ts
+++ src/api.ts
@@ -3,53 +3,32 @@ import { Todo, User } from './types';
 const URL_USERS = 'https://mate.academy/students-api/users';
 const URL_TODOS = 'https://mate.academy/students-api/todos';
 
-interface ResponseData<D> {
-  data: D;
-  error?: string;
+export async function loadUsers(): Promise<User[]> {
+  try {
+    const response = await fetch(URL_USERS);
+    const data = await response.json();
+
+    if (response.ok) {
+      return data.data;
+    }
+
+    throw new Error(`${response.statusText}`);
+  } catch (error) {
+    throw new Error(`${error.message} Users, try again`);
+  }
 }
 
-type UsersData = ResponseData<User[]>;
-type TodosData = ResponseData<Todo[]>;
+export async function loadTodos(): Promise<Todo[]> {
+  try {
+    const response = await fetch(URL_TODOS);
+    const data = await response.json();
 
-export function loadUsers(): Promise<User[]> {
-  return fetch(URL_USERS)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
+    if (response.ok) {
+      return data.data;
+    }
 
-      throw new Error(`Response of Users API not successful ${response.statusText}`);
-    })
-    .then(({ data }: UsersData) => data)
-    .catch(error => {
-      // eslint-disable-next-line no-console
-      console.error(error.message);
-
-      throw new Error('Failed to Fetch Users, press button again');
-    });
-}
-
-// export async function loadUsers2(): Promise<User[]> {
-//   const response = await fetch(URL_USERS);
-//   const data = await response.json();
-
-//   return data;
-// }
-
-export function loadTodos(): Promise<Todo[]> {
-  return fetch(URL_TODOS)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-
-      throw new Error(`Response of Todos API ot successful ${response.statusText}`);
-    })
-    .then(({ data }: TodosData) => data)
-    .catch(error => {
-      // eslint-disable-next-line no-console
-      console.error(error.message);
-
-      throw new Error('Failed to Fetch Todos, press button again');
-    });
+    throw new Error(`${response.statusText}`);
+  } catch (error) {
+    throw new Error(`${error.message} Todos, try again`);
+  }
 }
