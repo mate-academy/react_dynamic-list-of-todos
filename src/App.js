0b4001e700259d5adefa3cd198dde94b0651a1b7
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-09-08 17:56:46 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { getTodos, getUser } from './api';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
@@ -7,20 +8,106 @@ class App extends React.Component {
   state = {
     todos: [],
     selectedUserId: 0,
+    user: [],
+    todosCopy: [],
   };
 
+  componentDidMount = () => {
+    getTodos()
+      .then((todos) => {
+        this.setState({
+          todos: todos.data,
+          todosCopy: todos.data,
+        });
+      });
+  };
+
+  componentDidUpdate = () => {
+    if (this.state.selectedUserId !== 0
+      && this.state.selectedUserId !== this.state.user.id) {
+      getUser(this.state.selectedUserId)
+        .then((user) => {
+          this.setState({ user: user.data });
+        });
+    }
+  }
+
+  changeUser = (id) => {
+    this.setState({ selectedUserId: id });
+  }
+
+  changeInput = (val) => {
+    this.setState(prevState => ({
+      todosCopy: prevState.todos.filter(todo => (
+        todo.title
+          ? todo.title.includes(val)
+          : ''
+      )),
+    }));
+  }
+
+  check = (event) => {
+    const { value } = event.target;
+
+    this.setState(prevState => ({
+      todosCopy: prevState.todosCopy.map(todo => ((todo.id !== Number(value))
+        ? { ...todo }
+        : {
+          ...todo,
+          completed: !todo.completed,
+        })),
+    }));
+  }
+
+  onClear = () => {
+    this.setState({
+      user: '',
+      selectedUserId: 0,
+    });
+  }
+
+  filterTodos = (event) => {
+    switch (event.target.value) {
+      case 'All':
+        this.setState(state => ({
+          todosCopy: [...state.todos],
+        }));
+        break;
+      case 'Active':
+        this.setState(state => ({
+          todosCopy: [...state.todos.filter(todo => !todo.completed)],
+        }));
+        break;
+      case 'Completed':
+        this.setState(state => ({
+          todosCopy: [...state.todos.filter(todo => todo.completed)],
+        }));
+        break;
+      default:
+        return false;
+    }
+  }
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { todosCopy, selectedUserId, user } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todosCopy}
+            changeUser={this.changeUser}
+            changeInput={this.changeInput}
+            filterTodos={this.filterTodos}
+            check={this.check}
+          />
         </div>
-
         <div className="App__content">
           {selectedUserId ? (
-            <CurrentUser userId={selectedUserId} />
+            <CurrentUser
+              user={user}
+              onClear={this.onClear}
+            />
           ) : 'No user selected'}
         </div>
       </div>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import './TodoList.scss';
 
-export const TodoList = () => (
+export const TodoList = ({ todos, changeUser, changeInput, filterTodos, check }) => (
   <div className="TodoList">
+    <input
+      type="text"
+      placeholder="Type search todo"
+      onChange={(event) => {
+        changeInput(event.target.value)
+      }}
+    />
+    <select
+      className="filters"
+      onChange={filterTodos}
+    >
+      <option
+        name="All"
+      >
+        All
+      </option>
+      <option
+        name="Active"
+      >
+        Active
+      </option>
+      <option
+        name="Completed"
+      >
+        Completed
+      </option>
+    </select>
     <h2>Todos:</h2>
-
     <ul className="TodoList__list">
-      <li className="TodoList__item">
-        <label>
-          <input type="checkbox" readOnly />
-          delectus aut autem
-        </label>
-
-        <button type="button">User&nbsp;#1</button>
-      </li>
-
-      <li className="TodoList__item">
-        <label>
-          <input type="checkbox" checked readOnly />
-          distinctio vitae autem nihil ut molestias quo
-        </label>
-
-        <button type="button">User&nbsp;#2</button>
-      </li>
+      {todos.map(todo=>(
+        <li className="TodoList__item">
+          <p>ID : {todo.id}</p>
+          <label>
+            <input
+              type="checkbox"
+              className="toggle"
+              onChange={check}
+              id={todo.id}
+              value={todo.id}
+              checked={todo.completed}
+            />
+            {todo.title}
+          </label>
+          <button
+            type="button"
+            onClick={()=>{changeUser(todo.userId)}}
+          >
+            User&nbsp;#{todo.userId}
+          </button>
+        </li>
+      ))}
     </ul>
   </div>
 );
