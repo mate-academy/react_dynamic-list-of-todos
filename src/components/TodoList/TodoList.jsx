02b0a4a7ceaee8cef993fa420ab4b9beb886ebbe
irenhh <iriskag@ukr.net>
2020-09-08 18:55:34 +0300

    redesigned the app

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,24 +5,40 @@ export const TodoList = () => (
   <div className="TodoList">
     <h2>Todos:</h2>
 
-    <ul className="TodoList__list">
-      <li className="TodoList__item">
-        <label>
-          <input type="checkbox" readOnly />
-          delectus aut autem
-        </label>
+    <div className="TodoList__list-container">
+      <ul className="TodoList__list">
+        <li className="TodoList__item TodoList__item--unchecked">
+          <label>
+            <input type="checkbox" readOnly />
+            <p>delectus aut autem</p>
+          </label>
 
-        <button type="button">User&nbsp;#1</button>
-      </li>
+          <button
+            className="
+              TodoList__user-button
+              TodoList__user-button--selected
+              button
+            "
+            type="button"
+          >
+            User&nbsp;#1
+          </button>
+        </li>
 
-      <li className="TodoList__item">
-        <label>
-          <input type="checkbox" checked readOnly />
-          distinctio vitae autem nihil ut molestias quo
-        </label>
+        <li className="TodoList__item TodoList__item--checked">
+          <label>
+            <input type="checkbox" checked readOnly />
+            <p>distinctio vitae autem nihil ut molestias quo</p>
+          </label>
 
-        <button type="button">User&nbsp;#2</button>
-      </li>
-    </ul>
+          <button
+            className="TodoList__user-button button"
+            type="button"
+          >
+            User&nbsp;#2
+          </button>
+        </li>
+      </ul>
+    </div>
   </div>
 );
