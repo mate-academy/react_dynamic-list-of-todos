3d485e11f7f65c29e4f52f491cb13a2430e6da35
martakupka <marta-k2008@ukr.net>
2020-09-10 21:30:43 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,51 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './helpers/api';
 
 class App extends React.Component {
   state = {
     todos: [],
     selectedUserId: 0,
+    selectedTodoId: 0,
   };
 
+  componentDidMount() {
+    getTodos()
+      .then(todos => this.setState({ todos }));
+  }
+
+  onSelection = (selectedUserId, selectedTodoId) => {
+    this.setState({
+      selectedUserId, selectedTodoId,
+    });
+  }
+
+  onClear = () => {
+    this.setState({ selectedUserId: 0 });
+  }
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { todos, selectedUserId, selectedTodoId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            userId={selectedUserId}
+            onSelection={this.onSelection}
+            todoId={selectedTodoId}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                onClear={this.onClear}
+              />
             ) : 'No user selected'}
           </div>
         </div>
