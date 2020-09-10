ba0f4ef2fe548ae06088b88c77cfd4f21a7cebb1
Elena-brv <leena.brv@gmail.com>
2020-09-10 16:32:02 +0300

    dynamic list of todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,74 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getAllTodos } from './api/api';
 
 class App extends React.Component {
   state = {
     todos: [],
     selectedUserId: 0,
+    query: '',
+    filterOfTodos: 'all',
   };
 
+  componentDidMount() {
+    (async() => {
+      const allTodos = await getAllTodos();
+
+      this.setState({ todos: allTodos });
+    })();
+  }
+
+  selectUser = (userId) => {
+    this.setState({ selectedUserId: userId });
+  }
+
+  clearUser = () => {
+    this.setState({ selectedUserId: 0 });
+  }
+
+  filterTodosByTitle = (value) => {
+    this.setState({ query: value });
+  }
+
+  selectTodos = (filter) => {
+    this.setState({ filterOfTodos: filter });
+  }
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { todos, selectedUserId, query, filterOfTodos } = this.state;
+    const queryToFind = query.toLowerCase();
+    const todosToShow = todos.filter(
+      todo => todo.title
+      && todo.title.toLowerCase().includes(queryToFind),
+    );
+
+    let selectedTodosToSHow = todosToShow;
+
+    if (filterOfTodos === 'completed') {
+      selectedTodosToSHow = todosToShow.filter(todo => todo.completed);
+    } else if (filterOfTodos === 'active') {
+      selectedTodosToSHow = todosToShow.filter(todo => !todo.completed);
+    }
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={selectedTodosToSHow}
+            selectUser={this.selectUser}
+            filterTodos={this.filterTodosByTitle}
+            selectTodos={this.selectTodos}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                clearUser={this.clearUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
