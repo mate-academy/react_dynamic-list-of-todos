916f5c7004a68d2cde163159d524a25db9ba3f5a
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-09-11 17:51:54 +0300

    Solution with QUESTIONS

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodosFromServer } from './api';
 
 class App extends React.Component {
   state = {
@@ -10,19 +11,62 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
+  componentDidMount() {
+    getTodosFromServer().then((data) => {
+      this.setState({
+        todos: data,
+      });
+    });
+  }
+
+  changeSelectedUser = (id) => {
+    this.setState({
+      selectedUserId: id,
+    });
+  }
+
   render() {
     const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <button
+            type="button"
+            onClick={() => {
+              getTodosFromServer().then((data) => {
+                // eslint-disable-next-line no-alert
+                window.alert('you are realy naughty baby');
+
+                this.setState({
+                  todos: data.slice(0, 3),
+                // it dont updates because of I already define state in TodoList
+                // continue is in TodoList
+                });
+              });
+            }}
+          >
+            get data again (DONT TOUCH, BOMB WILL BOOM)
+          </button>
+
+          {todos.length > 1
+            ? (
+              <TodoList
+                todos={todos}
+                changeSelectedUser={this.changeSelectedUser}
+              />
+            ) : ''}
+          {/* how to fix this crutch? (crutch above)
+          I will cant to update it again */}
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                changeSelectedUser={this.changeSelectedUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,44 +1,132 @@
 import React from 'react';
 import './TodoList.scss';
+import ClassName from 'classnames';
+import PropTypes from 'prop-types';
 
-export const TodoList = () => (
-  <div className="TodoList">
-    <h2>Todos:</h2>
+export class TodoList extends React.Component {
+  state = {
+    searchQuery: '',
+    requiredStatus: 'all',
+    preparedTodos: this.props.todos,
+    // i want to update it every time when TodoList or his parent updates
+    // BUT! I dont want infinite loop
+    // AND! I dont know what the condition to write in didMountUpdate
+  }
 
-    <div className="TodoList__list-container">
-      <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
+  filtrator = (status, searchQuery) => {
+    let filteredTodos = [...this.props.todos];
 
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
-          >
-            User&nbsp;#1
-          </button>
-        </li>
+    filteredTodos = filteredTodos.filter(
+      todo => todo.title && todo.title.includes(searchQuery),
+    );
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
+    switch (status) {
+      case 'active':
+        filteredTodos = filteredTodos.filter(todo => !todo.completed);
+        break;
+      case 'completed':
+        filteredTodos = filteredTodos.filter(todo => todo.completed);
+        break;
+      default: (() => 'I love linter')();
 
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+        break;
+    }
+
+    this.setState({
+      preparedTodos: filteredTodos,
+    });
+  }
+
+  inputHandler = (event) => {
+    // this.setState(() => ({
+    //   searchQuery: event.target.value
+    // }))
+    // why it dont work???
+
+    this.setState({
+      searchQuery: event.target.value,
+    });
+
+    this.filtrator(this.state.requiredStatus, event.target.value);
+  }
+
+  selectHandler = (event) => {
+    this.setState({
+      requiredStatus: event.target.value,
+    });
+
+    this.filtrator(event.target.value, this.state.searchQuery);
+  }
+
+  render() {
+    const { changeSelectedUser } = this.props;
+    const todos = this.state.preparedTodos;
+
+    return (
+      <div className="TodoList">
+        <h2>Todos:</h2>
+
+        <div className="TodoList__list-container">
+          <div>
+            <input
+              type="text"
+              placeholder="enter query"
+              value={this.state.searchQuery}
+              onChange={this.inputHandler}
+            />
+            <select
+              value={this.state.requiredStatus}
+              onChange={this.selectHandler}
+            >
+              <option value="all">All</option>
+              <option value="active">Active</option>
+              <option value="completed">Completed</option>
+            </select>
+          </div>
+          <ul className="TodoList__list">
+            {todos.map(todo => (
+              <li
+                key={todo.id}
+                className={ClassName({
+                  TodoList__item: true,
+                  'TodoList__item--checked': todo.completed,
+                  'TodoList__item--unchecked': !todo.completed,
+                })}
+              >
+                <label>
+                  <input type="checkbox" checked={todo.completed} readOnly />
+                  <p>{todo.title}</p>
+                </label>
+
+                <button
+                  className="
+                    TodoList__user-button
+                    TodoList__user-button--selected
+                    button
+                  "
+                  type="button"
+                  onClick={() => {
+                    changeSelectedUser(todo.userId);
+                  }}
+                >
+                  User&nbsp;#
+                  {todo.userId}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    userId: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+  })).isRequired,
+  changeSelectedUser: PropTypes.func.isRequired,
+};
