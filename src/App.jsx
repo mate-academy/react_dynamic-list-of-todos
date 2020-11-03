58035b53953a064d82898ec6507d3a704e1cdd1a
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-11-03 14:28:26 +0200

    finalized filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
+import { getTodos } from './API/api';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
 class App extends React.Component {
   state = {
-    todos: [],
+    allTodos: [],
     selectedUserId: 0,
+    filteredTodos: [],
   };
 
+  componentDidMount() {
+    getTodos()
+      .then(todos => this.setState({
+        allTodos: todos,
+        filteredTodos: [...todos],
+      }));
+  }
+
+  updateUserId = (id) => {
+    if (id !== this.state.selectedUserId) {
+      this.setState({ selectedUserId: id });
+    }
+  }
+
+  updateTodos = (arrTodos) => {
+    this.setState({ filteredTodos: arrTodos });
+  }
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { allTodos, filteredTodos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            allTodos={allTodos}
+            filteredTodos={filteredTodos}
+            updateUserId={this.updateUserId}
+            updateTodos={this.updateTodos}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                updateUserId={this.updateUserId}
+              />
             ) : 'No user selected'}
           </div>
         </div>
