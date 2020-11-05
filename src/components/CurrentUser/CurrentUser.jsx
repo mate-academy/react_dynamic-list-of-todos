6f07120993c5c94ff1161d4a5012b6dbab21d9b7
Yazheviks <vicsergeyeva@gmail.com>
2020-11-05 23:02:28 +0200

    solution

--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -1,12 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CurrentUser.scss';
+import { getUser } from '../../api/api';
 
-export const CurrentUser = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+export class CurrentUser extends React.PureComponent {
+  state = {
+    user: {},
+  }
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+  componentDidMount() {
+    this.updateUser(this.props.userId);
+  }
+
+  componentDidUpdate() {
+    this.updateUser(this.props.userId);
+  }
+
+  updateUser(newUserId) {
+    if (this.state.user && this.state.user.id === newUserId) {
+      return;
+    }
+
+    getUser(newUserId)
+      .then((newUser) => {
+        this.setState({
+          user: newUser,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          user: null,
+        });
+      });
+  }
+
+  render() {
+    const { user } = this.state;
+    const { onButtonClick } = this.props;
+
+    return (
+      <div className="CurrentUser">
+        {user
+          ? (
+            <>
+              <h2 className="CurrentUser__title">
+                <span>
+                  {`Selected user: ${user.id}`}
+                </span>
+              </h2>
+
+              <h3 className="CurrentUser__name">{user.name}</h3>
+              <p className="CurrentUser__email">{user.email}</p>
+              <p className="CurrentUser__phone">{user.phone}</p>
+
+              <button
+                type="button"
+                onClick={() => {
+                  onButtonClick();
+                }}
+                className="btn btn-danger"
+              >
+                X
+              </button>
+            </>
+          )
+          : (
+            <p>User is not found</p>
+          )}
+      </div>
+    );
+  }
+}
+
+CurrentUser.propTypes = {
+  userId: PropTypes.number.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,44 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TodoList.scss';
+import { Todo } from '../Todo';
 
-export const TodoList = () => (
-  <div className="TodoList">
-    <h2>Todos:</h2>
+export class TodoList extends React.PureComponent {
+  state = {
+    search: '',
+    filteredTodos: 'all',
+  }
 
-    <div className="TodoList__list-container">
-      <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
+  filter = {
+    all: () => true,
+    completed: todo => todo.completed,
+    active: todo => !todo.completed,
+  }
 
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
+  changeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { todos, selectedUserId, onButtonClick } = this.props;
+    const { search, filteredTodos } = this.state;
+    const todosToShow = todos.filter(todo => (
+      todo.title.includes(search) && this.filter[filteredTodos](todo)
+    ));
+
+    return (
+      <div className="TodoList">
+        <h2>Todos:</h2>
+
+        <div className="TodoList__list-container">
+          <input
+            name="search"
+            placeholder="Search"
+            className="input"
+            value={search}
+            onChange={this.changeHandler}
+          />
+
+          <select
+            name="filteredTodos"
+            value={filteredTodos}
+            onChange={this.changeHandler}
           >
-            User&nbsp;#1
-          </button>
-        </li>
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="active">active</option>
+          </select>
+          <ul className="TodoList__list">
+            {todosToShow.map(todo => (
+              <Todo
+                todo={todo}
+                onButtonClick={onButtonClick}
+                isSelectedUser={selectedUserId === todo.userId}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
-
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    userId: PropTypes.number,
+    id: PropTypes.number,
+    title: PropTypes.string,
+    completed: PropTypes.bool,
+  })).isRequired,
+  selectedUserId: PropTypes.number.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+};
