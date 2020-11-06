f71be56d23d2bbf64d65b542d60c0f3be3b40f9e
milla-romankova <milla-romankova@gmail.com>
2020-11-06 14:51:44 +0200

    add if no todos

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,18 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList
-            todos={todos}
-            selectUser={selectUser}
-            selectedUserId={selectedUserId}
-          />
+          {todos.length
+            ? (
+              <TodoList
+                todos={todos}
+                selectUser={selectUser}
+                selectedUserId={selectedUserId}
+              />
+            )
+            : (
+              'Everything is done!'
+            )
+          }
         </div>
 
         <div className="App__content">
