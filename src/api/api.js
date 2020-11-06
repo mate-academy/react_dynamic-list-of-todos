84178cc937a720d68aba431fbfc5e11d1b0068ca
Yazheviks <vicsergeyeva@gmail.com>
2020-11-06 14:31:17 +0200

    var names change

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 export const BASE_URL = 'https://mate-api.herokuapp.com';
 
 export const request = (url, options) => fetch(`${BASE_URL}${url}`, options)
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`${res.status} - ${res.statusText}`);
+  .then((result) => {
+    if (!result.ok) {
+      throw new Error(`${result.status} - ${result.statusText}`);
     }
 
-    return res.json();
+    return result.json();
   })
-  .then(res => res.data);
+  .then(result => result.data);
 
 export const getTodos = () => request('/todos');
 export const getUser = id => request(`/users/${id}`);
