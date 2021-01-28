246303439f8c691907895894fd2e252691012c87
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-29 01:13:28 +0200

    fix

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -106,38 +106,6 @@ export class TodoList extends React.Component {
                 </button>
               </li>
             ))}
-
-            <li className="TodoList__item TodoList__item--unchecked">
-              <label>
-                <input type="checkbox" readOnly />
-                <p>delectus aut autem</p>
-              </label>
-
-              <button
-                className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-                type="button"
-              >
-                User&nbsp;#1
-              </button>
-            </li>
-
-            <li className="TodoList__item TodoList__item--checked">
-              <label>
-                <input type="checkbox" checked readOnly />
-                <p>distinctio vitae autem nihil ut molestias quo</p>
-              </label>
-
-              <button
-                className="TodoList__user-button button"
-                type="button"
-              >
-                User&nbsp;#2
-              </button>
-            </li>
           </ul>
         </div>
       </div>
