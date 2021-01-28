193b2607c0e7d98fb96ac1752ea7d116aab6306b
ShyianO <ol.shyian@gmacil.com>
2021-01-28 19:50:26 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { getTodos } from './api/api';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
@@ -10,19 +12,53 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
+  componentDidMount() {
+    getTodos()
+      .then((todos) => {
+        this.setState({ todos: todos.data });
+      });
+  }
+
+  handleCheck = (taskId) => {
+    this.setState(state => ({
+      todos: state.todos.map((todo) => {
+        if (todo.id !== taskId) {
+          return todo;
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }),
+    }));
+  }
+
+  handleSelect = (userId) => {
+    this.setState({ selectedUserId: userId });
+  }
+
+  handleClear = () => {
+    this.setState({ selectedUserId: 0 });
+  }
+
   render() {
     const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            onCheck={this.handleCheck}
+            onSelect={this.handleSelect}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser userId={selectedUserId} onClear={this.handleClear} />
             ) : 'No user selected'}
           </div>
         </div>
