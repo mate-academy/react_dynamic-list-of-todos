abda3b547613ac1cc7c552f4769c3ea91f3266b3
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-28 15:37:05 +0200

    deleted comments

--- src/api.js
+++ src/api.js
@@ -1,24 +1,3 @@
-// eslint-disable-next-line
-// const API_URL = `https://mate-api.herokuapp.com`;
-
-// export function getAllTodos() {
-//   return fetch(`${API_URL}/todos`)
-//     .then(confirmFetch);
-// }
-
-// export function getUser(userId) {
-//   return fetch(`${API_URL}/users/${userId}`)
-//     .then(confirmFetch);
-// }
-
-// function confirmFetch(response) {
-//   if (!response.ok) {
-//     throw new Error(`${response.status} - ${response.statusText}`);
-//   }
-
-//   return response.json();
-// }
-
 const BASE_URL = 'https://mate-api.herokuapp.com';
 
 const request = (url, options) => fetch(`${BASE_URL}${url}`, options)
