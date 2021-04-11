b914c23ee6fd434070bcce81d1cdaefd5c356e84
Ihor Drul <druleger@gmail.com>
2021-04-11 21:24:16 +0300

    Solution with bad filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+
+import { getTodos } from './api/todos';
 
 class App extends React.Component {
   state = {
@@ -10,19 +13,51 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
+  componentDidMount() {
+    this.getData();
+  }
+
+  selectUser = (id) => {
+    this.setState({
+      selectedUserId: id,
+    });
+  }
+
+  getData = async() => {
+    const todosFromApi = await getTodos();
+
+    this.setState({ todos: todosFromApi });
+  }
+
+  removeUser = () => {
+    this.setState({ selectedUserId: 0 });
+  }
+
   render() {
     const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          {
+            todos.length === 0
+              ? <CircularProgress />
+              : (
+                <TodoList
+                  todos={todos}
+                  selectUser={this.selectUser}
+                />
+              )
+          }
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                removeUser={this.removeUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
