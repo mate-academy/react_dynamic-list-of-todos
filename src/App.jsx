5177165d3a7129316ff6faa674823a9b68eca0ee
Taras Chaban <tarekchaban@gmail.com>
2021-04-19 15:56:43 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 class App extends React.Component {
   state = {
@@ -10,25 +11,37 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
-  render() {
-    const { todos, selectedUserId } = this.state;
+  componentDidMount() {
+    getTodos()
+      .then(todos => this.setState({ todos }));
+  }
 
-    return (
-      <div className="App">
-        <div className="App__sidebar">
-          <TodoList todos={todos} />
-        </div>
+    selectUser = (userId) => {
+      this.setState({
+        selectedUserId: userId,
+      });
+    }
 
-        <div className="App__content">
-          <div className="App__content-container">
-            {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
-            ) : 'No user selected'}
+    render() {
+      const { todos, selectedUserId } = this.state;
+
+      return (
+        <div className="App">
+          <div className="App__sidebar">
+            {todos.length > 0
+          && <TodoList todos={todos} selectUser={this.selectUser} />}
+          </div>
+
+          <div className="App__content">
+            <div className="App__content-container">
+              {selectedUserId ? (
+                <CurrentUser userId={selectedUserId} />
+              ) : 'No user selected'}
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default App;
