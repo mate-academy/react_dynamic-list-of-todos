4236495861447dbaf203d1cbe67de8cd24bdeaf2
Oleg Hluhk <glug.oleg@gmail.com>
2021-04-20 13:03:57 +0300

    some fix

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import className from 'classnames';
+import { List } from '../List';
+import { Form } from '../Form';
 import './TodoList.scss';
 
 export class TodoList extends React.Component {
@@ -73,69 +74,17 @@ export class TodoList extends React.Component {
           >
             Random
           </button>
-          <form className="TodoList__form">
-            <input
-              className="TodoList__input"
-              label="Search todos"
-              name="inputTitle"
-              id="outline-size-small"
-              value={inputTitle}
-              onChange={this.handleInput}
-            />
-            <select
-              className="TodoList__select"
-              name="defaultSelect"
-              value={defaultSelect}
-              onChange={this.handleInput}
-            >
-              <option value="Show All">
-                Show All
-              </option>
-              <option value="Show Completed">
-                Show Completed
-              </option>
-              <option value="Show Active">
-                Show Active
-              </option>
-            </select>
-          </form>
+          <Form
+            handleInput={this.handleInput}
+            inputTitle={inputTitle}
+            defaultSelect={defaultSelect}
+          />
         </div>
-        <ul className="TodoList__list">
-          {filterTodos.map(todo => (
-            <li
-              key={todo.id}
-              className={className('TodoList__item', {
-                'TodoList__item--checked': todo.completed === true,
-                ' TodoList__item--unchecked': todo.completed === false,
-              })}
-            >
-              <label>
-                <input
-                  type="checkbox"
-                  name="completed"
-                  onChange={() => checkedHandler(todo.id)}
-                  checked={todo.completed}
-                  readOnly
-                />
-                <p>{todo.title}</p>
-              </label>
-              <button
-                className="
-                TodoList__user-button
-                TodoList__user-button--selected
-                button
-                "
-                type="button"
-                onClick={() => {
-                  selecUser(todo.userId);
-                }}
-              >
-                {`User #${todo.userId}`}
-              </button>
-            </li>
-          ))
-          }
-        </ul>
+        <List
+          filterTodos={filterTodos}
+          checkedHandler={checkedHandler}
+          selecUser={selecUser}
+        />
       </div>
     );
   }
