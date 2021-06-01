6da8226dff0c46d87d8dd3e9b72bc54c113fdc4a
Alex Fedorchenko <mralex.fed@gmail.com>
2021-06-01 12:48:19 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,141 @@
+/* eslint-disable react/no-access-state-in-setstate */
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
+import classNames from 'classnames';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 class App extends React.Component {
   state = {
     todos: [],
+    copiedTodos: [],
     selectedUserId: 0,
+    title: '',
+    selectOption: '',
+    randomOption: false,
   };
 
+  componentDidMount() {
+    this.updateTodos();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.title !== this.state.title && this.state.title !== '') {
+      this.titleParse();
+    }
+
+    if (prevState.randomOption !== this.state.randomOption) {
+      this.randomOrder();
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  }
+
+  updateTodos = async() => {
+    const todos = await getTodos();
+
+    this.setState({
+      todos,
+      copiedTodos: [...todos],
+    });
+  }
+
+  selectUserId = (id) => {
+    this.setState({ selectedUserId: id });
+  }
+
+  randomOrder() {
+    const newArr = (this.state.randomOption)
+      ? this.state.copiedTodos.sort(() => Math.random() - 0.5)
+      : this.titleParse();
+
+    this.setState({ copiedTodos: newArr });
+  }
+
+  titleParse() {
+    const newTodos = this.state.todos.filter((todo) => {
+      if (todo.title === null) {
+        return false;
+      }
+
+      return todo.title.includes(this.state.title);
+    });
+
+    this.setState({ copiedTodos: newTodos });
+
+    return newTodos;
+  }
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { selectedUserId, title, copiedTodos, selectOption, randomOption }
+      = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <div className="filter">
+            <input
+              className="filter-item"
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={title}
+              onChange={this.handleChange}
+            />
+            <select
+              className="filter-item"
+              name="selectOption"
+              value={selectOption}
+              onChange={this.handleChange}
+            >
+              <option
+                selected
+                value="all"
+              >
+                All
+              </option>
+              <option
+                value="active"
+              >
+                Active
+              </option>
+              <option
+                value="completed"
+              >
+                Comleted
+              </option>
+            </select>
+            <button
+              className={classNames('filter-item', 'button', {
+                'TodoList__user-button--selected': randomOption,
+              })}
+              type="submit"
+              onClick={() => this.setState({ randomOption: !randomOption })}
+            >
+              {randomOption ? 'Standart order' : 'Random order'}
+            </button>
+          </div>
+          <TodoList
+            todos={copiedTodos}
+            onSelected={this.selectUserId}
+            selectOption={selectOption}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                onClear={this.selectUserId}
+                onSelected={this.selectUserId}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
 import './CurrentUser.scss';
+import { getUser } from '../../api/api';
 
-export const CurrentUser = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+export const CurrentUser = ({ userId, onClear, onSelected }) => {
+  const [user, setUser] = useState(0);
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+  useMemo(async() => {
+    const curentUser = await getUser(userId);
+
+    if (!curentUser) {
+      onSelected(0);
+    } else {
+      setUser(curentUser);
+    }
+  }, [userId]);
+
+  return (
+    <div className="CurrentUser">
+      <h2 className="CurrentUser__title">
+        <span>
+          Selected user:&nbsp;
+          {user.id}
+        </span>
+      </h2>
+
+      <h3 className="CurrentUser__name">{user.name}</h3>
+      <p className="CurrentUser__email">{user.email}</p>
+      <p className="CurrentUser__phone">{user.phone}</p>
+      <button
+        type="submit"
+        onClick={() => onClear(0)}
+      >
+        Clear
+      </button>
+    </div>
+  );
+};
+
+CurrentUser.propTypes = {
+  userId: PropTypes.number.isRequired,
+  onClear: PropTypes.func.isRequired,
+  onSelected: PropTypes.func.isRequired,
+};
