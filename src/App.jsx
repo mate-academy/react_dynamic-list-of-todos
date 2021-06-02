eeecdee2aaeb7df6c5ed5134e4cc2746c8a809ba
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-06-02 11:27:13 +0300

    Solution v0.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getData } from './api/api';
 
 class App extends React.Component {
   state = {
@@ -10,19 +11,50 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
+  componentDidMount() {
+    this.loadTodoList();
+  }
+
+  onSelectedUser = (userId) => {
+    this.setState({
+      selectedUserId: userId,
+    });
+  }
+
+  loadTodoList = async() => {
+    const prepearedTodoList = await getData(`/todos`);
+
+    this.setState({
+      todos: prepearedTodoList.data,
+    });
+  }
+
+  clearSelectedUser = () => {
+    this.setState({
+      selectedUserId: 0,
+    });
+  }
+
   render() {
     const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            onSelectedUser={this.onSelectedUser}
+            userId={selectedUserId}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                clearSelectedUser={this.clearSelectedUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
