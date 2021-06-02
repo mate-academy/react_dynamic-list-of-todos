3f472ec507d4ec7a8e483d81755f8d27810cebae
Sergey Zub <vook1996@gmail.com>
2021-06-02 20:16:58 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,91 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './components/api';
 
 class App extends React.Component {
   state = {
     todos: [],
     selectedUserId: 0,
+    query: '',
+    filter: 'all',
   };
 
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos });
+  }
+
+  async componentDidUpdate() {
+    let todos;
+    const { filter } = this.state;
+
+    if (filter === 'all') {
+      todos = await getTodos();
+    }
+
+    if (filter === 'active') {
+      todos = await getTodos()
+        .then(result => result.filter(todo => todo.completed === false));
+    }
+
+    if (filter === 'completed') {
+      todos = await getTodos()
+        .then(result => result.filter(todo => todo.completed === true));
+    }
+
+    this.pushTodos(todos);
+  }
+
+  clearUser = () => {
+    this.setState({
+      selectedUserId: 0,
+    });
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  pushTodos(todos) {
+    this.setState({ todos });
+  }
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { todos, selectedUserId, query, filter } = this.state;
+
+    const normalizeQuery = query.toLocaleLowerCase();
+    const filtredTodos = todos.filter(
+      todo => !!todo.title
+      && todo.title.toLocaleLowerCase().includes(normalizeQuery),
+    );
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={filtredTodos}
+            filter={filter}
+            handleChange={this.handleChange}
+            userId={selectedUserId}
+            selectUser={(userId) => {
+              this.setState({ selectedUserId: userId });
+            }}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                selectedUserId={selectedUserId}
+                clearUser={this.clearUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { getUser } from '../api';
 import './CurrentUser.scss';
 
-export const CurrentUser = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+export class CurrentUser extends React.Component {
+  state = {
+    user: null,
+  }
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+  componentDidMount() {
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.selectedUserId !== this.props.selectedUserId) {
+      this.loadData();
+    }
+  }
+
+  async loadData() {
+    const user = await getUser(this.props.selectedUserId);
+
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+
+    if (!user) {
+      return 'Wait please';
+    }
+
+    return (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title">
+          <span>
+            Selected user:
+            {user.userId}
+          </span>
+        </h2>
+
+        <h3 className="CurrentUser__name">{user.name}</h3>
+        <p className="CurrentUser__email">{user.email}</p>
+        <p className="CurrentUser__phone">{user.phone}</p>
+
+        <button
+          type="button"
+          onClick={this.props.clearUser}
+          className="button"
+        >
+          Clear User
+        </button>
+      </div>
+    );
+  }
+}
+
+CurrentUser.propTypes = {
+  selectedUserId: PropTypes.number.isRequired,
+  clearUser: PropTypes.func.isRequired,
+};
