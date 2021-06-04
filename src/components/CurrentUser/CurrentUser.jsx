0b9f8c4543eb19035f597d2d9fdafd03e583d377
Ivan Shpynda <ivanshpynda@gmail.com>
2021-06-04 15:43:57 +0300

    polished some parts

--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -6,13 +6,11 @@ import { getUser } from '../../api';
 
 export class CurrentUser extends React.Component {
   state = {
-    user: {},
+    user: null,
   }
 
   componentDidMount() {
-    getUser(this.props.userId).then((user) => {
-      this.setState({ user });
-    });
+    this.dowloadUser();
   }
 
   componentDidUpdate() {
@@ -20,6 +18,10 @@ export class CurrentUser extends React.Component {
       return;
     }
 
+    this.dowloadUser();
+  }
+
+  dowloadUser = () => {
     getUser(this.props.userId).then((user) => {
       this.setState({ user });
     });
@@ -29,24 +31,29 @@ export class CurrentUser extends React.Component {
     const { user } = this.state;
 
     return (
-      <div className="CurrentUser">
-        <h2 className="CurrentUser__title">
-          <span>
-            Selected user:
-            {user.id}
-          </span>
-        </h2>
-        <h3 className="CurrentUser__name">{user.name}</h3>
-        <p className="CurrentUser__email">{user.email}</p>
-        <p className="CurrentUser__phone">{user.phone}</p>
-        <button
-          type="button"
-          className="CurrentUser__clear"
-          onClick={this.props.clearUser}
-        >
-          Clear
-        </button>
-      </div>
+      <>
+      {user &&
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>
+              Selected user:
+              {user.id}
+            </span>
+          </h2>
+          <h3 className="CurrentUser__name">{user.name}</h3>
+          <p className="CurrentUser__email">{user.email}</p>
+          <p className="CurrentUser__phone">{user.phone}</p>
+          <button
+            type="button"
+            className="CurrentUser__clear"
+            onClick={this.props.clearUser}
+          >
+            Clear
+          </button>
+        </div>}
+        
+      </>
+      
     );
   }
 }
