5b215a0d260e0de5bde9fd5cce8f1e9f7fd398b9
Georgy Dzumenko <erterj05@gmail.com>
2021-06-05 22:59:17 +0300

    Solution

--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -1,12 +1,69 @@
 import React from 'react';
 import './CurrentUser.scss';
 
-export const CurrentUser = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+import PropTypes from 'prop-types';
+import { request } from '../api';
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export class CurrentUser extends React.Component {
+  state = {
+    user: {},
+  }
+
+  async componentDidMount() {
+    this.updateUserInfo();
+  }
+
+  async componentDidUpdate(props) {
+    if (this.props.userId !== props.userId) {
+      this.updateUserInfo();
+    }
+  }
+
+  async updateUserInfo() {
+    const { userId } = this.props;
+    let user = await request('/users');
+
+    user = user.find(person => person.id === userId);
+    this.setState({ user });
+  }
+
+  render() {
+    return (
+      <div className="CurrentUser">
+        {this.state.user ? (
+          <>
+            <h2 className="CurrentUser__title">
+              <span>
+                Selected user:
+                {this.props.userId}
+              </span>
+            </h2>
+
+            <h3 className="CurrentUser__name">{this.state.user.name}</h3>
+            <p className="CurrentUser__email">{this.state.user.email}</p>
+            <p className="CurrentUser__phone">{this.state.user.phone}</p>
+            <button
+              type="button"
+              style={{
+                position: 'relative',
+                transform: 'translate(-50%)',
+                left: '50%',
+                marginTop: '15px',
+              }}
+              className="TodoList__user-button button"
+              onClick={this.props.onClear}
+            >
+              Clear
+            </button>
+          </>
+        ) : 'no such user found'
+        }
+      </div>
+    );
+  }
+}
+
+CurrentUser.propTypes = {
+  userId: PropTypes.number.isRequired,
+  onClear: PropTypes.func.isRequired,
+};
