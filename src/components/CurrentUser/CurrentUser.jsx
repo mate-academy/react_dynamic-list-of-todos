5a2caa16b5c350baafb6ddf5b1d25d59b55ac997
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-07-08 22:05:23 +0300

    Fixed v0.6.6

--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -29,23 +29,21 @@ export class CurrentUser extends Component {
 
     if (user) {
       return (
-        <>
-          <div className="CurrentUser">
-            <h2 className="CurrentUser__title">
-              <span>{`Selected user: ${user.id}`}</span>
-            </h2>
-            <h3 className="CurrentUser__name">{user.name}</h3>
-            <p className="CurrentUser__email">{user.email}</p>
-            <p className="CurrentUser__phone">{user.phone}</p>
-            <button
-              type="button"
-              className="my-button"
-              onClick={() => this.props.clear()}
-            >
-              Clear
-            </button>
-          </div>
-        </>
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>{`Selected user: ${user.id}`}</span>
+          </h2>
+          <h3 className="CurrentUser__name">{user.name}</h3>
+          <p className="CurrentUser__email">{user.email}</p>
+          <p className="CurrentUser__phone">{user.phone}</p>
+          <button
+            type="button"
+            className="my-button"
+            onClick={() => this.props.clear()}
+          >
+            Clear
+          </button>
+        </div>
       );
     }
 
