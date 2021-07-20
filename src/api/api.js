c5171ea7ba66619afef0c18462ba1f44b408c63c
Tolik <tolikdolinskiy@gmail.com>
2021-07-20 18:01:17 +0300

    some message

---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+const url = 'https://mate-api.herokuapp.com';
+
+export async function getTodosApi() {
+  const list = await fetch(`${url}/todos`)
+    .then(response => response.json());
+
+  return list;
+}
+
+export async function getUserInfoApi(userId) {
+  const list = await fetch(`${url}/users/${userId}`)
+    .then(response => response.json());
+
+  return list;
+}
