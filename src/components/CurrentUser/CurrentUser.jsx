b04be398eb55b3a777e00e369d4182f92a3322fa
Mykola shcherbak <hohotun21121987@gmail.com>
2021-08-03 16:03:46 +0300

    first try

--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CurrentUser.scss';
+import PropTypes from 'prop-types';
+import { getUsersId } from '../../utils';
 
-export const CurrentUser = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+export class CurrentUser extends Component {
+  state = {
+    user: {},
+  }
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+  componentDidMount() {
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.selectedUserId !== this.props.selectedUserId) {
+      this.loadData();
+    }
+  }
+
+  async loadData() {
+    const user = await getUsersId(this.props.selectedUserId);
+
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    const { onClick } = this.props;
+    const { id, name, email, phone } = user;
+
+    return (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title">
+          <span>{`Selected user: ${id}`}</span>
+        </h2>
+
+        <h3 className="CurrentUser__name">{name}</h3>
+        <p className="CurrentUser__email">{email}</p>
+        <p className="CurrentUser__phone">{phone}</p>
+        <button
+          type="button"
+          className="button button--user"
+          onClick={onClick}
+        >
+          Clear
+        </button>
+      </div>
+    );
+  }
+}
+
+CurrentUser.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+  selectedUserId: PropTypes.number.isRequired,
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,44 +1,82 @@
 import React from 'react';
 import './TodoList.scss';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-export const TodoList = () => (
+export const TodoList = ({
+  todos,
+  onClick,
+  chooseStatus,
+  value,
+  chooseTitle,
+  status,
+
+}) => (
   <div className="TodoList">
     <h2>Todos:</h2>
-
+    <p>Title:</p>
+    <input
+      className="input"
+      value={value}
+      onChange={chooseTitle}
+      placeholder="Type here"
+    />
+    <p>Status:</p>
+    <select
+      value={status}
+      onChange={chooseStatus}
+    >
+      <option value="all">
+        all
+      </option>
+      <option value="in process">
+        active
+      </option>
+      <option value="completed">
+        completed
+      </option>
+    </select>
     <div className="TodoList__list-container">
       <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
-
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
+        {todos.map(todo => (
+          <li
+            key={todo.id}
+            className={classNames('TodoList__item', {
+              'TodoList__item--unchecked': todo.completed,
+              'TodoList__item--checked': !todo.completed,
+            })}
           >
-            User&nbsp;#1
-          </button>
-        </li>
+            <label>
+              <input type="checkbox" readOnly />
+              <p>{todo.title}</p>
+            </label>
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
-
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
+            <button
+              className="TodoList__user-button button"
+              type="button"
+              onClick={() => onClick(todo.userId)}
+            >
+              {`User ${todo.userId}`}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   </div>
 );
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+      userId: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+  onClick: PropTypes.func.isRequired,
+  chooseStatus: PropTypes.func.isRequired,
+  chooseTitle: PropTypes.func.isRequired,
+  status: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
