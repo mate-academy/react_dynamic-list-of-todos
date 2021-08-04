275001071c713d198971463e6026b14493a71c48
Andrii Balitskyi <10balian10@gmail.com>
2021-08-04 22:38:17 +0300

    React dynamic list of TODOs implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+
 import './App.scss';
 import './styles/general.scss';
-import { TodoList } from './components/TodoList';
+
+import { TodoList } from './components/TodoList/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+
+import { getTodos } from './api';
 
 class App extends React.Component {
   state = {
@@ -10,19 +14,48 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
+  componentDidMount() {
+    getTodos('/todos').then(todos => this.setState({
+      todos: todos.filter(
+        todo => todo.title && todo.userId && todo.completed !== null,
+      ),
+    }));
+  }
+
+  handleUserIdSelection = (selectedUserId) => {
+    this.setState((state) => {
+      if (state.selectedUserId === selectedUserId) {
+        return null;
+      }
+
+      return { selectedUserId };
+    });
+  }
+
+  clearUser = () => {
+    this.setState({ selectedUserId: 0 });
+  }
+
   render() {
     const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            handleUserIdSelection={this.handleUserIdSelection}
+            selectedUserId={selectedUserId}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                clearUser={this.clearUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
