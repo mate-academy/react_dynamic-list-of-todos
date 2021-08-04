53e7176cac82a7b50d50b14636e8769bfd577b95
Yehor Merzlenko <yehor.merzlenko@gmail.com>
2021-08-04 17:55:02 +0300

    fixed the method of requesting data from the server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://mate-api.herokuapp.comm';
+const BASE_URL = 'https://mate-api.herokuapp.com';
 
 export const request = url => fetch(`${BASE_URL}${url}`)
   .then((res) => {
