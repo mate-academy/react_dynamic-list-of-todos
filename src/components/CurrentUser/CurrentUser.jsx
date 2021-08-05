2b73d2bee2b6d5d290639903e39d0ba4b31b1eaa
Сакевич Анастасія <asakevych@ASMAC.local>
2021-08-05 22:05:36 +0200

    todo list

--- src/components/CurrentUser/CurrentUser.jsx
+++ src/components/CurrentUser/CurrentUser.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
 import './CurrentUser.scss';
+import PropTypes from 'prop-types';
+import { getUser } from '../../api';
 
-export const CurrentUser = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+export class CurrentUser extends React.Component {
+  state = {
+    user: {},
+  }
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.userId !== this.props.userId) {
+      this.loadUser();
+    }
+  }
+
+  async loadUser() {
+    const user = await getUser(this.props.userId);
+
+    this.setState({ user: user.data });
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title">
+          <span>
+            Selected user:
+            {user.id}
+          </span>
+        </h2>
+
+        <h3 className="CurrentUser__name">{user.name}</h3>
+        <p className="CurrentUser__email">{user.email}</p>
+        <p className="CurrentUser__phone">{user.phone}</p>
+
+        <button
+          className="
+                TodoList__user-button
+                button
+              "
+          type="button"
+          onClick={() => {
+            this.props.selectedUser(0);
+          }}
+        >
+          Clear
+        </button>
+
+      </div>
+    );
+  }
+}
+
+CurrentUser.propTypes = {
+  userId: PropTypes.number.isRequired,
+  selectedUser: PropTypes.func.isRequired,
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,44 +1,109 @@
 import React from 'react';
 import './TodoList.scss';
+import PropTypes from 'prop-types';
 
-export const TodoList = () => (
-  <div className="TodoList">
-    <h2>Todos:</h2>
+export class TodoList extends React.Component {
+  state = {
+    query: '',
+    sortBy: 'All',
+  }
 
-    <div className="TodoList__list-container">
-      <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
+  queryChange = (event) => {
+    this.setState({ query: event.target.value });
+  }
 
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
-          >
-            User&nbsp;#1
-          </button>
-        </li>
+  showFilter = (event) => {
+    this.setState({ sortBy: event.target.value });
+  }
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
+  render() {
+    const { todos, selectedUser, selectedUserId } = this.props;
 
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+    let visibleTodos = todos.filter(
+      todo => todo.title && todo.title.includes(this.state.query),
+    );
+
+    if (this.state.sortBy === 'Active') {
+      visibleTodos = visibleTodos.filter(todo => todo.completed === false);
+    }
+
+    if (this.state.sortBy === 'Completed') {
+      visibleTodos = visibleTodos.filter(todo => todo.completed === true);
+    }
+
+    return (
+      <div className="TodoList">
+        <h2>Todos:</h2>
+
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={this.queryChange}
+          placeholder="Enter something"
+        />
+        <select
+          value={this.state.sortBy}
+          onChange={this.showFilter}
+        >
+          <option value="All">All</option>
+          <option value="Active">Active</option>
+          <option value="Completed">Completed</option>
+        </select>
+
+        <div className="TodoList__list-container">
+          <ul className="TodoList__list">
+            {visibleTodos.map(todo => (
+              <li
+                key={todo.id}
+                className={`TodoList__item ${todo.completed
+                  ? 'TodoList__item--checked'
+                  : 'TodoList__item--unchecked'}`}
+              >
+                <label>
+                  <input type="checkbox" readOnly />
+                  <p>{todo.title}</p>
+                </label>
+
+                {selectedUserId === todo.userId ? (
+                  <button
+                    className="
+                    TodoList__user-button
+                    TodoList__user-button--selected
+                    button
+                  "
+                    type="button"
+                  >
+                    User&nbsp;#
+                    {todo.userId}
+                  </button>
+                ) : (
+                  <button
+                    className="
+                    TodoList__user-button
+                    button
+                  "
+                    type="button"
+                    onClick={() => {
+                      selectedUser(todo.userId);
+                    }}
+                  >
+                    User&nbsp;#
+                    {todo.userId}
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape().isRequired,
+  ).isRequired,
+  selectedUser: PropTypes.func.isRequired,
+  selectedUserId: PropTypes.number.isRequired,
+};
