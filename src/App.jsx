f64d867cf39380eee1beb0ef7f1656d2b9d95fd6
Taras <hevko.tars@gmail.com>
2021-08-05 19:35:09 +0300

    yep

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api';
 
 class App extends React.Component {
   state = {
@@ -10,19 +11,39 @@ class App extends React.Component {
     selectedUserId: 0,
   };
 
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos: todos.filter(todo => todo.title) });
+  }
+
+  onSelected = (selectedUserId) => {
+    this.setState({ selectedUserId });
+  }
+
+  onClear = () => {
+    this.setState({ selectedUserId: 0 });
+  }
+
   render() {
     const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            onSelected={this.onSelected}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+              <CurrentUser
+                userId={selectedUserId}
+                onClear={this.onClear}
+              />
             ) : 'No user selected'}
           </div>
         </div>
