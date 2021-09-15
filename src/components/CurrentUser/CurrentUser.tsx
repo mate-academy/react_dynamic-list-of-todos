6b710c49b2a6a150df1724a473527744ec936e4f
Sasha Havryliuk <akwaccc@gmail.com>
2021-09-15 18:44:05 +0300

    Solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,63 @@
 import React from 'react';
 import './CurrentUser.scss';
+import { getUser } from '../../api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+interface Props {
+  selectedUserId: number;
+  clearUser: () => void;
+}
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+interface State {
+  user: null | User;
+}
+
+export class CurrentUser extends React.Component<Props, State> {
+  state: State = {
+    user: null,
+  };
+
+  componentDidMount() {
+    getUser(this.props.selectedUserId)
+      .then(user => {
+        this.setState({
+          user,
+        });
+      });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps !== this.props) {
+      getUser(this.props.selectedUserId)
+        .then(user => {
+          this.setState({
+            user,
+          });
+        });
+    }
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <>
+        {user && (
+          <div className="CurrentUser">
+            <h2 className="CurrentUser__title"><span>{`Selected user: ${user.id}`}</span></h2>
+            <h3 className="CurrentUser__name">{user.name}</h3>
+            <p className="CurrentUser__email">{user.email}</p>
+            <p className="CurrentUser__phone">{user.phone}</p>
+
+            <button
+              type="button"
+              className="button CurrentUser__clear"
+              onClick={this.props.clearUser}
+            >
+              Clear User
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
+}
