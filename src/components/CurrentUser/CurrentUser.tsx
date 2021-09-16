7833b36414e871d69a4ec0f1b60fb8c5ec82ceda
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-09-16 10:37:26 +0300

    Fixed

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -48,34 +48,36 @@ export class CurrentUser extends React.Component<Props, State> {
     const { onReset } = this.props;
 
     return (
-      <div className="CurrentUser">
-        {hasLoadingError
-          ? <h2>Cannot load user</h2>
-          : (
-            <>
-              <h2 className="CurrentUser__title">
-                <span>{`Selected user: ${currentUser?.id}`}</span>
-              </h2>
-              <h3 className="CurrentUser__name">
-                {currentUser?.name}
-              </h3>
-              <p className="CurrentUser__email">
-                {currentUser?.email}
-              </p>
-              <p className="CurrentUser__phone">
-                {currentUser?.phone}
-              </p>
+      currentUser && (
+        <div className="CurrentUser">
+          {hasLoadingError
+            ? <h2>Cannot load user</h2>
+            : (
+              <>
+                <h2 className="CurrentUser__title">
+                  <span>{`Selected user: ${currentUser?.id}`}</span>
+                </h2>
+                <h3 className="CurrentUser__name">
+                  {currentUser?.name}
+                </h3>
+                <p className="CurrentUser__email">
+                  {currentUser?.email}
+                </p>
+                <p className="CurrentUser__phone">
+                  {currentUser?.phone}
+                </p>
 
-              <button
-                type="button"
-                className="button CurrentUser__clear"
-                onClick={onReset}
-              >
-                Clear
-              </button>
-            </>
-          )}
-      </div>
+                <button
+                  type="button"
+                  className="button CurrentUser__clear"
+                  onClick={onReset}
+                >
+                  Clear
+                </button>
+              </>
+            )}
+        </div>
+      )
     );
   }
 }
