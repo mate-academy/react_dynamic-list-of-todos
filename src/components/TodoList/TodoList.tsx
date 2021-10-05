7251822ebc64435b3c1ef3c5d7ec880a1bc41dfd
Vladyslav Polets <46418908+vpolets@users.noreply.github.com>
2021-10-05 16:43:00 +0300

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,43 +1,101 @@
 import React from 'react';
 import './TodoList.scss';
 
-export const TodoList: React.FC = () => (
+import cn from 'classnames';
+
+interface Props {
+  todos: Todo[],
+  activeTodoId: number,
+  filterQuery: string,
+  showTodos: string,
+  onRandomizer: any,
+  handleShowTodos: any,
+  handleFilterQuery: any,
+  handleClick: any,
+  handleChange: any,
+}
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  activeTodoId,
+  filterQuery,
+  showTodos,
+  onRandomizer,
+  handleShowTodos,
+  handleFilterQuery,
+  handleClick,
+  handleChange,
+}) => (
   <div className="TodoList">
     <h2>Todos:</h2>
 
     <div className="TodoList__list-container">
+
+      <input
+        type="text"
+        className="TodoList__item inputFilter"
+        placeholder="Filter the todos by title"
+        value={filterQuery}
+        onChange={handleFilterQuery}
+      />
+
+      <select
+        className="TodoList__item select"
+        value={showTodos}
+        onChange={handleShowTodos}
+      >
+        <option value="all">All</option>
+        <option value="active">Active</option>
+        <option value="completed">Completed</option>
+      </select>
+
+      <button
+        className="button"
+        type="button"
+        onClick={(event => onRandomizer(event))}
+      >
+        Randomize
+      </button>
+
       <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
-
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
+        {todos.map(({
+          id,
+          userId,
+          title,
+          completed,
+        }) => (
+          <li
+            key={id}
+            className={cn(
+              'TodoList__item',
+              { 'TodoList__item--unchecked': !completed },
+              { 'TodoList__item--checked': completed },
+            )}
           >
-            User&nbsp;#1
-          </button>
-        </li>
+            <label>
+              <input
+                type="checkbox"
+                checked={completed}
+                readOnly
+                onChange={event => handleChange(event, id)}
+              />
+              <p>{title}</p>
+            </label>
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
-
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
+            <button
+              className={cn(
+                { 'TodoList__user-button': activeTodoId !== id },
+                { 'TodoList__user-button--selected': activeTodoId === id },
+                'button',
+              )}
+              type="button"
+              onClick={(event => handleClick(event, userId, id))}
+            >
+              User&nbsp;#
+              {userId}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   </div>
