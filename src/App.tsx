f29eba0bfad3f53a1076d941b5d985731e85ae24
ruslan-bondarenko <ruslan21bondarenko@gmail.com>
2021-10-22 13:44:24 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
-import { TodoList } from './components/TodoList';
-import { CurrentUser } from './components/CurrentUser';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { getTodos, getUser } from './api';
+
+import { TodoList } from './components/TodoList/TodoList';
+import { CurrentUser } from './components/CurrentUser/CurrentUser';
 
 interface State {
-  selectedUserId: number;
+  currentUser: User | null;
+  todos: Todo[];
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
-    selectedUserId: 0,
+    currentUser: null,
+    todos: [],
+  };
+
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos });
+  }
+
+  unselectUser = () => {
+    this.setState({ currentUser: null });
+  };
+
+  currentUser = async (id: number): Promise<void> => {
+    const currentUser = await getUser(id);
+
+    if (this.state.currentUser?.id !== id) {
+      this.setState({ currentUser });
+    }
   };
 
   render() {
-    const { selectedUserId } = this.state;
+    const { currentUser, todos } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList {...{ todos, setUser: this.currentUser }} />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
-            {selectedUserId ? (
-              <CurrentUser />
-            ) : 'No user selected'}
+            {currentUser ? (
+              <CurrentUser user={currentUser} unselect={this.unselectUser} />
+            ) : (
+              <p style={{ textAlign: 'center' }}>No user selected</p>
+            )}
           </div>
         </div>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import './CurrentUser.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+interface Props {
+  user: User;
+  unselect: () => void;
+}
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
+export const CurrentUser: React.FC<Props> = ({ user, unselect }) => (
+  <div className="CurrentUser notification is-info">
+    <h3 className="CurrentUser__name title">
+      {user.name}
+    </h3>
+    <h2 className="CurrentUser__title subtitle ">
+      <span>
+        {'Selected user: '}
+        {user?.id}
+      </span>
+    </h2>
+    <h3 className="CurrentUser__email subtitle is-6">
+      <i className="far fa-envelope" />
+      {'   '}
+      {user.email}
+    </h3>
+    <h3 className="CurrentUser__phone subtitle is-6">
+      <i className="far fa-address-book" />
+      {'   '}
+      {user.phone}
+    </h3>
+    <button
+      type="button"
+      className="delete"
+      value=""
+      onClick={unselect}
+    >
+      text
+    </button>
   </div>
 );
+
+export default CurrentUser;
