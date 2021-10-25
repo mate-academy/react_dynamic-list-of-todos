9f5b2531fd39985063a91d1b9774bb15677922b8
Victor Shevchuk <callmevitok@gmail.com>
2021-10-25 12:23:05 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,50 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './Api';
 
 interface State {
   selectedUserId: number;
+  loadedTodos: Todo[] | [];
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     selectedUserId: 0,
+    loadedTodos: [],
+  };
+
+  componentDidMount() {
+    getTodos().then((todos) => this.setState({ loadedTodos: todos }));
+  }
+
+  selectUser = (userId: number) => {
+    this.setState({ selectedUserId: userId });
   };
 
   render() {
-    const { selectedUserId } = this.state;
+    const {
+      selectedUserId,
+      loadedTodos,
+    } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList
+            todos={loadedTodos}
+            selectUser={this.selectUser}
+            selectedUserId={selectedUserId}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser
+                selectedUserId={selectedUserId}
+                clearUser={this.selectUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,62 @@
 import React from 'react';
 import './CurrentUser.scss';
+import { getUser } from '../../Api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  selectedUserId: number;
+  clearUser: (userId: number) => void;
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+type State = {
+  currentUser: User | null;
+};
+
+export class CurrentUser extends React.Component<Props, State> {
+  state: State = {
+    currentUser: null,
+  };
+
+  async componentDidMount() {
+    await this.setUser();
+  }
+
+  async componentDidUpdate(prevProps: Props) {
+    if (this.props.selectedUserId !== prevProps.selectedUserId) {
+      await this.setUser();
+    }
+  }
+
+  async setUser() {
+    const user = await getUser(this.props.selectedUserId);
+
+    this.setState({ currentUser: user });
+  }
+
+  render() {
+    const { currentUser } = this.state;
+
+    // // eslint-disable-next-line no-console
+    // console.log(currentUser);
+    // // eslint-disable-next-line no-console
+    // console.log(getUser(this.props.selectedUserId));
+    // // eslint-disable-next-line no-console
+    // console.log(this.props.selectedUserId);
+
+    return currentUser && (
+      <div className="CurrentUser">
+        <button
+          type="button"
+          className="button"
+          onClick={() => this.props.clearUser(0)}
+        >
+          Clear
+        </button>
+        <h2 className="CurrentUser__title"><span>{this.props.selectedUserId}</span></h2>
+
+        <h3 className="CurrentUser__name">{currentUser.name}</h3>
+        <p className="CurrentUser__email">{`Email: ${currentUser.email}`}</p>
+        <p className="CurrentUser__phone">{`Phone: ${currentUser.phone}`}</p>
+      </div>
+    );
+  }
+}
