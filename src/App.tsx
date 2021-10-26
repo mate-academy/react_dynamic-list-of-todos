2ed0f9f9aea1954c8452da9af0ac456eb7b6d160
VladSaen <qwefghjklgfd@gmail.com>
2021-10-26 11:28:06 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,49 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 interface State {
+  todos: Todo[]
   selectedUserId: number;
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
+    todos: [],
     selectedUserId: 0,
   };
 
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos });
+  }
+
   render() {
-    const { selectedUserId } = this.state;
+    const { todos, selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList
+            todos={todos}
+            currentUser={selectedUserId}
+            selectedUserId={(userId) => {
+              this.setState({ selectedUserId: userId });
+            }}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser
+                userId={selectedUserId}
+                resetUser={() => {
+                  this.setState({ selectedUserId: 0 });
+                }}
+              />
             ) : 'No user selected'}
           </div>
         </div>
