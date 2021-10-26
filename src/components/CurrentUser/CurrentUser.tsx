c2e31aa888417033abfcd9c38bb14fc94dc69339
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-10-26 09:02:11 +0300

    solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,72 @@
 import React from 'react';
 import './CurrentUser.scss';
+import { getUser } from '../../api/api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+interface Props {
+  userId: number,
+  clearUser: () => void,
+}
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+interface State {
+  user: User | null
+}
+
+export class CurrentUser extends React.Component<Props, State> {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.setUser();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.userId !== this.props.userId) {
+      this.setUser();
+    }
+  }
+
+  async setUser() {
+    const { userId } = this.props;
+    const user = await getUser(userId)
+      .catch(() => this.setState({ user: null }));
+
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+
+    if (!user) {
+      return (
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title"><span>User not found</span></h2>
+          <button
+            type="button"
+            className="button TodoList__user-button"
+            onClick={this.props.clearUser}
+          >
+            Remove selected user
+          </button>
+        </div>
+      );
+    }
+
+    return user && (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title"><span>{`Selected user: ${(user as User).id}`}</span></h2>
+
+        <h3 className="CurrentUser__name">{(user as User).name}</h3>
+        <p className="CurrentUser__email">{(user as User).email}</p>
+        <p className="CurrentUser__phone">{(user as User).phone}</p>
+        <button
+          type="button"
+          className="button CurrentUser__user-button"
+          onClick={this.props.clearUser}
+        >
+          Remove selected user
+        </button>
+      </div>
+    );
+  }
+}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,17 @@
-/// <reference types="react-scripts" />
+type Todo = {
+  id: number,
+  userId: number,
+  completed: boolean,
+  title: string,
+  createdAt: string,
+  updatedAt: string,
+}; /* TODO: DESCRIBE */
 
-type Todo = {}; /* TODO: DESCRIBE */
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+};
