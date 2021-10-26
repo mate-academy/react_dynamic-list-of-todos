6f46552203f1d75cf293c625be380ded771d87b5
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-10-26 23:10:39 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,84 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
+import { Todo } from './types';
 
 interface State {
   selectedUserId: number;
+  todos: Todo[];
+  query: string;
+  searchedChars: string;
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     selectedUserId: 0,
+    todos: [],
+    query: '',
+    searchedChars: '',
+  };
+
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos });
+  }
+
+  setQuery = (query: string) => {
+    this.setState({ query });
+  };
+
+  setChars = (str: string) => {
+    this.setState({ searchedChars: str });
+  };
+
+  selectUserId = (id: number) => {
+    this.setState({ selectedUserId: id });
   };
 
   render() {
-    const { selectedUserId } = this.state;
+    const { selectedUserId, query, searchedChars } = this.state;
+
+    let currentTodos = this.state.todos;
+
+    switch (this.state.query) {
+      case 'all':
+        currentTodos = this.state.todos;
+        break;
+      case 'active':
+        currentTodos = this.state.todos.filter(todo => todo.completed === false);
+        break;
+      case 'completed':
+        currentTodos = this.state.todos.filter(todo => todo.completed === true);
+        break;
+      default:
+        currentTodos = this.state.todos;
+    }
+
+    const todosByChars = currentTodos.filter(({ title }) => {
+      return title.toLowerCase().includes(searchedChars.toLowerCase());
+    });
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList
+            setChars={this.setChars}
+            searchedChars={searchedChars}
+            todos={todosByChars}
+            selectUserId={this.selectUserId}
+            query={query}
+            setQuery={this.setQuery}
+          />
         </div>
-
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser
+                userId={selectedUserId}
+                callback={this.selectUserId}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
+import { getUserById } from '../../api/api';
 import './CurrentUser.scss';
+import { User } from '../../types';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+interface State {
+  user: User | null,
+}
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+type Props = {
+  userId: number,
+  callback: (id:number) => void,
+};
+
+export class CurrentUser extends React.Component <Props, State> {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.setUser();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.userId !== this.props.userId) {
+      this.setUser();
+    }
+  }
+
+  async setUser() {
+    const { userId } = this.props;
+
+    const user = await getUserById(userId)
+      .catch(() => this.setState({ user: null }));
+
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    const { callback } = this.props;
+
+    if (!user) {
+      return '';
+    }
+
+    return (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title">{`Selected user: ${(user as User).id}`}</h2>
+
+        <h3 className="CurrentUser__name">{(user as User).name}</h3>
+        <p className="CurrentUser__email">{(user as User).email}</p>
+        <p className="CurrentUser__phone">{(user as User).phone}</p>
+        <button
+          className="CurrentUser__button"
+          type="button"
+          onClick={() => callback(0)}
+        >
+          Clear
+        </button>
+      </div>
+    );
+  }
+}
