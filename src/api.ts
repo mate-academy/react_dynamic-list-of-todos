dcf3abce48ecb13419b9da5def7cf76e45341568
Zhenia Verbitskaya <jvzns17@gmail.com>
2021-10-26 19:54:47 +0300

    Solution

---
+++ src/api.ts
@@ -0,0 +1,15 @@
+const API_URL = 'https://mate.academy/students-api';
+
+export const request = (url: string) => {
+  return fetch(`${API_URL}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+};
+
+export const getTodos = () => request('/todos');
+export const getUser = (id: number) => request(`/users/${id}`);
