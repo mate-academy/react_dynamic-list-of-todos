00eda1b93a2ba423c7c4de181125c343cd1153f4
sgw8 <ionart3737@gmail.com>
2021-11-01 15:33:24 +0300

    fix

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
 import './CurrentUser.scss';
+import { getUserById } from '../../api/api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type User = {
+  id: number,
+  name: string,
+  email: string,
+  phone: number,
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+type Props = {
+  currentUserId: number;
+  clearSelectedUser: () => void;
+};
+
+type State = {
+  currentUser: User | null;
+};
+
+export class CurrentUser extends React.Component<Props, State> {
+  state: State = {
+    currentUser: null,
+  };
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.currentUserId !== this.props.currentUserId) {
+      this.loadUser();
+    }
+  }
+
+  loadUser = () => {
+    getUserById(this.props.currentUserId)
+      .then(currentUser => {
+        this.setState({ currentUser });
+      });
+  };
+
+  render() {
+    const { currentUser } = this.state;
+    const { clearSelectedUser } = this.props;
+
+    return currentUser && (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title">
+          <span>{`Selected user: ${currentUser.id}`}</span>
+        </h2>
+        <h3 className="CurrentUser__name">{currentUser.name}</h3>
+        <p className="CurrentUser__email">{currentUser.email}</p>
+        <p className="CurrentUser__phone">{currentUser.phone}</p>
+        <button
+          className="CurrentUser__clear button"
+          type="button"
+          onClick={clearSelectedUser}
+        >
+          Hide User
+        </button>
+      </div>
+    );
+  }
+}
