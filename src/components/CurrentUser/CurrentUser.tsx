458c067458fbea2b2072a1e43772ad0d3e2b267e
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-11-18 22:44:59 +0200

    Fixed throwing error

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -9,11 +9,13 @@ interface Props {
 
 interface State {
   user: User,
+  errorMessage: boolean
 }
 
 export class CurrentUser extends React.Component<Props, State> {
   state = {
     user: {} as User,
+    errorMessage: false,
   };
 
   componentDidMount() {
@@ -27,35 +29,50 @@ export class CurrentUser extends React.Component<Props, State> {
   }
 
   async loadUserInfo() {
-    const user: User = await getUserByID(this.props.userId);
+    try {
+      const user: User = await getUserByID(this.props.userId);
 
-    this.setState({
-      user,
-    });
+      this.setState({
+        user,
+        errorMessage: false,
+      });
+    } catch (error) {
+      this.setState({
+        errorMessage: true,
+      });
+    }
   }
 
   render() {
-    const { user } = this.state;
+    const { user, errorMessage } = this.state;
 
     return (
       <div className="CurrentUser">
-        <h2 className="CurrentUser__title">
-          <span className="CurrentUser__title--span">
-            {`Selected user: ${user.id}`}
-          </span>
-        </h2>
+        {errorMessage
+          ? (
+            <h2>There is no such user</h2>
+          )
+          : (
+            <>
+              <h2 className="CurrentUser__title">
+                <span className="CurrentUser__title--span">
+                  {`Selected user: ${user.id}`}
+                </span>
+              </h2>
 
-        <h3 className="CurrentUser__name">{`${user.name} ${user.username}`}</h3>
-        <p className="CurrentUser__email">{user.email}</p>
-        <p className="CurrentUser__phone">{user.phone}</p>
+              <h3 className="CurrentUser__name">{`${user.name} ${user.username}`}</h3>
+              <p className="CurrentUser__email">{user.email}</p>
+              <p className="CurrentUser__phone">{user.phone}</p>
 
-        <button
-          type="button"
-          className="button"
-          onClick={this.props.clearSelection}
-        >
-          Clear user
-        </button>
+              <button
+                type="button"
+                className="button"
+                onClick={this.props.clearSelection}
+              >
+                Clear user
+              </button>
+            </>
+          )}
       </div>
     );
   }
