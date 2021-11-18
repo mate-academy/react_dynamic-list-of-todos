2a2872a10cbfa5b110d1e37181c654c962981ee2
Volodymyr Taranin <jaffplay@gmail.com>
2021-11-18 20:40:30 +0200

    add input and select

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,16 @@ import { Todo } from './types/types';
 interface State {
   todos: Todo[];
   selectedUserId: number;
+  search: string,
+  status: string,
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     todos: [],
     selectedUserId: 0,
+    search: '',
+    status: 'all',
   };
 
   async componentDidMount() {
@@ -33,14 +37,61 @@ class App extends React.Component<{}, State> {
     });
   };
 
+  handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   render() {
-    const { todos, selectedUserId } = this.state;
+    const {
+      todos,
+      selectedUserId,
+      search,
+      status,
+    } = this.state;
+    let filteredTodos = todos.filter(todo => todo.title.includes(search));
+
+    if (status === 'active') {
+      filteredTodos = filteredTodos.filter(todo => !todo.completed);
+    }
+
+    if (status === 'completed') {
+      filteredTodos = filteredTodos.filter(todo => todo.completed);
+    }
 
     return (
       <div className="App">
         <div className="App__sidebar">
+          <input
+            type="text"
+            name="search"
+            value={search}
+            onChange={this.handleChange}
+          />
+
+          <select
+            name="status"
+            onChange={this.handleChange}
+          >
+            <option value="all">
+              All
+            </option>
+
+            <option value="active">
+              Active
+            </option>
+
+            <option value="completed">
+              Completed
+            </option>
+          </select>
+
           <TodoList
-            todos={todos}
+            todos={filteredTodos}
             selectUser={this.selectUser}
           />
         </div>
