33aa869412cf78ddd82f8a8f87963d67a388436a
Darina Savytska <coldshine99@gmail.com>
2021-11-20 17:53:23 +0200

    Solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,86 @@
 import React from 'react';
 import './CurrentUser.scss';
+import { getUser } from '../../api/api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  userId: number,
+  onClear: () => void,
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+type State = {
+  user: User | null,
+  userError: boolean,
+};
+
+export class CurrentUser extends React.Component<Props, State> {
+  state = {
+    user: {} as User,
+    userError: false,
+  };
+
+  async componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.userId !== prevProps.userId) {
+      this.loadUser();
+    }
+  }
+
+  loadUser = async () => {
+    try {
+      const user = await getUser(this.props.userId);
+
+      this.setState({
+        user,
+        userError: false,
+      });
+    } catch {
+      this.setState({
+        userError: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      !this.state.userError ? (
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>
+              Selected user:
+              {' '}
+              {this.state.user.id}
+            </span>
+          </h2>
+
+          <h3 className="CurrentUser__name">
+            {this.state.user.name}
+          </h3>
+
+          <p className="CurrentUser__email">
+            {this.state.user.email}
+          </p>
+
+          <p className="CurrentUser__phone">
+            {this.state.user.phone}
+          </p>
+
+          <button
+            type="button"
+            className="button"
+            onClick={() => {
+              this.props.onClear();
+            }}
+          >
+            Clear user
+          </button>
+        </div>
+      )
+        : (
+          `User id #${this.state.user.id} not found`
+        )
+    );
+  }
+}
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,44 +1,123 @@
 import React from 'react';
 import './TodoList.scss';
+import classNames from 'classnames';
 
-export const TodoList: React.FC = () => (
-  <div className="TodoList">
-    <h2>Todos:</h2>
+type Props = {
+  todos: Todo[],
+  onCheckted: (userId: number) => void,
+  selectedUserId: number,
+};
 
-    <div className="TodoList__list-container">
-      <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
+type State = {
+  titleInput: string,
+  sortBy: string,
+};
 
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
-          >
-            User&nbsp;#1
-          </button>
-        </li>
+export class TodoList extends React.Component<Props, State> {
+  state = {
+    titleInput: '',
+    sortBy: 'all',
+  };
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      titleInput: event.target.value.toLowerCase(),
+    });
+  };
 
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+  handleSelectedChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({
+      sortBy: event.target.value,
+    });
+  };
+
+  getSortedBySelected = (todos: Todo[]) => {
+    if (this.state.sortBy !== 'all') {
+      if (this.state.sortBy === 'ready') {
+        return todos.filter(todo => todo.completed === true);
+      }
+
+      return todos.filter(todo => todo.completed === false);
+    }
+
+    return todos;
+  };
+
+  getSearchTodos = (todos: Todo[]) => {
+    const { titleInput } = this.state;
+
+    if (titleInput) {
+      return todos.filter(todo => todo.title.toLowerCase().includes(titleInput));
+    }
+
+    return todos;
+  };
+
+  render() {
+    const { todos, onCheckted, selectedUserId } = this.props;
+    const todosByStatus = this.getSortedBySelected(todos);
+    const todosByInput = this.getSearchTodos(todosByStatus);
+
+    return (
+      <div className="TodoList">
+        <h2>Todos:</h2>
+
+        <input
+          type="text"
+          name="sortByText"
+          id="sortByText"
+          placeholder="Search todo"
+          value={this.state.titleInput}
+          onChange={this.handleInputChange}
+        />
+
+        <select
+          name="sortBySelect"
+          id="sortBySelect"
+          value={this.state.sortBy}
+          onChange={this.handleSelectedChange}
+        >
+          <option value="all">
+            Show all
+          </option>
+          <option value="ready">
+            Show complited
+          </option>
+          <option value="notReady">
+            Show not complited
+          </option>
+        </select>
+
+        <div className="TodoList__list-container">
+          <ul className="TodoList__list">
+            {todosByInput.map(todo => (
+              <li
+                key={todo.id}
+                className={classNames('TodoList__item',
+                  { 'TodoList__item--unchecked': todo.completed === false },
+                  { 'TodoList__item--checked': todo.completed === true })}
+              >
+                <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */}</>
+                <label>
+                  <input type="checkbox" readOnly />
+                  <p>{todo.title}</p>
+                </label>
+
+                <button
+                  className={classNames('TodoList__user-button', 'button',
+                    { 'TodoList__user-button--selected ': todo.userId === selectedUserId })}
+                  type="button"
+                  onClick={() => onCheckted(todo.userId)}
+                >
+                  User
+                  {' '}
+                  {todo.userId}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
