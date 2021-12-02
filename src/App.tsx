b757f97831415beb6b6a2c19861cf9b816b485f6
Vladyslav.Sak <vlsak@deloitte.com>
2021-12-02 14:30:26 +0200

    Solved main task and additional

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,58 @@
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
-import { TodoList } from './components/TodoList';
-import { CurrentUser } from './components/CurrentUser';
+import * as apiService from './api/api';
+import CurrentUser from './components/CurrentUser/CurrentUser';
+import TodoList from './components/TodoList/TodoList';
 
 interface State {
   selectedUserId: number;
+  todos: Todo[];
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     selectedUserId: 0,
+    todos: [],
   };
 
+  componentDidMount() {
+    this.loadTodos();
+  }
+
+  handleSelectUser = (userId: number) => {
+    this.setState({ selectedUserId: userId });
+  };
+
+  clearSelectedUser = () => {
+    this.setState({ selectedUserId: 0 });
+  };
+
+  async loadTodos() {
+    const todos = await apiService.getAllTodos();
+
+    this.setState({ todos });
+  }
+
   render() {
-    const { selectedUserId } = this.state;
+    const { selectedUserId, todos } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          {todos.length > 0 && (
+            <TodoList
+              selectedUser={selectedUserId}
+              onSelectUser={this.handleSelectUser}
+              todos={todos}
+            />
+          )}
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser onClear={this.clearSelectedUser} userId={selectedUserId} />
             ) : 'No user selected'}
           </div>
         </div>
