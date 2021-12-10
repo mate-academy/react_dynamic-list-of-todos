0fe574cb9fc2d5c5b2010ba0131948e1ba4e2fc3
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-12-10 14:07:54 +0200

    Files Added

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type Todo = {
+  id: number,
+  completed: boolean,
+  title: string,
+  userId: number,
+};
+
+export type User = {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  phone: string,
+};
