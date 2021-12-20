7b85144c3453f0d4e618a206bdaa6310282d397e
Maryna Saikun <marynasaikun@gmail.com>
2021-12-20 21:12:28 +0200

    Solution

---
+++ src/api/api.ts
@@ -0,0 +1,20 @@
+export const BASE_URL = 'https://mate.academy/students-api';
+
+const loadData = (url: string) => {
+  return fetch(`${BASE_URL}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error of loading');
+      }
+
+      return response.json();
+    });
+};
+
+export const loadTodos = () => {
+  return loadData('/todos');
+};
+
+export const loadUser = (userId: number) => {
+  return loadData(`/users/${userId}`);
+};
