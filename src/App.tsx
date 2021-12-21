2219c318aee94deed7f566eda0b5c61c8f07c298
JOJO\38097 <kartemoko@gmail.com>
2021-12-21 18:40:52 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,33 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 interface State {
-  selectedUserId: number;
+  selectedUserId: number,
+  todos: Todo[],
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     selectedUserId: 0,
+    todos: [],
+  };
+
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos });
+  }
+
+  checkedUser = (userId: number) => {
+    this.setState({ selectedUserId: userId });
+  };
+
+  unselectAll = () => {
+    this.setState({
+      selectedUserId: 0,
+    });
   };
 
   render() {
@@ -19,13 +38,20 @@ class App extends React.Component<{}, State> {
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList
+            todos={this.state.todos}
+            onCheckted={this.checkedUser}
+            selectedUserId={this.state.selectedUserId}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser
+                userId={this.state.selectedUserId}
+                onClear={this.unselectAll}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,21 @@
 /// <reference types="react-scripts" />
 
-type Todo = {}; /* TODO: DESCRIBE */
+type Todo = {
+  id: number,
+  createdAt: string,
+  updatedAt: string,
+  userId: number,
+  title: string,
+  completed: boolean,
+};
+
+type User = {
+  id: number,
+  createAt: string,
+  updatedAt: string,
+  name: string,
+  username: string | null,
+  email: string | null,
+  phone: string | null,
+  website: string | null,
+};
