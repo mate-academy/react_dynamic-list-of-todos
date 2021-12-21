c7753281d0085d1159731e8da5723a93f7243c18
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-21 11:37:33 +0200

    solution was added

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,66 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
+
+import { Todo } from './types/Todo';
 
 interface State {
   selectedUserId: number;
+  todos: Todo[] | [],
+  errorMessage: string,
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     selectedUserId: 0,
+    todos: [],
+    errorMessage: '',
+  };
+
+  async componentDidMount() {
+    try {
+      const todos = await getTodos();
+
+      this.setState({ todos });
+    } catch {
+      this.setState({ errorMessage: 'List is not found' });
+    }
+  }
+
+  selectUser = (userId: number) => {
+    if (this.state.selectedUserId === userId) {
+      return;
+    }
+
+    this.setState({ selectedUserId: userId });
+  };
+
+  clearUser = () => {
+    this.setState({ selectedUserId: 0 });
   };
 
   render() {
-    const { selectedUserId } = this.state;
+    const { selectedUserId, todos, errorMessage } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          {todos.length > 0
+            ? (
+              <TodoList
+                todos={todos}
+                selectUser={this.selectUser}
+                selectedUserId={selectedUserId}
+              />
+            )
+            : <p>{errorMessage}</p>}
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser userId={selectedUserId} clearUser={this.clearUser} />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,94 @@
 import React from 'react';
 import './CurrentUser.scss';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+import { User } from '../../types/User';
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+import { getUser } from '../../api/api';
+
+type Props = {
+  userId: number,
+  clearUser: () => void,
+};
+
+type State = {
+  user: User | null,
+  errorMessage: string,
+};
+
+export class CurrentUser extends React.Component<Props, State> {
+  state = {
+    user: {
+      id: 1,
+      createdAt: '',
+      updatedAt: '',
+      name: '',
+      username: '',
+      email: '',
+      phone: '',
+      website: '',
+    },
+    errorMessage: '',
+  };
+
+  async componentDidMount() {
+    this.loadUserFromServer();
+  }
+
+  async componentDidUpdate(prevProps: Props) {
+    if (prevProps.userId !== this.props.userId) {
+      this.loadUserFromServer();
+    }
+  }
+
+  loadUserFromServer = async () => {
+    try {
+      const user = await getUser(this.props.userId);
+
+      this.setState({ user, errorMessage: '' });
+    } catch (error) {
+      this.setState({ user: null, errorMessage: 'User was not found' });
+    }
+  };
+
+  render() {
+    const { user, errorMessage } = this.state;
+
+    if (!user) {
+      return (
+        <>
+          <p>Loading User...</p>
+        </>
+      );
+    }
+
+    return (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title">
+          <span>
+            Selected user:
+            {this.props.userId}
+          </span>
+        </h2>
+        {user ? (
+          <>
+            <h3 className="CurrentUser__name">{user.name}</h3>
+            <p className="CurrentUser__email">{user.email}</p>
+            <p className="CurrentUser__phone">{user.phone}</p>
+
+            <button
+              className="CurrentUser__button button"
+              type="button"
+              onClick={this.props.clearUser}
+            >
+              Clear
+            </button>
+          </>
+        ) : (
+          <p>
+            {errorMessage}
+          </p>
+        )}
+      </div>
+    );
+  }
+}
