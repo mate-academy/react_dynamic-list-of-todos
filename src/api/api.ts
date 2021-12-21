27aa4c4bb6df0e6f1ffc3fb663fe790cbff5f61f
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-21 22:01:39 +0200

    implemented task

---
+++ src/api/api.ts
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://mate.academy/students-api/';
+
+export const getTodos = () => {
+  return fetch(`${BASE_URL}/todos`)
+    .then(response => response.json());
+};
+
+export const getUsers = (userId:number) => {
+  return fetch(`${BASE_URL}/users/${userId}`)
+    .then(response => response.json());
+};
