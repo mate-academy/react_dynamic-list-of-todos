8629a8ce74757fc2fe044fdfdec223f5bc8bd1f0
Ivan Meleganych <imi92@ukr.net>
2021-12-22 11:48:46 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
+import { getTodos } from './api';
+import { Todo } from './react-app-env';
+
 interface State {
   selectedUserId: number;
+  todos: Todo[];
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
-    selectedUserId: 0,
+    selectedUserId: 1,
+    todos: [],
   };
+
+  componentDidMount() {
+    this.load();
+  }
+
+  onSelect = (userId: number) => {
+    this.setState({ selectedUserId: userId });
+  };
+
+  clear = () => {
+    this.setState({ selectedUserId: 0 });
+  };
+
+  async load() {
+    this.setState({ todos: await getTodos() });
+  }
 
   render() {
     const { selectedUserId } = this.state;
@@ -19,13 +41,20 @@ class App extends React.Component<{}, State> {
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList
+            todos={this.state.todos}
+            selectedUserId={this.state.selectedUserId}
+            onSelect={this.onSelect}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser
+                userId={selectedUserId}
+                clear={this.clear}
+              />
             ) : 'No user selected'}
           </div>
         </div>
