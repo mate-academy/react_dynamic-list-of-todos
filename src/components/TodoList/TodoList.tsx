de35e2e086815d993a995db22036e61e3c6e2a19
lanebx <havchikos@gmail.com>
2021-12-23 14:34:22 +0200

    added select

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-len */
@@ -13,23 +14,29 @@ interface Props {
 
 interface State {
   inputValue?: string;
+  selectValue?: string;
 }
 
 export class TodoList extends React.Component<Props, State> {
   state: State = {
     inputValue: '',
+    selectValue: 'all',
   };
 
   handleChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.ChangeEvent<HTMLInputElement>
+    | React.ChangeEvent<HTMLSelectElement>,
   ) => {
     const { name, value } = event.target;
+
+    console.log(name, value);
 
     this.setState({ [name]: value });
   };
 
   render() {
     const { todos, onSelectUserID, selectID } = this.props;
+    const { inputValue, selectValue } = this.state;
 
     return (
       <div className="TodoList">
@@ -39,15 +46,28 @@ export class TodoList extends React.Component<Props, State> {
           type="text"
           name="inputValue"
           className="TodoList__input-filter"
-          value={this.state.inputValue}
+          value={inputValue}
           onChange={(e) => {
             this.handleChange(e);
           }}
         />
 
+        <select name="selectValue" onChange={(e) => this.handleChange(e)} value={selectValue}>
+          <option value="all">all</option>
+          <option value="false">active</option>
+          <option value="true">completed</option>
+        </select>
+
         <div className="TodoList__list-container">
           <ul className="TodoList__list">
             {todos
+              .filter(todo => {
+                if (selectValue === 'all') {
+                  return true;
+                }
+
+                return String(todo.completed) === selectValue;
+              })
               .filter(todo => todo.title.includes(this.state.inputValue || ''))
               .map((todo: Todo) => {
                 return (
