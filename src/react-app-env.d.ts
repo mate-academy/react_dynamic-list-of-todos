0d70a3772b35c378388ee890d0dc003a1e45d66d
Alina Shchavinska <shchavinska.alina@gmail.com>
2021-12-23 12:58:33 +0200

    Solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,13 @@
-/// <reference types="react-scripts" />
+export type Todo = {
+  id: number;
+  userId: number;
+  completed: boolean;
+  title: string;
+};
 
-type Todo = {}; /* TODO: DESCRIBE */
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+};
