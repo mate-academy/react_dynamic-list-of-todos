4889a9318315bce3d697aa7ba1f43f8f2a8150e2
Pavlo Melnyk <pashamelnyk95@gmail.com>
2022-02-01 20:37:54 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,134 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api';
 
 interface State {
   selectedUserId: number;
+  todos: Todo[];
+  titleToSearch: string;
+  todosToShow: string;
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     selectedUserId: 0,
+    todos: [],
+    titleToSearch: '',
+    todosToShow: 'all',
+  };
+
+  async componentDidMount() {
+    const todos = await getTodos();
+
+    this.setState({ todos: [...todos] });
+  }
+
+  selectUser = (selectedUserId: number) => {
+    if (selectedUserId === this.state.selectedUserId) {
+      this.setState({ selectedUserId: 0 });
+    } else {
+      this.setState({ selectedUserId });
+    }
+  };
+
+  prepareTodos = () => {
+    const {
+      todos,
+      titleToSearch,
+      todosToShow,
+    } = this.state;
+
+    let copiedTodos = [...todos];
+
+    if (titleToSearch) {
+      copiedTodos = copiedTodos.filter(
+        todo => todo.title.toLowerCase().includes(titleToSearch.toLowerCase()),
+      );
+    }
+
+    switch (todosToShow) {
+      case 'active':
+        return copiedTodos.filter(todo => !todo.completed);
+
+      case 'completed':
+        return copiedTodos.filter(todo => todo.completed);
+
+      default:
+        return copiedTodos;
+    }
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>
+  | React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.currentTarget;
+
+    this.setState((state) => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  changeTodoStatus = (todoId: number) => {
+    const { todos } = this.state;
+
+    const changedTodos = todos.map(todo => {
+      if (todo.id === todoId) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+
+      return todo;
+    });
+
+    this.setState({
+      todos: changedTodos,
+    });
+  };
+
+  randomizeOrder = () => {
+    const { todos } = this.state;
+    const copiedTodos = [...todos];
+
+    copiedTodos.sort(() => Math.random() - 0.5);
+
+    this.setState({
+      todos: copiedTodos,
+    });
   };
 
   render() {
-    const { selectedUserId } = this.state;
+    const {
+      selectedUserId,
+      titleToSearch,
+      todosToShow,
+    } = this.state;
+    const preparedTodos = this.prepareTodos();
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList />
+          <TodoList
+            todos={preparedTodos}
+            selectUser={this.selectUser}
+            selectedUserId={selectedUserId}
+            handleChange={this.handleChange}
+            titleToSearch={titleToSearch}
+            todosToShow={todosToShow}
+            randomize={this.randomizeOrder}
+            changeTodoStatus={this.changeTodoStatus}
+          />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
             {selectedUserId ? (
-              <CurrentUser />
+              <CurrentUser
+                selectedUserId={selectedUserId}
+                selectUser={this.selectUser}
+              />
             ) : 'No user selected'}
           </div>
         </div>
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,63 @@
 import React from 'react';
 import './CurrentUser.scss';
+import { getUser } from '../../api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  selectedUserId: number;
+  selectUser: (selectedUserId: number) => void;
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+type State = {
+  user: User | null;
+};
+
+export class CurrentUser extends React.Component<Props, State> {
+  state: State = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.selectedUserId !== prevProps.selectedUserId) {
+      this.loadUser();
+    }
+  }
+
+  async loadUser() {
+    const user = await getUser(this.props.selectedUserId);
+
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    const { selectedUserId, selectUser } = this.props;
+
+    return (
+      <>
+        {user && (
+          <div className="CurrentUser">
+            <h2 className="CurrentUser__title">
+              <span>{`Selected user: ${selectedUserId}`}</span>
+            </h2>
+
+            <h3 className="CurrentUser__name">{user.name}</h3>
+            <p className="CurrentUser__email">{user.email}</p>
+            <p className="CurrentUser__phone">{user.phone}</p>
+
+            <button
+              type="button"
+              className="button CurrentUser__clear"
+              onClick={() => selectUser(0)}
+            >
+              Clear
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
+}
