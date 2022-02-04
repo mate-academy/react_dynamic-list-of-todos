d1b9be236ce3b7603d99c8e3ae7cb818541c8fab
DenysCheporniuk <denys.cheporniuk@gmail.com>
2022-02-04 11:46:56 +0200

    React dynamic list of TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/todos';
 
-interface State {
-  selectedUserId: number;
-}
+const App: React.FC = () => {
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-class App extends React.Component<{}, State> {
-  state: State = {
-    selectedUserId: 0,
-  };
+  useEffect(() => {
+    const fetchTodo = async () => {
+      const todosFromServer = await getTodos();
 
-  render() {
-    const { selectedUserId } = this.state;
+      setTodos(todosFromServer);
+    };
 
-    return (
-      <div className="App">
-        <div className="App__sidebar">
-          <TodoList />
-        </div>
+    fetchTodo();
+  }, []);
 
-        <div className="App__content">
-          <div className="App__content-container">
-            {selectedUserId ? (
-              <CurrentUser />
-            ) : 'No user selected'}
-          </div>
+  return (
+    <div className="App">
+      <div className="App__sidebar">
+        <TodoList
+          todos={todos}
+          setUserId={setSelectedUserId}
+        />
+      </div>
+
+      <div className="App__content">
+        <div className="App__content-container">
+          {selectedUserId ? (
+            <CurrentUser
+              userId={selectedUserId}
+            />
+          ) : 'No user selected'}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
