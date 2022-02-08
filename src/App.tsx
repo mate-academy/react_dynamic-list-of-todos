eec9dad3c82e02e71f637db38e139fc5f19fa930
Valeria Valuyskaya <valeriavaluyskaya@gmail.com>
2022-02-08 17:02:18 +0200

    structure is fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,58 +3,14 @@ import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
-import { getTodos } from './api';
 
 type State = {
-  todos: Todo[],
   selectedUserId: number,
-  searchTitle: string,
-  selectTodo: string,
 };
 
 class App extends React.Component<{}, State> {
   state: State = {
-    todos: [],
     selectedUserId: 0,
-    searchTitle: '',
-    selectTodo: 'all',
-  };
-
-  async componentDidMount() {
-    const todos = await getTodos();
-
-    this.setState({
-      todos: [...todos],
-    });
-  }
-
-  handleSearchTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      searchTitle: event.target.value,
-    });
-  };
-
-  handleSearchStatus = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({
-      selectTodo: event.target.value,
-    });
-  };
-
-  getTodos = () => {
-    const { searchTitle, todos, selectTodo } = this.state;
-
-    const copyTodos = todos.filter(todo => todo.title.toLowerCase()
-      .includes(searchTitle.toLowerCase()));
-
-    if (selectTodo === 'active') {
-      return copyTodos.filter(todo => !todo.completed);
-    }
-
-    if (selectTodo === 'completed') {
-      return copyTodos.filter(todo => todo.completed);
-    }
-
-    return copyTodos;
   };
 
   selectedUser = (selectedUserId: number) => {
@@ -67,39 +23,14 @@ class App extends React.Component<{}, State> {
     this.setState({ selectedUserId: 0 });
   };
 
-  changeStatus = (userId: number) => {
-    const { todos } = this.state;
-
-    const newTodoStatus = todos.map(todo => {
-      if (todo.id === userId) {
-        return { ...todo, completed: !todo.completed };
-      }
-
-      return todo;
-    });
-
-    this.setState({
-      todos: newTodoStatus,
-    });
-  };
-
   render() {
-    const getSelectedTodos = this.getTodos();
-
-    const { selectedUserId, searchTitle, selectTodo } = this.state;
+    const { selectedUserId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
           <TodoList
-            todos={getSelectedTodos}
-            handleSearchTitle={this.handleSearchTitle}
-            searchTitle={searchTitle}
             selectedUser={this.selectedUser}
-            handleSearchStatus={this.handleSearchStatus}
-            changeStatus={this.changeStatus}
-            selectTodo={selectTodo}
-
           />
         </div>
 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,91 +1,151 @@
 import React from 'react';
 import './TodoList.scss';
 import classNames from 'classnames';
+import { getTodos } from '../../api';
 
-type Props = {
-  todos: Todo[],
-  selectedUser: (userId: number) => void,
+type State = {
   searchTitle: string,
-  handleSearchTitle: (event: React.ChangeEvent<HTMLInputElement>) => void,
-  handleSearchStatus: (event: React.ChangeEvent<HTMLSelectElement>) => void,
   selectTodo: string,
-  changeStatus: (userId: number) => void,
+  todos: Todo[],
 };
 
-export const TodoList: React.FC<Props> = ({
-  todos,
-  searchTitle,
-  handleSearchTitle,
-  selectedUser,
-  handleSearchStatus,
-  selectTodo,
-  changeStatus,
-}) => (
+type Props = {
+  selectedUser: (userId: number) => void,
+};
 
-  <div className="TodoList">
-    <h2>Todos:</h2>
-    <div className="TodoList__search">
-      <label htmlFor="search-query" className="label">
-        Search todo
-        <div className="control">
-          <input
-            type="text"
-            id="search-query"
-            className="TodoList__item"
-            placeholder="Type search todo"
-            value={searchTitle}
-            onChange={handleSearchTitle}
-          />
-        </div>
-      </label>
-    </div>
+export class TodoList extends React.Component<Props, State> {
+  state: State = {
+    searchTitle: '',
+    selectTodo: '',
+    todos: [],
+  };
 
-    <div className="select">
-      <select
-        name="selectTodo"
-        value={selectTodo}
-        onChange={handleSearchStatus}
-        className="TodoList__item"
-      >
-        <option value="all">Show all</option>
-        <option value="active">Active</option>
-        <option value="completed">Completed</option>
-      </select>
-    </div>
+  async componentDidMount() {
+    const todos = await getTodos();
 
-    <div className="TodoList__list-container">
-      <ul className="TodoList__list">
-        {todos.map(todo => (
-          <li
-            className={classNames('TodoList__item',
-              { 'TodoList__item--unchecked': !todo.completed },
-              { 'TodoList__item--checked': todo.completed })}
-            key={todo.id}
-          >
-            <label htmlFor="search-query">
+    this.setState({
+      todos: [...todos],
+    });
+  }
+
+  handleSearchTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      searchTitle: event.target.value,
+    });
+  };
+
+  handleSearchStatus = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({
+      selectTodo: event.target.value,
+    });
+  };
+
+  getTodos = () => {
+    const { searchTitle, selectTodo, todos } = this.state;
+
+    const copyTodos = todos.filter(todo => todo.title.toLowerCase()
+      .includes(searchTitle.toLowerCase()));
+
+    if (selectTodo === 'active') {
+      return copyTodos.filter(todo => !todo.completed);
+    }
+
+    if (selectTodo === 'completed') {
+      return copyTodos.filter(todo => todo.completed);
+    }
+
+    return copyTodos;
+  };
+
+  changeStatus = (userId: number) => {
+    const { todos } = this.state;
+
+    const newTodoStatus = todos.map(todo => {
+      if (todo.id === userId) {
+        return { ...todo, completed: !todo.completed };
+      }
+
+      return todo;
+    });
+
+    this.setState({
+      todos: newTodoStatus,
+    });
+  };
+
+  render() {
+    const { searchTitle, selectTodo } = this.state;
+    const { selectedUser } = this.props;
+
+    const getSelectedTodos = this.getTodos();
+
+    return (
+      <div className="TodoList">
+        <h2>Todos:</h2>
+        <div className="TodoList__search">
+          <label htmlFor="search-query" className="label">
+            Search todo
+            <div className="control">
               <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => changeStatus(todo.id)}
+                type="text"
+                id="search-query"
+                className="TodoList__item"
+                placeholder="Type search todo"
+                value={searchTitle}
+                onChange={this.handleSearchTitle}
               />
-              <p>{todo.title}</p>
-            </label>
+            </div>
+          </label>
+        </div>
 
-            <button
-              className="
-                TodoList__user-button
-                TodoList__user-button--selected
-                button
-              "
-              type="button"
-              onClick={() => selectedUser(+todo.userId)}
-            >
-              User #
-              {todo.userId}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-);
+        <div className="select">
+          <select
+            name="selectTodo"
+            value={selectTodo}
+            onChange={this.handleSearchStatus}
+            className="TodoList__item"
+          >
+            <option value="all">Show all</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </div>
+
+        <div className="TodoList__list-container">
+          <ul className="TodoList__list">
+            {getSelectedTodos.map(todo => (
+              <li
+                className={classNames('TodoList__item',
+                  { 'TodoList__item--unchecked': !todo.completed },
+                  { 'TodoList__item--checked': todo.completed })}
+                key={todo.id}
+              >
+                <label htmlFor="search-query">
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => this.changeStatus(todo.id)}
+                  />
+                  <p>{todo.title}</p>
+                </label>
+
+                <button
+                  className="
+                    TodoList__user-button
+                    TodoList__user-button--selected
+                    button
+                  "
+                  type="button"
+                  onClick={() => selectedUser(+todo.userId)}
+                >
+                  User #
+                  {todo.userId}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
