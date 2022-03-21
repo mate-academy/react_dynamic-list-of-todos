55e7d9d3c1b079041f05c737684bb3f86505faf9
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-03-21 18:54:06 +0200

    fixed api fetch

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,13 +1,10 @@
 const API_URL = 'https://mate.academy/students-api';
 
-const getData = (endpoint: string): Promise<Todo[]> => {
+const getData = (endpoint: string) => {
   return fetch(`${API_URL}${endpoint}`)
     .then(response => response.json());
 };
 
-export const getTodos = () => getData('/todos');
+export const getTodos = (): Promise<Todo[]> => getData('/todos');
 
-export const getUser = (userId: number): Promise<User> => {
-  return fetch(`${API_URL}/users/${userId}`)
-    .then(response => response.json());
-};
+export const getUser = (userId: number): Promise<User> => getData(`/users/${userId}`);
