f8dec83a195755892246a738c064b80f3c972d2f
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-03-21 17:26:20 +0200

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,53 @@ import { getTodos } from './api/api';
 const App: React.FC = () => {
   const [selectedUserId, setSelectedUserId] = useState(0);
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [selectValue, setSelectValue] = useState<string>('all');
 
   useEffect(() => {
     getTodos().then(response => setTodos(response));
   }, []);
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+
+    setSelectValue(value);
+  };
+
+  const getPreparedTodos = () => {
+    const queryCase = query.toLowerCase();
+
+    const preparedTodos = todos.filter(todo => (
+      todo.title.toLowerCase().includes(queryCase)
+    ));
+
+    switch (selectValue) {
+      case 'completed':
+        return preparedTodos.filter(todo => todo.completed);
+      case 'not':
+        return preparedTodos.filter(todo => !todo.completed);
+      default:
+        return preparedTodos;
+    }
+  };
+
   return (
     <div className="App">
       <div className="App__sidebar">
         <TodoList
-          todos={todos}
+          handleChange={handleChange}
+          handleSelectChange={handleSelectChange}
+          todos={getPreparedTodos()}
           onUserSelect={setSelectedUserId}
           userId={selectedUserId}
+          query={query}
+          selectValue={selectValue}
         />
       </div>
 
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useCallback,
+} from 'react';
 import { getUser } from '../../api/api';
 import './CurrentUser.scss';
 
@@ -9,30 +14,45 @@ type Props = {
 
 export const CurrentUser: React.FC<Props> = ({ userId, selectedUser }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setLoading] = useState(true);
+
+  const textLoadingUser = useMemo(() => {
+    return isLoading ? <p>Loading...</p> : <p>User not found</p>;
+  }, [isLoading]);
+
+  const uploadUser = useCallback(async () => {
+    setLoading(true);
+    setUser(null);
+    setUser(await getUser(userId));
+    setLoading(false);
+  }, [userId]);
 
   useEffect(() => {
-    getUser(userId)
-      .then(setUser);
+    uploadUser();
   }, [userId]);
 
   return (
-    <div className="CurrentUser">
-      <h2 className="CurrentUser__title">
-        <span>
-          Selected user:
-          {user?.id}
-        </span>
-      </h2>
+    !user
+      ? textLoadingUser
+      : (
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>
+              Selected user:
+              {user?.id}
+            </span>
+          </h2>
 
-      <h3 className="CurrentUser__name">{user?.name}</h3>
-      <p className="CurrentUser__email">{user?.email}</p>
-      <p className="CurrentUser__phone">{user?.phone}</p>
-      <button
-        type="button"
-        onClick={() => selectedUser(0)}
-      >
-        Clear
-      </button>
-    </div>
+          <h3 className="CurrentUser__name">{user?.name}</h3>
+          <p className="CurrentUser__email">{user?.email}</p>
+          <p className="CurrentUser__phone">{user?.phone}</p>
+          <button
+            type="button"
+            onClick={() => selectedUser(0)}
+          >
+            Clear
+          </button>
+        </div>
+      )
   );
 };
