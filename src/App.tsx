22cac6dbd0d3c92c10e06feae38dcccbaf47c323
Juliya <jursylian@gmail.com>
2022-03-22 23:43:28 +0200

    Created dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './components/api';
 
 const App: React.FC = () => {
   const [selectedUserId, setSelectedUserId] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [hasLoadingError, setHasLoadingError] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .then(() => setLoading(true))
+      .catch(() => {
+        setHasLoadingError(true);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <div className="App__sidebar">
-        <TodoList />
-      </div>
+      {hasLoadingError ? (
+        <p>Data is not found</p>
+      ) : (
+        <>
+          <div className="App__sidebar">
+            {!isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <TodoList
+                todos={todos}
+                selectUserId={setSelectedUserId}
+              />
+            )}
 
-      <div className="App__content">
-        <div className="App__content-container">
-          {selectedUserId ? (
-            <CurrentUser />
-          ) : 'No user selected'}
-        </div>
-      </div>
+          </div>
+
+          <div className="App__content">
+            <div className="App__content-container">
+              {selectedUserId ? (
+                <CurrentUser
+                  userId={selectedUserId}
+                  clearId={setSelectedUserId}
+                />
+              ) : 'No user selected'}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
