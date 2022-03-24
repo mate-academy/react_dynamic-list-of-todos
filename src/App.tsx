9e0a4dc8876770e98e1de932d86741dce5bbef6b
Maksym Mishchanchuk <yssam.nn@gmail.com>
2022-03-24 17:28:46 +0200

    with server error

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,49 @@ import { getTodos } from './components/api';
 const App: React.FC = () => {
   const [selectedUserId, setSelectedUserId] = useState(0);
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [serverError, setServerError] = useState<boolean>(false);
+
+  const loading = () => {
+    setIsLoading(true);
+  };
+
+  const getServerError = () => {
+    setServerError(true);
+  };
 
   useEffect(() => {
-    getTodos().then((setTodos));
+    getTodos().then((setTodos))
+      .then(loading)
+      .catch(getServerError);
   }, []);
 
   return (
     <div className="App">
-      <div className="App__sidebar">
-        <TodoList
-          todos={todos}
-          selectUserId={setSelectedUserId}
-        />
-      </div>
+      {serverError ? (
+        <p>Server Error</p>
+      ) : (
+        <>
+          {isLoading ? (
+            <div className="App__sidebar">
+              <TodoList
+                todos={todos}
+                selectUserId={setSelectedUserId}
+              />
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
 
-      <div className="App__content">
-        <div className="App__content-container">
-          {selectedUserId ? (
-            <CurrentUser userId={selectedUserId} selectUserId={setSelectedUserId} />
-          ) : 'No user selected'}
-        </div>
-      </div>
+          <div className="App__content">
+            <div className="App__content-container">
+              {selectedUserId ? (
+                <CurrentUser userId={selectedUserId} selectUserId={setSelectedUserId} />
+              ) : 'No user selected'}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
