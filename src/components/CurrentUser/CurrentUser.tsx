af0f14d32c707a3d12e914f8c4dcb6bb021acae9
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-03-30 11:24:06 +0300

    first solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../../api/api';
 import './CurrentUser.scss';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  userId: number,
+  selectedUser: (userId: number) => void,
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export const CurrentUser: React.FC<Props> = ({ userId, selectedUser }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(false);
+
+  useEffect(() => {
+    getUser(userId)
+      .then(response => setUser(response))
+      .then(() => setLoading(true))
+      .catch(() => setLoadingError(true));
+  }, [userId]);
+
+  return (
+    <>
+      {!loading && (
+        <p>Please wait, loading</p>
+      )}
+
+      {(loading && loadingError) && (
+        <p>Failed to load</p>
+      )}
+
+      {(loading && !loadingError) && (
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>
+              {`Current user: ${user?.id}`}
+            </span>
+          </h2>
+          <h3 className="CurrentUser__name">{user?.name}</h3>
+          <p className="CurrentUser__email">{user?.email}</p>
+          <p className="CurrentUser__phone">{user?.phone}</p>
+          <button
+            type="button"
+            onClick={() => selectedUser(0)}
+          >
+            Unselect
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
