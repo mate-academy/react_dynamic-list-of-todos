15aceb1463e1eff5b22da1696efbf33eaba221e9
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-06 03:31:07 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,91 @@
-import React, { useState } from 'react';
+/* eslint-disable no-plusplus */
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api';
+import { Todo } from './types';
+import { debounce } from './helpers/wrappers';
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [selectedUserId, setSelectedUserId] = useState(0);
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedStatus, setSelectedStatus] = useState('all');
+
+  useEffect(() => {
+    getTodos().then(loadedTodos => setTodos(loadedTodos));
+  }, []);
+
+  const selectHandler = (event:React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedStatus(event.target.value);
+  };
+
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, 1000),
+    [],
+  );
+
+  const selectUser = (userId:number) => {
+    setSelectedUserId(userId);
+  };
+
+  const clear = () => {
+    setSelectedUserId(0);
+  };
+
+  const getFiltredTodos = () => {
+    return todos.filter(todo => {
+      switch (selectedStatus) {
+        case 'completed':
+          return todo.title.toLowerCase().startsWith(appliedQuery) && todo.completed;
+        case 'active':
+          return todo.title.toLowerCase().startsWith(appliedQuery) && !todo.completed;
+        default:
+          return todo.title.toLowerCase().startsWith(appliedQuery);
+      }
+    });
+  };
+
+  const shuffleTodos = () => {
+    const shuffled = [...todos];
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const currTodo = shuffled[i];
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+
+      shuffled[i] = shuffled[randomIndex];
+      shuffled[randomIndex] = currTodo;
+    }
+
+    setTodos(shuffled);
+  };
+
+  const filtredTodos = getFiltredTodos();
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList
+          todos={filtredTodos}
+          selectUser={selectUser}
+          selectedUser={selectedUserId}
+          changeQuery={applyQuery}
+          changeSelectedStatus={selectHandler}
+          randomizeTodos={shuffleTodos}
+        />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser userId={selectedUserId} clearUser={clear} />
           ) : 'No user selected'}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../../api';
+import { User } from '../../types';
 import './CurrentUser.scss';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+export const CurrentUser: React.FC<Props> = ({ userId, clearUser }) => {
+  const [user, setUser] = useState<User>();
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+  useEffect(() => {
+    getUser(userId).then(loadedUser => setUser(loadedUser));
+  }, [userId]);
+
+  return (
+    <div className="CurrentUser">
+      <h2 className="CurrentUser__title">
+        <span>
+          Selected user:
+          {user?.id}
+        </span>
+      </h2>
+
+      <h3 className="CurrentUser__name">{user?.name}</h3>
+      <p className="CurrentUser__email">{user?.email}</p>
+      <p className="CurrentUser__phone">{user?.phone}</p>
+
+      <button
+        className="button CurrentUser__clear"
+        type="button"
+        onClick={() => {
+          clearUser();
+        }}
+      >
+        Clear
+      </button>
+    </div>
+  );
+};
+
+interface Props {
+  userId:number,
+  clearUser: () => void,
+}
