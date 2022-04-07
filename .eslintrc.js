a54ba6ca19daf6a6fd256930a34632ba0a490757
YuriiHoliuk <yurii.holiuk@mate.academy>
2022-04-07 16:06:44 +0300

    Update and init

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
       ignoreTemplateLiterals: true,
       ignoreComments: true,
     }],
-    'jsx-a11y/label-has-associated-control': ["error", { assert: "either" } ]
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
+    }],
   },
 };
