c07083ffe5010d7d91c1ff7dc95e0d94d6b48d1e
Roman Khemych <roman19940912@gmail.com>
2022-04-13 15:15:35 +0300

    Dynamic List Of Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+      });
+  }, []);
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        {todos.length > 0
+          ? (
+            <TodoList
+              todos={todos}
+              setSelectedId={setSelectedUserId}
+            />
+          ) : (
+            <h2>Todos is Loading</h2>
+          )}
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
-          {selectedUserId ? (
-            <CurrentUser />
-          ) : 'No user selected'}
+          {selectedUserId
+            ? (
+              <CurrentUser
+                selectedId={selectedUserId}
+                setClearId={() => setSelectedUserId(0)}
+              />
+            ) : (
+              <h3>
+                No user selected
+              </h3>
+            )}
         </div>
       </div>
     </div>
--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUsersById } from '../../api/api';
 import './CurrentUser.scss';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+interface Props {
+  selectedId: number;
+  setClearId: (userId: number) => void;
+}
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export const CurrentUser: React.FC<Props> = ({ selectedId, setClearId }) => {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    getUsersById(selectedId)
+      .then(data => setUser(data));
+  }, [selectedId]);
+
+  return (
+    <div className="CurrentUser">
+      <h2 className="CurrentUser__title">
+        <span>{`Selected user: ${selectedId}`}</span>
+      </h2>
+
+      <h3 className="CurrentUser__name">{user?.name}</h3>
+      <p className="CurrentUser__email">{user?.email}</p>
+      <p className="CurrentUser__phone">{user?.phone}</p>
+
+      <button
+        type="button"
+        className="TodoList__user-button button"
+        onClick={() => setClearId(0)}
+      >
+        Clear
+      </button>
+    </div>
+  );
+};
