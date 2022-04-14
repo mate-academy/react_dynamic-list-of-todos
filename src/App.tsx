6846b4f1ef1fc4004465250640ba9e8c8185def5
Roma Boreiko <roma.boreiko@gmail.com>
2022-04-14 10:33:37 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,29 @@ import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
 const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+
+  const chooseUser = (userId: number) => {
+    setSelectedUserId(userId);
+  };
+
+  const clearUser = () => {
+    setSelectedUserId(0);
+  };
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList chooseUser={chooseUser} selectedUserId={selectedUserId} />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              selectedUserId={selectedUserId}
+              clearUser={clearUser}
+            />
           ) : 'No user selected'}
         </div>
       </div>
