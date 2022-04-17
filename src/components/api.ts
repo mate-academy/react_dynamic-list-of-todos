c6c85315189c9377ae98c4c79a4b70fc947ee8e2
Juliya <jursylian@gmail.com>
2022-04-18 00:03:50 +0300

    Created dynamic list

---
+++ src/components/api.ts
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://mate.academy/students-api/';
+
+export function getTodos() {
+  return (
+    fetch(`${BASE_URL}/todos`).then(response => response.json())
+  );
+}
+
+export function getUser(userId: number) {
+  return (
+    fetch(`${BASE_URL}/users/${userId}`).then(response => response.json())
+  );
+}
