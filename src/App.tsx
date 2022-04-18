b0531c929312067b41c6a52ef1b01b67932cf880
Yevhen Stepanenko <yevhen.s@mate.academy>
2022-04-18 10:08:20 +0300

    Reference solution added

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
-const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+interface State {
+  selectedUserId: number;
+}
 
-  return (
-    <div className="App">
-      <div className="App__sidebar">
-        <TodoList />
-      </div>
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedUserId: 0,
+  };
 
-      <div className="App__content">
-        <div className="App__content-container">
-          {selectedUserId ? (
-            <CurrentUser />
-          ) : 'No user selected'}
+  selectUser = (userId: number) => {
+    this.setState({
+      selectedUserId: userId,
+    });
+  };
+
+  handleClear = () => {
+    this.selectUser(0);
+  };
+
+  render() {
+    const { selectedUserId } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__sidebar">
+          <TodoList
+            selectUser={this.selectUser}
+            userId={selectedUserId}
+          />
+        </div>
+
+        <div className="App__content">
+          <div className="App__content-container">
+            {selectedUserId ? (
+              <CurrentUser
+                userId={selectedUserId}
+                removeUser={this.handleClear}
+              />
+            ) : 'No user selected'}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
