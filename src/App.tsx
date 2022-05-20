2e6471aba09c2ab029c866d9f9db721d3d7e5664
Vlad Skoromnyi <VectraSton@gmail.com>
2022-05-20 10:23:14 +0300

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React, { useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api';
 
-const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+const App: FC = () => {
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const clearUser = () => setSelectedUserId(0);
+
+  const getData = async () => {
+    const data = await getTodos();
+
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList
+          todos={todos}
+          setSelectedUserId={setSelectedUserId}
+          // selectedUserId={selectedUserId}
+        />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              userId={selectedUserId}
+              clearUser={clearUser}
+            />
           ) : 'No user selected'}
         </div>
       </div>
