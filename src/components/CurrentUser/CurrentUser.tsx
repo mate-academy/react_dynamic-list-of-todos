03fc312f9c1e0ba5ccf250bfb1d2b2698ed1a66b
Misha Kravtsov <aniahucm@gmail.com>
2022-05-21 23:34:47 +0300

    solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { User } from '../../types/User';
+import { getUserByIdFromServer } from '../../api/api';
 import './CurrentUser.scss';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  selectedUserId: number;
+  clearSelectedUserId: () => void;
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export const CurrentUser: React.FC<Props>
+= React.memo(({ selectedUserId, clearSelectedUserId }) => {
+  const [isLoadingError, setLoadingError] = useState(false);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  const getUser = useCallback(async () => {
+    try {
+      const user = await getUserByIdFromServer(selectedUserId);
+
+      setLoadingError(false);
+      setCurrentUser(user);
+    } catch {
+      setLoadingError(true);
+      setCurrentUser(null);
+    }
+  }, [selectedUserId]);
+
+  useEffect(() => {
+    getUser();
+  }, [selectedUserId]);
+
+  return (
+    <>
+      { !isLoadingError && currentUser ? (
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>
+              Selected user:
+              {currentUser.id}
+            </span>
+          </h2>
+
+          <h3 className="CurrentUser__name">
+            {currentUser.name}
+          </h3>
+
+          <p className="CurrentUser__email">
+            {currentUser.email}
+          </p>
+
+          <p className="CurrentUser__phone">
+            {currentUser.phone}
+          </p>
+          <button
+            className="CurrentUser__btn-clear"
+            type="button"
+            onClick={clearSelectedUserId}
+          >
+            Clear
+          </button>
+        </div>
+      )
+        : (
+          <div className="CurrentUser">
+            <h2 className="CurrentUser__title">
+              <span>Loading Error</span>
+            </h2>
+
+            <h3 className="CurrentUser__name">
+              No user data
+            </h3>
+          </div>
+        )}
+    </>
+  );
+});
