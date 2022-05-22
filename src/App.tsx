dcc046c807df65874f11e4cf8da4752d858138f6
Oleg Lementarev <lementarev@gmail.com>
2022-05-22 18:15:22 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 const App: React.FC = () => {
   const [
+    todos,
+    setTodos,
+  ] = useState<Todo[]>([]);
+
+  const [
     selectedUserId,
-    // setSelectedUserId,
+    setSelectedUserId,
   ] = useState(0);
+
+  const selectUser = (userId: number) => {
+    setSelectedUserId(userId);
+  };
+
+  const getTodosFromServer = async () => {
+    const todosFromServer = await getTodos();
+
+    setTodos(todosFromServer);
+  };
+
+  useEffect(() => {
+    getTodosFromServer();
+  }, []);
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList
+          todos={todos}
+          selectUser={selectUser}
+          selectedUserId={selectedUserId}
+        />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              userId={selectedUserId}
+              selectUser={selectUser}
+            />
           ) : 'No user selected'}
         </div>
       </div>
