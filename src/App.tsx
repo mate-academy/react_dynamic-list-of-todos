ef7a60c3f559b3796a8c46ec16ad99293a7bf49a
Oleksii Fedorenko <alex4rest09@gmail.com>
2022-05-23 16:44:09 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
+import { getTodos } from './api';
+
 const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [query, setQuery] = useState('');
+  const [selectedValue, setSelectedValue] = useState('all');
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  }, []);
+
+  const handleChangeInput = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value);
+    }, [],
+  );
+
+  const handleChangeSelect = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setSelectedValue(event.target.value);
+    }, [],
+  );
+
+  const preparedTodos = () => {
+    const filteredTodos = todos
+      .filter(todo => todo.title.includes(query.toLowerCase()));
+
+    if (selectedValue === 'active') {
+      return filteredTodos.filter(todo => !todo.completed);
+    }
+
+    if (selectedValue === 'completed') {
+      return filteredTodos.filter(todo => todo.completed);
+    }
+
+    return filteredTodos;
+  };
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList
+          selectUserId={setSelectedUserId}
+          loadedTodos={preparedTodos()}
+          inputQuery={query}
+          setInputQuery={handleChangeInput}
+          selectedValue={selectedValue}
+          handleChangeSelect={handleChangeSelect}
+        />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              setSelectedUser={setSelectedUserId}
+              userId={selectedUserId}
+            />
           ) : 'No user selected'}
         </div>
       </div>
