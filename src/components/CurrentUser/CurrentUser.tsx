bfb11a9f8d83bec93a85b6f0cc791fbb48089a85
SeKaM0 <polozhentsew02@gmail.com>
2022-05-24 14:30:03 +0300

    Finish task

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CurrentUser.scss';
+import { getUser } from '../../api/api';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+ type Props = {
+   userId: number,
+   selectUser: (x:number) => void
+ };
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export const CurrentUser: React.FC<Props> = ({ userId, selectUser }) => {
+  const [user, setUser] = useState({
+    id: userId,
+    name: 'Anonymus',
+    phone: '7777',
+    email: 'Anonymus',
+  });
+
+  useEffect(() => {
+    const API_USER_URL = `https://mate.academy/students-api/users/${userId}`;
+
+    getUser(API_USER_URL)
+      .then((userFromServer) => {
+        setUser(userFromServer);
+      });
+  }, [userId]);
+
+  return (
+    <div className="CurrentUser">
+      <h2 className="CurrentUser__title">
+        <span>
+          Selected user:
+          {userId}
+        </span>
+      </h2>
+
+      <h3 className="CurrentUser__name">{user.name}</h3>
+      <p className="CurrentUser__email">{user.email}</p>
+      <p className="CurrentUser__phone">{user.phone}</p>
+
+      <button
+        type="button"
+        className="button"
+        onClick={() => {
+          selectUser(0);
+        }}
+      >
+        Clear
+      </button>
+    </div>
+  );
+};
