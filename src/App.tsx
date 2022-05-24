c5572371946a93d4dd4eb9258b46833ce71face3
Helen Petrovska <paznachka@gmail.com>
2022-05-24 14:52:33 +0300

    dynamic todos were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api';
 
 const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+
+  const getTodosFromServer = async () => {
+    const todosFromServer = await getTodos();
+
+    setTodos(todosFromServer);
+  };
+
+  const selectUser = (userId: number) => {
+    setSelectedUserId(userId);
+  };
+
+  useEffect(() => {
+    getTodosFromServer();
+  }, []);
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList
+          todos={todos}
+          selectUser={selectUser}
+        />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              userId={selectedUserId}
+              selectUser={selectUser}
+            />
           ) : 'No user selected'}
         </div>
       </div>
