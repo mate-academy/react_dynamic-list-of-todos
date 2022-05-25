dde10f514e020038ee959c792f361a931e5c7f96
Eugenia <eugeniasaf@gmail.com>
2022-05-25 15:10:31 +0200

    solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { getUser } from '../../api';
 import './CurrentUser.scss';
+import { User } from '../../react-app-env';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  userId: number,
+  clear: (id: number) => void,
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export const CurrentUser: React.FC<Props> = ({ userId, clear }) => {
+  const [currentUser, setUser] = useState<User | null>(null);
+
+  const getUsersDetails = useCallback(async () => {
+    const usersDetails = await getUser(userId);
+
+    setUser(usersDetails);
+  }, [userId]);
+
+  useEffect(() => {
+    getUsersDetails();
+  }, [userId]);
+
+  return (
+    <>
+      { currentUser && (
+        <div className="CurrentUser">
+          <h2 className="CurrentUser__title">
+            <span>
+              {`Selected user: ${currentUser.id}`}
+            </span>
+          </h2>
+          <h3
+            className="CurrentUser__name"
+            data-cy="userName"
+          >
+            {currentUser.name}
+          </h3>
+
+          <p className="CurrentUser__email">
+            {currentUser.email}
+          </p>
+
+          <p className="CurrentUser__phone">
+            {currentUser.phone}
+          </p>
+
+          <button
+            type="button"
+            className="CurrentUser__button"
+            onClick={() => clear(0)}
+          >
+            Clear
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
