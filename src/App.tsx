3d910c947615d54f3eff5275b7427b6c27dd483a
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2022-05-25 14:54:04 +0300

    solution w0 randomizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
+
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api/api';
 
 const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    getTodos()
+      .then(data => setTodos(data));
+  });
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        {todos ? (
+          <TodoList
+            todos={todos}
+            selectedUserId={selectedUserId}
+            onSelectUserId={setSelectedUserId}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              userId={selectedUserId}
+              clearUser={setSelectedUserId}
+            />
           ) : 'No user selected'}
         </div>
       </div>
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,44 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './TodoList.scss';
 
-export const TodoList: React.FC = () => (
-  <div className="TodoList">
-    <h2>Todos:</h2>
+type Props = {
+  todos: Todo[],
+  onSelectUserId: (userId: number) => void,
+  selectedUserId: number,
+};
 
-    <div className="TodoList__list-container">
-      <ul className="TodoList__list">
-        <li className="TodoList__item TodoList__item--unchecked">
-          <label>
-            <input type="checkbox" readOnly />
-            <p>delectus aut autem</p>
-          </label>
+enum Status {
+  All = 'all',
+  Completed = 'completed',
+  Active = 'active',
+}
 
-          <button
-            className="
-              TodoList__user-button
-              TodoList__user-button--selected
-              button
-            "
-            type="button"
-          >
-            User&nbsp;#1
-          </button>
-        </li>
+export const TodoList: React.FC<Props> = ({
+  todos,
+  onSelectUserId,
+  selectedUserId,
+}) => {
+  const [query, setQuery] = useState('');
+  const [todosStatus, setTodosStatus] = useState('');
 
-        <li className="TodoList__item TodoList__item--checked">
-          <label>
-            <input type="checkbox" checked readOnly />
-            <p>distinctio vitae autem nihil ut molestias quo</p>
-          </label>
+  const filterByTitle = () => {
+    return todos.filter(todo => (
+      todo.title.toLowerCase().includes(query.toLowerCase())
+    ));
+  };
 
-          <button
-            className="TodoList__user-button button"
-            type="button"
-          >
-            User&nbsp;#2
-          </button>
-        </li>
-      </ul>
+  const preparingTodos = () => {
+    const filteredByTitle = filterByTitle();
+
+    if (todosStatus === Status.Completed) {
+      return filteredByTitle.filter(todo => todo.completed);
+    }
+
+    if (todosStatus === Status.Active) {
+      return filteredByTitle.filter(todo => !todo.completed);
+    }
+
+    return filteredByTitle;
+  };
+
+  const visibleTodos = preparingTodos();
+
+  return (
+    <div className="TodoList">
+      <h2>Todos:</h2>
+
+      <div className="TodoList__list-container">
+
+        <label>
+          Filter by title:
+          {' '}
+          <input
+            data-cy="filterByTitle"
+            type="text"
+            value={query}
+            onChange={(event => setQuery(event.target.value))}
+          />
+        </label>
+
+        <select
+          className="TodoList__select"
+          onChange={(event) => {
+            setTodosStatus(event.target.value);
+          }}
+        >
+          <option value={Status.All}>
+            All
+          </option>
+          <option value={Status.Active}>
+            Active
+          </option>
+          <option value={Status.Completed}>
+            Completed
+          </option>
+        </select>
+
+        <ul
+          className="TodoList__list"
+          data-cy="listOfTodos"
+        >
+          {visibleTodos.map(todo => (
+            <li
+              key={todo.id}
+              className={classNames(
+                'TodoList__item',
+                { 'TodoList__item--checked': todo.completed },
+                { 'TodoList__item--unchecked': !todo.completed },
+              )}
+            >
+              <label>
+                <input type="checkbox" checked readOnly />
+                <p>{todo.title}</p>
+              </label>
+
+              {todo.userId && (
+                <button
+                  data-cy="userButton"
+                  className={classNames('TodoList__user-button button', {
+                    'TodoList__user-button--selected':
+                      todo.userId === selectedUserId,
+                  })}
+                  type="button"
+                  onClick={() => onSelectUserId(todo.userId)}
+                >
+                  {`User ${todo.userId}`}
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
