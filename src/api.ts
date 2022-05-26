232b1b246a1dcacb5ea6f58b26f5937a883c12ba
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-05-26 14:52:46 +0200

    fixed

---
+++ src/api.ts
@@ -0,0 +1,16 @@
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+const BASE_URL = 'https://mate.academy/students-api/';
+
+export const getTodos = async (): Promise<Todo[]> => {
+  const response = await fetch(`${BASE_URL}/todos`);
+
+  return response.json();
+};
+
+export const getUser = async (id: number): Promise<User> => {
+  const response = await fetch(`${BASE_URL}/users/${id}`);
+
+  return response.json();
+};
