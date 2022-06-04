11164dbbacd528004887c3d689af392a8dfba4e1
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-06-05 01:22:56 +0300

    updated handling try catch

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -13,11 +13,16 @@ export const CurrentUser: React.FC<Props> = React.memo(({
   setSelectedUserId,
 }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [isError, setIsError] = useState<boolean | null>(null);
   const selectUser = useCallback(async () => {
     try {
-      setCurrentUser(await getUser(selectedUserId));
+      const newCurrentUser = await getUser(selectedUserId);
+
+      setCurrentUser(newCurrentUser);
+      setIsError(null);
     } catch (error) {
-      throw new Error(`${error}`);
+      setCurrentUser(null);
+      setIsError(true);
     }
   }, [selectedUserId]);
 
@@ -26,9 +31,9 @@ export const CurrentUser: React.FC<Props> = React.memo(({
   }, [selectedUserId]);
 
   return (
-    <>
+    <div className="CurrentUser">
       {currentUser && (
-        <div className="CurrentUser">
+        <>
           <h2 className="CurrentUser__title">
             <span>{`Selected user: ${selectedUserId}`}</span>
           </h2>
@@ -45,15 +50,20 @@ export const CurrentUser: React.FC<Props> = React.memo(({
             {currentUser.phone}
           </p>
 
-          <button
-            type="button"
-            onClick={() => setSelectedUserId(0)}
-            className="CurrentUser__clear button"
-          >
-            Clear
-          </button>
-        </div>
+        </>
       )}
-    </>
+
+      {isError && (
+        <h3>Can&apos;t load user, please try again.</h3>
+      )}
+
+      <button
+        type="button"
+        onClick={() => setSelectedUserId(0)}
+        className="CurrentUser__clear button"
+      >
+        Clear
+      </button>
+    </div>
   );
 });
