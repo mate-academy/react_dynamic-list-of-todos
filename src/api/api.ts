ef8a2d29bec11da6d40805617b2f7217cfe7f496
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-06-17 10:00:51 +0300

    solution

---
+++ src/api/api.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/quotes */
+const API_URL_TODO = `https://mate.academy/students-api/todos`;
+const API_URL_USERS = `https://mate.academy/students-api/users`;
+
+export const getTodo = () => {
+  return fetch(API_URL_TODO)
+    .then(response => response.json());
+};
+
+export const getUser = (id: number) => {
+  return fetch(`${API_URL_USERS}/${id}`)
+    .then(response => response.json());
+};
