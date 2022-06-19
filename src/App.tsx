9199cf5754e5f3c144c04a50e093f0602b81e90b
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-06-19 19:34:34 +0300

    dynamic-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { getTodos } from './api';
 
 const App: React.FC = () => {
   const [
     selectedUserId,
-    // setSelectedUserId,
+    setSelectedUserId,
   ] = useState(0);
+
+  const selectUser = (userId: number) => {
+    setSelectedUserId(userId);
+  };
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+
+  const getTodosFromServer = async () => {
+    const todosFromServer = await getTodos();
+
+    setTodos(todosFromServer);
+    setFilteredTodos(todosFromServer);
+  };
+
+  const onFilterTitle = (title: string) => {
+    setFilteredTodos([
+      ...todos.filter(item => item.title.includes(title)),
+    ]);
+  };
+
+  const onFilterStatus = (status: string) => {
+    setFilteredTodos([
+      ...todos.filter(item => {
+        switch (status) {
+          case 'all':
+            return true;
+            break;
+          case 'completed':
+            return item.completed;
+            break;
+          case 'active':
+            return !item.completed;
+            break;
+          default:
+            return false;
+        }
+      }),
+    ]);
+  };
+
+  useEffect(() => {
+    getTodosFromServer();
+  }, []);
 
   return (
     <div className="App">
       <div className="App__sidebar">
-        <TodoList />
+        <TodoList
+          todos={filteredTodos}
+          selectUser={selectUser}
+          onFilterTitle={onFilterTitle}
+          onFilterStatus={onFilterStatus}
+        />
       </div>
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <>
+              <CurrentUser selectedUserId={selectedUserId} />
+              <button
+                type="button"
+                data-cy="userButton"
+                onClick={() => setSelectedUserId(0)}
+              >
+                Clear
+              </button>
+            </>
           ) : 'No user selected'}
         </div>
       </div>
