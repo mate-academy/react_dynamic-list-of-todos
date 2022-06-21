cc30073f1507b65517a0c6cdb6e7fc11db68cfe1
Daria Liubymova <dellachaise@gmail.com>
2022-06-21 16:56:43 +0300

    Chahnged search and select, moved them to todoList component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { getTodos } from './api';
 const App: React.FC = () => {
   const [selectedUserId, setSelectedUserId] = useState(0);
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [query, setQuery] = useState('');
 
   const changeUser = (userId: number) => setSelectedUserId(userId);
 
@@ -23,67 +22,11 @@ const App: React.FC = () => {
     getCurrentTodos();
   }, []);
 
-  const selectHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    if (event.target.value === 'all') {
-      getCurrentTodos();
-    }
-
-    if (event.target.value === 'complited') {
-      getTodos()
-        .then((currentTodosFromServer) => {
-          setTodos(
-            currentTodosFromServer
-              .filter((todo: Todo) => todo.completed),
-          );
-        });
-    }
-
-    if (event.target.value === 'uncomplited') {
-      getTodos()
-        .then((currentTodosFromServer) => {
-          setTodos(
-            currentTodosFromServer
-              .filter((todo: Todo) => !todo.completed),
-          );
-        });
-    }
-  };
-
   return (
     <div className="App">
       <div className="App__sidebar">
-        <input
-          type="text"
-          id="search-query"
-          className="App__search"
-          value={query}
-          placeholder="Type search word"
-          onChange={event => setQuery(event.target.value.toLowerCase())}
-          data-cy="filterByTitle"
-        />
-        <select
-          className="App__select"
-          onChange={selectHandler}
-        >
-          <option
-            value="all"
-          >
-            Show all
-          </option>
-          <option
-            value="complited"
-          >
-            Show complited
-          </option>
-          <option
-            value="uncomplited"
-          >
-            Show uncomplited
-          </option>
-        </select>
         <TodoList
           todos={todos}
-          currentQuery={query}
           changeUser={changeUser}
           selectedUserId={selectedUserId}
         />
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,33 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoList.scss';
 import { Todo } from '../Todo';
 
 type Props = {
   todos: Todo[];
-  currentQuery: string;
   changeUser: (userId: number) => void;
   selectedUserId: number,
 };
 
+enum FilterOptions {
+  all,
+  complited,
+  uncomplited,
+}
+
 export const TodoList: React.FC<Props> = ({
   todos,
-  currentQuery,
   changeUser,
   selectedUserId,
 }) => {
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState<FilterOptions>(FilterOptions.all);
+
   const visibleTodos = todos.filter((todo) => {
     const { title } = todo;
 
-    return title.toLowerCase().includes(currentQuery);
+    return title.toLowerCase().includes(query);
+  });
+
+  const filteredTodos = visibleTodos.filter((todo) => {
+    switch (filter) {
+      case FilterOptions.complited: {
+        return todo.completed === true;
+      }
+
+      case FilterOptions.uncomplited: {
+        return todo.completed === false;
+      }
+
+      default:
+        return todo;
+    }
   });
 
   return (
     <div className="TodoList">
+      <input
+        type="text"
+        id="search-query"
+        className="TodoList__search"
+        value={query}
+        placeholder="Type search word"
+        onChange={event => setQuery(event.target.value.toLowerCase())}
+        data-cy="filterByTitle"
+      />
+      <select
+        className="TodoList__select"
+        onChange={(event) => setFilter(+event.target.value)}
+      >
+        <option
+          value={FilterOptions.all}
+        >
+          Show all
+        </option>
+        <option
+          value={FilterOptions.complited}
+        >
+          Show complited
+        </option>
+        <option
+          value={FilterOptions.uncomplited}
+        >
+          Show uncomplited
+        </option>
+      </select>
       <h2>Todos:</h2>
 
       <div className="TodoList__list-container">
         <ul className="TodoList__list" data-cy="listOfTodos">
-          {visibleTodos.map(todo => (
+          {filteredTodos.map(todo => (
             <Todo
               key={todo.id}
               todo={todo}
