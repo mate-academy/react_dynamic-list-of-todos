5565f2c1263a3daec522c097f76b42df3bac10ec
Danylo-Dziuban <dz.danylo@gmail.com>
2022-06-23 14:18:15 +0300

    Solution

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useEffect, useState } from 'react';
+import { requestUser } from '../../api/api';
 import './CurrentUser.scss';
 
-export const CurrentUser: React.FC = () => (
-  <div className="CurrentUser">
-    <h2 className="CurrentUser__title"><span>Selected user: 2</span></h2>
+type Props = {
+  userId: number,
+  setUserId: (userId: number) => void,
+};
 
-    <h3 className="CurrentUser__name">Ervin Howell</h3>
-    <p className="CurrentUser__email">Shanna@melissa.tv</p>
-    <p className="CurrentUser__phone">010-692-6593 x09125</p>
-  </div>
-);
+export const CurrentUser: React.FC<Props> = ({ userId, setUserId }) => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    requestUser(userId).then(promise => setSelectedUser(promise));
+  }, [userId]);
+
+  return (
+    <div className="CurrentUser">
+      <h2 className="CurrentUser__title"><span>{`Selected user: ${selectedUser?.id}`}</span></h2>
+
+      <h3 data-cy="userName" className="CurrentUser__name">{selectedUser?.name}</h3>
+      <p className="CurrentUser__email">{selectedUser?.email}</p>
+      <p className="CurrentUser__phone">{selectedUser?.phone}</p>
+      <button
+        type="button"
+        className="TodoList__user-button button"
+        onClick={
+          () => setUserId(0)
+        }
+      >
+        Clear
+      </button>
+    </div>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,31 @@
 /// <reference types="react-scripts" />
 
-type Todo = {}; /* TODO: DESCRIBE */
+type Todo = {
+  id: number,
+  userId: number,
+  completed: boolean,
+  title: string,
+  createdAt: string,
+  updatedAt: string
+};
+
+type User = {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  phone: string,
+  website: string,
+  createdAt: string,
+  updatedAt: string,
+  address: {
+    id: number,
+    userId: number,
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    createdAt: string,
+    updatedAt: string,
+  }
+};
