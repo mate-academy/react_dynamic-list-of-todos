7a490729ba491a0310752296949490c1727c1422
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2022-06-24 12:54:47 +0300

    implemet todo list with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
 
-const App: React.FC = () => {
-  const [
-    selectedUserId,
-    // setSelectedUserId,
-  ] = useState(0);
+const App: React.FC = () => (
+  <div className="App">
+    <div className="App__sidebar">
+      <TodoList />
+    </div>
 
-  return (
-    <div className="App">
-      <div className="App__sidebar">
-        <TodoList />
-      </div>
-
-      <div className="App__content">
-        <div className="App__content-container">
-          {selectedUserId ? (
-            <CurrentUser />
-          ) : 'No user selected'}
-        </div>
+    <div className="App__content">
+      <div className="App__content-container">
+        <CurrentUser />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,29 @@
 /// <reference types="react-scripts" />
 
-type Todo = {}; /* TODO: DESCRIBE */
+export interface Todo {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+}
+
+export interface User {
+  id: number,
+  createdAt: string,
+  updatedAt: string,
+  name: string,
+  username: string,
+  email: string,
+  phone: string,
+  website: string,
+}
+
+export interface State {
+  todos: Todo[],
+  user: User | null,
+}
+
+export interface Action {
+  type: string,
+  payload: any,
+}
