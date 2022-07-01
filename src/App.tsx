eeaf1dbd656091182b109504b36d776f0b20acd2
Andrei Sucman <sucmanandrei@gmail.com>
2022-07-01 19:21:30 +0300

    task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { TodoList } from './components/TodoList';
 import { CurrentUser } from './components/CurrentUser';
+import { fetchTodos } from './api/api';
 
 const App: React.FC = () => {
   const [
     selectedUserId,
-    // setSelectedUserId,
+    setSelectedUserId,
   ] = useState(0);
+
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+
+  useEffect(
+    () => {
+      fetchTodos()
+        .then(response => {
+          return setTodosFromServer(response);
+        });
+    },
+    [],
+  );
+
+  const onSelectUser = (userId: number) => {
+    setSelectedUserId(userId);
+  };
 
   return (
     <div className="App">
-      <div className="App__sidebar">
-        <TodoList />
-      </div>
+      {todosFromServer.length && (
+        <div className="App__sidebar">
+          <TodoList
+            todoList={todosFromServer}
+            handleSelectUser={onSelectUser}
+            selectedUserId={selectedUserId}
+          />
+        </div>
+      )}
 
       <div className="App__content">
         <div className="App__content-container">
           {selectedUserId ? (
-            <CurrentUser />
+            <CurrentUser
+              selectedUserId={selectedUserId}
+              handleSelectUser={onSelectUser}
+            />
           ) : 'No user selected'}
         </div>
       </div>
