8dccb1faff4c8448f5de48432c61badf47c67fb4
Amit Patil <amitp0156@gmail.com>
2022-07-06 01:16:14 +0530

    added React.memo

--- src/components/CurrentUser/CurrentUser.tsx
+++ src/components/CurrentUser/CurrentUser.tsx
@@ -7,48 +7,50 @@ interface Props {
   onSelectHandler: (userId: number) => void;
 }
 
-export const CurrentUser: React.FC<Props> = ({
-  userId,
-  onSelectHandler,
-}) => {
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
+export const CurrentUser: React.FC<Props> = React.memo(
+  ({
+    userId,
+    onSelectHandler,
+  }) => {
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
 
-  const selectUser = useCallback(
-    async (id: number) => {
-      try {
-        const selectedUser = await getUser(id);
+    const selectUser = useCallback(
+      async (id: number) => {
+        try {
+          const selectedUser = await getUser(id);
 
-        if (selectedUser.id) {
-          setCurrentUser(selectedUser);
+          if (selectedUser.id) {
+            setCurrentUser(selectedUser);
+          }
+        } catch {
+          setCurrentUser(null);
         }
-      } catch {
-        setCurrentUser(null);
-      }
-    }, [userId],
-  );
+      }, [userId],
+    );
 
-  useEffect(() => {
-    selectUser(userId);
+    useEffect(() => {
+      selectUser(userId);
 
-    return () => setCurrentUser(null); // to clean up previous selected user before new selected user
+      return () => setCurrentUser(null); // to clean up previous selected user before new selected user
+    },
+    [userId]);
+
+    return (
+      <div className="CurrentUser">
+        <h2 className="CurrentUser__title"><span>{`Selected user: ${currentUser?.id}`}</span></h2>
+
+        <h3 className="CurrentUser__name">{currentUser?.name}</h3>
+        <p className="CurrentUser__email">{currentUser?.email}</p>
+        <p className="CurrentUser__phone">{currentUser?.phone}</p>
+
+        <button
+          type="button"
+          className="button"
+          onClick={() => onSelectHandler(0)}
+        >
+          Clear
+        </button>
+      </div>
+    );
   },
-  [userId]);
-
-  return (
-    <div className="CurrentUser">
-      <h2 className="CurrentUser__title"><span>{`Selected user: ${currentUser?.id}`}</span></h2>
-
-      <h3 className="CurrentUser__name">{currentUser?.name}</h3>
-      <p className="CurrentUser__email">{currentUser?.email}</p>
-      <p className="CurrentUser__phone">{currentUser?.phone}</p>
-
-      <button
-        type="button"
-        className="button"
-        onClick={() => onSelectHandler(0)}
-      >
-        Clear
-      </button>
-    </div>
-  );
-};
+);
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,88 +8,90 @@ interface Props {
   onSelectHandler: (userId: number) => void;
 }
 
-export const TodoList: React.FC<Props> = ({
-  todos,
-  onSelectHandler,
-  selectedUserId,
-}) => {
-  const [title, setTitle] = useState('');
-  const [filterByStaus, setFilterByStatus] = useState('all');
+export const TodoList: React.FC<Props> = React.memo(
+  ({
+    todos,
+    onSelectHandler,
+    selectedUserId,
+  }) => {
+    const [title, setTitle] = useState('');
+    const [filterByStaus, setFilterByStatus] = useState('all');
 
-  let filteredTodos = todos.filter(todo => (
-    todo.title.toLowerCase().includes(title.toLowerCase())
-  ));
+    let filteredTodos = todos.filter(todo => (
+      todo.title.toLowerCase().includes(title.toLowerCase())
+    ));
 
-  if (filterByStaus !== 'all') {
-    filteredTodos = filterByStaus === 'completed'
-      ? filteredTodos.filter(todo => todo.completed)
-      : filteredTodos.filter(todo => !todo.completed);
-  }
+    if (filterByStaus !== 'all') {
+      filteredTodos = filterByStaus === 'completed'
+        ? filteredTodos.filter(todo => todo.completed)
+        : filteredTodos.filter(todo => !todo.completed);
+    }
 
-  return (
-    <div className="TodoList">
-      <h2 className="title">Todos</h2>
+    return (
+      <div className="TodoList">
+        <h2 className="title">Todos</h2>
 
-      <input
-        type="text"
-        data-cy="filterByTitle"
-        placeholder="search by title"
-        value={title}
-        onChange={(event) => (
-          setTitle(event.target.value)
-        )}
-      />
+        <input
+          type="text"
+          data-cy="filterByTitle"
+          placeholder="search by title"
+          value={title}
+          onChange={(event) => (
+            setTitle(event.target.value)
+          )}
+        />
 
-      <select
-        value={filterByStaus}
-        onChange={(event) => (
-          setFilterByStatus(event.target.value)
-        )}
-      >
-        <option value="all">All</option>
-        <option value="active">Not Completed</option>
-        <option value="completed">Completed</option>
-      </select>
+        <select
+          value={filterByStaus}
+          onChange={(event) => (
+            setFilterByStatus(event.target.value)
+          )}
+        >
+          <option value="all">All</option>
+          <option value="active">Not Completed</option>
+          <option value="completed">Completed</option>
+        </select>
 
-      <div className="TodoList__list-container">
-        <ul className="TodoList__list">
-          {filteredTodos.map(todo => (
-            <li
-              key={todo.id}
-              className={cn(
-                'TodoList__item',
-                { 'TodoList__item--unchecked': !todo.completed },
-                { 'TodoList__item--checked': todo.completed },
-              )}
-            >
-              <label>
-                <input
-                  type="checkbox"
-                  readOnly
-                  checked={todo.completed}
-                />
-                <p>{todo.title}</p>
-              </label>
-              <button
+        <div className="TodoList__list-container">
+          <ul className="TodoList__list">
+            {filteredTodos.map(todo => (
+              <li
+                key={todo.id}
                 className={cn(
-                  'TodoList__user-button',
-                  'button',
-                  {
-                    'TodoList__user-button--selected':
-                  selectedUserId === todo.userId,
-                  },
+                  'TodoList__item',
+                  { 'TodoList__item--unchecked': !todo.completed },
+                  { 'TodoList__item--checked': todo.completed },
                 )}
-                type="button"
-                data-cy="userButton"
-                onClick={() => onSelectHandler(todo.userId)}
               >
+                <label>
+                  <input
+                    type="checkbox"
+                    readOnly
+                    checked={todo.completed}
+                  />
+                  <p>{todo.title}</p>
+                </label>
+                <button
+                  className={cn(
+                    'TodoList__user-button',
+                    'button',
+                    {
+                      'TodoList__user-button--selected':
+                    selectedUserId === todo.userId,
+                    },
+                  )}
+                  type="button"
+                  data-cy="userButton"
+                  onClick={() => onSelectHandler(todo.userId)}
+                >
 
-                {`User #${todo.userId}`}
-              </button>
-            </li>
-          ))}
-        </ul>
+                  {`User #${todo.userId}`}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
