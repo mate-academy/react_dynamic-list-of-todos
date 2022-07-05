64491df6357ae91e4475c3e74262274c28cabb80
PRO-GRAM-MER <ankitbaage3@gmail.com>
2022-07-05 21:01:04 +0530

    Solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,21 @@
 /// <reference types="react-scripts" />
 
-type Todo = {}; /* TODO: DESCRIBE */
+type Todo = {
+  'id': number,
+  'createdAt': string,
+  'updatedAt': string,
+  'userId': number,
+  'title': string,
+  'completed': boolean,
+};
+
+type User = {
+  'id': number,
+  'createdAt': string,
+  'updatedAt': string,
+  'name': string,
+  'username': string,
+  'email': string,
+  'phone': string,
+  'website': string,
+};
