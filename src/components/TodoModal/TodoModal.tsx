e80dddab17c2fc526e778944b08da296dfd4fa68
Roman Marunkevich <vinnibea@gmail.com>
2022-07-27 00:46:21 +0300

    changed modal title

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -39,7 +39,7 @@ export const TodoModal: React.FC<Modal> = ({
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo ${selectedTodoId}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
