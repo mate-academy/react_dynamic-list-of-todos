afa50d11e040516606e03759bd13cc5b5cb42040
SAMAEV <andreysamaev@gmail.com>
2022-07-26 21:53:47 +0300

    todosSelected1sec

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,32 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todosToShow, setTodosToShow] = useState(todos);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [modal, setModal] = useState(false);
+
+  const showTodos = getTodos();
+
+  useEffect(() => {
+    showTodos.then((currentTodos) => {
+      setTodos(currentTodos);
+      setTodosToShow(currentTodos);
+    });
+
+    if (selectedTodo) {
+      getUser(selectedTodo.userId)
+        .then(user => setSelectedUser(user));
+    }
+  }, [selectedTodo]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +41,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={todos}
+                onSettingTodo={setTodosToShow}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={todosToShow}
+                    setSelectedTodo={setSelectedTodo}
+                    setModal={setModal}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modal && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          selectedUser={selectedUser}
+          onSelectTodo={setSelectedTodo}
+          setSelectedUser={setSelectedUser}
+          setModal={setModal}
+        />
+      )}
     </>
   );
 };
