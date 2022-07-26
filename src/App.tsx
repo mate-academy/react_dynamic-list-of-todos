aed5c13c13356ac293df6e2c5e04cbb40c3704c1
Andrii Nychaiuk <anichajuk@gmail.com>
2022-07-26 15:13:12 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,33 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos, getUser } from './api';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [initialTodos, setInitialTodos] = useState<Todo[]>([]);
+  const [selectedTodo, selectTodo] = useState<Todo | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [isModalVisible, setModalVisbility] = useState(false);
+  const [visibleTodos, setVisibleTodos] = useState(initialTodos);
+
+  useEffect(() => {
+    getTodos().then(allTodos => {
+      setInitialTodos(allTodos);
+      setVisibleTodos(allTodos);
+    });
+
+    if (selectedTodo) {
+      getUser(selectedTodo.userId)
+        .then(currentUser => setUser(currentUser));
+    }
+  }, [selectedTodo]);
+
+  if (initialTodos.length === 0) {
+    return <Loader />;
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +42,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={initialTodos}
+                onSetVisibleTodos={setVisibleTodos}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              <TodoList
+                todos={visibleTodos}
+                onSelectTodo={selectTodo}
+                onSetModalVisibility={setModalVisbility}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalVisible && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          currentUser={user}
+          onResetTodo={selectTodo}
+          onResetUser={setUser}
+          onResetModalVisibility={setModalVisbility}
+        />
+      )}
     </>
   );
 };
