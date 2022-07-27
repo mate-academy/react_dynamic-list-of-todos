b2f9ccf89c9f2bc9f8c79c7116b618f01e2d81bf
Andrew Bilous <bilous.a.work@gmail.com>
2022-07-27 20:11:44 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+
+import { Todo } from './types/Todo';
+import { SelectOptions } from './types/Select_otions';
+
+import { getTodos } from './api';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
@@ -9,6 +14,46 @@ import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+
+  useEffect(() => {
+    const todoToLoad = async () => {
+      await getTodos().then(todos => {
+        setTodosFromServer(todos);
+        setFilteredTodos(todos);
+      });
+      setIsLoaded(false);
+    };
+
+    todoToLoad();
+  }, []);
+
+  const todoFilter = (query: string, option: string) => {
+    const todos = todosFromServer.filter(todo => {
+      switch (option) {
+        case SelectOptions.ALL:
+          return todo.title.includes(query);
+
+        case SelectOptions.ACTIVE:
+          return !todo.completed && todo.title.includes(query);
+
+        case SelectOptions.COMPLETED:
+          return todo.completed && todo.title.includes(query);
+
+        default:
+          return undefined;
+      }
+    });
+
+    setFilteredTodos(todos);
+  };
+
+  const idSelector = (id: number) => setSelectedTodoId(id);
+  const modalTodo = filteredTodos.find(todo => todo.id === selectedTodoId);
+
   return (
     <>
       <div className="section">
@@ -17,18 +62,20 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter todoFilter={todoFilter} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoaded
+                ? <Loader />
+                : <TodoList todos={filteredTodos} selectTodo={idSelector} />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodoId && modalTodo
+        && <TodoModal todo={modalTodo} selectUser={idSelector} />}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,78 @@
-import React from 'react';
-import { Loader } from '../Loader';
+import React, { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+import { Loader } from '../Loader';
+import { getUser } from '../../api';
+
+type Props = {
+  todo: Todo;
+  selectUser: (value: number) => void;
+};
+
+export const TodoModal: React.FC<Props> = ({ todo, selectUser }) => {
+  const [user, setUser] = useState<User>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (todo) {
+      getUser(todo.userId)
+        .then(person => {
+          setUser(person);
+          setIsLoading(false);
+        });
+    }
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+      {isLoading
+        ? <Loader />
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${todo.id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => selectUser(0)}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {todo.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {/* <strong className="has-text-success">Done</strong> */}
+                {todo.completed ? (
+                  <strong className="has-text-success">Done</strong>
+                ) : (
+                  <strong className="has-text-danger">Planned</strong>
+                )}
+
+                {' by '}
+
+                {user && (
+                  <a href={`mailto:${user.email}`}>
+                    {user.name}
+                  </a>
+                )}
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
