6022bee378d99b0ec29ee0143a6d2d3349551207
Sasha Skrinnik <sasha.skrinnik@gmail.com>
2022-07-27 15:58:22 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,74 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState<string>(Filter.ALL);
+  const [isTodoLoaded, setTodoLoaded] = useState<boolean>(false);
+  const [savedTodo, setSavedTodo] = useState<Todo>();
+  const [modalIsOpened, setModalIsOpened] = useState(false);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      await getTodos()
+        .then(todosFromServer => {
+          setTodos(todosFromServer);
+          setVisibleTodos(todosFromServer);
+        });
+
+      setTodoLoaded(true);
+    };
+
+    loadTodos();
+  }, []);
+
+  const searchByWord = (title: string) => {
+    const params = query.toLowerCase();
+
+    return title.toLocaleLowerCase().includes(params);
+  };
+
+  useEffect(() => {
+    switch (filter) {
+      case Filter.ALL:
+        setVisibleTodos(todos.filter(todo => searchByWord(todo.title)));
+        break;
+
+      case Filter.ACTIVE:
+        setVisibleTodos(todos.filter(todo => !todo.completed && searchByWord(todo.title)));
+        break;
+
+      case Filter.COMPLITED:
+        setVisibleTodos(todos.filter(todo => todo.completed && searchByWord(todo.title)));
+        break;
+      default:
+        break;
+    }
+  }, [query, filter]);
+
+  const todoOpener = (todo: Todo) => {
+    setSavedTodo(todo);
+    setModalIsOpened(true);
+  };
+
+  const modalCloser = () => {
+    setModalIsOpened(false);
+  };
+
+  const getQuery = (userQuery: string) => {
+    setQuery(userQuery);
+  };
+
+  const selectedFilter = (userFilter: string) => {
+    setFilter(userFilter);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +83,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                getQuery={getQuery}
+                selectedFilter={selectedFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!isTodoLoaded
+                ? <Loader />
+                : isTodoLoaded && (
+                  <TodoList
+                    todos={visibleTodos}
+                    todoOpener={todoOpener}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modalIsOpened
+      && savedTodo
+      && (
+        <TodoModal
+          todo={savedTodo}
+          modalCloser={modalCloser}
+        />
+      )}
     </>
   );
 };
