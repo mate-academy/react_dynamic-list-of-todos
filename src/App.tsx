d539dea26408f77187b422449469e0af02ab59f3
Sasha Havryliuk <princesasha9178@icloud.com>
2022-07-27 20:48:25 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 import { Loader } from './components/Loader';
+import { TodoList } from './components/TodoList';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+
+  const [selectTodo, setSelectTodo] = useState(0);
+  const [isLoad, setIsLoad] = useState(true);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      const listTodos = await getTodos();
+
+      setTodos(listTodos);
+      setIsLoad(false);
+      setVisibleTodos(listTodos);
+    };
+
+    loadTodos();
+  }, []);
+
+  const todosFilter = (inputType: string, typeSelect: string) => {
+    let visibleMovies = todos.filter(
+      ({ title }) => title.toLowerCase().includes(inputType),
+    );
+
+    if (typeSelect === 'active') {
+      visibleMovies = todos
+        .filter(({ completed }) => completed === false);
+    }
+
+    if (typeSelect === 'completed') {
+      visibleMovies = todos
+        .filter(({ completed }) => completed === true);
+    }
+
+    setVisibleTodos(visibleMovies);
+  };
+
+  const select = (todoId: number) => setSelectTodo(todoId);
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                getInput={todosFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoad
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    select={select}
+                    selectTodo={selectTodo}
+                  />
+                ) }
             </div>
+
+            {selectTodo > 0 && (
+              <TodoModal
+                todo={visibleTodos.find(todo => todo.id === selectTodo)}
+                select={select}
+              />
+            )}
           </div>
         </div>
       </div>
-
-      <TodoModal />
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,66 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useEffect, useState } from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  getInput: (searchInput: string, typeSelect: string) => void,
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({ getInput }) => {
+  const [searchInput, setSearchInput] = useState('');
+  const [typeSelect, setTypeSelect] = useState('all');
+
+  useEffect(() => {
+    getInput(searchInput, typeSelect);
+  }, [searchInput, typeSelect]);
+
+  return (
+    <>
+      <form className="field has-addons">
+        <p className="control">
+          <span className="select">
+            <select
+              data-cy="statusSelect"
+              value={typeSelect}
+              onChange={(event) => {
+                setTypeSelect(event.target.value);
+              }}
+            >
+              <option value="all">All</option>
+              <option value="active">Active</option>
+              <option value="completed">Completed</option>
+            </select>
+          </span>
+        </p>
+
+        <p className="control is-expanded has-icons-left has-icons-right">
+          <input
+            data-cy="searchInput"
+            type="text"
+            className="input"
+            placeholder="Search..."
+            value={searchInput}
+            onChange={(event) => {
+              setSearchInput(event.target.value);
+            }}
+          />
+          <span className="icon is-left">
+            <i className="fas fa-magnifying-glass" />
+          </span>
+
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {searchInput && (
+              // eslint-disable-next-line jsx-a11y/control-has-associated-label
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={() => {
+                  setSearchInput('');
+                }}
+              />
+            )}
+          </span>
+        </p>
+      </form>
+    </>
+  );
+};
