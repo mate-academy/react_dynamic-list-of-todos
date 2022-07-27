d8726e6cb918bf22a1e8c64d9956510fbf1ebeba
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-07-27 22:35:04 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,68 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
+import { getTodos } from './api';
+
+import { Todo } from './types/Todo';
 import { Loader } from './components/Loader';
 
+let todos: Todo [] = [];
+
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filteredTodo, setFilteredTodo] = useState<Todo []>([]);
+  const [status, setStatus] = useState('All');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const selectTodoHadler = (todo:Todo) => setSelectedTodo({ ...todo });
+  const clearCurrentTodo = () => setSelectedTodo(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then((todosFromServer) => {
+        todos = todosFromServer;
+
+        return todos;
+      })
+      .then(
+        (todosFromServer) => setFilteredTodo(
+          todosFromServer.map((todo: Todo) => ({ ...todo })),
+        ),
+      )
+      .then(() => setLoading(false));
+  }, []);
+
+  const filterHandler = () => {
+    let copyTodos = todos.map((todo: Todo) => ({ ...todo }));
+
+    if (searchTerm.length > 0) {
+      copyTodos = copyTodos.filter((todo:Todo) => todo.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    }
+
+    switch (status) {
+      case 'active':
+        copyTodos = copyTodos.filter((todo:Todo) => !todo.completed);
+        break;
+
+      case 'completed':
+        copyTodos = copyTodos.filter((todo:Todo) => todo.completed);
+        break;
+      default:
+        break;
+    }
+
+    setFilteredTodo(copyTodos);
+  };
+
+  useEffect(filterHandler, [searchTerm, status]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +71,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={status}
+                setStatus={setStatus}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={filteredTodo}
+                    selectedTodo={selectedTodo}
+                    selectTodoHadler={selectTodoHadler}
+                  />
+                )}
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo
+        && (
+          <TodoModal
+            todo={selectedTodo}
+            closeHadler={clearCurrentTodo}
+          />
+        )}
     </>
   );
 };
