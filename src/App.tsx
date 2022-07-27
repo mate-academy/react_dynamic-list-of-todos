722d99732c5ff01e0d521a7c9d9cedd81ca1841f
Dmytro Mostovyi <7kozita@gmail.com>
2022-07-27 17:37:14 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,11 +7,73 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterValue, setFilterValue] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => setTodos([...todosFromServer]));
+  }, []);
+
+  const lowCaseQuery = query.toLocaleLowerCase();
+
+  const visibleTodos = todos.filter(todo => {
+    if ((filterValue === 'active' && todo.completed)
+    || (filterValue === 'completed' && !todo.completed)) {
+      return false;
+    }
+
+    const lowCaseTitle = todo.title.toLocaleLowerCase();
+
+    return lowCaseTitle.includes(lowCaseQuery);
+  });
+
   return (
     <>
       <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter
+                query={query}
+                onSetQuery={setQuery}
+                onSetFilterValue={setFilterValue}
+              />
+            </div>
+
+            <div className="block">
+              {todos.length === 0 ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos}
+                  selectedTodoId={selectedTodo?.id}
+                  setSelectedTodo={setSelectedTodo}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {selectedTodo?.id && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          cleanSelectedTodo={setSelectedTodo}
+        />
+      )}
+
+      {/* --------------------------------------------------------------------- */}
+      {/* <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
@@ -22,13 +84,13 @@ export const App: React.FC = () => {
 
             <div className="block">
               <Loader />
-              <TodoList />
+              <TodoList todos={todos} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal /> */}
     </>
   );
 };
