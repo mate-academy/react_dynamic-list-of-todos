23bfe17715077bd02b97976d591b5479a2633d4e
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-07-31 23:28:51 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,49 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+
+const URL
+= 'https://mate-academy.github.io/react_dynamic-list-of-todos/api/todos.json';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState< Todo[] | null >(null);
+  const [data, setData] = useState< Todo[] | null >(null);
+  const [options, setOptions] = useState('all');
+  const [query, setQuery] = useState<string>('');
+  const [show, setShow] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    const fetchFoo = async () => {
+      fetch(URL)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(`${res.statusText}`);
+          } else {
+            return res.json();
+          }
+        })
+        .then(res => setTodos(res))
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.warn(error.message);
+        });
+    };
+
+    fetchFoo();
+  }, []);
+
+  useEffect(() => {
+    if (todos !== null) {
+      setData(todos);
+    }
+  }, [todos]);
+
+  if (todos) {
+    // eslint-disable-next-line no-console
+    console.log(todos);
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setOptions={setOptions}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {
+                data === null
+                  ? <Loader />
+                  : (
+                    <TodoList
+                      todos={data}
+                      options={options}
+                      query={query}
+                      setShow={setShow}
+                      show={show}
+                    />
+                  )
+              }
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {
+        show !== null
+        && <TodoModal show={show} setShow={setShow} />
+      }
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  show: Todo;
+  setShow: React.Dispatch<React.SetStateAction<Todo | null>>;
+};
+
+export const TodoModal: React.FC<Props> = ({ show, setShow }) => {
+  const [userInfo, setUserInfo] = useState<null | User>(null);
+
+  const URL = `https://mate-academy.github.io/react_dynamic-list-of-todos/api/users/${show.userId}.json`;
+
+  useEffect(() => {
+    const fetchFunk = async () => {
+      fetch(URL)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(`${res.statusText}`);
+          } else {
+            return res.json();
+          }
+        })
+        .then(res => {
+          setUserInfo(res);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-alert
+          alert(error.message);
+        });
+    };
+
+    fetchFunk();
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      { userInfo === null ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +48,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${show.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +56,30 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => {
+                setShow(null);
+              }}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {show.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
+              {
+                show.completed
+                  ? (
+                    <strong className="has-text-success">Done</strong>
+                  ) : (
+                    <strong className="has-text-danger">Planned</strong>
+                  )
+              }
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={userInfo.email}>
+                {userInfo.name}
               </a>
             </p>
           </div>
