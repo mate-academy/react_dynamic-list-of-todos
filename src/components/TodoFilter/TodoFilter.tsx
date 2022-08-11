b6654f6fb2c60015e3299042ac68ff696a3078e9
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-08-11 11:45:35 +0300

    Fixed

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -8,54 +8,50 @@ type Props = {
   onSearchFilter: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export const TodoFilter: React.FC<Props> = React.memo(
-  (
-    {
-      selectFilter,
-      searchFilter,
-      onSelectFilter,
-      onSearchFilter,
-    },
-  ) => {
-    const [filter, setFilter] = useState(searchFilter);
-    const applyQuery = useCallback(debounce(onSearchFilter, 500), []);
+export const TodoFilter: React.FC<Props> = React.memo(({
+  selectFilter,
+  searchFilter,
+  onSelectFilter,
+  onSearchFilter,
+}) => {
+  const [filter, setFilter] = useState(searchFilter);
+  const applyQuery = useCallback(debounce(onSearchFilter, 500), []);
 
-    window.console.log('Filter');
-
-    return (
-      <form className="field has-addons">
-        <p className="control">
-          <span className="select">
-            <select
-              data-cy="statusSelect"
-              value={selectFilter}
-              onChange={(event) => {
-                onSelectFilter(event.target.value);
-              }}
-            >
-              <option value="all">All</option>
-              <option value="active">Active</option>
-              <option value="completed">Completed</option>
-            </select>
-          </span>
-        </p>
-
-        <p className="control is-expanded has-icons-left has-icons-right">
-          <input
-            data-cy="searchInput"
-            type="text"
-            className="input"
-            placeholder="Search..."
-            value={filter}
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectFilter}
             onChange={(event) => {
-              setFilter(event.currentTarget.value);
-              applyQuery(event.currentTarget.value);
+              onSelectFilter(event.target.value);
             }}
-          />
-          <span className="icon is-left">
-            <i className="fas fa-magnifying-glass" />
-          </span>
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
 
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={filter}
+          onChange={(event) => {
+            setFilter(event.currentTarget.value);
+            applyQuery(event.currentTarget.value);
+          }}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {searchFilter && (
           <span className="icon is-right" style={{ pointerEvents: 'all' }}>
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
@@ -68,8 +64,8 @@ export const TodoFilter: React.FC<Props> = React.memo(
               }}
             />
           </span>
-        </p>
-      </form>
-    );
-  },
-);
+        )}
+      </p>
+    </form>
+  );
+});
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Todo } from '../../types/Todo';
 
 type Props = {
@@ -8,79 +8,83 @@ type Props = {
   searchFilter: string;
 };
 
-export const TodoList: React.FC<Props> = React.memo(
-  (
-    {
-      todos,
-      onSetTodo,
-      selectFilter,
-      searchFilter,
-    },
-  ) => {
-    let prepareTodos = [...todos].filter(todo => {
+export const TodoList: React.FC<Props> = React.memo(({
+  todos,
+  onSetTodo,
+  selectFilter,
+  searchFilter,
+}) => {
+  const prepareTodos = useMemo(() => {
+    let newTodos = [...todos].filter(todo => {
       switch (selectFilter) {
-        case 'all':
-          return true;
         case 'completed':
           return todo.completed;
         case 'active':
           return !todo.completed;
         default:
-          return false;
+          return true;
       }
     });
 
     if (searchFilter) {
-      prepareTodos = prepareTodos.filter(todo => (
+      newTodos = newTodos.filter(todo => (
         todo.title.includes(searchFilter)
       ));
     }
 
-    return (
-      <table className="table is-narrow is-fullwidth">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>
-              <span className="icon">
-                <i className="fas fa-check" />
-              </span>
-            </th>
-            <th>Title</th>
-            <th> </th>
-          </tr>
-        </thead>
+    return newTodos;
+  }, [searchFilter, selectFilter]);
 
-        <tbody>
-          {prepareTodos.map(todo => (
-            <tr key={todo.id} data-cy="todo" className="">
-              <td className="is-vcentered">{todo.id}</td>
-              <td className="is-vcentered" />
-              <td className="is-vcentered is-expanded">
-                {todo.completed ? (
-                  <p className="has-text-success">{todo.title}</p>
-                ) : (
-                  <p className="has-text-danger">{todo.title}</p>
-                )}
-              </td>
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => {
-                    onSetTodo(todo);
-                  }}
-                >
-                  <span className="icon">
-                    <i className="far fa-eye" />
-                  </span>
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  },
-);
+  return (
+    <table className="table is-narrow is-fullwidth">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>
+            <span className="icon">
+              <i className="fas fa-check" />
+            </span>
+          </th>
+          <th>Title</th>
+          <th> </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {prepareTodos.map(todo => (
+          <tr key={todo.id} data-cy="todo" className="">
+            <td className="is-vcentered">{todo.id}</td>
+            <td className="is-vcentered">
+              {todo.completed && (
+                <span className="icon">
+                  <i className="fas fa-check" />
+                </span>
+              )}
+            </td>
+            <td className="is-vcentered is-expanded">
+              {todo.completed ? (
+                <p className="has-text-success">{todo.title}</p>
+              ) : (
+                <p className="has-text-danger">{todo.title}</p>
+              )}
+            </td>
+            <td className="has-text-right is-vcentered">
+              <button
+                data-cy="selectButton"
+                className="button"
+                type="button"
+                onClick={() => {
+                  onSetTodo(todo);
+                }}
+              >
+                <span className="icon">
+                  <i className="far fa-eye" />
+                </span>
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+});
