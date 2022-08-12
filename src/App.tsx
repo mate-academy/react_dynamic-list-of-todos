b973fdaf3f99bb26221cb3970a2437d2863b113d
Misha Dzhemula <medvedmisha23@gmail.com>
2022-08-12 20:58:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,57 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
+const filterByStatus = (todos: Todo[], method: string) => {
+  if (method === 'all') {
+    return todos;
+  }
+
+  return todos.filter(todo => (method === 'active'
+    ? !todo.completed
+    : todo.completed));
+};
+
+const filterByTitle = (todos: Todo[], query: string) => {
+  if (query === '') {
+    return todos;
+  }
+
+  return todos.filter(
+    todo => todo.title.toLowerCase().includes(query.toLowerCase()),
+  );
+};
+
+const bebounce = (f: React.Dispatch<React.SetStateAction<string>>, delay: number) => {
+  let timerId: number;
+
+  return (...args: string[]) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(f, delay, ...args);
+  };
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterMethod, setFilterMethod] = useState('all');
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(loadedTodos => setTodos(loadedTodos));
+  }, []);
+
+  const visibleTodos = filterByTitle(
+    filterByStatus(todos, filterMethod),
+    appliedQuery,
+  );
+
+  const applyQuery = useCallback(bebounce(setAppliedQuery, 1000), []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +66,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSelect={setFilterMethod}
+                onInput={setQuery}
+                onInputApplied={applyQuery}
+                onCancel={setAppliedQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleTodos.length ? (
+                <TodoList
+                  todos={visibleTodos}
+                  onSelect={setSelectedTodo}
+                  selectedTodo={selectedTodo}
+                />
+              ) : (
+                <Loader />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal todo={selectedTodo} onX={setSelectedTodo} />
+      )}
     </>
   );
 };
