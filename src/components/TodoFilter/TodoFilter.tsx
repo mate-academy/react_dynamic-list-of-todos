0952fd39438b6409c7de34226bc5cd5e0d03a428
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-08-18 17:19:55 +0300

    clear button fixed

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -41,15 +41,17 @@ export const TodoFilter: FC<Props> = ({
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-          onClick={() => changeQuery('')}
-        />
-      </span>
+      {query && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => changeQuery('')}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
