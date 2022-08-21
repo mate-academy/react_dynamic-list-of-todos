089d31b0fc04e52e958f188be92398dfaef75540
Oleg Vetrov <oleg94038@gmail.com>
2022-08-21 16:52:06 +0300

    Added todoList implementation

--- src/types/User.ts
+++ src/types/User.ts
@@ -3,4 +3,21 @@ export interface User {
   name: string;
   email: string;
   phone: string;
+  website: string,
+  username: string,
+  address: {
+    city: string,
+    geo: {
+      lat: string,
+      lng: string,
+    },
+    street: string,
+    suite: string,
+    zipcode: string,
+  },
+  company: {
+    bs: string,
+    catchPhrase: string,
+    name: string,
+  },
 }
