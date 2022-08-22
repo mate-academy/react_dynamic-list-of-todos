7d66e207e0d168a0fd8ee95adc13ed99534a0b54
Oleg Vetrov <oleg94038@gmail.com>
2022-08-22 15:43:37 +0300

    Fixed some bugs

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -5,7 +5,7 @@ import { getUser } from '../../api';
 import { Todo } from '../../types/Todo';
 
 interface Props {
-  todo: Todo | null,
+  todo: Todo,
   hideInfo: CallableFunction,
 }
 
@@ -14,9 +14,7 @@ export const TodoModal: React.FC<Props> = (props) => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    if (todo !== null) {
-      getUser(todo.userId).then(userFromServer => setUser(userFromServer));
-    }
+    getUser(todo.userId).then(userFromServer => setUser(userFromServer));
   }, []);
 
   return (
@@ -35,7 +33,7 @@ export const TodoModal: React.FC<Props> = (props) => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              {`Todo #${todo?.id}`}
+              {`Todo #${todo.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -51,20 +49,18 @@ export const TodoModal: React.FC<Props> = (props) => {
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              {todo?.title}
+              {todo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {todo?.completed ? (
+              {todo.completed ? (
                 <strong className="has-text-success">Done</strong>
               ) : (
                 <strong className="has-text-danger">Planned</strong>
               )}
-
               {' by '}
-
-              <a href={`mailto:${user?.email}`}>
-                {user?.name}
+              <a href={`mailto:${user.email}`}>
+                {user.name}
               </a>
             </p>
           </div>
