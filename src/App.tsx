c80689694393496db5323f66bbf274721d1c1917
Volodymyr Dolishnii <leedeelee3@gmail.com>
2022-08-23 10:37:24 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, {
+  useEffect, useState, useCallback, useMemo,
+} from 'react';
+import './App.css';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +9,56 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { FilterType } from './types/filterType';
+
+const debounce = (func: (arg: string) => void, delay = 300) => {
+  let timerId: number;
+
+  return (...args: string[]) => {
+    clearInterval(timerId);
+
+    timerId = window.setTimeout(func, delay, ...args);
+  };
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [filterType, setFilterType] = useState(FilterType.All);
+  const [inputValue, setinputValue] = useState('');
+  const [appliedInputValue, setApliedInputValue] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(response => setTodos(response))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const applyInputValue = useCallback(debounce(setApliedInputValue), []);
+
+  const filteredTodos = useMemo(() => {
+    return todos.filter(todo => {
+      const { title, completed } = todo;
+      const checkInput = title
+        .toLowerCase()
+        .includes(inputValue.toLowerCase());
+
+      switch (filterType) {
+        case FilterType.All:
+          return checkInput;
+        case FilterType.Active:
+          return checkInput && !completed;
+        case FilterType.Completed:
+          return checkInput && completed;
+        default:
+          return true;
+      }
+    });
+  }, [todos, appliedInputValue, filterType]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +67,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                inputValue={inputValue}
+                filterType={filterType}
+                onFilter={setFilterType}
+                onInputChange={setinputValue}
+                onAppliedInputChange={applyInputValue}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              {!isLoading && (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  selectTodo={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          onClose={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,33 @@
-export const TodoFilter = () => (
+import React from 'react';
+import { FilterType } from '../../types/filterType';
+
+type Props = {
+  filterType: FilterType,
+  onFilter: (type: FilterType) => void,
+  inputValue: string,
+  onInputChange: (val: string) => void,
+  onAppliedInputChange: (val: string) => void,
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  filterType,
+  onFilter,
+  inputValue,
+  onInputChange,
+  onAppliedInputChange,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={filterType}
+          onChange={(event) => {
+            const { value } = event.target;
+
+            onFilter(value as FilterType);
+          }}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,19 +41,33 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={inputValue}
+        onChange={(event) => {
+          const { value } = event.target;
+
+          onInputChange(value);
+          onAppliedInputChange(value);
+        }}
       />
+
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
+      {inputValue && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => {
+              onInputChange('');
+              onAppliedInputChange('');
+            }}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
