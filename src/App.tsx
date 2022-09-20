d279ae9948662099a5f3aad3f1d34cb26ddf802f
Maks Sobko <tiserettt@gmail.com>
2022-09-20 13:53:37 +0300

    implemented people table with sort and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,41 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [todoId, setTodoId] = useState(0);
+  const [filterValue, setFilterValue] = useState('all');
+  const [queryFilter, setQueryFilter] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(response => {
+        setTodos(response);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const visibleTodos = todos
+    .filter(todo => {
+      switch (filterValue) {
+        case 'active':
+          return !todo.completed;
+
+        case 'completed':
+          return todo.completed;
+
+        default:
+          return true;
+      }
+    })
+    .filter(todo => {
+      return todo.title.toLowerCase().includes(queryFilter.toLowerCase());
+    });
+
   return (
     <>
       <div className="section">
@@ -17,18 +50,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={todos}
+                value={filterValue}
+                setValue={setFilterValue}
+                query={queryFilter}
+                setQuery={setQueryFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading
+                ? (<Loader />)
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodoId={todoId}
+                    selectTodo={todo => setTodoId(todo)}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {todoId !== 0 && (
+        <TodoModal
+          todoId={todoId}
+          todos={visibleTodos}
+          selectTodo={todo => setTodoId(todo)}
+        />
+      )}
     </>
   );
 };
