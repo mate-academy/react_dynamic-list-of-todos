7556327c5d69137f5f45f386cea5e6b3559cfcfe
Nazar Kulchevych <gamebandit38@gmail.com>
2022-09-27 23:33:40 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,52 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
+enum GroupBy {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [visibleTodos, setVisibleTodos] = useState(todos);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(todos[0]);
+
+  useEffect(() => {
+    getTodos().then(response => {
+      setTodos(response);
+      setVisibleTodos(response);
+      setIsLoaded(true);
+    });
+  }, []);
+
+  const filterTodos = (groupBy: string, query: string) => {
+    setVisibleTodos(
+      todos.filter(todo => {
+        switch (groupBy) {
+          case GroupBy.All:
+            return true;
+          case GroupBy.Active:
+            return todo.completed === false;
+          case GroupBy.Completed:
+            return todo.completed === true;
+          default:
+            return true;
+        }
+      }).filter(todo => (
+        todo.title.toLowerCase().includes(query.toLocaleLowerCase())
+      )),
+    );
+  };
+
+  const setTodo = (todo: Todo | null) => {
+    setSelectedTodo(todo);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,26 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter filterTodos={filterTodos} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoaded ? (
+                <TodoList
+                  todos={visibleTodos}
+                  setTodo={setTodo}
+                  selectedTodo={selectedTodo}
+                />
+              ) : (
+                <Loader />
+              )}
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && <TodoModal todo={selectedTodo} setTodo={setTodo} />}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,77 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useEffect, useState } from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  filterTodos: (query: string, groupBy: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({ filterTodos }) => {
+  const [query, setQuery] = useState('');
+  const [groupBy, setGroupBy] = useState('all');
+
+  useEffect(() => {
+    filterTodos(groupBy, query);
+  }, [query, groupBy]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>
+  | React.ChangeEvent<HTMLInputElement>) => {
+    const { value, localName } = event.target;
+
+    if (localName === 'input') {
+      setQuery(value);
+    }
+
+    if (localName === 'select') {
+      setGroupBy(value);
+    }
+  };
+
+  const resetQuery = () => {
+    setQuery('');
+  };
+
+  return (
+    <form
+      className="field has-addons"
+    >
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={handleChange}
+            value={groupBy}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={handleChange}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query.trim().length > 0 ? (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={resetQuery}
+            />
+          </span>
+        ) : ''}
+      </p>
+    </form>
+  );
+};
