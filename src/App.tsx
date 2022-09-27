69526c1af995e9497a4a4d9fb6d593b84f431e35
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-09-27 14:54:47 +0300

    maby right

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,37 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [activeTodo, setActiveTodo] = useState<Todo | null>(null);
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [vsibleTodos, setVisibleTodos] = useState(todos);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+        setVisibleTodos(todosFromServer);
+      });
+  }, []);
+
+  const handleShowModal = (id: number) => {
+    setActiveTodo(todos.find(todo => todo.id === id) || todos[0]);
+    setIsModalActive(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalActive(false);
+    setActiveTodo(null);
+  };
+
+  const handleFilter = (visibleTodos: Todo[]) => {
+    setVisibleTodos(visibleTodos);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +46,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter filter={handleFilter} todos={todos} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length > 0 ? (
+                <TodoList
+                  todos={vsibleTodos}
+                  showModal={handleShowModal}
+                  activeTodo={activeTodo}
+                />
+              ) : (
+                <Loader />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalActive
+        && activeTodo
+        && (
+          <TodoModal
+            todo={activeTodo}
+            closeModal={handleCloseModal}
+          />
+        )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,84 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  filter: (visibleTodos: Todo[]) => void;
+  todos: Todo[];
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({ filter, todos }) => {
+  const [query, setQuerty] = useState('');
+  const [todoStatus, setTodoStatus] = useState('all');
+
+  useEffect(() => {
+    filter(todos
+      .filter(todo => (
+        todo.title.toLowerCase().includes(query.trim().toLowerCase())
+      ))
+      .filter(todo => {
+        switch (todoStatus) {
+          case 'all':
+            return true;
+
+          case 'active':
+            return !todo.completed;
+
+          case 'completed':
+            return todo.completed;
+
+          default:
+            throw new Error('wrong todo status');
+        }
+      }));
+  }, [query, todoStatus]);
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={(event) => {
+              setTodoStatus(event.target.value);
+            }}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={event => setQuerty(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {query !== '' && (
+            <>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={() => {
+                  setQuerty('');
+                }}
+              />
+            </>
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
