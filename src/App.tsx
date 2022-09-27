58df3610cd294fc101b5c532e37002963d254bcd
Oleksii Pustovit <91pustovit91@gmail.com>
2022-09-27 17:49:06 +0300

    Solution of react_dinamic-list-of-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,39 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todoId, setTodoId] = useState<number>(0);
+  const [loader, setLoader] = useState<boolean>(false);
+  const [filterByValue, setFilterByValue] = useState<string>('all');
+  const [filterByText, setFilterByText] = useState<string>('');
+
+  useEffect(() => {
+    getTodos()
+      .then(response => {
+        setTodos(response);
+        setLoader(true);
+      });
+  }, []);
+
+  const filteredTodos = todos.filter(todo => {
+    switch (filterByValue) {
+      case 'active':
+        return !todo.completed;
+
+      case 'completed':
+        return todo.completed;
+
+      default:
+        return todo;
+    }
+  }).filter(todo => {
+    return todo.title.toLowerCase().includes(filterByText.toLowerCase());
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={todos}
+                value={filterByValue}
+                text={filterByText}
+                setValue={setFilterByValue}
+                setText={setFilterByText}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              { !loader ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodoId={todoId}
+                  selectedTodo={(todo) => setTodoId(todo)}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {todoId !== 0 && (
+        <TodoModal
+          todoId={todoId}
+          todos={todos}
+          selectedTodo={(todo) => setTodoId(todo)}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,73 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { ChangeEvent } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  todos: Todo[];
+  value: string;
+  setValue: (argument: string) => void;
+  text: string;
+  setText: (argument: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({
+  value,
+  setValue,
+  text,
+  setText,
+}) => {
+  const handleChangeValue = (event: ChangeEvent<HTMLSelectElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const handleChangeText = (event:
+  ChangeEvent<HTMLInputElement>) => {
+    setText(event.currentTarget.value);
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={value}
+            onChange={handleChangeValue}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          type="text"
+          data-cy="searchInput"
+          className="input"
+          placeholder="Search..."
+          value={text}
+          onChange={handleChangeText}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+        <span
+          className="icon is-right"
+          style={{ pointerEvents: 'all' }}
+        >
+          {text !== '' && (
+            <>
+              <button
+                type="button"
+                data-cy="clearSearchButton"
+                className="delete"
+                onClick={() => setText('')}
+                aria-label="Clear search query"
+              />
+            </>
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
