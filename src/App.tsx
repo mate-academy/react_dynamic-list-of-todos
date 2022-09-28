0a4d76c4826b08b130b84d48669ed4931dfec3e6
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-09-28 12:56:47 +0300

    Dynamic list of todos implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,65 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+let todosFromServer: Todo[] = [];
+
+function getFilteredTodos(
+  todos: Todo[],
+  searchValue: string,
+  selectValue: string,
+): Todo[] {
+  let newTodos = [...todos];
+
+  switch (selectValue) {
+    case 'completed':
+      newTodos = newTodos.filter(todo => todo.completed);
+      break;
+
+    case 'active':
+      newTodos = newTodos.filter(todo => !todo.completed);
+      break;
+    default:
+      break;
+  }
+
+  return newTodos.filter(todo => todo.title.toLowerCase().includes(searchValue.toLowerCase()));
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todoListLoaded, setTodolistLoaded] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [selectValue, setSelectValue] = useState('all');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  const handleSearchInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const clearSearchInput = () => {
+    setSearchValue('');
+  };
+
+  const handleSelectInput = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectValue(event.target.value);
+  };
+
+  useEffect(() => {
+    setTodos(getFilteredTodos(todosFromServer, searchValue, selectValue));
+  }, [searchValue, selectValue]);
+
+  useEffect(() => {
+    (async () => {
+      todosFromServer = await getTodos();
+
+      setTodos(getFilteredTodos(todosFromServer, searchValue, selectValue));
+      setTodolistLoaded(true);
+    })();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +74,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                searchValue={searchValue}
+                selectValue={selectValue}
+                onSearchInput={handleSearchInput}
+                onSelectInput={handleSelectInput}
+                clearSearch={clearSearchInput}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todoListLoaded
+                ? (
+                  <TodoList
+                    todos={todos}
+                    selectedTodo={selectedTodo}
+                    setSelectedTodo={setSelectedTodo}
+                  />
+                )
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {!!selectedTodo
+      && <TodoModal selectedTodo={selectedTodo} setSelectedTodo={setSelectedTodo} />}
     </>
   );
 };
