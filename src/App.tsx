d9ed0ac0a6f9d1d63de859b9492c5781ff1cfb92
Ganna Krainiuk <anya.krainiuk@gmail.com>
2022-09-28 23:25:00 +0300

    dynamic list of todo solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,52 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+export function getFilteredTodo(
+  todos: Todo[],
+  filterTypeTodo: string,
+  query: string,
+) {
+  const filterTodoBy = todos.filter((todo) => {
+    switch (filterTypeTodo) {
+      case 'all':
+        return todo;
+
+      case 'completed':
+        return todo.completed === true;
+
+      case 'active':
+        return todo.completed === false;
+
+      default:
+        return 0;
+    }
+  });
+
+  return filterTodoBy.filter(({ title }) => (
+    title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  ));
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [selectedTodo, setSelectedTodo] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filteredBy, setFilteredBy] = useState('all');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(todo => setTodos(todo))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const filteredTodos = getFilteredTodo(todos, filteredBy, query);
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,49 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setFilteredBy={setFilteredBy}
+                setQuery={(queryInput) => {
+                  setQuery(queryInput);
+                }}
+                filteredBy={filteredBy}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+
+              { isLoading
+                ? <Loader />
+                : (
+                  <TodoList
+                    todo={filteredTodos}
+                    selectedTodoId={selectedTodo}
+                    selectedTodo={(id) => {
+                      setSelectedTodo(id);
+                    }}
+                    selectedUserId={(userId) => {
+                      setSelectedUserId(userId);
+                    }}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo
+        && (
+          <TodoModal
+            userId={selectedUserId}
+            selectedTodoId={selectedTodo}
+            selectedTodo={(id) => {
+              setSelectedTodo(id);
+            }}
+            selectedUserId={(userId) => {
+              setSelectedUserId(userId);
+            }}
+          />
+        )}
     </>
   );
 };
