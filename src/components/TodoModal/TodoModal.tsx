9249d65b8d8f801c8cc5ce430604c00072fe23a0
Dima Kobzar <kobzar7dima@gmail.com>
2022-09-28 23:44:09 +0300

    Solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+
+type Props = {
+  selectedTodo: Todo;
+  setClose: (item: null) => void
+};
+
+export const TodoModal: React.FC<Props> = ({ selectedTodo, setClose }) => {
+  const {
+    id,
+    title,
+    completed,
+    userId,
+  } = selectedTodo;
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUser(userId).then(users => {
+      setUser(users);
+    });
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,30 +39,29 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${id}`}
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            { /* eslint-disable-next-line jsx-a11y/control-has-associated-label */ }
             <button
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => setClose(null)}
             />
           </header>
-
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {title}
             </p>
-
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
+              {completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
               {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user.email}`}>
+                {user.name}
               </a>
             </p>
           </div>
