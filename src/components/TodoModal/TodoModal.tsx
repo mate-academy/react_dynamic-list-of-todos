80f82c3aa130f14e04b0de62ff1929405315818f
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-09-29 20:53:38 +0300

    my decision

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../../api';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todos: Todo[];
+  selectedUserId: number,
+  selectUser: (value: number) => number | void,
+};
+
+export const TodoModal: React.FC<Props> = ({
+  selectedUserId,
+  selectUser,
+  todos,
+}) => {
+  const curentTodo = todos.find(todo => todo.id === selectedUserId);
+  const [user, setUsers] = useState<User | null>(null);
+  const [cardOpen, setCardClose] = useState(true);
+
+  useEffect(() => {
+    if (curentTodo) {
+      getUser(curentTodo.userId)
+        .then(response => {
+          setUsers(response);
+        });
+    }
+  }, []);
+
+  if (cardOpen === false) {
+    return null;
+  }
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +45,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${curentTodo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,12 +53,16 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => {
+                selectUser(0);
+                setCardClose(false);
+              }}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {curentTodo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
@@ -36,10 +70,12 @@ export const TodoModal: React.FC = () => {
               <strong className="has-text-danger">Planned</strong>
 
               {' by '}
+              {user && (
+                <a href={`mailto:${user.email}`}>
+                  {user.name}
+                </a>
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
+              )}
             </p>
           </div>
         </div>
