c3de04a53d88aef35a37760dc27923b4e2bbe81a
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2022-09-29 15:21:31 +0300

    fixed

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -51,7 +51,7 @@ export const TodoFilter: React.FC<Props> = ({
         </span>
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {query.length > 0 && (
+          {query && (
             <button
               data-cy="clearSearchButton"
               type="button"
