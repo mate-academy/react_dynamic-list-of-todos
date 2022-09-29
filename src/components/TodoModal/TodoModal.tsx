0f3140293ddd44964ad80cd5693825eecd63e1e3
Shahmatov David <davidsahmatov4@gmail.com>
2022-09-29 17:51:57 +0300

    Version 0.1

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { getTodos, getUser } from '../../api';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  userId: number;
+  selectedTodoId: number;
+  selectedTodo: (value: number) => void,
+  selectedUserId: (value: number) => void,
+};
+
+export const TodoModal: React.FC<Props> = (props) => {
+  const [user, setUser] = useState<User>();
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const {userId, selectedTodoId, selectedTodo, selectedUserId} = props;
+
+  useEffect(() => {
+    const loadData = async () => {
+      const [userFromServer, todosFromServer] = await Promise.all([
+        getUser(userId), getTodos()]);
+
+      setUser(userFromServer);
+      setTodos(todosFromServer);
+    };
+
+    loadData();
+  }, [selectedTodoId]);
+
+  const selectedTodos = todos.filter(({ id }) => id === selectedTodoId);
+
+  const hadlerModalClose = () => {
+    selectedTodo(0);
+    selectedUserId(0);
+  };
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
-            </div>
 
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
+          {selectedTodos.map(({ id, title, completed }) => (
+            <>
+              <header className="modal-card-head">
+                <div
+                  className="modal-card-title has-text-weight-medium"
+                  data-cy="modal-header"
+                >
+                  {`Todo #${id}`}
+                </div>
 
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  type="button"
+                  className="delete"
+                  data-cy="modal-close"
+                  onClick={hadlerModalClose}
+                />
+              </header>
+              <div className="modal-card-body">
+                <p className="block" data-cy="modal-title">
+                  {title}
+                </p>
 
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+                <p className="block" data-cy="modal-user">
+                  {completed === true
+                    ? (<strong className="has-text-success">Done</strong>)
+                    : (<strong className="has-text-danger">Planned</strong>)}
 
-              {' by '}
+                  {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
-          </div>
+                  <a href="mailto:Sincere@april.biz">
+                    {user.name}
+                  </a>
+                </p>
+              </div>
+
+            </>
+
+          ))}
+
         </div>
       )}
     </div>
-  );
-};
+  )}
