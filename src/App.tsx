870d6d5305d989971942ae13fa1d24a8609dbbd3
Roman Yakymiuk <romeronice93@gmail.com>
2022-09-30 09:46:01 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,64 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
+const defaultTodo = {
+  id: 0,
+  title: 'no title',
+  completed: false,
+  userId: 0,
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoader, setIsLoader] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo>(defaultTodo);
+  const [selectValue, setSelectValue] = useState('all');
+  const [filterValue, setFilterValue] = useState('');
+
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      const receivedTodos = await getTodos();
+
+      setTodos(receivedTodos);
+      setIsLoader(false);
+    };
+
+    getTodosAsync();
+  }, []);
+
+  const handleTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleSelect = (value: string) => {
+    setSelectValue(value);
+  };
+
+  const handleChange = (value: string) => {
+    setFilterValue(value);
+  };
+
+  const todosForRender
+    = todos
+      .filter(({ completed }) => {
+        switch (selectValue) {
+          case 'active':
+            return !completed;
+
+          case 'completed':
+            return completed;
+
+          default:
+            return true;
+        }
+      })
+      .filter(({ title }) => (
+        title.toLowerCase().includes(filterValue.toLowerCase())
+      ));
+
   return (
     <>
       <div className="section">
@@ -17,18 +73,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selectValue={selectValue}
+                filterValue={filterValue}
+                handleSelect={handleSelect}
+                handleChange={handleChange}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoader
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={todosForRender}
+                    selectedTodoId={selectedTodo.id}
+                    onSelect={handleTodo}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo.id
+      && (
+        <TodoModal
+          todo={selectedTodo}
+          onSelect={handleTodo}
+        />
+      )}
     </>
   );
 };
