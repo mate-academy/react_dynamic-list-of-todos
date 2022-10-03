dc5de3c439e9c279c0a5a2ad2fbe8406c93eff5b
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-10-03 15:28:15 +0300

    email added

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -80,7 +80,7 @@ export const TodoModal: React.FC<Props> = ({
 
                     {' by '}
 
-                    <a href="mailto:Sincere@april.biz">
+                    <a href={`mailto:${user.email}`}>
                       {user.name}
                     </a>
                   </p>
