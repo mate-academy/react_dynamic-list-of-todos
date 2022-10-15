b837a761ab1aeefa0e9f65d1c3b88712408c7132
Max Bovtun <maxbovtun@gmail.com>
2022-10-15 09:37:54 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,51 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [isLoadingUser, setIsLoadingUser] = useState(false);
+  const [isLoadingTodo, setIsLoadingTodo] = useState(true);
+  const [isOpenTodo, setIsOpenTodo] = useState(0);
+  const [qwery, setQwery] = useState('');
+  const [category, setCategory] = useState('all');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    getTodos().then((loadedTodos) => {
+      setTodos(loadedTodos);
+      setIsLoadingTodo(false);
+    });
+  }, []);
+
+  const onOpenTodo = (todoId: number) => {
+    setIsOpenTodo(todoId);
+    setIsLoadingUser(true);
+  };
+
+  const filterTodos = () => {
+    const byQwery = todos.filter(todo => {
+      const title = todo.title.toLowerCase();
+
+      return title.match(qwery.toLowerCase());
+    });
+
+    if (category === 'completed') {
+      return byQwery.filter(todo => (todo.completed));
+    }
+
+    if (category === 'active') {
+      return byQwery.filter(todo => (!todo.completed));
+    }
+
+    return byQwery;
+  };
+
+  const getTodo = (todoId: number) => {
+    return todos.find(todo => todo.id === todoId) || todos[0];
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +60,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setCategory={setCategory}
+                setQwery={setQwery}
+                qwery={qwery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoadingTodo ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filterTodos()}
+                  onOpen={onOpenTodo}
+                  opened={isOpenTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isOpenTodo && (
+        <TodoModal
+          isLoading={isLoadingUser}
+          setIsLoadingUser={setIsLoadingUser}
+          onClose={setIsOpenTodo}
+          currentTodo={getTodo(isOpenTodo)}
+        />
+      )}
+
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,61 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+type Props = {
+  setCategory: (str: string) => void,
+  setQwery: (str: string) => void,
+  qwery: string,
+};
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+export const TodoFilter: React.FC<Props> = ({
+  setCategory,
+  setQwery,
+  qwery,
+}) => {
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={(event) => {
+              setCategory(event.target.value);
+            }}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={qwery}
+          onChange={(event) => {
+            setQwery(event.target.value);
+          }}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {qwery && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => {
+                setQwery('');
+              }}
+            />
+          </span>
+        )}
+
+      </p>
+    </form>
+  );
+};
