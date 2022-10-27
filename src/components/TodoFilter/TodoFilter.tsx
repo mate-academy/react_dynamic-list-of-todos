b55cf9eca32d127d6aaa86be66e6fd88fb41a7c4
Max Bovtun <maxbovtun@gmail.com>
2022-10-27 23:18:27 +0300

    debounce decorator for filter qwery created

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,4 +1,16 @@
+import { useCallback, useState } from 'react';
 import { Category } from '../../types/Category';
+
+// DEBOUNCE DECORATOR:
+const debounce = (func: (qwery: string) => void, wait: number) => {
+  let timer: NodeJS.Timeout;
+
+  return (newQwery: string) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(func, wait, newQwery);
+  };
+};
 
 type Props = {
   setCategory: (category: Category) => void,
@@ -10,52 +22,63 @@ export const TodoFilter: React.FC<Props> = ({
   setCategory,
   setQwery,
   qwery,
-}) => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select
-          data-cy="statusSelect"
-          onChange={(event) => {
-            setCategory(event.target.value as Category);
-          }}
-        >
-          <option value={Category.ALL}>All</option>
-          <option value={Category.ACTIVE}>Active</option>
-          <option value={Category.COMPLETED}>Completed</option>
-        </select>
-      </span>
-    </p>
+}) => {
+  const [currentQwery, setCurrentQwery] = useState('');
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-        value={qwery}
-        onChange={(event) => {
-          setQwery(event.target.value);
-        }}
-      />
+  const setQweryDebounce = useCallback(
+    debounce(setQwery, 700),
+    [],
+  );
 
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
-
-      {qwery && (
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-            onClick={() => {
-              setQwery('');
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={(event) => {
+              setCategory(event.target.value as Category);
             }}
-          />
+          >
+            <option value={Category.ALL}>All</option>
+            <option value={Category.ACTIVE}>Active</option>
+            <option value={Category.COMPLETED}>Completed</option>
+          </select>
         </span>
-      )}
-    </p>
-  </form>
-);
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={currentQwery}
+          onChange={(event) => {
+            setCurrentQwery(event.target.value);
+            setQweryDebounce(event.target.value);
+          }}
+        />
+
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {qwery && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => {
+                setQwery('');
+                setCurrentQwery('');
+              }}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
