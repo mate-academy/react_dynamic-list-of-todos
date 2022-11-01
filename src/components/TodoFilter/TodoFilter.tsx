5fbbb2566f2b35b55116f2b9fd2c17dc664a2a89
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-11-01 18:21:43 +0200

    Dynamic list of todos was implemented

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,26 @@
-export const TodoFilter = () => (
+import { Status } from '../../types/Status';
+
+type Props = {
+  query: string;
+  onSetQuery: (value: string) => void;
+  onSetStatus: (value: string) => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  query,
+  onSetQuery,
+  onSetStatus,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          onChange={event => onSetStatus(event.target.value)}
+        >
+          <option value={Status.All}>All</option>
+          <option value={Status.Active}>Active</option>
+          <option value={Status.Completed}>Completed</option>
         </select>
       </span>
     </p>
@@ -16,18 +31,23 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={event => onSetQuery(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
+        {query && (
+          // eslint-disable-next-line jsx-a11y/control-has-associated-label
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => onSetQuery('')}
+          />
+        )}
       </span>
     </p>
   </form>
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,81 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
+
+import { getUser } from '../../api';
+
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: Todo;
+  onRemoveTodoSelection: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  selectedTodo,
+  onRemoveTodoSelection,
+}) => {
+  const [
+    userForSelectedTodo,
+    setUserForSelectedTodo,
+  ] = useState<User | null>(null);
+  const [isUserLoaded, setIsUserLoaded] = useState(false);
+
+  const getUserForSelectedTodo = useCallback(async (userId) => {
+    const userById = await getUser(userId);
+
+    setUserForSelectedTodo(userById);
+    setIsUserLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    getUserForSelectedTodo(selectedTodo.userId);
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+      {!isUserLoaded
+        ? <Loader />
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${selectedTodo.id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => onRemoveTodoSelection()}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {selectedTodo.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {selectedTodo.completed
+                  ? <strong className="has-text-success">Done</strong>
+                  : <strong className="has-text-danger">Planned</strong>}
+
+                {' by '}
+
+                <a href={`mailto:${userForSelectedTodo?.email}`}>
+                  {userForSelectedTodo?.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
