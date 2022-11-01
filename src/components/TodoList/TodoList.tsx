228b4794c952df3de862c4b3808e83437473314e
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-11-01 17:27:14 +0200

    TodoList component was implemented

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,36 +1,88 @@
 import React from 'react';
 
-export const TodoList: React.FC = () => (
-  <table className="table is-narrow is-fullwidth">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>
-          <span className="icon">
-            <i className="fas fa-check" />
-          </span>
-        </th>
-        <th>Title</th>
-        <th> </th>
-      </tr>
-    </thead>
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
 
-    <tbody>
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
+type Props = {
+  todos: Todo[];
+  setSelectedTodoId: React.Dispatch<React.SetStateAction<number>>;
+  selectedTodoId: number;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  setSelectedTodoId,
+  selectedTodoId,
+}) => {
+  const handleClick = (todo: Todo) => {
+    setSelectedTodoId(todo.id);
+  };
+
+  return (
+    <table className="table is-narrow is-fullwidth">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>
             <span className="icon">
-              <i className="far fa-eye" />
+              <i className="fas fa-check" />
             </span>
-          </button>
-        </td>
-      </tr>
-      <tr data-cy="todo" className="has-background-info-light">
+          </th>
+          <th>Title</th>
+          <th> </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {todos.map(todo => {
+          const { id, title, completed } = todo;
+
+          return (
+            <tr
+              data-cy="todo"
+              className={cn(
+                { 'has-background-info-light': todo.id === selectedTodoId },
+              )}
+              key={id}
+            >
+              <td className="is-vcentered">{id}</td>
+              <td className="is-vcentered">
+                {completed && (
+                  <span className="icon" data-cy="iconCompleted">
+                    <i className="fas fa-check" />
+                  </span>
+                )}
+              </td>
+              <td className="is-vcentered is-expanded">
+                <p className={cn('', {
+                  'has-text-success': completed,
+                  'has-text-danger': !completed,
+                })}
+                >
+                  {title}
+
+                </p>
+              </td>
+              <td className="has-text-right is-vcentered">
+                <button
+                  data-cy="selectButton"
+                  className="button"
+                  type="button"
+                  onClick={() => handleClick(todo)}
+                >
+                  <span className="icon">
+                    <i className={cn('far', {
+                      'fa-eye': todo.id !== selectedTodoId,
+                      'fa-eye-slash': todo.id === selectedTodoId,
+                    })}
+                    />
+                  </span>
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+        {/* <tr data-cy="todo" className="has-background-info-light">
         <td className="is-vcentered">2</td>
         <td className="is-vcentered" />
         <td className="is-vcentered is-expanded">
@@ -94,7 +146,8 @@ export const TodoList: React.FC = () => (
             </span>
           </button>
         </td>
-      </tr>
-    </tbody>
-  </table>
-);
+      </tr> */}
+      </tbody>
+    </table>
+  );
+};
