df7a109ef89edd5343143442569b3ff240c241f6
Vlad Harkusha <sivari888@gmail.com>
2022-11-02 21:27:42 +0200

    implement loading todos and user from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,40 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isTodosLoading, setIsTodosLoading] = useState(false);
+  const [todoId, setTodoId] = useState(0);
+
+  const loadTodos = async () => {
+    setIsTodosLoading(true);
+
+    try {
+      const todosFromServer = await getTodos();
+
+      setTodos(todosFromServer);
+      setIsTodosLoading(false);
+    } catch (error) {
+      setIsTodosLoading(false);
+      throw new Error(`something went wrong ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const handleTodoId = (id: number) => {
+    setTodoId(id);
+  };
+
+  const currentTodo = todos.find(todo => todo.id === todoId) || null;
+
+  const closeModal = () => setTodoId(0);
+
   return (
     <>
       <div className="section">
@@ -21,14 +53,21 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isTodosLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={todos}
+                  selectedTodoId={todoId}
+                  selectTodo={handleTodoId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal todo={currentTodo} closeModal={closeModal} />}
     </>
   );
 };
