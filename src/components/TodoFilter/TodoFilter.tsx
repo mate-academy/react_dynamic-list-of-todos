59443553728d7656d28c443171deb0e505ca2ef0
Serhii Hluhovskyi <glugovsky@proton.me>
2022-11-02 22:23:47 +0200

    Implemented dynamic list of todos solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,82 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React from 'react';
+import { SortBy } from '../../types/SortBy';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  sortBy: SortBy;
+  setSortBy: React.Dispatch<React.SetStateAction<SortBy>>;
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({
+  query,
+  setQuery,
+  sortBy,
+  setSortBy,
+}) => {
+  const handleQueryInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const clearQueryInput = () => setQuery('');
+
+  const handleSortBy = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    switch (event.target.value) {
+      case SortBy.Completed:
+        setSortBy(SortBy.Completed);
+        break;
+
+      case SortBy.Active:
+        setSortBy(SortBy.Active);
+        break;
+
+      default:
+        setSortBy(SortBy.All);
+        break;
+    }
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={sortBy}
+            onChange={handleSortBy}
+          >
+            <option value={SortBy.All}>All</option>
+            <option value={SortBy.Active}>Active</option>
+            <option value={SortBy.Completed}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={handleQueryInput}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {query && (
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              aria-label="Clear query"
+              onClick={clearQueryInput}
+            />
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import cn from 'classnames';
+import React, { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+
+interface Props {
+  selectedTodo: Todo;
+  handleClose: () => void;
+}
+
+export const TodoModal: React.FC<Props> = ({
+  selectedTodo,
+  handleClose,
+}) => {
+  const {
+    userId,
+    id,
+    title,
+    completed,
+  } = selectedTodo;
+  const [user, setUser] = useState<User | null>(null);
+  const [isUserLoaded, setIsUserLoaded] = useState(false);
+
+  useEffect(() => {
+    getUser(userId)
+      .then((userFromServer) => {
+        setUser(userFromServer);
+        setIsUserLoaded(true);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!isUserLoaded ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,30 +48,38 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${id}`}
             </div>
 
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
               type="button"
               className="delete"
               data-cy="modal-close"
+              aria-label="Close todo modal"
+              onClick={handleClose}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong className={cn({
+                'has-text-success': completed,
+                'has-text-danger': !completed,
+              })}
+              >
+                {completed
+                  ? 'Done'
+                  : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
               </a>
             </p>
           </div>
