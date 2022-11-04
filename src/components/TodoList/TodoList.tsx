a79825a43f7373bb65c8d7c2ea0db8a7734ceebe
Dima Romanenko <demigod177712@gmail.com>
2022-11-04 19:29:16 +0200

    isSelected variable fix

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -13,77 +13,76 @@ export const TodoList: React.FC<Props> = ({
   todos,
   onSelectedTodo,
   selectedTodo,
-}) => {
-  const isSelected = !!selectedTodo;
+}) => (
+  <table className="table is-narrow is-fullwidth">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>
+          <span className="icon">
+            <i className="fas fa-check" />
+          </span>
+        </th>
+        <th>Title</th>
+        <th> </th>
+      </tr>
+    </thead>
 
-  return (
-    <table className="table is-narrow is-fullwidth">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>
-            <span className="icon">
-              <i className="fas fa-check" />
-            </span>
-          </th>
-          <th>Title</th>
-          <th> </th>
-        </tr>
-      </thead>
+    <tbody>
+      {todos.map(({ id, title, completed }) => {
+        const isSelected = (selectedTodo)
+          ? selectedTodo.id === id
+          : false;
 
-      <tbody>
-        {todos.map(({ id, title, completed }) => {
-          return (
-            <tr
-              data-cy="todo"
-              className={classNames({
-                'has-background-info-light':
-                isSelected ? selectedTodo.id === id : false,
-              })}
-              key={id}
-            >
-              <td className="is-vcentered">{id}</td>
+        return (
+          <tr
+            data-cy="todo"
+            className={classNames({
+              'has-background-info-light': isSelected,
+            })}
+            key={id}
+          >
+            <td className="is-vcentered">{id}</td>
 
-              <td className="is-vcentered">
-                {completed && (
-                  <span className="icon" data-cy="iconCompleted">
-                    <i className="fas fa-check" />
-                  </span>
-                )}
-              </td>
+            <td className="is-vcentered">
+              {completed && (
+                <span className="icon" data-cy="iconCompleted">
+                  <i className="fas fa-check" />
+                </span>
+              )}
+            </td>
 
-              <td className="is-vcentered is-expanded">
-                <p
-                  className={classNames({
-                    'has-text-danger': !completed,
-                    'has-text-success': completed,
-                  })}
-                >
-                  {title}
-                </p>
-              </td>
+            <td className="is-vcentered is-expanded">
+              <p
+                className={classNames({
+                  'has-text-danger': !completed,
+                  'has-text-success': completed,
+                })}
+              >
+                {title}
+              </p>
+            </td>
 
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => onSelectedTodo(id)}
-                >
-                  <span className="icon">
-                    <i className={classNames(
-                      'far',
-                      { 'fa-eye-slash': selectedTodo?.id === id },
-                      { 'fa-eye': selectedTodo?.id !== id },
-                    )}
-                    />
-                  </span>
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-};
+            <td className="has-text-right is-vcentered">
+              <button
+                data-cy="selectButton"
+                className="button"
+                type="button"
+                onClick={() => onSelectedTodo(id)}
+              >
+                <span className="icon">
+                  <i className={classNames(
+                    'far',
+                    { 'fa-eye-slash': isSelected },
+                    { 'fa-eye': !isSelected },
+                  )}
+                  />
+                </span>
+              </button>
+            </td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+);
