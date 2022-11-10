bd23c207416395583fabdd792e2c3dc6f5de33d9
Paul Voronin <voron.paul@gmail.com>
2022-11-10 19:25:02 +0200

    Solved. Ver.1.0

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useCallback, useState } from 'react';
+import debounce from 'lodash.debounce';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  selectFilter: string;
+  searchFilter: string;
+  onSelectFilter: React.Dispatch<React.SetStateAction<string>>;
+  onSearchFilter: React.Dispatch<React.SetStateAction<string>>;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = React.memo(({
+  selectFilter,
+  searchFilter,
+  onSelectFilter,
+  onSearchFilter,
+}) => {
+  const [filter, setFilter] = useState(searchFilter);
+  const applyQuery = useCallback(debounce(onSearchFilter, 500), []);
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectFilter}
+            onChange={(e) => {
+              onSelectFilter(e.target.value);
+            }}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={filter}
+          onChange={(e) => {
+            setFilter(e.currentTarget.value);
+            applyQuery(e.currentTarget.value);
+          }}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {searchFilter && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => {
+                setFilter('');
+                applyQuery('');
+              }}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+});
