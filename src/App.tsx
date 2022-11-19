94485903a03781adfc5d788333f837a4a7fe9fad
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-11-19 17:16:24 +0800

    version1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -9,6 +9,36 @@ import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  // const [loaderState, setLoaderState] = useState(false);
+  const [allTodos, setAllTodos] = useState([]);
+  // const [allUsers, setAllUsers] = useState([]);
+  const [userId, setUserId] = useState(0);
+  // const [user, setUser] = useState(null);
+
+  const BASE_URL = 'https://mate-academy.github.io'
+  + '/react_dynamic-list-of-todos/api';
+
+  const request = (url: any) => {
+    return fetch(`${BASE_URL}${url}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(result => result);
+  };
+
+  const getTodos = () => request('/todos.json');
+
+  useEffect(() => {
+    getTodos()
+      .then(todos => {
+        setAllTodos(todos);
+      });
+  }, []);
+
+  const handleclick = (id: any) => {
+    setUserId(id);
+  };
+
   return (
     <>
       <div className="section">
@@ -22,7 +52,12 @@ export const App: React.FC = () => {
 
             <div className="block">
               <Loader />
-              <TodoList />
+              <TodoList
+                todos={allTodos}
+                selectTodo={handleclick}
+                // handleClick={handleClick}
+                // getTodos={getTodos}
+              />
             </div>
           </div>
         </div>
@@ -32,3 +67,36 @@ export const App: React.FC = () => {
     </>
   );
 };
+
+// const getTodos = (id) => request(`/todos.json/${id}`);
+// const getUsers = () => request('/users/1.json');
+// function componentDidMount() {
+//   throw new Error('Function not implemented.');
+// }
+// getUsers()
+//   .then(user => {
+//     console.log(user);
+//   });
+
+// const handleClick = () => {
+//   getTodos()
+//     .then(todos => {
+//       return todos;
+//     });
+// };
+
+// getUsers()
+//   .then(user => {
+//     console.log(user);
+//   });
+
+// const handleClick = () => {
+//   getUsers()
+//     .then(todos => {
+//       return todos;
+//     });
+// };
+
+// if(!user) {
+
+// }
