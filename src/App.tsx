dcf0b840c0fba7924ba5f442cfdf86754b1ed76c
Mykola Yakymiv <mykola8426@gmail.com>
2022-11-22 22:55:36 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo []>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo []>([]);
+  const [selectedTodoId, setSelectedTodoId] = useState<number>(0);
+  const [selectedUser, setSelectedUserId] = useState<number>(0);
+
+  useEffect(() => {
+    getTodos().then(allTodos => {
+      setTodos(allTodos);
+    });
+  }, []);
+
+  const handleOnChoose = (userId : number, todoId: number) => {
+    setSelectedUserId(userId);
+    setSelectedTodoId(todoId);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +35,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter setFilteredTodos={setFilteredTodos} todos={todos} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {
+                todos.length === 0
+                  ? <Loader />
+                  : (
+                    <TodoList
+                      todos={filteredTodos}
+                      handleOnChoose={handleOnChoose}
+                      selectedTodoId={selectedTodoId}
+                    />
+                  )
+              }
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodoId && (
+        <TodoModal
+          todoId={selectedTodoId}
+          userId={selectedUser}
+          handleOnChoose={handleOnChoose}
+          todos={todos}
+        />
+      )}
     </>
   );
 };
