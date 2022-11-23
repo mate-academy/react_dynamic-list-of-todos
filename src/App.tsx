8d4e5b52e9ab52420fe2865f44db2e8cefeeee91
Gizem Ece Yılmaz <gizemeceylmaz@gmail.com>
2022-11-23 15:45:30 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,56 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { TodosCategory } from './types/TodosCategory';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [isTodosLoaded, setIsTodosLoaded] = useState(false);
+  const [query, setQuery] = useState('');
+  const [selectValue, setSelectValue] = useState<TodosCategory>(
+    TodosCategory.All,
+  );
+
+  const chooseUser = useCallback((todo: Todo | null) => {
+    setSelectedTodo(todo);
+  }, []);
+
+  const filterTodos = () => {
+    const todosMatchedQuery = todos.filter(({ title }) => {
+      const lowerCasedTitle = title.toLowerCase();
+      const lowerCasedQuery = query.toLowerCase();
+
+      return lowerCasedTitle.includes(lowerCasedQuery);
+    });
+
+    return todosMatchedQuery.filter(todo => {
+      switch (selectValue) {
+        case TodosCategory.Active:
+          return !todo.completed;
+
+        case TodosCategory.Completed:
+          return todo.completed;
+
+        default:
+          return todo;
+      }
+    });
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+        setIsTodosLoaded(true);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +64,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selectValue={selectValue}
+                setSelectValue={setSelectValue}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isTodosLoaded
+                ? (
+                  <TodoList
+                    todos={filterTodos()}
+                    selectedTodo={selectedTodo}
+                    onSelect={chooseUser}
+                  />
+                )
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onClose={() => chooseUser(null)}
+        />
+      )}
     </>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,5 @@
 import { Todo } from './types/Todo';
 import { User } from './types/User';
-
 // eslint-disable-next-line max-len
 const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
 
@@ -14,11 +13,10 @@ function get<T>(url: string): Promise<T> {
   // eslint-disable-next-line prefer-template
   const fullURL = BASE_URL + url + '.json';
 
-  return wait(300)
+  return wait(500)
     .then(() => fetch(fullURL))
     .then(res => res.json());
 }
 
 export const getTodos = () => get<Todo[]>('/todos');
-
 export const getUser = (userId: number) => get<User>(`/users/${userId}`);
