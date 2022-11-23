b3a61de673dd6e91a2f99abe76b6bbf969045cd2
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2022-11-23 20:40:12 +0100

    Solution for Dynamic list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,37 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [status, setStatus] = useState('All');
+  const [query, setQuery] = useState('');
+
+  const filteredTodos = todos.filter((todo) => {
+    if (status === 'Active' && todo.completed) {
+      return false;
+    }
+
+    if (status === 'Completed' && !todo.completed) {
+      return false;
+    }
+
+    return todo.title.toLowerCase().includes(query.toLowerCase());
+  });
+
+  const loadTodos = async () => {
+    const loadedtodos = await getTodos();
+
+    setTodos(loadedtodos);
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +46,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onChangeQuery={setQuery}
+                onChangeStatus={setStatus}
+                statusOptions={['All', 'Active', 'Completed']}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length > 0
+                ? (
+                  <TodoList
+                    todos={filteredTodos}
+                    onSelectTodo={setSelectedTodo}
+                  />
+                )
+                : (
+                  <Loader />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          deselectTodo={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,29 @@
-export const TodoFilter = () => (
+type Props = {
+  onChangeQuery: (query: string) => void;
+  onChangeStatus: (status: string) => void;
+  statusOptions: string[];
+  query: string;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  onChangeQuery,
+  onChangeStatus,
+  statusOptions,
+  query,
+}) => (
+
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          onChange={(event) => onChangeStatus(event.target.value)}
+        >
+          {statusOptions.map(status => (
+            <option value={status}>
+              {status}
+            </option>
+          ))}
         </select>
       </span>
     </p>
@@ -14,8 +32,10 @@ export const TodoFilter = () => (
       <input
         data-cy="searchInput"
         type="text"
+        value={query}
         className="input"
         placeholder="Search..."
+        onChange={(event) => onChangeQuery(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
