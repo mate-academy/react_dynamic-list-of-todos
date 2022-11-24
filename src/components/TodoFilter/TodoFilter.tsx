bacfd10ffc7f74eada5a756b03ed8715137a34b3
Alyona Pisotska <pisotska.o@gmail.com>
2022-11-24 12:27:04 +0200

    react: dynamic list of todos

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useState } from 'react';
+import { getActiveTodos, getCompletedTodos, getTodos } from '../../api';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  changeQuery: (value: string) => void,
+  handleChangeTodos: (value: Promise<Todo[]>) => void,
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({
+  changeQuery,
+  handleChangeTodos,
+}) => {
+  const [searchValue, setSearchValue] = useState('');
+  const changeSearchValue = (value: string) => {
+    changeQuery(value);
+    setSearchValue(value);
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={(event) => {
+              switch (event.target.value) {
+                case 'active':
+                  return handleChangeTodos(getActiveTodos());
+                case 'completed':
+                  return handleChangeTodos(getCompletedTodos());
+                default:
+                  return handleChangeTodos(getTodos());
+              }
+            }}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchValue}
+          onChange={(event) => changeSearchValue(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {searchValue.length > 0 && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => changeSearchValue('')}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
