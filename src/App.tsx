c1b710bb18ff077eeca6fa0c2e8ffa16b3d51b12
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-11-28 22:12:32 +0200

    React dynamic list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,69 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [isLoadingTodos, setIsLoadingTodos] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('All');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const loadTodos = useCallback(async () => {
+    setIsLoadingTodos(true);
+    const data = await getTodos();
+
+    if (data.length !== 0) {
+      setTodos(data);
+      setIsLoadingTodos(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const handleSelect = (selectedTodoId: number) => {
+    const selected = todos.find(({ id }) => id === selectedTodoId);
+
+    if (selected) {
+      setSelectedTodo(selected);
+    }
+
+    setIsModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+    setSelectedTodo(null);
+  };
+
+  const handleOptionFilter = (option: string): Todo[] => {
+    switch (option) {
+      case 'All':
+        return todos;
+
+      case 'active':
+        return todos.filter(todo => !todo.completed);
+
+      case 'completed':
+        return todos.filter(todo => todo.completed);
+
+      default:
+        return todos;
+    }
+  };
+
+  const handleSearchFilter = (filteredTodos: Todo[], query: string) => {
+    return filteredTodos.filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+  };
+
+  const filteredOptionTodos = handleOptionFilter(selectedOption);
+  const visibleTodos = handleSearchFilter(filteredOptionTodos, searchQuery);
+
   return (
     <>
       <div className="section">
@@ -17,18 +78,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoadingTodos ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos}
+                  onSelect={handleSelect}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalOpen && (
+        <TodoModal
+          onClose={handleClose}
+          todo={selectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+  memo,
+  useCallback,
+} from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  onClose: () => void;
+  todo: Todo | null;
+};
+
+export const TodoModal: React.FC<Props> = memo(({
+  onClose,
+  todo,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const loadUser = useCallback(async () => {
+    if (todo) {
+      const userFromServer = await getUser(todo.userId);
+
+      setUser(userFromServer);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +45,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${todo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,12 +53,13 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={onClose}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
@@ -37,8 +68,8 @@ export const TodoModal: React.FC = () => {
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
               </a>
             </p>
           </div>
@@ -46,4 +77,4 @@ export const TodoModal: React.FC = () => {
       )}
     </div>
   );
-};
+});
