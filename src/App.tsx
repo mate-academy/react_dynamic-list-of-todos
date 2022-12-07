8928e232b52912f081c459cf38dcd21d431639f3
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-12-07 15:12:46 +0200

    Implement dynamic todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [visibleToDos, setVisibleToDos] = useState<Todo[]>([]);
+  const [select, setSelect] = useState('all');
+  const [query, setQuery] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  const isMatch = (str: string) => str.toLowerCase().includes(query.toLowerCase());
+
+  const getSelectedTodos = (todos: Todo[]) => {
+    const filteredTodos = todos.filter(({ completed, title }) => {
+      switch (select) {
+        case 'active':
+          return !completed && isMatch(title);
+
+        case 'completed':
+          return completed && isMatch(title);
+
+        default:
+          return isMatch(title);
+      }
+    });
+
+    setVisibleToDos(filteredTodos);
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(todos => getSelectedTodos(todos));
+  }, [select, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSelect={setSelect}
+                selectValue={select}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleToDos.length || query
+                ? (
+                  <TodoList
+                    todos={visibleToDos}
+                    chooseTodo={setSelectedTodo}
+                    selectedTodo={selectedTodo}
+                  />
+                )
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          deleteSelection={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,26 @@
-export const TodoFilter = () => (
+import React from 'react';
+
+type Props = {
+  onSelect: (value: string) => void;
+  selectValue: string;
+  setQuery: (value: string) => void;
+  query: string;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  onSelect,
+  selectValue,
+  setQuery,
+  query,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={selectValue}
+          onChange={e => onSelect(e.target.value)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,19 +34,24 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={e => setQuery(e.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
+      {query && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => setQuery('')}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
