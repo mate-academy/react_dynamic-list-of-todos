5ea3c8e9218f17d9c89e3404f9c06d4b01c874b7
Andrii Bassarab <andrijbassarab@gmail.com>
2022-12-08 22:17:56 +0200

    first part

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,23 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [visibleGoods, setVisibleGoods] = useState(todos);
+  const [userId, setUserId] = useState(0);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFrom => {
+        setTodos(todosFrom);
+        setVisibleGoods(todosFrom);
+      });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +32,53 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterGoods={(showGoods) => setVisibleGoods(showGoods)}
+                todos={todos}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleGoods.length > 0 && todos.length > 0
+                && (
+                  <TodoList
+                    todos={visibleGoods}
+                    selectedUserId={userId}
+                    selectUserId={(newUserId) => setUserId(newUserId)}
+                    selectedTodo={(todo) => setSelectedTodo(todo)}
+                  />
+                )}
+              {visibleGoods.length === 0 && todos.length === 0 && <Loader />}
+              {visibleGoods.length === 0 && todos.length > 0
+                && (
+                  <table className="table is-narrow is-fullwidth">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>
+                          <span className="icon">
+                            <i className="fas fa-check" />
+                          </span>
+                        </th>
+                        <th>Title</th>
+                        <th> </th>
+                      </tr>
+                    </thead>
+                  </table>
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {userId !== 0
+        && (
+          <TodoModal
+            userId={userId}
+            selectedTodo={selectedTodo}
+            closeTodoModal={() => setUserId(0)}
+          />
+        )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,57 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useState } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  filterGoods: (showGoods: Todo[]) => void,
+  todos: Todo[],
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({ filterGoods, todos }) => {
+  const [query, setQuery] = useState('');
+  const [selectedField, setSelectedFieldy] = useState('all');
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectedField}
+            onChange={(event) => setSelectedFieldy(event.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={(event) => {
+            setQuery(event.target.value);
+            filterGoods(todos.filter(todo => todo.title.toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())));
+          }}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+          />
+        </span>
+      </p>
+    </form>
+  );
+};
