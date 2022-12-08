c8372d3d11a49f16c2ea910042af0ac37ec4aadb
Illia Skladnik <scladnik@gmail.com>
2022-12-08 19:28:57 +0200

    dynamic list of todos implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+} from 'react';
+
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +12,60 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [allTodos, setAllTodos] = useState<Todo[]>([]);
+  const [activeTodo, setActiveTodo] = useState<Todo | null>(null);
+  const [status, setStatus] = useState('all');
+  const [searchInput, setSearchInput] = useState('');
+
+  const changeStatus = (newStatus: string) => {
+    if (newStatus !== status) {
+      setStatus(newStatus);
+    }
+  };
+
+  const changeInput = (newInput: string) => {
+    if (newInput !== searchInput) {
+      setSearchInput(newInput);
+    }
+  };
+
+  const visibleTodos = useMemo(() => {
+    return allTodos.filter((todo: Todo) => {
+      let statusFilter = true;
+      let inputFilter = true;
+
+      if (status === 'active') {
+        statusFilter = todo.completed === false;
+      }
+
+      if (status === 'completed') {
+        statusFilter = todo.completed === true;
+      }
+
+      if (searchInput !== '') {
+        inputFilter = (
+          todo.title.toLowerCase().includes(searchInput.toLowerCase().trim())
+        );
+      }
+
+      return inputFilter && statusFilter;
+    });
+  }, [status, searchInput, allTodos]);
+
+  const handleClose = useCallback(() => {
+    return setActiveTodo(null);
+  }, []);
+
+  useEffect(() => {
+    getTodos().then(todos => {
+      setAllTodos(todos);
+    });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +74,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                searchInput={searchInput}
+                status={status}
+                changeStatus={changeStatus}
+                changeInput={changeInput}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {allTodos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    setActiveTodo={setActiveTodo}
+                    activeTodo={activeTodo}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {activeTodo && (
+        <TodoModal
+          activeTodo={activeTodo}
+          onClose={handleClose}
+        />
+      )}
     </>
   );
 };
