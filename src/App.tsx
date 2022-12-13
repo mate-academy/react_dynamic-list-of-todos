44fc111141634870febf19c72bb60ad97728b4f6
Viktor Nikolaiev <referee1988@gmail.com>
2022-12-13 12:04:43 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,43 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { SortType } from './types/SortType';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [sortType, setSortType] = useState(SortType.all);
+  const [query, setQuery] = useState('');
+  const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+
+  useEffect(() => {
+    getTodos().then(loadedTodos => {
+      setTodos(loadedTodos);
+    });
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    return todos
+      .filter(todo => {
+        switch (sortType) {
+          case 'all':
+            return true;
+          case 'active':
+            return !todo.completed;
+          case 'completed':
+            return todo.completed;
+          default:
+            return true;
+        }
+      })
+      .filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+  }, [todos, sortType, query]);
+
+  const selectedTodo = useMemo(() => {
+    return visibleTodos.find(todo => todo.id === selectedTodoId) || null;
+  }, [todos, selectedTodoId]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +52,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                sortType={sortType}
+                onSelectSortType={setSortType}
+                query={query}
+                onChangeQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!todos.length
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodoId={selectedTodoId}
+                    onSelectedTodo={setSelectedTodoId}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          onCLose={setSelectedTodoId}
+          selectedTodo={selectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,66 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React from 'react';
+import { SortType } from '../../types/SortType';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  sortType: SortType,
+  onSelectSortType: (sortType: SortType) => void;
+  query: string;
+  onChangeQuery: (query: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
-    </p>
-  </form>
+export const TodoFilter: React.FC<Props> = React.memo(
+  (props) => {
+    const {
+      sortType,
+      onSelectSortType,
+      query,
+      onChangeQuery,
+    } = props;
+
+    return (
+      <form className="field has-addons">
+        <p className="control">
+          <span className="select">
+            <select
+              data-cy="statusSelect"
+              value={sortType}
+              onChange={
+                event => onSelectSortType(event.target.value as SortType)
+              }
+            >
+              <option value={SortType.all}>All</option>
+              <option value={SortType.active}>Active</option>
+              <option value={SortType.completed}>Completed</option>
+            </select>
+          </span>
+        </p>
+
+        <p className="control is-expanded has-icons-left has-icons-right">
+          <input
+            data-cy="searchInput"
+            type="text"
+            className="input"
+            placeholder="Search..."
+            value={query}
+            onChange={event => onChangeQuery(event.target.value)}
+          />
+          <span className="icon is-left">
+            <i className="fas fa-magnifying-glass" />
+          </span>
+
+          {!query.length || (
+            <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={() => onChangeQuery('')}
+              />
+            </span>
+          )}
+        </p>
+      </form>
+    );
+  },
 );
