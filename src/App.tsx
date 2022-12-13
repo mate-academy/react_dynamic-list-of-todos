0d1d3f0a7c8603ae6562f162be3d3e9a21f1e55f
Alex Tsimba <alexander.tsimba@gmail.com>
2022-12-13 10:22:34 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 /* eslint-disable max-len */
-import React from 'react';
+import {
+  useState,
+  useEffect,
+  useMemo,
+} from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { Todo } from './types/Todo';
+import { FilterOptions } from './types/FilterOptions';
+import { getTodos } from './api';
+import { Loader } from './components/Loader';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState<string>('');
+  const [filterOption, setFilterOption] = useState<string>(FilterOptions.all);
+
+  useEffect(() => {
+    getTodos().then(setTodosFromServer);
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    return todosFromServer.filter(todo => {
+      const matchedWithSearch = todo.title.toLocaleLowerCase()
+        .includes(query.toLocaleLowerCase());
+
+      switch (filterOption) {
+        case FilterOptions.active:
+          return !todo.completed && matchedWithSearch;
+        case FilterOptions.completed:
+          return todo.completed && matchedWithSearch;
+        case FilterOptions.all:
+        default:
+          return matchedWithSearch;
+      }
+    });
+  }, [filterOption, query, todosFromServer]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +50,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                onQueryChange={setQuery}
+                selectedTodos={filterOption}
+                onSelectedTodosChange={setFilterOption}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todosFromServer.length > 0
+                ? (
+                  <TodoList
+                    todos={visibleTodos}
+                    currentTodo={currentTodo}
+                    onTodoClick={setCurrentTodo}
+                  />
+                )
+                : <Loader />}
+
             </div>
           </div>
         </div>
       </div>
+      {currentTodo && <TodoModal todo={currentTodo} onClose={setCurrentTodo} />}
 
-      <TodoModal />
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,66 @@
-import React from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+interface Props {
+  todo: Todo,
+  onClose: React.Dispatch<React.SetStateAction<Todo | null>>,
+}
+
+export const TodoModal: FC<Props> = ({ todo, onClose }) => {
+  const [userDetails, setUserDetails] = useState<User>();
+
+  useEffect(() => {
+    getUser(todo.userId)
+      .then(setUserDetails);
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+      {userDetails
+        ? (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${todo.id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => onClose(null)}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {todo.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                <strong className="has-text-danger">
+                  {todo.completed ? 'Done' : 'Planned'}
+                </strong>
+
+                {' by '}
+
+                <a href={userDetails.email}>
+                  {userDetails.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )
+        : <Loader />}
     </div>
   );
 };
