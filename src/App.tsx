4a211488be57a1ace9a56f9fe2b172afc64771df
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-12-24 00:22:03 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,48 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [targetRowId, settargetRowId] = useState(0);
+  const [selectedOption, setSelectedOption] = useState('all');
+  const [inputValue, setInputValue] = useState('');
+  const [targetUserId, setTargetUserId] = useState(0);
+
+  useEffect(() => {
+    getTodos().then(items => setTodosFromServer(items));
+  }, []);
+
+  let listOfTodos = [...todosFromServer];
+
+  function filterBy(value: string, subStr: string) {
+    switch (value) {
+      case 'active':
+        listOfTodos = [...todosFromServer]
+          .filter(todo => !todo.completed)
+          .filter(item => item.title.includes(subStr.toLowerCase()));
+        break;
+
+      case 'completed':
+        listOfTodos = [...todosFromServer]
+          .filter(todo => todo.completed)
+          .filter(item => item.title.includes(subStr.toLowerCase()));
+        break;
+
+      default:
+        listOfTodos = [...todosFromServer].filter(
+          item => item.title.includes(subStr.toLowerCase()),
+        );
+        break;
+    }
+  }
+
+  filterBy(selectedOption, inputValue);
+
+  const targetTodo = listOfTodos.find(todo => todo.id === targetRowId);
+
   return (
     <>
       <div className="section">
@@ -17,18 +57,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setSelectedOption={setSelectedOption}
+                setInputValue={setInputValue}
+                inputValue={inputValue}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!listOfTodos.length && <Loader />}
+              {listOfTodos.length && (
+                <TodoList
+                  todos={listOfTodos}
+                  targetRowId={targetRowId}
+                  settargetRowId={settargetRowId}
+                  setTargetUserId={setTargetUserId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {targetRowId !== 0 && (
+        <TodoModal
+          targetUserId={targetUserId}
+          setTargetUserId={setTargetUserId}
+          settargetRowId={settargetRowId}
+          targetTodo={targetTodo}
+        />
+      )}
     </>
   );
 };
