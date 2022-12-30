98f89a7c0887276358931d6aaacd6aaac2e326b1
Demut <uddarendeli1@gmail.com>
2022-12-30 04:37:06 +0300

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,63 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+type Props = {
+  query: string;
+  filterType: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  setFilterType: React.Dispatch<React.SetStateAction<string>>;
+};
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+export const TodoFilter = ({
+  query,
+  filterType,
+  setQuery,
+  setFilterType,
+}: Props) => {
+  const clearQuery = () => setQuery('');
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={filterType}
+            onChange={(e => setFilterType(e.target.value))}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder="Search..."
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span
+          className="icon is-right"
+          style={{ pointerEvents: 'all' }}
+        >
+          {query
+            && (
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={clearQuery}
+                aria-label="Clear"
+              />
+            )}
+        </span>
+      </p>
+    </form>
+  );
+};
