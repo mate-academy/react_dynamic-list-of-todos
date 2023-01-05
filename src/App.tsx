a7f832f438d6f40a6309ccc3e374fa778fd41f29
John Rust <johnrust12@gmail.com>
2023-01-05 14:06:10 +0200

    Solution 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,39 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState('');
+  const [option, setOption] = useState('all');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [checkedTodo, setCheckedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then((data) => setTodos(data))
+      .then(() => setLoading(false));
+  }, [todos]);
+
+  const filteredTodos = () => {
+    const queryTodo = todos.filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+
+    return queryTodo.filter(todo => {
+      switch (option) {
+        case 'active':
+          return !todo.completed;
+
+        case 'completed':
+          return todo.completed;
+
+        default:
+          return todo;
+      }
+    });
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                option={option}
+                setOption={setOption}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && (<Loader />)}
+              <TodoList
+                todos={filteredTodos()}
+                checkedTodo={checkedTodo}
+                onCheckedTodo={setCheckedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {checkedTodo && (
+        <TodoModal
+          todo={checkedTodo}
+          onClose={() => setCheckedTodo(null)}
+        />
+      )}
     </>
   );
 };
