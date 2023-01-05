5dded86102c6afddcd8759ed160731d43296ae66
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-01-05 18:58:23 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
+import { Condition } from './types/Condition';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [filterType, setFilterType] = useState('');
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  const getApiTodos = async () => {
+    const data = await getTodos();
+
+    setTodos(data);
+    setDataLoaded(true);
+  };
+
+  useEffect(() => {
+    getApiTodos();
+  });
+
+  const normalizeTodosWithQuery = todos.filter(({ title }) => {
+    return title.toLowerCase().includes(query.toLowerCase());
+  });
+
+  const filteredTodos = normalizeTodosWithQuery.filter(todo => {
+    switch (filterType) {
+      case Condition.active:
+        return todo.completed === false;
+      case Condition.completed:
+        return todo.completed === true;
+      default:
+        return Condition.all;
+    }
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +51,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                setFilterType={setFilterType}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!dataLoaded && <Loader />}
+              <TodoList
+                todos={filteredTodos}
+                setSelectedTodoId={setSelectedTodoId}
+                selectedTodoId={selectedTodoId}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodoId
+        && (
+          <TodoModal
+            todos={filteredTodos}
+            setSelectedTodoId={setSelectedTodoId}
+            selectedTodoId={selectedTodoId}
+          />
+        )}
     </>
   );
 };
---
+++ src/types/Condition.ts
@@ -0,0 +1,5 @@
+export enum Condition {
+  all = 'all',
+  active = 'active',
+  completed = 'completed',
+}
