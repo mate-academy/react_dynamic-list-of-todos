dd6aa291de47c999a66c9124d966da47defeeae0
Kamil Witek <kamilprogramuje@gmail.com>
2023-01-06 15:50:12 +0100

    removed return keyword

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -19,52 +19,50 @@ export const TodoFilter: FC<Props> = memo(
     handleChangeSearchInput,
     handleRemoveSearchInput,
     onStatusChange,
-  }) => {
-    return (
-      <form className="field has-addons">
-        <p className="control">
-          <span className="select">
-            <select
-              data-cy="statusSelect"
-              value={selectedStatus}
-              onChange={onStatusChange}
-            >
-              <option value="all">All</option>
-              <option value="active">Active</option>
-              <option value="completed">Completed</option>
-            </select>
-          </span>
-        </p>
+  }) => (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectedStatus}
+            onChange={onStatusChange}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
 
-        <p className="control is-expanded has-icons-left has-icons-right">
-          <input
-            data-cy="searchInput"
-            type="text"
-            className="input"
-            placeholder="Search..."
-            value={searchInput}
-            onChange={handleChangeSearchInput}
-          />
-          <span className="icon is-left">
-            <i className="fas fa-magnifying-glass" />
-          </span>
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchInput}
+          onChange={handleChangeSearchInput}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
 
-          {
-            searchInput
-            && (
-              <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                <button
-                  data-cy="clearSearchButton"
-                  type="button"
-                  className="delete"
-                  onClick={handleRemoveSearchInput}
-                />
-              </span>
-            )
-          }
-        </p>
-      </form>
-    );
-  },
+        {
+          searchInput
+          && (
+            <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={handleRemoveSearchInput}
+              />
+            </span>
+          )
+        }
+      </p>
+    </form>
+  ),
 );
