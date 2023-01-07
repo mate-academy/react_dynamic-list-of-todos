0e4ae20051bfcb15c5dff17db67844942e16de43
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-01-07 11:52:09 +0200

    solution update 5

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -37,6 +37,7 @@ export const TodoFilter: React.FC<Props> = ({
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
+
       {query && (
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
           <button
