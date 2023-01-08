dd7ac835346ca8becdf8306bd5da02bf1b99b632
Anastasiia Horidieieva <gordeevaisgreat@gmail.com>
2023-01-08 22:36:51 +0200

    forgot to add a small detail

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -64,7 +64,7 @@ export const TodoModal: React.FC<Props> = ({ selectedTodo, setTodoId }) => {
 
               {' by '}
 
-              <a href={`mailto:${user.email}`}>
+              <a href={`mailto:${user?.email}`}>
                 {user?.name}
               </a>
             </p>
