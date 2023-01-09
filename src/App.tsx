45cf997cfd8af27a30676c7c1aae0c9f8fbacff3
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2023-01-09 13:25:49 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,68 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+
+import { Todo } from './types/Todo';
+import { MainFilter } from './types/MainFilter';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterPosition, setFilterPosition] = useState('');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    async function todosFetch() {
+      const data = await getTodos();
+
+      setTodos(data);
+      setIsLoaded(true);
+    }
+
+    todosFetch();
+  }, []);
+
+  const selectTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const closeTodo = () => {
+    setSelectedTodo(null);
+  };
+
+  const seacrhTodos = (position: string, searchQuery: string) => {
+    switch (position) {
+      case MainFilter.active:
+        return todos.filter(todo => !todo.completed
+           && todo.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
+      case MainFilter.completed:
+        return todos.filter(todo => todo.completed
+          && todo.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
+      default:
+        if (searchQuery.length > 0) {
+          return todos.filter(
+            todo => todo.title.toLowerCase().includes(searchQuery.toLowerCase()),
+          );
+        }
+
+        return todos;
+    }
+  };
+
+  const newQuery = (searchQuery: string) => {
+    setQuery(searchQuery);
+  };
+
+  const emptyQuery = () => {
+    setQuery('');
+  };
+
+  const filteredTodos = seacrhTodos(filterPosition, query);
+
   return (
     <>
       <div className="section">
@@ -17,18 +77,29 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSelect={setFilterPosition}
+                onQuery={newQuery}
+                onClear={emptyQuery}
+                query={query}
+
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!isLoaded ? <Loader /> : (
+                <TodoList
+                  todos={filteredTodos}
+                  onSelect={selectTodo}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && <TodoModal todo={selectedTodo} onClose={closeTodo} />}
     </>
   );
 };
---
+++ src/types/MainFilter.ts
@@ -0,0 +1,5 @@
+export enum MainFilter {
+  all = '',
+  active = 'active',
+  completed = 'completed',
+}
