d5bb8c88d2ae9a38c0c1f0724445fe361301777b
Lefteris Loizides <loizides.lef@gmail.com>
2023-01-11 19:10:20 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -61,16 +61,16 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              {todos.length > 0
-                ? (
+              {todos.length === 0
+                ? <Loader />
+                : (
                   <TodoList
                     todos={visibleTodos}
                     selectedTodo={todo}
                     setTodo={(newTodo: Todo) => setTodo(newTodo)}
                     setUserId={(id: number) => setUserId(id)}
                   />
-                )
-                : <Loader />}
+                )}
             </div>
           </div>
         </div>
