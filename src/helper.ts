a98a3749adb849ce2e1a490cf21a47ba58827b81
mr-valentos <nerpavlad@gmail.com>
2023-01-15 18:47:06 +0200

    dynamic list of TODOs

---
+++ src/helper.ts
@@ -0,0 +1,28 @@
+import { Todo } from './types/Todo';
+
+export const getVisibleTodos = (
+  todos: Todo[],
+  query: string,
+  filter: string,
+) => {
+  return todos.filter(todo => {
+    const isIncludeQuery = todo.title.toLowerCase().includes(query);
+    let isFiltered;
+
+    switch (filter) {
+      case 'active':
+        isFiltered = todo.completed === false;
+        break;
+
+      case 'completed':
+        isFiltered = todo.completed === true;
+        break;
+
+      default:
+        isFiltered = true;
+        break;
+    }
+
+    return isIncludeQuery && isFiltered;
+  });
+};
