8c1653ae8350be1520cda347a5c4a63e19d67bf1
Kolya Movchan <kmovchan261@gmail.com>
2023-01-18 21:58:39 +0200

    react_dynamic-list-of-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,76 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { getTodos, getUser } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState('');
+  const [filter, setFilter] = useState('all');
+  const [isLoading, setisLoading] = useState(true);
+  const [activeTodoId, setActiveTodoId] = useState(0);
+
+  const initialUser: User = {
+    id: 0,
+    name: '',
+    email: '',
+    phone: '',
+  };
+
+  const [userDetails, setUserDetails] = useState(initialUser);
+
+  const filterTodos = () => {
+    return (todos.filter(todo => {
+      const title = todo.title.toLowerCase();
+      const input = inputValue.toLowerCase();
+      const queryMatch = title.includes(input);
+
+      switch (filter) {
+        case 'active':
+          return !todo.completed && queryMatch;
+
+        case 'completed':
+          return todo.completed && queryMatch;
+
+        default:
+          return queryMatch;
+      }
+    }));
+  };
+
+  const handleClick = (id: number, userId: number) => {
+    setActiveTodoId(id);
+
+    getUser(userId).then(user => {
+      setUserDetails({
+        ...user,
+        id,
+      });
+    });
+  };
+
+  const closeUserCard = () => {
+    setUserDetails(initialUser);
+    setActiveTodoId(0);
+  };
+
+  const visibleTodos = filterTodos();
+
+  const activeTodo = todos.filter(todo => todo.id === activeTodoId)[0];
+
+  async function loadDataFromServer() {
+    const todosFromServer = await getTodos();
+
+    setTodos(todosFromServer);
+    setisLoading(false);
+  }
+
+  useEffect(() => {
+    loadDataFromServer();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +84,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSelectChange={setFilter}
+                filter={filter}
+                onChange={setInputValue}
+                inputValue={inputValue}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!todos.length && isLoading
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    activeTodoId={activeTodoId}
+                    onClick={handleClick}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {activeTodoId > 0 && (
+        <TodoModal
+          activeTodo={activeTodo}
+          userDetails={userDetails}
+          onClose={closeUserCard}
+        />
+      )}
     </>
   );
 };
