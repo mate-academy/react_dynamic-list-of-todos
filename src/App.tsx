5a086cc83c7bb1f5195954261bc5e13c293f798f
dant0n <dmibuc@gmail.com>
2023-01-19 11:27:02 +0200

    Finish hw.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,64 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { Loader } from './components/Loader';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo>();
+  const [filterString, setFilterString] = useState('');
+  const [filterState, setFilterState] = useState('');
+
+  useEffect(() => {
+    let cancel = false;
+
+    async function fetchTodos() {
+      setLoading(true);
+      const fetchedTodos = await getTodos();
+
+      if (!cancel) {
+        setTodos(fetchedTodos);
+      }
+
+      setLoading(false);
+    }
+
+    fetchTodos();
+
+    return () => {
+      cancel = true;
+    };
+  }, []);
+
+  const filterStatePredicate = (todo: Todo) => {
+    switch (filterState) {
+      case 'active':
+        return !todo.completed;
+      case 'completed':
+        return todo.completed;
+      default:
+        return true;
+    }
+  };
+
+  const filterTodos = (todoList: Todo[]) => todoList
+    .filter(todo => todo.title.toLowerCase().includes(filterString.toLowerCase()))
+    .filter(filterStatePredicate);
+
+  const renderedTodos = useMemo(() => filterTodos(todos), [filterString, filterState, todos]);
+
+  const handleModalClose = () => {
+    setSelectedTodo(undefined);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +67,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onInputUpdate={setFilterString}
+                onInputClear={() => setFilterString('')}
+                onSelect={setFilterState}
+                input={filterString}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={renderedTodos}
+                  selectedTodo={selectedTodo}
+                  onShowClick={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          onModalClose={handleModalClose}
+        />
+      )}
     </>
   );
 };
