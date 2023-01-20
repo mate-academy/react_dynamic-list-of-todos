93ec1d3730edb356234ffe4cad6e2c543fbe5347
Nadiia Koch <nadia.dovganenko@gmail.com>
2023-01-20 12:36:54 +0200

    React dynamic list of TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -10,19 +10,28 @@ import { Loader } from './components/Loader';
 
 import { getTodos } from './api';
 import { Todo } from './types/Todo';
-
-// console.log('todos', getTodos());
-// console.log('user', getUser(1));
+import { getFilteredTodos } from './getFilteredTodos';
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [selectTodoId, setSelectedTodoId] = useState(0);
-  const [isLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedFilter, setSelectedFilter] = useState('all');
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     getTodos()
-      .then(todosFromServer => setTodos(todosFromServer));
+      .then(todosFromServer => setTodos(todosFromServer))
+      .then(() => setIsLoading(false));
   }, []);
+
+  const selectedTodo = useMemo(() => {
+    return todos.find(todo => todo.id === selectTodoId) || null;
+  }, [selectTodoId]);
+
+  const visibleTodos = useMemo(() => (
+    getFilteredTodos(todos, selectedFilter, searchQuery)
+  ), [todos, selectedFilter, searchQuery]);
 
   return (
     <>
@@ -32,25 +41,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selectedFilter={selectedFilter}
+                searchQuery={searchQuery}
+                onSelectedFilter={(filterType) => setSelectedFilter(filterType)}
+                onSearchQuery={(query) => setSearchQuery(query)}
+              />
             </div>
 
             <div className="block">
               {isLoading && <Loader />}
+
               {todos.length > 0
-               && (
-                 <TodoList
-                   todos={todos}
-                   onSelectTodo={(todoId) => setSelectedTodoId(todoId)}
-                   selectTodoId={selectTodoId}
-                 />
-               )}
+                && (
+                  <TodoList
+                    todos={visibleTodos}
+                    onSelectTodo={(todoId) => setSelectedTodoId(todoId)}
+                    selectTodoId={selectTodoId}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      {selectTodoId !== 0 && <TodoModal />}
+      {selectTodoId !== 0
+        && (
+          <TodoModal
+            todo={selectedTodo}
+            onClose={() => setSelectedTodoId(0)}
+          />
+        )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,28 @@
-export const TodoFilter = () => (
+import React, { memo } from 'react';
+
+type Props = {
+  selectedFilter: string
+  searchQuery: string,
+  onSelectedFilter: (selectedFilter: string) => void,
+  onSearchQuery: (searchQuery: string) => void,
+};
+
+export const TodoFilter: React.FC<Props> = memo(({
+  selectedFilter,
+  searchQuery,
+  onSelectedFilter,
+  onSearchQuery,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={selectedFilter}
+          onChange={(event) => {
+            onSelectedFilter(event.target.value);
+          }}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,19 +36,27 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={searchQuery}
+        onChange={(event) => {
+          onSearchQuery(event.target.value);
+        }}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
+      {searchQuery
+        && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => onSearchQuery('')}
+            />
+          </span>
+        )}
     </p>
   </form>
-);
+));
