5ad0372a3bbfa02933096012bd579a7ca1adc992
Olha Mamai <olgga.ju@gmail.com>
2023-01-22 22:03:52 +0200

    todos list created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+// import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todosList, setTodosList] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  getTodos()
+    .then(data => (setTodosList(() => data)))
+    .then(() => setLoading(false));
+
   return (
     <>
       <div className="section">
@@ -21,14 +30,18 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && (
+                <Loader />
+              )}
+              {todosList.length && (
+                <TodoList todos={todosList} />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
   );
 };
