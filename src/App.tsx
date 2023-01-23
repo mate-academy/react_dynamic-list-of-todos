333451136401c884544931765cd5dba930ae789f
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2023-01-23 11:48:47 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+//  useCallback,
+} from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +11,41 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isTodosLoading, setIsTodosLoading] = useState(false);
+  const [isTodoError, setIsTodoError] = useState(false);
+  const [query, setQuery] = useState('');
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+
+  useEffect(() => {
+    setIsTodosLoading(true);
+
+    getTodos()
+      .then((loadedTodos) => setTodos(loadedTodos))
+      .catch(() => setIsTodoError(true))
+      .finally(() => setIsTodosLoading(false));
+  }, []);
+
+  //  const unselectedUser = useCallback(() => {
+  //    setSelectedTodoId(0);
+  //  }, []);
+
+  const visibleTodos = useMemo(() => {
+    return todos.filter((todo) => (
+      todo.title.toLowerCase().includes(query.toLowerCase())
+    ));
+  }, [query, todos]);
+
+  // const selectedTodo = useMemo(() => {
+  //   return todos.find(todo => todo.id === selectedTodoId);
+  // }, [selectedTodoId, todos]);
+
+  const isNoFiltersResult = query && !visibleTodos.length;
+
   return (
     <>
       <div className="section">
@@ -17,12 +54,30 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isTodosLoading
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodoId={selectedTodoId}
+                    onSelect={setSelectedTodoId}
+                  />
+                )}
+
+              {isTodoError && (
+                <p>Something went wrong</p>
+              )}
+
+              {isNoFiltersResult && (
+                <p>No totd matched your filters</p>
+              )}
             </div>
           </div>
         </div>
