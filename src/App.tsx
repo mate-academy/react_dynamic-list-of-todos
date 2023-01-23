5a0d2aa9675e32c737ed24725a2ead8c640aadf0
Olena Yanovska <alena.yn.adw@gmail.com>
2023-01-23 02:49:01 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+  const [query, setQuery] = useState('');
+  const [searchBy, setSearchBy] = useState('all');
+
+  useEffect(() => {
+    getTodos()
+      .then((loadedTodos) => setTodos(loadedTodos));
+  }, []);
+
+  const selectTodoId = (todoId: number) => {
+    setSelectedTodoId(todoId);
+  };
+
+  const visibleTodos = todos.filter(todo => {
+    const normalizedTitle = todo.title.toLowerCase();
+    const normalizedQuery = query.toLowerCase();
+    const isTitleIncludesQuery = normalizedTitle.includes(normalizedQuery);
+
+    switch (searchBy) {
+      case 'completed':
+        return todo.completed && isTitleIncludesQuery;
+
+      case 'active':
+        return !todo.completed && isTitleIncludesQuery;
+
+      case 'all':
+        return isTitleIncludesQuery;
+
+      default:
+        return isTitleIncludesQuery;
+    }
+  });
+
+  const selectedTodo = visibleTodos.find(
+    todo => todo.id === selectedTodoId,
+  );
+
+  const closeTodoModal = () => {
+    setSelectedTodoId(0);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                searchBy={searchBy}
+                setSearchBy={setSearchBy}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleTodos.length > 0
+                ? (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodoId={selectedTodoId}
+                    selectTodoId={selectTodoId}
+                  />
+                )
+                : (<Loader />)}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          closeTodoModal={closeTodoModal}
+        />
+      )}
     </>
   );
 };
