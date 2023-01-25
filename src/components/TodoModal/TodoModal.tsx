5234a21f4d63863ca42376a04a6ac052f6bed1ca
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-01-26 00:38:46 +0200

    Comments were taken into account

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -12,50 +12,44 @@ type Props = {
 
 export const TodoModal: FC<Props> = ({ selectedTodo, onClose }) => {
   const [user, setUser] = useState<User | null>(null);
-  const [isUserLoading, setIsUserLoading] = useState(false);
-  const [isUserLoadingError, setIsUserLoadingError] = useState(false);
 
   useEffect(() => {
-    setIsUserLoading(true);
-
-    getUser(selectedTodo.id)
+    getUser(selectedTodo.userId)
       .then((loadedUser) => setUser(loadedUser))
-      .catch(() => setIsUserLoadingError(true))
-      .finally(() => setIsUserLoading(false));
-  }, [selectedTodo.id]);
+      .catch(() => setUser(null));
+  }, [selectedTodo.userId]);
 
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            {`Todo #${selectedTodo.id}`}
-          </div>
+      {!user
+        ? <Loader />
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${selectedTodo.id}`}
+              </div>
 
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            type="button"
-            className="delete"
-            data-cy="modal-close"
-            onClick={onClose}
-          />
-        </header>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={onClose}
+              />
+            </header>
 
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            {selectedTodo.title}
-          </p>
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {selectedTodo.title}
+              </p>
 
-          {isUserLoading
-            ? <Loader />
-            : (
               <p className="block" data-cy="modal-user">
-                {/* <strong className="has-text-success">Done</strong> */}
                 {selectedTodo.completed
                   ? <strong className="has-text-danger">Done</strong>
                   : <strong className="has-text-danger">Planned</strong>}
@@ -66,13 +60,9 @@ export const TodoModal: FC<Props> = ({ selectedTodo, onClose }) => {
                   {user?.name}
                 </a>
               </p>
-            )}
-
-          {isUserLoadingError && (
-            <p>Error occured while loading user</p>
-          )}
-        </div>
-      </div>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
