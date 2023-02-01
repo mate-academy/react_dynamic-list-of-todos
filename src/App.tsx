a37ff3bfdf9c14eced16d25d3b26f725e7fe0b6d
Elias Montauti <egsm147@gmail.com>
2023-02-01 10:28:36 +0000

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,43 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo>();
+  const [isSelected, setIsSelected] = useState(false);
+  const [filter, setSelectedFilter] = useState('all');
+
+  const loadingAllTodos = async () => {
+    const loadedTodos = await getTodos();
+
+    const completedTodos = loadedTodos.filter((todo) => todo.completed);
+
+    const activeTodos = loadedTodos.filter((todo) => !todo.completed);
+
+    switch (filter) {
+      case 'all':
+        return setTodos(loadedTodos);
+      case 'active':
+        return setTodos(activeTodos);
+      case 'completed':
+        return setTodos(completedTodos);
+
+      default:
+        return 0;
+    }
+  };
+
+  useEffect(() => {
+    loadingAllTodos();
+  }, [filter]);
+
+  useEffect(() => {
+    setSelectedTodo(selectedTodo);
+  }, [selectedTodo]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +52,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSetSelectedFilter={setSelectedFilter}
+                filter={filter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? (
+                  <Loader />
+                )
+                : (
+                  <TodoList
+                    todos={todos}
+                    onSetSelectedTodo={setSelectedTodo}
+                    onSetIsSelected={setIsSelected}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && isSelected && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          onSetIsSelected={setIsSelected}
+        />
+      )}
     </>
   );
 };
