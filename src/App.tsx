b23a9fa7ad5a142734ed994d59735ed0a603d6ef
Rafael Mamedov <rafmamedov@gmail.com>
2023-02-17 01:56:57 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,67 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isTodosFetching, setIsTodosFetching] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [isUserFetching, setIsUserFetching] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  const searchTodo = (todo: Todo) => {
+    const lowerCasedQuery = searchQuery.toLowerCase().trim();
+    const lowerCasedTitle = todo.title.toLowerCase();
+
+    return lowerCasedTitle.includes(lowerCasedQuery);
+  };
+
+  const visibleTodos = todos
+    .filter(searchTodo)
+    .filter(todo => {
+      switch (filter) {
+        case 'completed':
+          return todo.completed;
+
+        case 'active':
+          return !todo.completed;
+
+        default:
+          return true;
+      }
+    });
+
+  const handleGetUser = async (todo: Todo) => {
+    setIsUserFetching(true);
+    const selectedUser = await getUser(todo.userId);
+
+    setUser(selectedUser);
+    setIsUserFetching(false);
+  };
+
+  const handleShowButton = (todo: Todo) => {
+    setSelectedTodo(todo);
+    handleGetUser(todo);
+    setIsModalVisible(true);
+  };
+
+  const handleGetTodos = async () => {
+    const todosList = await getTodos();
+
+    setTodos(todosList);
+    setIsTodosFetching(false);
+  };
+
+  useEffect(() => {
+    handleGetTodos();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +76,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={searchQuery}
+                handleSearch={setSearchQuery}
+                handleFilter={setFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isTodosFetching
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    handleShowButton={handleShowButton}
+                    selectedTodo={selectedTodo}
+                    isModalVisible={isModalVisible}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {isModalVisible && (
+        <TodoModal
+          todo={selectedTodo}
+          user={user}
+          isVisible={isModalVisible}
+          isFetching={isUserFetching}
+          setIsModalVisible={setIsModalVisible}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo | null;
+  user: User | null;
+  isVisible: boolean;
+  isFetching: boolean;
+  setIsModalVisible: (isVisibe: boolean) => void;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  todo,
+  user,
+  isVisible,
+  isFetching,
+  setIsModalVisible,
+}) => {
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div
+      className={classNames('modal', {
+        'is-active': isVisible,
+      })}
+      data-cy="modal"
+    >
       <div className="modal-background" />
 
-      {true ? (
+      {isFetching ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +37,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${todo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +45,24 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => setIsModalVisible(false)}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {todo?.completed
+                ? <strong className="has-text-success">Done</strong>
+                : <strong className="has-text-danger">Planned</strong>}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto: ${user?.email}`}>
+                {user?.name}
               </a>
             </p>
           </div>
