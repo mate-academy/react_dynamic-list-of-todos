013b0c397a1a86885d9cc8e060313832ef42bdd0
svitjojo <kornotavr@gmail.com>
2023-02-16 21:22:47 +0200

    Dynamic list of todos was added

---
+++ src/types/SelectValue.ts
@@ -0,0 +1,5 @@
+export const enum SelectValue {
+  ALL = 'all',
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+}
---
+++ src/utils/getVisibleTodos.ts
@@ -0,0 +1,30 @@
+import { SelectValue } from '../types/SelectValue';
+import { Todo } from '../types/Todo';
+
+export function getVisibleTodos(
+  todos: Todo[],
+  select: string,
+  query: string,
+): Todo[] {
+  let visibleTodos = [...todos];
+
+  switch (select) {
+    case SelectValue.ACTIVE:
+      visibleTodos = visibleTodos.filter(todo => !todo.completed);
+      break;
+    case SelectValue.COMPLETED:
+      visibleTodos = visibleTodos.filter(todo => todo.completed);
+      break;
+    case SelectValue.ALL:
+    default:
+      visibleTodos = [...todos];
+  }
+
+  if (query) {
+    visibleTodos = visibleTodos.filter(
+      ({ title }) => title.toLowerCase().includes(query.toLowerCase()),
+    );
+  }
+
+  return visibleTodos;
+}
