c9117dd1182155bf9471f4caaa2cc320b3f99599
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-02-17 00:45:17 +0200

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +8,30 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { FilterBy, filterTodo } from './utils/filterTodos';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterBy, setFilterBy] = useState<FilterBy>(FilterBy.All);
+  const [query, setQuery] = useState('');
+
+  const selectTodo = (todo: Todo | null) => {
+    setSelectedTodo(todo);
+  };
+
+  const visibleTodo = filterTodo(todos, filterBy, query);
+
+  useEffect(() => {
+    getTodos().then(result => {
+      setTodos(result);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +40,20 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter query={query} filterBy={filterBy} setFilterBy={setFilterBy} setQuery={setQuery} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {
+                isLoading
+                  ? <Loader />
+                  : <TodoList todos={visibleTodo} selectTodo={selectTodo} selectedTodo={selectedTodo} />
+              }
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && <TodoModal todo={selectedTodo} selectTodo={selectTodo} />}
     </>
   );
 };
