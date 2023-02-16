abadd95bcb4041359931cc32ae4c056d9d8e1399
OlehSharha <oleh.3sharga@gmail.com>
2023-02-16 14:47:09 +0200

    fix user email

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -65,7 +65,7 @@ export const TodoModal: React.FC<Props> = ({ selectTodo, todo }) => {
 
                 {' by '}
 
-                <a href="mailto:Sincere@april.biz">
+                <a href={`mailto:${user.email}`}>
                   {user.name}
                 </a>
               </p>
