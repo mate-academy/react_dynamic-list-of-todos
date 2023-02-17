9d87e89a814e15fe45c99fca7dfb131b1d76ea91
Ihor Mariash <imariash86@gmail.com>
2023-02-17 16:32:49 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,57 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterBy, setFilterBy] = useState('all');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getTodos();
+
+      setTodos(data);
+    };
+
+    fetchData();
+  }, []);
+
+  const openModal = useCallback((todo: Todo | null) => {
+    setSelectedTodo(todo);
+  }, []);
+
+  const filterTodos = useCallback((selection: string) => {
+    setFilterBy(selection);
+  }, []);
+
+  const changeQuery = useCallback((value: string) => {
+    setQuery(value);
+  }, []);
+
+  const resetQuery = useCallback(() => {
+    setQuery('');
+  }, []);
+
+  let visibleTodos = todos.filter(todo => {
+    switch (filterBy) {
+      case 'all':
+      default:
+        return [];
+      case 'active':
+        return todo.completed === false;
+      case 'completed':
+        return todo.completed === true;
+    }
+  });
+
+  if (query) {
+    visibleTodos = [...visibleTodos].filter(todo => todo.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()));
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +66,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterTodos={filterTodos}
+                query={query}
+                changeQuery={changeQuery}
+                resetQuery={resetQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? (
+                  <Loader />
+                )
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    openModal={openModal}
+                    selectedTodo={selectedTodo}
+                  />
+                )}
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && <TodoModal selectedTodo={selectedTodo} openModal={openModal} />}
+
     </>
   );
 };
