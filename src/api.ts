2a822333a015a8679cc3bc8e19609ee1930a5908
Yurii Demkiv <demkiv123@gmail.com>
2023-02-17 13:07:03 +0200

    Initial commit

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import { User } from './types/User';
 // eslint-disable-next-line max-len
 const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
 
-// This function creates a promime
+// This function creates a promise
 // that is resolved after a given delay
 function wait(delay: number): Promise<void> {
   return new Promise(resolve => {
