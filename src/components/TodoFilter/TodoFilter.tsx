47d32ec32cce3a8f50b91bcb2f6832197e777008
Artem_Sobolevskiy <artemsobolevskuy@gmail.com>
2023-02-20 03:27:29 +0200

    solution was added

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,69 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { FilterBy } from '../../types/FilterBy';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  setFilterBy: (filter:FilterBy) => void;
+  setQuery: (inputValue: string) => void;
+  query: string;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = (
+  {
+    setFilterBy,
+    setQuery,
+    query,
+  },
+) => {
+  const handleClick = () => {
+    setQuery('');
+  };
+
+  const handleOptionClick = (inputValue:FilterBy) => {
+    setFilterBy(inputValue);
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={
+              (event) => handleOptionClick(event.target.value as FilterBy)
+            }
+          >
+            <option value={FilterBy.ALL}>All</option>
+            <option value={FilterBy.ACTIVE}>Active</option>
+            <option value={FilterBy.COMPLETED}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          { query
+            && (
+              /* eslint-disable-next-line jsx-a11y/control-has-associated-label */
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={handleClick}
+              />
+            )}
+        </span>
+      </p>
+    </form>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  changeModalState: () => void;
+  selectedTodo: Todo;
+};
+
+export const TodoModal: React.FC<Props> = (
+  {
+    changeModalState,
+    selectedTodo,
+  },
+) => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  const handleClick = () => {
+    changeModalState();
+  };
+
+  const getSelectedUser = async () => {
+    const user = await getUser(selectedTodo.userId);
+
+    setSelectedUser(user);
+    setIsLoaded(true);
+  };
+
+  useEffect(() => {
+    getSelectedUser();
+  }, []);
+
+  const {
+    id,
+    title,
+    completed,
+  } = selectedTodo;
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
-
-      {true ? (
+      {!isLoaded ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +51,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,24 +59,30 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={handleClick}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {title}
             </p>
-
+            {selectedUser
+          && (
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {
+                completed
+                  ? <strong className="has-text-success">Done</strong>
+                  : <strong className="has-text-danger">Planned</strong>
+              }
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${selectedUser.email}`}>
+                {selectedUser.name}
               </a>
             </p>
+          )}
           </div>
         </div>
       )}
