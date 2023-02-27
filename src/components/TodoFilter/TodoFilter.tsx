9d1f710766ba467ba39333fab53e685859173911
Illia Kyselov <illya.kis99@gmail.com>
2023-02-27 16:42:51 +0200

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -44,7 +44,7 @@ export const TodoFilter: React.FC<Props> = ({
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
-      {query.length > 0 && (
+      {query && (
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           <button
