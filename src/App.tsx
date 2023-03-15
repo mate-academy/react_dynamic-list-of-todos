422e796196ec84d4bee809f2846da40bbc1d639a
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-03-15 21:33:37 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,91 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+
+function filterBySelect(
+  selectValue: string,
+  todos: Todo[],
+) {
+  switch (selectValue) {
+    case 'active':
+      return todos
+        .filter(({ completed }) => completed === false);
+
+    case 'completed':
+      return todos
+        .filter(({ completed }) => completed === true);
+
+    default:
+      return todos;
+  }
+}
+
+function getTodo(todos: Todo[], id: number) {
+  return todos.find(todo => todo.id === id);
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [selectFilter, setSelectFilter] = useState('all');
+  const [activeTodo, setActiveTodo] = useState<Todo | undefined>(undefined);
+  const [activeUser, setActiveUser] = useState<User | null>(null);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleQueryUpdate = (value: string) => {
+    setQuery(value);
+  };
+
+  const handleSelectUpdate = (value: string) => {
+    setSelectFilter(value);
+  };
+
+  const handleModal = (id?: number) => {
+    setIsOpenModal(current => !current);
+
+    if (id) {
+      const newActiveTodo = getTodo(todos, id);
+
+      setActiveTodo(newActiveTodo);
+      const fetchUser = async () => {
+        setActiveUser(await getUser(newActiveTodo?.userId));
+      };
+
+      fetchUser();
+    } else {
+      setActiveTodo(undefined);
+      setActiveUser(null);
+    }
+  };
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setTodos(await getTodos());
+    };
+
+    fetchTodos();
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    const filteredByQuery = [...todos.filter(({ title }) => {
+      const lowerCaseTitle = title.toLocaleLowerCase();
+      const lowerCaseQuery = query.toLocaleLowerCase();
+
+      return lowerCaseTitle.includes(lowerCaseQuery);
+    }),
+    ];
+
+    return filterBySelect(selectFilter, filteredByQuery);
+  }, [query, todos, selectFilter]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +94,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                handleQueryUpdate={handleQueryUpdate}
+                handleSelectUpdate={handleSelectUpdate}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    handleTodoClick={handleModal}
+                    activeTodo={activeTodo}
+                    todos={filteredTodos}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {isOpenModal
+        && (
+          <TodoModal
+            todo={activeTodo}
+            user={activeUser}
+            handleModal={handleModal}
+          />
+        )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,21 @@
-export const TodoFilter = () => (
+type TodoFilterProps = {
+  handleSelectUpdate: (value: string) => void,
+  handleQueryUpdate: (value: string) => void,
+  query: string,
+};
+
+export const TodoFilter: React.FC<TodoFilterProps> = ({
+  handleSelectUpdate,
+  handleQueryUpdate,
+  query,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          onChange={(e) => handleSelectUpdate(e.target.value)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -13,21 +26,26 @@ export const TodoFilter = () => (
     <p className="control is-expanded has-icons-left has-icons-right">
       <input
         data-cy="searchInput"
+        value={query}
         type="text"
         className="input"
         placeholder="Search..."
+        onInput={(e) => handleQueryUpdate(e.currentTarget.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
+        {query && (
+          /* eslint-disable-next-line jsx-a11y/control-has-associated-label */
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => handleQueryUpdate('')}
+          />
+        )}
       </span>
     </p>
   </form>
