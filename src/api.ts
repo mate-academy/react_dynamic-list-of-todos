30f8a1c054e430bdcf07c6c4122a3334d0683bbc
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-04-03 14:51:32 +0300

    fixed users url

--- src/api.ts
+++ src/api.ts
@@ -34,4 +34,4 @@ function get<T>(url: string): Promise<T> {
 
 export const getTodos = () => get<Todo[]>('/todos');
 
-export const getUser = (userId: number) => get<User>(`/use/${userId}`);
+export const getUser = (userId: number) => get<User>(`/users/${userId}`);
