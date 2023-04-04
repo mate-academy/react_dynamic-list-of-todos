657da9bfb4f1d0334f0e10853fd162b01869e1c6
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-04-04 19:21:22 +0200

    fixed error handle in modal

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
-import { Loader } from '../Loader';
 import { getUser } from '../../api';
 import { Todo } from '../../types/Todo';
 import { User } from '../../types/User';
+import { Loader } from '../Loader';
 
 type Props = {
   todo: Todo;
@@ -13,6 +13,7 @@ type Props = {
 export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
   const [user, setUser] = useState<User>();
   const [isError, setIsError] = useState(false);
+  const [isloading, setIsLoading] = useState(true);
 
   const {
     id,
@@ -29,6 +30,8 @@ export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
         setUser(userFromServer);
       } catch {
         setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -36,57 +39,63 @@ export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
   }, []);
 
   return (
-    <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+    <>
+      {isloading
+        ? <Loader />
+        : (
+          <div className="modal is-active" data-cy="modal">
+            <div className="modal-background" />
 
-      {(!user || isError) ? (
-        <Loader />
-      ) : (
-        <div className={classNames(
-          'modal-card',
-          'notification',
-          {
-            'is-success': completed,
-            'is-danger': !completed,
-          },
-        )}
-        >
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              {`Todo #${id}`}
-            </div>
+            {(!user || isError)
+              ? <div className="modal-card">The error has happened...</div>
+              : (
+                <div className={classNames(
+                  'modal-card',
+                  'notification',
+                  {
+                    'is-success': completed,
+                    'is-danger': !completed,
+                  },
+                )}
+                >
+                  <header className="modal-card-head">
+                    <div
+                      className="modal-card-title has-text-weight-medium"
+                      data-cy="modal-header"
+                    >
+                      {`Todo #${id}`}
+                    </div>
 
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-              onClick={onClose}
-            />
-          </header>
+                    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                    <button
+                      type="button"
+                      className="delete"
+                      data-cy="modal-close"
+                      onClick={onClose}
+                    />
+                  </header>
 
-          <div className="modal-card-body">
-            <p className="block has-text-dark" data-cy="modal-title">
-              {title}
-            </p>
+                  <div className="modal-card-body">
+                    <p className="block has-text-dark" data-cy="modal-title">
+                      {title}
+                    </p>
 
-            <p className="block has-text-dark" data-cy="modal-user">
-              {completed
-                ? <strong className="has-text-success">Done</strong>
-                : <strong className="has-text-danger">Planned</strong>}
+                    <p className="block has-text-dark" data-cy="modal-user">
+                      {completed
+                        ? <strong className="has-text-success">Done</strong>
+                        : <strong className="has-text-danger">Planned</strong>}
 
-              {' by '}
+                      {' by '}
 
-              <a href={`mailto:${user.email}`}>
-                {user.name}
-              </a>
-            </p>
+                      <a href={`mailto:${user.email}`}>
+                        {user.name}
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              )}
           </div>
-        </div>
-      )}
-    </div>
+        )}
+    </>
   );
 };
