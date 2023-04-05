7d0976c1cded283ead46681988ddac6a65ee218a
mstrilec <MaxStrilec2005@gmail.com>
2023-04-05 14:21:46 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [query, setQuery] = useState('');
+  const [filterType, setFilterType] = useState('all');
+  const [activeTodoId, setActiveTodoId] = useState(0);
+
+  const activeTodo = todos.find(todo => todo.id === activeTodoId);
+
+  const getTodosList = async (promise: Promise<Todo[]>) => {
+    try {
+      setTodos(await promise);
+    } catch {
+      setHasError(true);
+    }
+  };
+
+  useEffect(() => {
+    getTodosList(getTodos());
+  }, []);
+
+  let visibleTodos: Todo[] = [...todos];
+
+  if (filterType === 'active') {
+    visibleTodos = visibleTodos.filter(todo => !todo.completed);
+  }
+
+  if (filterType === 'completed') {
+    visibleTodos = visibleTodos.filter(todo => todo.completed);
+  }
+
+  visibleTodos = visibleTodos
+    .filter(todo => todo.title.toLowerCase()
+      .includes(query.trim().toLowerCase()));
+
   return (
     <>
       <div className="section">
@@ -17,18 +54,49 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={todos}
+                setTodos={setTodos}
+                query={query}
+                setQuery={setQuery}
+                filterType={filterType}
+                setFilterType={setFilterType}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {(!todos.length || hasError) ? (
+                <Loader />
+              ) : (
+                visibleTodos.length === 0 ? (
+                  <article className="message is-warning">
+                    <div className="message-header">
+                      <p>Warning</p>
+                    </div>
+
+                    <div className="message-body">
+                      Warning: No users found!
+                    </div>
+                  </article>
+                ) : (
+                  <TodoList
+                    todos={visibleTodos}
+                    activeTodoId={activeTodoId}
+                    setActiveTodoId={setActiveTodoId}
+                  />
+                )
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {activeTodo && (
+        <TodoModal
+          activeTodo={activeTodo}
+          setActiveTodoId={setActiveTodoId}
+        />
+      )}
     </>
   );
 };
