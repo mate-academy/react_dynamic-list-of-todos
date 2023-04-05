42473f7c8edfc9b0a1a8d2bc1684b2c3c8438f74
Dovhun Artem <dovhun3d@gmail.com>
2023-04-05 20:54:05 +0300

    Loaded todo, and rendered TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +8,32 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { Todo } from './types/Todo';
+
+import { getTodos } from './api'; // , getUser
+
 export const App: React.FC = () => {
+  const [todos, setToods] = useState<Todo[]>([]);
+  const [isLoading] = useState(false); // setIsLoading
+
+  // console.log('todos:', todos);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const fetchedTodos = await getTodos();
+
+        setToods(fetchedTodos);
+      } catch (error) {
+        // console.error(error);
+
+        throw new Error(`We have a problem when we have loaded data - ${error}`);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +46,15 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+
+              <TodoList todos={todos} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isLoading && <TodoModal />}
     </>
   );
 };
