39066eb7dda5a6d5b019a7e7ba56896ed5a1fb6d
Vladyslav Mosor <mosorvlad@gmail.com>
2023-04-05 12:35:50 +0200

    dynamic list of todos implemented

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,83 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import {
+  FC,
+  Dispatch,
+  SetStateAction,
+  ChangeEvent,
+} from 'react';
+import { TodoStatus } from '../../types/TodoStatus';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  query: string,
+  setQuery: Dispatch<SetStateAction<string>>,
+  setTodoStatus: Dispatch<SetStateAction<TodoStatus>>,
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: FC<Props> = ({
+  query,
+  setQuery,
+  setTodoStatus,
+}) => {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    switch (e.target.value) {
+      case 'all':
+        setTodoStatus(TodoStatus.ALL);
+        break;
+      case 'active':
+        setTodoStatus(TodoStatus.ACTIVE);
+        break;
+      case 'completed':
+        setTodoStatus(TodoStatus.COMPLETED);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={handleChange}
+          >
+            {Object.values(TodoStatus).map((enumValue) => (
+              <option
+                key={enumValue}
+                value={enumValue}
+              >
+                {enumValue}
+              </option>
+            ))}
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => setQuery('')}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,66 @@
-import React from 'react';
+import {
+  FC,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
-  return (
+type Props = {
+  todo: Todo | null,
+  handleCloseModal: () => void,
+};
+
+export const TodoModal: FC<Props> = ({
+  todo,
+  handleCloseModal,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const loadUser = useCallback(async () => {
+    try {
+      const loadedUser = await getUser(todo?.userId || 0);
+
+      setUser(loadedUser);
+    } catch (error) {
+      setHasError(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  return hasError ? (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <div
+            className="modal-card-title has-text-danger"
+            data-cy="modal-header"
+          >
+            An error occurred when loading the details
+          </div>
 
-      {true ? (
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            type="button"
+            className="delete"
+            data-cy="modal-close"
+            onClick={handleCloseModal}
+          />
+        </header>
+      </div>
+    </div>
+  ) : (
+    <div className="modal is-active" data-cy="modal">
+      <div className="modal-background" />
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +69,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${todo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +77,26 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={handleCloseModal}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {todo?.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user.email}`}>
+                {user.name}
               </a>
             </p>
           </div>
