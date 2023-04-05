5af88b3485ae143411f0344d92cb68e2255a7c3f
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-04-05 21:54:29 +0300

    Solution v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,26 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [isTodoActive, setIsTodoActive] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const currentTodos = await getTodos();
+
+      setTodos(currentTodos);
+      setIsLoaded(true);
+      setIsTodoActive(false)
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +39,16 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoaded
+                ? <TodoList todos={todos} />
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isTodoActive
+        && <TodoModal />}
     </>
   );
 };
