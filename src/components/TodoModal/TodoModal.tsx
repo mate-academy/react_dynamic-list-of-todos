ac66d6e4a25eb18251744271acb80776f334d82b
Kateryna Babina <katerynababina01@gmail.com>
2023-04-06 11:28:22 +0300

    solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
+import { Error } from '../Error';
 
-export const TodoModal: React.FC = () => {
+interface TodoModalProps {
+  todo: Todo;
+  onClose: () => void;
+}
+
+export const TodoModal: React.FC<TodoModalProps> = ({ todo, onClose }) => {
+  const {
+    id,
+    userId,
+    title,
+    completed,
+  } = todo;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<User>();
+  const [hasError, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function loadUser() {
+      try {
+        const userData = await getUser(userId);
+
+        setUser(userData);
+      } catch (error) {
+        setError('An error occurred while loading the goods.');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadUser();
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {hasError
+        && (
+          <Error hasError={hasError} onModalClose={onClose} />
+        )}
+
+      {!hasError && (isLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +57,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,27 +65,34 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => onClose()}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
+              {
+                completed
+                  ? (
+                    <strong className="has-text-success">Done</strong>
+                  )
+                  : (
+                    <strong className="has-text-danger">Planned</strong>
+                  )
+              }
               {' by '}
 
               <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+                {user?.name}
               </a>
             </p>
           </div>
         </div>
-      )}
+      ))}
     </div>
   );
 };
