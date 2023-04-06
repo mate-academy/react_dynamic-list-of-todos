55ef09e9c8a33ace0c3238bbdbe84ac9c9cb3744
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-04-06 21:25:46 +0300

    fixed a lot of problems

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -38,58 +38,59 @@ export const TodoModal: React.FC<Props> = ({
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {inNotUserAndError ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              {`Todo #${id}`}
-            </div>
+      {inNotUserAndError
+        ? (
+          <Loader />
+        ) : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${id}`}
+              </div>
 
-            <button
-              type="button"
-              aria-label="clear"
-              className="delete"
-              data-cy="modal-close"
-              onClick={() => {
-                onsetSelectedTodo(null);
-              }}
+              <button
+                type="button"
+                aria-label="clear"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => {
+                  onsetSelectedTodo(null);
+                }}
 
-            />
-          </header>
+              />
+            </header>
 
-          <div className="modal-card-body">
-            <p className="block has-text-danger" data-cy="modal-title">
-              {errorUser
-                ? ('Failed to fetch user from server')
-                : title}
-            </p>
-
-            {isError || (
-              <p className="block" data-cy="modal-user">
-                <strong
-                  className={classNames(
-                    { 'has-text-danger': !completed },
-                    { 'has-text-success': completed },
-                  )}
-                >
-                  {completed ? 'Done' : 'Planned'}
-                </strong>
-
-                {' by '}
-
-                <a href={`mailto:${user?.email}`}>
-                  {user?.name}
-                </a>
+            <div className="modal-card-body">
+              <p className="block has-text-danger" data-cy="modal-title">
+                {errorUser
+                  ? ('Failed to fetch user from server')
+                  : title}
               </p>
-            )}
+
+              {isError || (
+                <p className="block" data-cy="modal-user">
+                  <strong
+                    className={classNames(
+                      { 'has-text-danger': !completed },
+                      { 'has-text-success': completed },
+                    )}
+                  >
+                    {completed ? 'Done' : 'Planned'}
+                  </strong>
+
+                  {' by '}
+
+                  <a href={`mailto:${user?.email}`}>
+                    {user?.name}
+                  </a>
+                </p>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
