5274a65be849840e6acff55a1e39b8b8bb7e1013
Stepan <102257376+Stepan-TS@users.noreply.github.com>
2023-04-06 17:35:53 +0300

    list-of-todos done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 import { TodoModal } from './components/TodoModal';
+import { filterByTitle, filterByTodoStatus } from './helpers';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [showTodoModal, setShowTodoModal] = useState(false);
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+  const [sortBy, setSortBy] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  let visiableTodos = filterByTodoStatus(todos, sortBy);
+
+  visiableTodos = filterByTitle(visiableTodos, query);
+
+  const selectedTodo = todos.find(todo => todo.id === selectedTodoId);
+
+  useEffect(() => {
+    if (selectedTodoId) {
+      setShowTodoModal(true);
+    }
+  }, [selectedTodoId]);
+
+  const closeTodoModal = () => {
+    setSelectedTodoId(0);
+    setShowTodoModal(false);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +52,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                changeSortBy={setSortBy}
+                query={query}
+                changeQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading
+                ? (<Loader />)
+                : (
+                  <TodoList
+                    todos={visiableTodos}
+                    selectedTodoId={selectedTodoId}
+                    setSelectedTodoId={setSelectedTodoId}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {showTodoModal
+        && (
+          <TodoModal
+            closeModal={closeTodoModal}
+            todo={selectedTodo}
+          />
+        )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,62 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { Dispatch, SetStateAction } from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  changeSortBy: Dispatch<SetStateAction<string>>;
+  query: string;
+  changeQuery: Dispatch<SetStateAction<string>>;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = (props) => {
+  const {
+    changeSortBy,
+    query,
+    changeQuery,
+  } = props;
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            onChange={(event) => changeSortBy(event.target.value)}
+            data-cy="statusSelect"
+          >
+            <option value="all">All</option>
+            <option
+              value="active"
+            >
+              Active
+            </option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={(event) => changeQuery(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => changeQuery('')}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
