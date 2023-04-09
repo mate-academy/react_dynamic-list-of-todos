928999ae1cdd024b9dfbb13af144f76c7bdb5ae9
Dmytro Litinskyi <litinskiupro100dima@gmail.com>
2023-04-09 18:33:32 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 /* eslint-disable max-len */
-import React from 'react';
+import {
+  useState,
+  useCallback,
+  useEffect,
+  useMemo,
+} from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +12,44 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { FilterBy } from './types/SortBy';
+import { getTodos, getvisibleTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+  const [query, setQuery] = useState('');
+  const [filterBy, setFilterBy] = useState(FilterBy.All);
+
+  const getTodosFromApi = async () => {
+    try {
+      const todosFromApi = await getTodos();
+
+      setTodos(todosFromApi);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTodosFromApi();
+  }, []);
+
+  const visibleTodos = getvisibleTodos(todos, query, filterBy);
+
+  const changeSelectedTodo = useCallback((id: number) => {
+    setSelectedTodoId(id);
+  }, []);
+
+  const activeTodo = useMemo(() => (
+    todos.find(({ id }) => id === selectedTodoId)
+  ), [selectedTodoId]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                onSetQuery={setQuery}
+                onChangeFilter={setFilterBy}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && !isError && (
+                <Loader />
+              )}
+
+              <TodoList
+                todos={visibleTodos}
+                onSelectedTodo={changeSelectedTodo}
+                selectedTodo={selectedTodoId}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {activeTodo && (
+        <TodoModal
+          todo={activeTodo}
+          onHide={() => changeSelectedTodo(0)}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,21 +1,68 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import classNames from 'classnames';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo,
+  onHide: () => void
+};
+
+export const TodoModal: React.FC<Props> = ({ todo, onHide }) => {
+  const {
+    id,
+    userId,
+    title,
+    completed,
+  } = todo;
+
+  const [user, setUser] = useState<User>();
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getUserFromApi = async () => {
+    try {
+      const userFromApi = await getUser(userId);
+
+      setUser(userFromApi);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUserFromApi();
+  }, []);
+
+  const displayModal = !isLoading && !isError && user;
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
+      {isLoading && (
+        <Loader />)}
+      {displayModal ? (
+        <div
+          className={classNames(
+            'modal-card',
+            'notification',
+            {
+              'is-success': completed,
+              'is-danger': !completed,
+            },
+          )}
+        >
           <header className="modal-card-head">
             <div
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,25 +70,54 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={onHide}
             />
           </header>
 
           <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+            <p className="block has-text-dark" data-cy="modal-title">
+              {title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={classNames({
+                  'has-text-danger': !completed,
+                  'has-text-success': completed,
+                })}
+              >
+                {completed ? 'Done' : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto ${user?.email}`}>
+                {user?.name}
               </a>
             </p>
           </div>
+        </div>
+      ) : (
+        <div className="modal-card notification is-danger">
+          <header className="modal-card-head">
+            <div className="modal-card-title">
+              Error
+            </div>
+            <button
+              aria-label="close-modal-button"
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onHide}
+            />
+          </header>
+
+          <article className="modal-card-body">
+            <p className="block has-text-dark has-text-weight-medium">
+              The error has happened...
+            </p>
+          </article>
         </div>
       )}
     </div>
