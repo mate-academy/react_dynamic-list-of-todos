f2b0b98241a4e65576806b97b7cd9d8e69daba10
Tetiana Koval <tetiana.koval.bit@gmail.com>
2023-04-10 16:14:35 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
+// import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState(():Todo[] => []);
+  const [todoId, selectTodo] = useState(0);
+  const [visibleTodos, getVisibleTodos] = useState(todos);
+
+  const selectedTodo = todos.find(todo => todo.id === todoId);
+
+  const loadData = (async () => {
+    const todosFromServer = await getTodos();
+
+    setTodos(todosFromServer);
+  });
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +35,29 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                getVisibleTodos={(todosAfterFiltering:Todo[]) => getVisibleTodos(todosAfterFiltering)}
+                todos={todos}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {/* <Loader /> */}
+              <TodoList
+                todos={visibleTodos}
+                selectedTodo={todoId}
+                selectTodo={(id:number) => selectTodo(id)}
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          selectTodo={(id:number) => selectTodo(id)}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo,
+  selectTodo: (id:number) => void,
+};
+
+export const TodoModal: React.FC<Props> = ({ todo, selectTodo }) => {
+  const [user, setUser] = useState(():User | null => null);
+
+  const LoadUsers = async () => {
+    const selectedUser = await getUser(todo.userId);
+
+    return setUser(selectedUser);
+  };
+
+  useEffect(() => {
+    LoadUsers();
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +35,8 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #
+              {todo.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +44,26 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => selectTodo(0)}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {todo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
               <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+                {user.name}
               </a>
             </p>
           </div>
