76d2a02e5cba9075bd647b3a4ee2b66512207dfa
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-04-10 13:27:58 +0300

    was fixed issue with modal

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -41,59 +41,57 @@ export const TodoModal: React.FC<Props> = ({ todo, setSelectedTodo }) => {
   }, [todo]);
 
   return (
-    <>
-      <div className="modal is-active" data-cy="modal">
-        <div className="modal-background" />
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <div
-                className="modal-card-title has-text-weight-medium"
-                data-cy="modal-header"
-              >
-                {`Todo #${id}`}
-              </div>
-
-              <button
-                aria-label="Close modal"
-                type="button"
-                className="delete"
-                data-cy="modal-close"
-                onClick={() => setSelectedTodo(null)}
-              />
-            </header>
-
-            <div className="modal-card-body">
-              <p className="block" data-cy="modal-title">
-                {title}
-              </p>
-
-              {errorMessage ? (
-                <p>{errorMessage}</p>
-              ) : (
-                <p className="block" data-cy="modal-user">
-                  <strong
-                    className={classNames({
-                      'has-text-danger': !completed,
-                      'has-text-success': completed,
-                    })}
-                  >
-                    {todo.completed ? 'Done' : 'Planned'}
-                  </strong>
-
-                  {' by '}
-
-                  <a href={`mailto:${currentUser?.email}`}>
-                    {currentUser?.name}
-                  </a>
-                </p>
-              )}
+    <div className="modal is-active" data-cy="modal">
+      <div className="modal-background" />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${id}`}
             </div>
+
+            <button
+              aria-label="Close modal"
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => setSelectedTodo(null)}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {title}
+            </p>
+
+            {errorMessage ? (
+              <p>{errorMessage}</p>
+            ) : (
+              <p className="block" data-cy="modal-user">
+                <strong
+                  className={classNames({
+                    'has-text-danger': !completed,
+                    'has-text-success': completed,
+                  })}
+                >
+                  {todo.completed ? 'Done' : 'Planned'}
+                </strong>
+
+                {' by '}
+
+                <a href={`mailto:${currentUser?.email}`}>
+                  {currentUser?.name}
+                </a>
+              </p>
+            )}
           </div>
-        )}
-      </div>
-    </>
+        </div>
+      )}
+    </div>
   );
 };
