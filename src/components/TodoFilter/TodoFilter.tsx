f8f305a226ce5a50768c5a049d6634c74b0599c9
Bogdan <bogdan_mykhailov@icloud.com>
2023-04-10 10:42:42 +0300

    :construction: fix issues

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -44,8 +44,8 @@ export const TodoFilter: FC<TodoFilterProps> = ({
             className="icon is-right"
             style={{ pointerEvents: 'all' }}
           >
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
+              aria-label="Mute volume"
               data-cy="clearSearchButton"
               type="button"
               className="delete"
