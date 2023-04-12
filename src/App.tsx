b510776bda9312c25ef8323e8f4e1aad00728acc
Ilya Ukraintsev <ukraintsev17@gmail.com>
2023-04-12 18:17:13 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { FilterType } from './types/FilterTypes';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+const getVisibleTodos = (
+  todos: Todo[],
+  sortType: string,
+  query: string,
+): Todo[] => {
+  let visibleTodos: Todo[] = [...todos];
+
+  if (sortType === FilterType.ACTIVE) {
+    visibleTodos = todos.filter(todo => !todo.completed);
+  }
+
+  if (sortType === FilterType.COMPLETED) {
+    visibleTodos = todos.filter(todo => todo.completed);
+  }
+
+  const handleFilterTodos = (value: string, searchQuery: string) => {
+    return (
+      value.toLowerCase().includes(searchQuery.toLowerCase().trim())
+    );
+  };
+
+  return visibleTodos.filter(({ title }) => (
+    handleFilterTodos(title, query)
+
+  ));
+};
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [selectedModal, setSelectedModal] = useState<Todo | null>(null);
+  const [isLodingPage, setIsLoadingPage] = useState(false);
+  const [selectedType, setSelectedType] = useState(FilterType.ALL);
+  const [hasLoadingPageError, setHasLoadingPageError] = useState(false);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => setHasLoadingPageError(true))
+      .finally(() => setIsLoadingPage(true));
+  }, []);
+
+  const visibleTodos = getVisibleTodos(todos, selectedType, query);
+
   return (
     <>
       <div className="section">
@@ -17,18 +64,49 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              {!hasLoadingPageError && (
+                <TodoFilter
+                  query={query}
+                  onQuery={setQuery}
+                  selectedType={selectedType}
+                  onSelectedType={setSelectedType}
+                />
+              )}
+
             </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
+            {hasLoadingPageError
+              ? (
+                <div className="notification is-danger">
+                  An error occured when loading data!
+                </div>
+              )
+              : (
+                <div className="block">
+                  {isLodingPage
+                    ? (
+                      <TodoList
+                        todos={visibleTodos}
+                        selectedModal={selectedModal}
+                        onSelectedModal={setSelectedModal}
+                      />
+                    )
+                    : (
+                      <Loader />
+                    )}
+                </div>
+              )}
+
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedModal && (
+        <TodoModal
+          selectedModal={selectedModal}
+          onSelectedModal={setSelectedModal}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { ChangeEvent } from 'react';
+import { FilterType } from '../../types/FilterTypes';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  query: string;
+  onQuery: (query: string) => void;
+  selectedType: string;
+  onSelectedType: (filter: FilterType) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({
+  query,
+  onQuery,
+  selectedType,
+  onSelectedType,
+}) => {
+  const handleQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    onQuery(event.target.value);
+  };
+
+  const resetQuery = () => {
+    onQuery('');
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectedType}
+            onChange={(event) => (
+              onSelectedType(event.target.value as FilterType))}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={handleQuery}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+
+            <button
+              data-cy="clearSearchButton"
+              aria-label="Mute volume"
+              type="button"
+              className="delete"
+              onClick={resetQuery}
+            />
+          </span>
+        )}
+
+      </p>
+    </form>
+  );
+};
