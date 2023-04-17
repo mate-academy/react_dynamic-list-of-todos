d7925476a2aad6182a31d32a5b124a4a200fa705
GUSILLUS <zebela2002gmail.com>
2023-04-17 21:21:08 +0300

    Solutions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,77 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { FilterBy } from './types/FilterBy';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { LoadingError } from './components/LoaderError/LoaderError';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [hasLoadingError, setHasLoadingError] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [filterBy, setFilterBy] = useState(FilterBy.All);
+
+  const loadTodos = async () => {
+    setLoading(true);
+
+    try {
+      const todosFromServer = await getTodos();
+
+      setTodos(todosFromServer);
+    } catch (error) {
+      setHasLoadingError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    let filteredTodos = [...todos];
+
+    if (query) {
+      filteredTodos = filteredTodos.filter(todo => (
+        todo.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      ));
+    }
+
+    switch (filterBy) {
+      case FilterBy.Completed:
+        filteredTodos = filteredTodos.filter(todo => todo.completed);
+        break;
+
+      case FilterBy.Active:
+        filteredTodos = filteredTodos.filter(todo => !todo.completed);
+        break;
+
+      default:
+        break;
+    }
+
+    return filteredTodos;
+  }, [query, filterBy, loading]);
+
+  const handleSelectedTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleModalCloseButtonClick = () => {
+    setSelectedTodo(null);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +80,52 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                onQueryChange={setQuery}
+                filterBy={filterBy}
+                onFilterChange={setFilterBy}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading
+                ? (
+                  <Loader />
+                )
+                : (
+                  <>
+                    {visibleTodos.length > 0
+                      ? (
+                        <TodoList
+                          todos={visibleTodos}
+                          selectedTodo={selectedTodo}
+                          onTodoSelected={handleSelectedTodo}
+                        />
+                      )
+                      : (
+                        <p>
+                          Not found any todos
+                        </p>
+                      )}
+
+                    {hasLoadingError && (
+                      <LoadingError />
+                    )}
+                  </>
+                )}
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onClose={handleModalCloseButtonClick}
+        />
+      )}
     </>
   );
 };
