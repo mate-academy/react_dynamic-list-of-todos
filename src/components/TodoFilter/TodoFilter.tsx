40a6b46c12cf6053f2f18a45a80d9424b2498fa8
Yelisieiev Kim <rbv271@outlook.com>
2023-04-18 14:43:32 +0300

    React dynamic todoList

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,23 @@
-export const TodoFilter = () => (
+import React from 'react';
+
+type Props = {
+  filterBy: string,
+  setFilterBy: (option: string) => void,
+  value: string,
+  setValue: (query: string) => void,
+};
+
+export const TodoFilter: React.FC<Props> = React.memo(({
+  filterBy, setFilterBy, value, setValue,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={filterBy}
+          onChange={(event) => setFilterBy(event.target.value)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,19 +31,24 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
+      {value && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            aria-label="delete-button"
+            onClick={() => setValue('')}
+          />
+        </span>
+      )}
     </p>
   </form>
-);
+));
