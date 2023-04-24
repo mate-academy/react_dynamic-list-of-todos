1795ac06249e58af1a656ea6a8636bf98e831129
Andrey Rudenko <rudenko.and@gmail.com>
2023-04-24 22:13:52 +0300

    done

--- src/api.ts
+++ src/api.ts
@@ -4,8 +4,6 @@ import { User } from './types/User';
 // eslint-disable-next-line max-len
 const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
 
-// This function creates a promime
-// that is resolved after a given delay
 function wait(delay: number): Promise<void> {
   return new Promise(resolve => {
     setTimeout(resolve, delay);
@@ -18,8 +16,25 @@ function get<T>(url: string): Promise<T> {
 
   // we add some delay to see now the laoder works
   return wait(300)
-    .then(() => fetch(fullURL))
-    .then(res => res.json());
+    .then(async () => {
+      const response = await fetch(fullURL);
+      const {
+        ok,
+        status,
+        statusText,
+        headers,
+      } = response;
+
+      if (!ok) {
+        throw new Error(`${status} - ${statusText}`);
+      }
+
+      if (!headers.get('content-type')?.includes('application/json')) {
+        throw new Error("Content type isn't json");
+      }
+
+      return response.json();
+    });
 }
 
 export const getTodos = () => get<Todo[]>('/todos');
