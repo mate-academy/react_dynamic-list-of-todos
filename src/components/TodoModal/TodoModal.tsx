056125538f11e5fee3fe54638d330e379aa81ec8
Yana Kotsulym <76750973+yana-kotsulym@users.noreply.github.com>
2023-04-27 09:21:31 +0200

    Delete the comments

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -51,9 +51,7 @@ export const TodoModal: React.FC<Props> = ({
               {todo?.completed
                 ? <strong className="has-text-success">Done</strong>
                 : <strong className="has-text-danger">Planned</strong>}
-              {/* <strong className="has-text-success">Done</strong> */}
-              {/* <strong className="has-text-danger">Planned</strong> */}
-
+              
               {' by '}
 
               <a href={`mailto:${user?.email}`}>
