4e071edfe2add6f7bbf10c4c97edafec6d8452d1
Pasha <pashamalyshkin2003@gmail.com>
2023-04-27 18:13:09 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,69 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [visibleTodos, setTodos] = useState<Todo[]>([]);
+  const [searchedTodo, setSearchedTodos] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [todoCategory, setTodoCategory] = useState('All');
+  const [todoId, setTodoId] = useState(0);
+  const [userId, setUserId] = useState(1);
+  const [todoStatus, setTodoStatus] = useState(false);
+
+  // eslint-disable-next-line no-console
+  useEffect(() => {
+    getTodos().then(todos => setTodos(todos));
+  }, []);
+
+  const resetUser = () => {
+    setSelectedTodo(null);
+    setTodoId(0);
+  };
+
+  const handleSearch = (search: string) => {
+    setSearchedTodos(search);
+  };
+
+  const handleFilter = (category: string) => {
+    setTodoCategory(category);
+  };
+
+  const handleTodoStatus = (status: boolean) => {
+    setTodoStatus(status);
+  };
+
+  const filterTodos = (search: string, category: string) => {
+    let categoryStatus: boolean | null = null;
+
+    switch (category) {
+      case 'active':
+        categoryStatus = false;
+        break;
+      case 'completed':
+        categoryStatus = true;
+        break;
+      default:
+        categoryStatus = null;
+    }
+
+    return visibleTodos.filter(todo => {
+      const formattedTitle = todo.title.toLowerCase();
+      const formattedSearch = search.toLowerCase();
+
+      if (categoryStatus !== null) {
+        return formattedTitle.includes(formattedSearch)
+          && todo.completed === categoryStatus;
+      }
+
+      return formattedTitle.includes(formattedSearch);
+    });
+  };
+
+  const filteredTodos = filterTodos(searchedTodo, todoCategory);
+
   return (
     <>
       <div className="section">
@@ -17,18 +78,38 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filter={todoCategory}
+                onFilter={handleFilter}
+                search={searchedTodo}
+                searchTodo={handleSearch}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleTodos.length > 0 || <Loader />}
+              <TodoList
+                selectedTodoId={todoId}
+                selectTodoId={(id: number) => setTodoId(id)}
+                selectUserId={(id: number) => setUserId((id))}
+                todos={filteredTodos}
+                setStatus={(status: boolean) => handleTodoStatus(status)}
+                selectTodo={(todo: Todo) => setSelectedTodo(todo)}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodoId={todoId}
+          status={todoStatus}
+          onReset={resetUser}
+          userId={userId}
+          todo={selectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+interface Props {
+  selectedTodoId: number;
+  todo: Todo | null;
+  status: boolean;
+  onReset: () => void;
+  userId: number;
+}
+export const TodoModal: React.FC<Props> = ({
+  selectedTodoId,
+  todo,
+  status,
+  onReset,
+  userId,
+}) => {
+  const [selectedUser, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getUser(userId).then(user => {
+      setUser(user);
+      setIsLoading(true);
+    });
+  }, []);
+
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div
+      className="modal is-active"
+      data-cy="modal"
+    >
       <div className="modal-background" />
 
-      {true ? (
+      {!isLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +44,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodoId}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +52,27 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={onReset}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {status ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${selectedUser?.email}`}>
+                {selectedUser?.name}
               </a>
             </p>
           </div>
