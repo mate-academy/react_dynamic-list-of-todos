e17895aa5d8581dfcd8dffb27e3aeff4eda53bdc
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-04-29 19:08:04 +0300

    Solution react_dynamic-list-of-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,56 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [status, setStatus] = useState<string>('all');
+  const [query, setQuery] = useState<string>('');
+
+  useEffect(() => {
+    getTodos()
+      .then(
+        setTodos,
+        setFilteredTodos,
+      );
+  }, []);
+
+  const filteredByStatus = (todosFromServer: Todo[], StateStatus: string) => {
+    switch (StateStatus) {
+      case 'all':
+        setFilteredTodos(todosFromServer);
+        break;
+      case 'active':
+        setFilteredTodos(todosFromServer.filter((todo) => !todo.completed));
+        break;
+      case 'completed':
+        setFilteredTodos(todosFromServer.filter((todo) => todo.completed));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const filterByQuery = (todosByStatus: Todo[], queryFromInput: string) => {
+    if (!queryFromInput) {
+      return todosByStatus;
+    }
+
+    return todosByStatus.filter(todo => todo.title.includes(queryFromInput.toLowerCase().trim()));
+  };
+
+  const handleTodoSelected = (todo: Todo | null): void => {
+    setSelectedTodo(todo);
+  };
+
+  useEffect(() => {
+    filteredByStatus(filterByQuery(todos, query), status);
+  }, [status, todos, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +65,40 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={status}
+                query={query}
+                onStatusChanged={(event) => setStatus(event.target.value)}
+                onQueryChanged={(event) => setQuery(event.target.value)}
+                onClearInput={() => setQuery('')}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    todoId={selectedTodo?.id}
+                    onTodoSelected={handleTodoSelected}
+                    todos={filteredTodos}
+                  />
+                )}
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {
+        selectedTodo
+        && (
+          <TodoModal
+            todo={selectedTodo}
+            onClearModal={() => setSelectedTodo(null)}
+          />
+        )
+      }
     </>
   );
 };
