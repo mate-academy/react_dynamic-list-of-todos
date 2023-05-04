653c3c9ec65f8f3a1185dacbbc0db8458ef1a2b0
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-05-04 23:49:34 +0300

    improved code style

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -61,7 +61,7 @@ export const TodoModal: React.FC<Props> = React.memo(
 
                 {' by '}
 
-                <a href="mailto:Sincere@april.biz">
+                <a href={`mailto:${user?.email}`}>
                   {user?.name}
                 </a>
               </p>
