acccc3b4f490b8224fccaee971bbaa81aba45240
Ihor <ikruzhynskyy@gmail.com>
2023-05-05 23:07:51 +0300

    created react-dynamic-list-of-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+    getTodos().then(result => setVisibleTodos(result));
+  }, []);
+
+  const filteredTodos = visibleTodos
+    .filter(({ title }) => title.toLowerCase().includes(query.toLowerCase()))
+    .filter(({ completed }) => {
+      if (filter === 'active') {
+        return completed === false;
+      }
+
+      if (filter === 'completed') {
+        return completed === true;
+      }
+
+      return true;
+    });
+
   return (
     <>
       <div className="section">
@@ -17,18 +42,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={((chars) => setQuery(chars))}
+                filter={filter}
+                setFilter={(category) => setFilter(category)}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleTodos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    visibleTodos={filteredTodos}
+                    setSelectedTodo={(todo) => setSelectedTodo(todo)}
+                    selectedTodo={selectedTodo}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          setSelectedTodo={(todo) => setSelectedTodo(todo)}
+        />
+      )}
     </>
   );
 };
