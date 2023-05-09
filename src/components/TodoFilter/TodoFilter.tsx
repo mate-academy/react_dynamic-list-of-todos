541ff4f9050ef912c9a16172596d506d911b30d8
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-05-09 11:12:52 +0300

    dynamic list of todos

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  onSelectChange: (condition: boolean | null) => void;
+  onQueryChange: (query: string) => void;
+  query: string;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
-    </p>
-  </form>
+export const TodoFilter: React.FC<Props> = React.memo(
+  ({ onSelectChange, onQueryChange, query }) => {
+    const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      switch (event.target.value) {
+        case 'all':
+          onSelectChange(null);
+          break;
+
+        case 'active':
+          onSelectChange(false);
+          break;
+
+        case 'completed':
+          onSelectChange(true);
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    return (
+      <form className="field has-addons">
+        <p className="control">
+          <span className="select">
+            <select onChange={handleSelect} data-cy="statusSelect">
+              <option value="all">All</option>
+              <option value="active">Active</option>
+              <option value="completed">Completed</option>
+            </select>
+          </span>
+        </p>
+
+        <p className="control is-expanded has-icons-left has-icons-right">
+          <input
+            data-cy="searchInput"
+            type="text"
+            value={query}
+            onChange={(event) => onQueryChange(event.target.value)}
+            className="input"
+            placeholder="Search..."
+          />
+          <span className="icon is-left">
+            <i className="fas fa-magnifying-glass" />
+          </span>
+
+          {query.length > 0 && (
+            <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={() => onQueryChange('')}
+              />
+            </span>
+          )}
+        </p>
+      </form>
+    );
+  },
 );
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Loader } from '../Loader';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: number;
+  setTodo: (id: number) => void;
+  todosList: Todo[];
+};
+
+export const TodoModal: React.FC<Props> = ({
+  selectedTodo,
+  todosList,
+  setTodo,
+}) => {
+  const [user, setUser] = React.useState<User>();
+
+  const todo = todosList.filter((todoItem) => todoItem.id === selectedTodo);
+
+  useEffect(() => {
+    getUser(todo[0].userId).then((person) => setUser(person));
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,12 +36,13 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${todo[0].id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
               type="button"
+              onClick={() => setTodo(0)}
               className="delete"
               data-cy="modal-close"
             />
@@ -28,18 +50,22 @@ export const TodoModal: React.FC = () => {
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo[0].title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={
+                  todo[0].completed ? 'has-text-success' : 'has-text-danger'
+                }
+              >
+                {todo[0].completed ? 'Done' : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
+              <a href="mailto:Sincere@april.biz">{user.name}</a>
             </p>
           </div>
         </div>
