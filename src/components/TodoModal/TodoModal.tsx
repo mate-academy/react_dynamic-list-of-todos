58362d5798756c6183057228b3102b2124ec2c02
Peti51 <galpeter31@gmail.com>
2023-05-09 20:29:42 +0200

    add task solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+interface Props {
+  setButton: (trueOrFalse: boolean) => (void)
+  todoObj: Todo,
+}
+
+export const TodoModal: React.FC<Props> = ({
+  setButton,
+  todoObj,
+}) => {
+  const [user, setUser]
+  = useState<User>({
+    id: 0,
+    name: '',
+    email: '',
+    phone: '',
+  });
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function getTodo() {
+      const fetchedData = await getUser(todoObj.userId);
+
+      setUser(fetchedData);
+      setLoading(false);
+    }
+
+    getTodo();
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
+      {isLoading
+        ? (<Loader />)
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                Todo #
+                {todoObj.id}
+              </div>
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => setButton(false)}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p
+                className={todoObj.completed
+                  ? 'has-text-success block'
+                  : 'has-text-danger block'}
+                data-cy="modal-title"
+              >
+                {todoObj.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {todoObj.completed
+                  ? <strong className="has-text-success">Done</strong>
+                  : <strong className="has-text-danger">Planned</strong>}
+
+                {' by '}
+
+                <a href={user.email}>
+                  {user.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )}
+
     </div>
   );
 };
