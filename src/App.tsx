d37bc45dd598cef8de838615ad914f77ec12a3f0
glots-a <glots@ukr.net>
2023-05-09 18:49:59 +0300

    dynamic list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+  const [option, setOption] = useState('all');
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(error => `${error} error in TODO's data`)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const handleModalClose = () => {
+    setSelectedTodo(null);
+  };
+
+  const visibleTodos = todos.filter(todo => {
+    const input = todo.title.toLowerCase().includes(inputValue.toLowerCase().trim());
+
+    switch (option) {
+      case 'active':
+        return input && !todo.completed;
+      case 'completed':
+        return input && todo.completed;
+      default:
+        return input;
+    }
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,29 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setInputValue={setInputValue}
+                inputValue={inputValue}
+                option={option}
+                setOption={setOption}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              <TodoList
+                todos={visibleTodos}
+                selectedTodo={selectedTodo}
+                onTodoSelect={setSelectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal selectedTodo={selectedTodo} handleModalClose={handleModalClose} />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,26 @@
-export const TodoFilter = () => (
+import React from 'react';
+
+type Props = {
+  setInputValue: (inputValue: string) => void;
+  inputValue: string;
+  option: string;
+  setOption: (option: string) => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  setInputValue,
+  inputValue,
+  option,
+  setOption,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={option}
+          onChange={(event) => setOption(event.target.value)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,18 +34,23 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={inputValue}
+        onChange={event => setInputValue(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
+        {inputValue && (
+          /* eslint-disable-next-line jsx-a11y/control-has-associated-label */
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => setInputValue('')}
+          />
+        )}
       </span>
     </p>
   </form>
