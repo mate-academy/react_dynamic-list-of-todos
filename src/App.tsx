f661ac47b014dba5120d23ea2943bd97a8169e68
Hamuud <artemlask2014@gmail.com>
2023-05-10 01:01:30 +0300

    Create dynamic todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,43 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { PrepaparedTodo } from './types/PreparedTodo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [sort, setSort] = useState('all');
+  const [selectedTodo, setSelectedTodo] = useState<PrepaparedTodo | null>(null);
+  const [todoModal, setTodoModal] = useState(false);
+  //  const [selectedTodosName, setSelecetedTodosName] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+      });
+  }, [JSON.stringify(getTodos())]);
+
+  const filteredTodos = todos.filter(todo => {
+    const filtered = todo.title.toLowerCase().includes(query.toLowerCase().trim());
+
+    switch (sort) {
+      case 'active':
+        return filtered && !todo.completed;
+      case 'completed':
+        return filtered && todo.completed;
+      default:
+        return filtered;
+    }
+  });
+
+  const visibleTodos = useMemo(
+    () => filteredTodos,
+    [JSON.stringify(filteredTodos)],
+  );
+
   return (
     <>
       <div className="section">
@@ -17,18 +52,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                sort={sort}
+                setSort={setSort}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!todos.length && <Loader />}
+              <TodoList
+                todos={visibleTodos}
+                setTodo={setSelectedTodo}
+                todoModal={todoModal}
+                setTodoModal={setTodoModal}
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {
+        todoModal && (
+          <TodoModal
+            selectedTodo={selectedTodo}
+            setSelectedTodo={setSelectedTodo}
+            setTodoModal={setTodoModal}
+          />
+        )
+      }
     </>
   );
 };
