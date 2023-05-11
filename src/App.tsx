35ec1c1c354cb9840fd1de709bf781d33ea39645
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-05-11 16:12:01 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +8,74 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { FilterBy } from './types/FilterBy';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedStatus, setSelectedStatus] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const loadTodos = useCallback(async () => {
+    try {
+      const todosFromServer = await getTodos();
+
+      setTodos(todosFromServer);
+    } catch (error) {
+      throw new Error();
+    }
+  }, []);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const handleSelectingTodo = useCallback((todo: Todo) => {
+    setSelectedTodo(todo);
+  }, []);
+
+  const handleSetQuery = useCallback((string: string) => {
+    setQuery(string);
+  }, []);
+
+  const handleCloseModal = () => setSelectedTodo(null);
+
+  const handleReset = () => {
+    setQuery('');
+  };
+
+  const preparedTodos = useMemo(() => {
+    let visibleTodos: Todo[] = [...todos];
+
+    visibleTodos = visibleTodos.filter(todo => {
+      switch (selectedStatus) {
+        case FilterBy.ALL:
+          return true;
+
+        case FilterBy.COMPLETED:
+          return todo.completed;
+
+        case FilterBy.ACTIVE:
+          return !todo.completed;
+
+        default:
+          return true;
+      }
+    });
+
+    if (query) {
+      const lowerCaseQuery = query.toLowerCase().trim();
+
+      visibleTodos = visibleTodos.filter(
+        ({ title }) => title.toLowerCase().includes(lowerCaseQuery),
+      );
+    }
+
+    return visibleTodos;
+  }, [selectedStatus, query, todos]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +84,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={selectedStatus}
+                handleSetStatus={setSelectedStatus}
+                query={query}
+                handleSetQuery={handleSetQuery}
+                handleReset={handleReset}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {preparedTodos.length
+                ? (
+                  <TodoList
+                    visibleTodos={preparedTodos}
+                    selectedTodoId={selectedTodo?.id}
+                    onSelectTodo={handleSelectingTodo}
+                  />
+                )
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onClose={handleCloseModal}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,24 @@
-export const TodoFilter = () => (
+import React from 'react';
+
+interface Props {
+  status: string;
+  query: string;
+  handleSetStatus: (selectedStatus: string) => void;
+  handleSetQuery: (query: string) => void;
+  handleReset: () => void;
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  status, query, handleSetStatus, handleSetQuery, handleReset,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={status}
+          onChange={(event) => handleSetStatus(event.target.value)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,18 +32,24 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={(event) => handleSetQuery(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
+        {query && (
+          /* eslint-disable-next-line jsx-a11y/control-has-associated-label */
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={handleReset}
+          />
+        )}
+
       </span>
     </p>
   </form>
