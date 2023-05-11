c3985d83c81451b87d16b8a992fc3fa924009d51
Valery Sazonov <vksazonov@gmail.com>
2023-05-11 12:20:38 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,63 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos, getUser } from './api';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const [sort, setSort] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, sethasError] = useState(false);
+
+  const loadTodos = useCallback(async () => {
+    try {
+      const todoList = await getTodos();
+
+      setIsLoading(false);
+      setTodos(todoList);
+    } catch {
+      sethasError(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const openModal = (userId: number, todo: Todo) => {
+    setModalOpen(true);
+    getUser(userId).then(user => setSelectedUser(user));
+    setSelectedTodo(todo);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedUser(null);
+    setSelectedTodo(null);
+  };
+
+  const filterTodos = todos.filter(todo => {
+    const intutFilter = todo.title.toLowerCase().includes(query.trim().toLowerCase());
+
+    switch (sort) {
+      case 'active': return intutFilter && !todo.completed;
+      case 'completed': return intutFilter && todo.completed;
+      default: return intutFilter;
+    }
+  });
+
+  const visibleTodos = filterTodos;
+
   return (
     <>
       <div className="section">
@@ -17,18 +72,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                sort={sort}
+                setSort={setSort}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              {hasError
+              && <h2>Sorry, something went wrong</h2>}
+              <TodoList
+                todos={visibleTodos}
+                selectedTodo={selectedTodo}
+                openModal={openModal}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalOpen && (
+        <TodoModal
+          selectedUser={selectedUser}
+          selectedTodo={selectedTodo}
+          closeModal={closeModal}
+        />
+      )}
     </>
   );
 };
