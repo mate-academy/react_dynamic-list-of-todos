ec31a0b0f72b3553d4a6a09f63152fe64bfdbca2
Tanya Sydoruk <tanyasydoruk113022@gmail.com>
2023-05-11 14:24:17 +0300

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,29 @@
-export const TodoFilter = () => (
+import { FilterBy } from '../../types/Filters';
+
+interface Props {
+  query: string,
+  filterBy: FilterBy,
+  onFilterChange: (filterBy: FilterBy) => void,
+  onQueryChange: (query: string) => void,
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  query,
+  filterBy,
+  onFilterChange,
+  onQueryChange,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          value={filterBy}
+          onChange={({ target }) => onFilterChange(target.value as FilterBy)}
+        >
+          <option value={FilterBy.ALL}>{FilterBy.ALL}</option>
+          <option value={FilterBy.ACTIVE}>{FilterBy.ACTIVE}</option>
+          <option value={FilterBy.COMPLETED}>{FilterBy.COMPLETED}</option>
         </select>
       </span>
     </p>
@@ -16,19 +34,26 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={(event) => onQueryChange(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
+      {query.length > 0 && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => {
+              onQueryChange('');
+            }}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
