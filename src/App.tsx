2b6ace06f9069a57e44b4edd1d7fc502de2166f9
Leonid <leonidbondarchuk6@gmail.com>
2023-05-20 10:32:37 +0300

    edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -11,7 +11,7 @@ import { Todo } from './types/Todo';
 import { SELECT } from './types/SELECT';
 import { getTodos } from './api';
 
-const { ALL, ACTIVE } = SELECT;
+const { ALL, ACTIVE, COMPLETED } = SELECT;
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -20,13 +20,22 @@ export const App: React.FC = () => {
   const [currentTodo, setCurrentTodo] = useState<Todo | null>(null);
 
   const todoFilter = (): Todo[] | [] => {
-    const result = selectValue === ALL
-      ? todos
-      : todos?.filter(todo => {
-        return selectValue === ACTIVE ? !todo.completed : todo.completed;
-      });
+    let result;
 
-    return result ? result.filter(todo => todo.title.replace(' ', '').toLowerCase()
+    switch (selectValue) {
+      case COMPLETED:
+        result = todos?.filter(todo => todo.completed);
+        break;
+
+      case ACTIVE:
+        result = todos?.filter(todo => !todo.completed);
+        break;
+
+      default:
+        result = todos;
+    }
+
+    return result ? result.filter(todo => todo.title.replaceAll(' ', '').toLowerCase()
       .includes(searchValue.toLowerCase().trim()))
       : [];
   };
@@ -35,7 +44,8 @@ export const App: React.FC = () => {
     getTodos().then((items: Todo[]) => setTodos(items));
   }, []);
 
-  const todoFilterList = todoFilter();
+  const todoFilterList = useMemo(() => todoFilter(), [searchValue, selectValue, todos.length]);
+
   const handleChangeSelect = (value: SELECT) => {
     setSelectValue(value);
   };
@@ -58,7 +68,7 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              {!todoFilterList.length
+              {!todos.length
                 ? <Loader />
                 : (
                   <TodoList
