de937ee70b297d14250546146425642a5533c90d
Serhii Melnychenko <8melya8@gmail.com>
2023-05-29 17:58:09 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +8,57 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
 export const App: React.FC = () => {
+  const [initialTodos, setInitialTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [searchType, setSearchType] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const formattedQuery = query.trim().toLowerCase();
+
+  const filter = (type: string) => {
+    const findQuery = (queryToSearch: string, todos: Todo[]) => {
+      return todos.filter((todo) => (
+        todo.title.toLowerCase().includes(queryToSearch)
+      ));
+    };
+
+    switch (type) {
+      case 'all':
+        return findQuery(formattedQuery, initialTodos);
+
+      case 'active': {
+        const activeTodos = initialTodos.filter((todo) => !todo.completed);
+
+        return findQuery(formattedQuery, activeTodos);
+      }
+
+      case 'completed': {
+        const completedTodos = initialTodos.filter((todo) => todo.completed);
+
+        return findQuery(formattedQuery, completedTodos);
+      }
+
+      default: return initialTodos;
+    }
+  };
+
+  const fetchGoods = () => (
+    getTodos()
+      .then((todo) => setInitialTodos(todo))
+      .catch(() => 'An error occured when loading todos'));
+
+  useEffect(() => {
+    fetchGoods();
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    return filter(searchType);
+  }, [searchType, initialTodos, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +67,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setSearchType={setSearchType}
+                setQuery={setQuery}
+                query={query}
+              />
             </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
+            {initialTodos.length > 0 ? (
+              <div className="block">
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  setSelectedTodo={setSelectedTodo}
+                />
+              </div>
+            ) : (<Loader />)}
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {!!selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          setSelectedTodo={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
