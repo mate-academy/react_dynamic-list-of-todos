7101fc4df7cbd11769086fe902dbae2ce9f943c4
Nadiein Oleh <olegnadein1994@gmail.com>
2023-05-30 12:00:06 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +8,50 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [currentTodo, setCurrentTodo] = useState<number>(0);
+  const [hideTodoModal, setHideTodoModal] = useState(false);
+
+  const [selectOption, setSelectOption] = useState('All');
+  const [inputValue, setInputValue] = useState('');
+
+  const filterTodo = () => {
+    const notCompleted = todos.filter(todo => !todo.completed);
+    const completed = todos.filter(todo => todo.completed);
+
+    switch (selectOption) {
+      case 'All':
+        return !inputValue
+          ? todos
+          : todos?.filter((todo: Todo) => todo.title.includes(inputValue.toLocaleLowerCase().trim()));
+
+      case 'active':
+        return !inputValue
+          ? notCompleted
+          : notCompleted?.filter((todo: Todo) => todo.title.includes(inputValue.toLocaleLowerCase().trim()));
+
+      case 'completed':
+        return !inputValue
+          ? completed
+          : completed?.filter((todo: Todo) => todo.title.includes(inputValue.toLocaleLowerCase().trim()));
+
+      default:
+        return todos;
+    }
+  };
+
+  const todoFilterList = filterTodo();
+  // todos.filter(todo => todo.includes(inputValue.trim()));
+
+  useEffect(() => {
+    getTodos()
+      .then(todo => setTodos(todo));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +60,39 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+                selectOption={selectOption}
+                setSelectOption={setSelectOption}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={todoFilterList}
+                    setCurrentTodo={setCurrentTodo}
+                    currentTodo={currentTodo}
+                    setHideTodoModal={setHideTodoModal}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {hideTodoModal
+        && (
+          <TodoModal
+            setHideTodoModal={setHideTodoModal}
+            resetTodo={setCurrentTodo}
+            currentTodo={currentTodo}
+            todos={todos}
+          />
+        )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  setHideTodoModal: (hide: boolean) => void;
+  resetTodo: (hide: number) => void;
+  currentTodo: number;
+  todos: Todo[];
+};
+
+export const TodoModal: React.FC<Props> = ({
+  setHideTodoModal,
+  resetTodo,
+  currentTodo,
+  todos,
+}) => {
+  const [currentUser, setCurrentUsers] = useState<User>();
+
+  useMemo(() => {
+    if (currentTodo !== 0) {
+      getUser(currentTodo).then((user) => setCurrentUsers(user));
+    }
+  }, [currentTodo]);
+
+  const findTodo = todos.find(todo => todo.id === currentTodo);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!currentUser ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +40,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${currentTodo}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +48,32 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={() => {
+                setHideTodoModal(false);
+                resetTodo(0);
+              }}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {findTodo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={`has-text-${findTodo?.completed ? 'succses' : 'danger'}`}
+              >
+                {findTodo?.completed
+                  ? 'Done'
+                  : 'Planned'}
+              </strong>
 
               {' by '}
 
               <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+                {currentUser.name}
               </a>
             </p>
           </div>
