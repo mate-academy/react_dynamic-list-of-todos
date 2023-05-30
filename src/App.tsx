60d8e11f67f3e3b3362c1f499aea1fb10d46949a
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-30 15:09:56 +0300

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,39 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [visibleToDos, setVisibleToDos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [select, setSelect] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const getQuery = (str: string) => str.toLowerCase().includes(query.toLowerCase());
+
+  const getSelectedTodos = (todos: Todo[]) => {
+    const filteredTodos = todos.filter(({ completed, title }) => {
+      switch (select) {
+        case 'active':
+          return !completed && getQuery(title);
+
+        case 'completed':
+          return completed && getQuery(title);
+
+        default:
+          return getQuery(title);
+      }
+    });
+
+    setVisibleToDos(filteredTodos);
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(todos => getSelectedTodos(todos));
+  }, [select, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSelect={setSelect}
+                selectValue={select}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {visibleToDos.length || query
+                ? (
+                  <TodoList
+                    todos={visibleToDos}
+                    chooseTodo={setSelectedTodo}
+                    selectedTodo={selectedTodo}
+                  />
+                )
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          deleteSelection={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
