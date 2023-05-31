26b9a8688b63c5fe3af20123a38fa3bc06b4e16c
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-05-31 16:39:23 +0200

    initial solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+type TodoFilterProps = {
+  setFilterType: (arg: string) => void,
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  handleQueryDelete: () => void,
+  query: string,
+};
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+export const TodoFilter = (
+  {
+    setFilterType,
+    handleInputChange,
+    handleQueryDelete,
+    query,
+  }
+  : TodoFilterProps,
+) => {
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select data-cy="statusSelect">
+            <option
+              value="all"
+              onClick={() => setFilterType('all')}
+            >
+              All
+            </option>
+            <option
+              value="active"
+              onClick={() => setFilterType('active')}
+            >
+              Active
+            </option>
+            <option
+              value="completed"
+              onClick={() => setFilterType('completed')}
+            >
+              Completed
+            </option>
+          </select>
+        </span>
+      </p>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={handleInputChange}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {query && (
+            // eslint-disable-next-line jsx-a11y/control-has-associated-label
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleQueryDelete}
+            />
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
