a78ab462ee8a6c09896360cc7066e6b0d7943d23
Natalia Klonowska <natalia742@gmail.com>
2023-05-31 23:54:27 +0200

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,93 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { ChangeEvent, useState } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface TodoFilterProps {
+  filterTodos: (newTodos: Todo[]) => void;
+  todos: Todo[],
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter = ({ filterTodos, todos }: TodoFilterProps) => {
+  const [selectedFilter, setSelectedFilter] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const filteredTodos = (filter: string, newQuery: string) => {
+    return todos.filter(todo => {
+      const lowerQuery = newQuery.toLowerCase();
+      const hasTitle = todo.title.toLowerCase().includes(lowerQuery);
+
+      if (!hasTitle) {
+        return false;
+      }
+
+      switch (filter) {
+        case 'all':
+          return true;
+        case 'active':
+          return !todo.completed;
+        case 'completed':
+          return todo.completed;
+        default:
+          throw new Error('wrong filter selected');
+      }
+    });
+  };
+
+  const reset = () => {
+    setQuery('');
+    filterTodos(filteredTodos(selectedFilter, ''));
+  };
+
+  const handleSelectedFilter = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedFilter(String(event.target.value));
+    filterTodos(filteredTodos(String(event.target.value), query));
+  };
+
+  const handleFilter = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(String(event.target.value));
+    filterTodos(filteredTodos(selectedFilter, String(event.target.value)));
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectedFilter}
+            onChange={handleSelectedFilter}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={(event) => handleFilter(event)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query !== '' && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => reset()}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,71 @@
-import React from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+interface TodoModalProps {
+  selectedUser: User | undefined,
+  selectedTodo: Todo | undefined,
+  reset: () => void;
+  isSelectedLoading: boolean,
+}
+
+export const TodoModal = ({
+  selectedUser, selectedTodo, reset, isSelectedLoading,
+}:TodoModalProps) => {
+  // const [selectedUser, setSelectedUser] = useState<User>();
+  // const [selectedTodo, setSelectedTodo] = useState<Todo>();
+
+  // useEffect(() => {
+  //   setSelectedTodo(todos.find(todo => todo.id === selectedId));
+  //   getUser(selectedTodo.userId).then(setSelectedUser);
+  //     // eslint-disable-next-line no-console
+  //     console.log('user', selectedUser);
+  // }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+      {isSelectedLoading
+        ? <Loader />
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${selectedTodo?.id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => reset()}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {selectedTodo?.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                <strong className={`has-text-${selectedTodo?.completed ? 'success' : 'danger'}`}>
+                  {selectedTodo?.completed ? 'Done' : 'Planned'}
+                </strong>
+
+                {' by '}
+
+                <a href={`mailto:${selectedUser?.email}`}>
+                  {selectedUser?.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
