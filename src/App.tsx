302261617506741dc7ea954dbec17f4c4c2abfb6
Ruslan Pashkov <nothingsfading@gmail.com>
2023-06-12 23:48:20 +0300

    Add React Dynamic List of Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,27 +8,103 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { Todo } from './types/Todo';
+
+import { normalize } from './helpers/normalize';
+
+import { getTodos } from './api';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [status, setStatus] = useState('all');
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  const selectTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const clearSelectedTodo = () => {
+    setSelectedTodo(null);
+  };
+
+  const filterQuery = (value: string) => {
+    setQuery(value);
+  };
+
+  const selectStatus = (value: string) => {
+    setStatus(value);
+  };
+
+  const visibleTodos = todos.filter((todo) => {
+    const normalizedQuery = normalize(query);
+    const normalizedTodo = normalize(todo.title);
+
+    const isTodoIncluded = normalizedTodo.includes(normalizedQuery);
+
+    let isStatusMatch: boolean;
+
+    switch (status) {
+      case 'active':
+        isStatusMatch = !todo.completed;
+        break;
+
+      case 'completed':
+        isStatusMatch = todo.completed;
+        break;
+
+      default:
+        isStatusMatch = true;
+    }
+
+    return isTodoIncluded && isStatusMatch;
+  });
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
-
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                status={status}
+                onFilter={filterQuery}
+                onSelectStatus={selectStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoaded
+                ? (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodo={selectedTodo}
+                    onSelect={selectTodo}
+                  />
+                )
+                : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onClose={clearSelectedTodo}
+        />
+      )}
     </>
   );
 };
