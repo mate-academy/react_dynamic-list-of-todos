414b1293c437dba2aac01d9b2e85226e49b837b3
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-06-16 11:19:41 +0300

    have removed all comments from the code

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -61,7 +61,6 @@ export const TodoModal: FC<Props> = ({
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
               {!completed
                 ? <strong className="has-text-danger">Planned</strong>
                 : <strong className="has-text-success">Done</strong>}
