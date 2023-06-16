c5c117bfcaf0f27ae3443ba02629ed01d8aacfbe
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-06-16 18:10:54 +0300

    fixed

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -65,7 +65,7 @@ export const TodoModal: FC<Props> = ({
                 ? <strong className="has-text-danger">Planned</strong>
                 : <strong className="has-text-success">Done</strong>}
 
-              <span>{'\u00A0by\u00A0'}</span>
+              <span>by</span>
 
               <a href={user?.email}>
                 {user?.name}
