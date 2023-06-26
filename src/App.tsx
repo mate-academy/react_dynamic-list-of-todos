9d1a04e364a714d593b88cf72ee26f4f31fda95e
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-06-26 18:54:00 +0300

    Solution1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,44 @@ import { Loader } from './components/Loader';
 import { getTodos } from './api';
 import { Todo } from './types/Todo';
 
+enum SortType {
+  ALL,
+  ACTIVE,
+  COMPLETED,
+}
+
+type DataSort = {
+  sortType: SortType;
+  query: string;
+};
+
+export function filterTodos(todos: Todo[], { sortType, query }: DataSort) {
+  let newTodos = [...todos];
+
+  newTodos = newTodos.filter(todo => {
+    return todo.title.toLowerCase().includes(query.toLowerCase());
+  });
+
+  if (sortType === SortType.ALL) {
+    return newTodos;
+  }
+
+  newTodos = newTodos.filter(todo => {
+    switch (sortType) {
+      case SortType.ACTIVE:
+        return !todo.completed;
+
+      case SortType.COMPLETED:
+        return todo.completed;
+
+      default:
+        return true;
+    }
+  });
+
+  return newTodos;
+}
+
 const debounce = (f: (...args: string[]) => void, delay: number) => {
   let timerId: NodeJS.Timeout;
 
@@ -25,21 +63,31 @@ const DEBOUNCE_DELAY = 1000;
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [filterTodos, setFilterTodos] = useState<Todo[]>([]);
-  const [sortMethod, setSortMethod] = useState('all');
-  const [applieQuery, setApplieQuery] = useState('');
+  const [sortType, setSortType] = useState<SortType>(SortType.ALL);
+  const [query, setQuery] = useState('');
   const [iLoading, setIsLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
   const [selectTodo, setSelectTodo] = useState<Todo | null>(null);
   const [visibiliteComponent, setVisibiliteComponent] = useState(false);
 
   const apllyQuery = useCallback(
-    debounce(setApplieQuery, DEBOUNCE_DELAY),
+    debounce(setQuery, DEBOUNCE_DELAY),
     [],
   );
 
   const handleFilterChange = (methodSort: string) => {
-    setSortMethod(methodSort);
+    switch (methodSort) {
+      case 'active':
+        setSortType(SortType.ACTIVE);
+        break;
+
+      case 'completed':
+        setSortType(SortType.COMPLETED);
+        break;
+
+      default:
+        setSortType(SortType.ALL);
+    }
   };
 
   const handleQueryText = (queryText: string) => {
@@ -47,31 +95,11 @@ export const App: React.FC = () => {
   };
 
   useEffect(() => {
-    const newFilterTodos = todos.filter(todo => {
-      switch (sortMethod) {
-        case 'active':
-          return !todo.completed;
-
-        case 'completed':
-          return todo.completed;
-
-        default:
-          return true;
-      }
-    }).filter(todo => {
-      return todo.title.toLowerCase().includes(applieQuery.toLowerCase());
-    });
-
-    setFilterTodos(newFilterTodos);
-  }, [todos, sortMethod, applieQuery]);
-
-  useEffect(() => {
     const loadTodos = async () => {
       try {
         const loadedTodos = await getTodos();
 
         setTodos(loadedTodos);
-        setFilterTodos(loadedTodos);
         setIsLoading(false);
       } catch (error) {
         if (error instanceof Error) {
@@ -84,6 +112,8 @@ export const App: React.FC = () => {
 
     loadTodos();
   }, []);
+
+  const visibleTodos = filterTodos(todos, { sortType, query });
 
   const choiseTodo = (todoId: number) => {
     setVisibiliteComponent(true);
@@ -118,7 +148,7 @@ export const App: React.FC = () => {
               {iLoading && <Loader />}
               <TodoList
                 errorMessage={errorMessage}
-                todos={filterTodos}
+                todos={visibleTodos}
                 handleClick={choiseTodo}
                 selectTodo={selectTodo}
               />
