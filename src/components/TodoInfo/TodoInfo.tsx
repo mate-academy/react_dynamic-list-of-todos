4a4aa93b8d9dc8702c2d6fa5362e2be4ff364138
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-06-27 19:00:13 +0300

    Add task solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+  selectedTodo: Todo | null;
+  onSelect: (todo: Todo) => void;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo,
+  selectedTodo,
+  onSelect,
+}) => {
+  const { id, title, completed } = todo;
+
+  const isTodoSelected = id === selectedTodo?.id;
+
+  return (
+    <tr
+      data-cy="todo"
+      className={cn({
+        'has-background-info-light': isTodoSelected,
+      })}
+    >
+      <td className="is-vcentered">
+        {id}
+      </td>
+
+      <td className="is-vcentered">
+        {completed && (
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+
+      <td className="is-vcentered is-expanded">
+        <p
+          className={cn({
+            'has-text-success': completed,
+            'has-text-danger': !completed,
+          })}
+        >
+          {title}
+        </p>
+      </td>
+
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={() => onSelect(todo)}
+        >
+          <span className="icon">
+            <i className={cn('far', {
+              'fa-eye': !isTodoSelected,
+              'fa-eye-slash': isTodoSelected,
+            })}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,77 @@
-import React from 'react';
-import { Loader } from '../Loader';
+import React, { useEffect, useState } from 'react';
 
-export const TodoModal: React.FC = () => {
+import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todo: Todo;
+  onClose: () => void;
+}
+
+export const TodoModal: React.FC<Props> = React.memo(({ todo, onClose }) => {
+  const {
+    id,
+    title,
+    completed,
+    userId,
+  } = todo;
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUser(userId)
+      .then(userFromServer => {
+        setUser(userFromServer);
+      });
+  }, []);
+
+  const handleCloseButtonClick = () => onClose();
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
+      {!user
+        ? <Loader />
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${id}`}
+              </div>
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+              <button
+                type="button"
+                className="delete"
+                aria-label="close"
+                data-cy="modal-close"
+                onClick={handleCloseButtonClick}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {completed
+                  ? <strong className="has-text-success">Done</strong>
+                  : <strong className="has-text-danger">Planned</strong>}
+
+                {' by '}
+
+                <a href={`mailto:${user.email}`}>
+                  {user.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
-};
+});
