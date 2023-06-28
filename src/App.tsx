69d84562dd44fc1186a8a7bc3e81448d49bcef46
Kiril Shorin <kirillshorin8838@gmail.com>
2023-06-29 00:06:34 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,49 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [isSelected, setIsSelected] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [selectStatus, setSelectStatus] = useState('all');
+
+  const getTodosFromApi = async (callback: () => Promise<Todo[]>) => {
+    try {
+      const todosFromApi = await callback();
+
+      setTodos(todosFromApi);
+    } catch (error) {
+      throw new Error();
+    }
+  };
+
+  useEffect(() => {
+    getTodosFromApi(getTodos);
+  }, []);
+
+  const [selectedTodo, setSelectedTodo] = useState<Todo>(todos[0]);
+
+  let visibleTodos = todos.filter(todo => (
+    todo.title.toLowerCase().includes(query.toLowerCase().trim())));
+
+  switch (selectStatus) {
+    case 'active':
+      visibleTodos = visibleTodos.filter(todo => (
+        todo.completed === false));
+      break;
+
+    case 'completed':
+      visibleTodos = visibleTodos.filter(todo => (
+        todo.completed === true));
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                setSelectStatus={setSelectStatus}
+              />
             </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
+            {todos.length === 0
+              ? (
+                <Loader />
+              ) : (
+                <div className="block">
+                  <TodoList
+                    todos={visibleTodos}
+                    setIsSelected={setIsSelected}
+                    isSelected={isSelected}
+                    setSelectedTodo={setSelectedTodo}
+                  />
+                </div>
+              )}
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isSelected && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          isSelected={isSelected}
+          setIsSelected={setIsSelected}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
-  return (
-    <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+interface Props {
+  selectedTodo: Todo
+  isSelected: boolean,
+  setIsSelected: (value: boolean) => void,
+}
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+export const TodoModal: React.FC<Props> = ({
+  selectedTodo,
+  isSelected,
+  setIsSelected,
+}) => {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    getUser(selectedTodo.userId)
+      .then(setUser)
+      // eslint-disable-next-line no-console
+      .catch((error) => console.log(error));
+  });
+
+  if (isSelected) {
+    return (
+      <div className="modal is-active" data-cy="modal">
+        <div className="modal-background" />
+
+        {!user ? (
+          <Loader />
+        ) : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${selectedTodo.id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => setIsSelected(false)}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {selectedTodo.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {/* <strong className="has-text-success">Done</strong> */}
+                {selectedTodo.completed
+                  ? <strong className="has-text-success">Completed</strong>
+                  : <strong className="has-text-danger">Planned</strong>}
+
+                {' by '}
+
+                <a href={`mailto:${user.email}`}>
+                  {user.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
+
+  return null;
 };
