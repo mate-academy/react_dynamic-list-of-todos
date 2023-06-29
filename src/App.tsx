40fbb5d4cc85f2d27c52cd833e77117dc4f08c7a
Oleksii Lysenko <nano77@ukr.net>
2023-06-30 00:54:56 +0300

    add functionality to dynamic-list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './App.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [filterQuery, setFilterQuery] = useState('all');
+  const [searchQuery, setSearchQuery] = useState<null | string>(null);
+  const [selectedTodo, setSelectedTodo] = useState<null | Todo>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(response => setTodos(response))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    const rawTodos = filterQuery === 'all'
+      ? todos
+      : todos.filter(todo => {
+        if (filterQuery === 'active') {
+          return !todo.completed;
+        }
+
+        return todo.completed;
+      });
+
+    if (searchQuery) {
+      return rawTodos.filter(todo => (
+        todo.title.toLowerCase().includes(searchQuery.toLowerCase())
+      ));
+    }
+
+    return rawTodos;
+  }, [todos, filterQuery, searchQuery]);
+
+  const applyFilter = useCallback((value) => setFilterQuery(value), []);
+  const applyQuery = useCallback((value) => setSearchQuery(value), []);
+  const clearSelectedTodo = useCallback(() => setSelectedTodo(null), []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterQuery={filterQuery}
+                applyFilter={applyFilter}
+                applyQuery={applyQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && visibleTodos
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodo={selectedTodo}
+                    choseTodo={setSelectedTodo}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          onClose={clearSelectedTodo}
+          todo={selectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { memo } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
 
-export const TodoList: React.FC = () => (
+type Props = {
+  todos: Todo[],
+  selectedTodo: null | Todo
+  choseTodo: (todo: null | Todo) => void,
+};
+export const TodoList: React.FC<Props> = memo(({
+  todos,
+  selectedTodo,
+  choseTodo,
+}) => (
   <table className="table is-narrow is-fullwidth">
     <thead>
       <tr>
@@ -16,20 +27,58 @@ export const TodoList: React.FC = () => (
     </thead>
 
     <tbody>
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
+      {todos.map(todo => {
+        const isSelected = todo.id === selectedTodo?.id;
+
+        return (
+          <tr
+            data-cy="todo"
+            className={classNames({
+              'has-background-info-light': isSelected,
+            })}
+            key={todo.id}
+          >
+            <td className="is-vcentered">{todo.id}</td>
+            <td className="is-vcentered">
+              {todo.completed && (
+                <span className="icon" data-cy="iconCompleted">
+                  <i className="fas fa-check" />
+                </span>
+              )}
+            </td>
+            <td className="is-vcentered is-expanded">
+              <p
+                className={classNames({
+                  'has-text-danger': !todo.completed,
+                  'has-text-success': todo.completed,
+                })}
+              >
+                {todo.title}
+              </p>
+            </td>
+            <td className="has-text-right is-vcentered">
+              <button
+                data-cy="selectButton"
+                className="button"
+                type="button"
+                onClick={() => {
+                  choseTodo(todo);
+                }}
+              >
+                <span className="icon">
+                  <i
+                    className={classNames('far', {
+                      'fa-eye': !isSelected,
+                      'fa-eye-slash': isSelected,
+                    })}
+                  />
+                </span>
+              </button>
+            </td>
+          </tr>
+        );
+      })}
+      {/*
       <tr data-cy="todo" className="has-background-info-light">
         <td className="is-vcentered">2</td>
         <td className="is-vcentered" />
@@ -94,7 +143,7 @@ export const TodoList: React.FC = () => (
             </span>
           </button>
         </td>
-      </tr>
+      </tr> */}
     </tbody>
   </table>
-);
+));
