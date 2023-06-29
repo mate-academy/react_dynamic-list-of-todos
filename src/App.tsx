3eccb6b93bc4c2a255da5a500ae94f77d99eb3ca
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-06-29 14:48:31 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './app.scss';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [todoStatus, setTodoStatus] = useState('all');
+  const [todoModalId, setTodoModalId] = useState<null | number>(null);
+  const [selectedTodo, setSelectedTodo] = useState<null | Todo>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .finally(() => (setIsLoading(false)));
+  }, []);
+
+  let visibleTodos = todos;
+
+  switch (todoStatus) {
+    case 'active':
+      visibleTodos = visibleTodos.filter(todo => !todo.completed);
+      break;
+
+    case 'completed':
+      visibleTodos = visibleTodos.filter(todo => todo.completed);
+      break;
+
+    default:
+      break;
+  }
+
+  if (searchQuery) {
+    visibleTodos = visibleTodos.filter(todo => todo.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  }
+
   return (
     <>
       <div className="section">
@@ -16,19 +51,39 @@ export const App: React.FC = () => {
           <div className="box">
             <h1 className="title">Todos:</h1>
 
-            <div className="block">
-              <TodoFilter />
+            <div
+              className="block"
+            >
+              <TodoFilter
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+                todoStatus={todoStatus}
+                setTodoStatus={setTodoStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              {!isLoading && (
+                <TodoList
+                  todos={visibleTodos}
+                  setTodoModalId={setTodoModalId}
+                  setSelectedTodo={setSelectedTodo}
+                  todoModalId={todoModalId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {todoModalId && (
+        <TodoModal
+          userId={todoModalId}
+          selectedTodo={selectedTodo}
+          setTodoModalId={setTodoModalId}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,24 @@
-export const TodoFilter = () => (
+interface Props {
+  searchQuery: string,
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>
+  todoStatus: string,
+  setTodoStatus: React.Dispatch<React.SetStateAction<string>>,
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  searchQuery,
+  setSearchQuery,
+  todoStatus,
+  setTodoStatus,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={todoStatus}
+          onChange={event => setTodoStatus(event.target.value)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -14,21 +30,29 @@ export const TodoFilter = () => (
       <input
         data-cy="searchInput"
         type="text"
+        value={searchQuery}
         className="input"
         placeholder="Search..."
+        onChange={(event) => setSearchQuery(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
-      </span>
+      {searchQuery && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => {
+              setTodoStatus('all');
+              setSearchQuery('');
+            }}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
