b36ec925c0d399fae729b2d3e7d144f12f64b5d7
Maksim Kadygrob <kadygrobmaksim33@gmail.com>
2023-06-30 10:17:45 +0300

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,67 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { FC } from 'react';
+import { FilterType } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  statusToFilterBy: FilterType;
+  setStatusToFilterBy: (status: FilterType) => void;
+  filterQuery: string;
+  setFilterQuery: (query: string) => void;
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: FC<Props> = ({
+  statusToFilterBy,
+  setStatusToFilterBy,
+  filterQuery,
+  setFilterQuery,
+}) => {
+  const filterByOption = Object.entries(FilterType);
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={statusToFilterBy}
+            onChange={(event) => setStatusToFilterBy(
+              event.target.value as FilterType,
+            )}
+          >
+            {filterByOption.map(([label, value]) => (
+              <option value={value} key={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={filterQuery}
+          onChange={(event) => setFilterQuery(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {filterQuery && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              aria-label="Clear Search Button"
+              onClick={() => setFilterQuery('')}
+            />
+          </span>
+        )}
+
+      </p>
+    </form>
+  );
+};
