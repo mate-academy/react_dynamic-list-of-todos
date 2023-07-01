4064e2107d963fb93acfa522b410a26db6533c02
Viktor Panchenko <panch.v1811@gmail.com>
2023-07-01 19:47:01 +0200

    react_dynamic-list-of-todos solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,56 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { StatusFilter } from './types/StatusFilter';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectorStatus, setSelectorStatus] = useState(StatusFilter.ALL);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        throw new Error('Loading todos error:', error.message);
+      });
+  }, []);
+
+  const visibleTodos = todos.filter(todo => {
+    const normalizedTodo
+    = todo.title.toLowerCase().trim().includes(searchQuery.toLowerCase());
+
+    switch (selectorStatus) {
+      case StatusFilter.ALL:
+        return normalizedTodo;
+      case StatusFilter.ACTIVE:
+        return normalizedTodo && !todo.completed;
+      case StatusFilter.COMPLETED:
+        return normalizedTodo && todo.completed;
+      default:
+        throw new Error('Unknown status selector');
+    }
+  });
+
+  const selectTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const filteredSearchInput = (value: string) => {
+    setSearchQuery(value);
+  };
+
+  const closeSelectedTodo = () => {
+    setSelectedTodo(null);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +64,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                searchQuery={searchQuery}
+                onSearch={filteredSearchInput}
+                onSelectStatus={setSelectorStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading
+                ? (
+                  <Loader />
+                ) : (
+                  <TodoList
+                    todos={visibleTodos}
+                    selectedTodo={selectedTodo}
+                    onSelectTodo={selectTodo}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onClose={closeSelectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,75 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { FC, ChangeEvent } from 'react';
+import { StatusFilter } from '../../types/StatusFilter';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  searchQuery: string;
+  onSearch: (searchInput: string) => void;
+  onSelectStatus(StatusSelector: StatusFilter): void;
+}
+export const TodoFilter: FC<Props> = ({
+  searchQuery,
+  onSearch,
+  onSelectStatus,
+}) => {
+  const handleSelectStatus = (event: ChangeEvent<HTMLSelectElement>) => {
+    onSelectStatus(event.target.value as StatusFilter);
+  };
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+  const handleSearchQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    onSearch(event.target.value);
+  };
+
+  const handleClearSearch = () => {
+    onSearch('');
+  };
+
+  const handlePreventDefault = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      className="field has-addons"
+      onSubmit={handlePreventDefault}
+    >
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={handleSelectStatus}
+          >
+            <option value={StatusFilter.ALL}>All</option>
+            <option value={StatusFilter.ACTIVE}>Active</option>
+            <option value={StatusFilter.COMPLETED}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchQuery}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {searchQuery && (
+            <button
+              aria-label="clearSearch"
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleClearSearch}
+            />
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
