eabf9e4cfc2d70c80c3470146915500b28010685
Skrudj <ssinilov@gmail.com>
2023-07-13 12:38:00 +0300

    impelement TodoList, TodoInfo components and onTodoSelect function

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import { TodoList } from './components/TodoList';
+// import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
+// import { TodoModal } from './components/TodoModal';
+// import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  // const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+//
+  // const onTodoSelect = (event: MouseEvent<HTMLButtonElement>, todoId: number) => {
+  // event.preventDefault();
+  // setSelectedTodoId(todoId);
+  // };
+
   return (
     <>
       <div className="section">
@@ -21,14 +28,14 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {/* <Loader /> */}
+              {/* <TodoList /> */}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
   );
 };
---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,63 @@
+import { MouseEvent } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+  selectedTodoId: number | null;
+  onTodoSelect: (event: MouseEvent<HTMLButtonElement>, todoId: number) => void;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo,
+  selectedTodoId,
+  onTodoSelect,
+}) => {
+  const { id, title, completed } = todo;
+  const selected = selectedTodoId === id;
+
+  return (
+    <tr
+      data-cy="todo"
+      className={classNames({
+        'has-background-info-light': selected,
+      })}
+    >
+      <td className="is-vcentered">{id}</td>
+      {completed ? (
+        <td className="is-vcentered">
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        </td>
+      ) : (
+        <td className="is-vcentered" />
+      )}
+      <td className="is-vcentered is-expanded">
+        <p className={classNames({
+          'has-text-danger': !completed,
+          'has-text-success': completed,
+        })}
+        >
+          {title}
+        </p>
+      </td>
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={event => onTodoSelect(event, id)}
+        >
+          <span className="icon">
+            <i className={classNames({
+              'far fa-eye': !selected,
+              'far fa-eye-slash': selected,
+            })}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
