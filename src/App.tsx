b4f5d702fafa805c89dd00155501699cded582b6
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-07-16 01:23:40 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,52 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selected, setSelected] = useState<Todo | null>();
+  const [filterSelect, setFilterSelect] = useState('');
+  const [search, setSearch] = useState('');
+
+  const getTodosFromServer = async () => {
+    let todosArr;
+
+    try {
+      todosArr = await getTodos();
+    } catch (error) {
+      throw new Error('Error todos loading');
+    }
+
+    switch (filterSelect) {
+      case 'all':
+        break;
+      case 'active':
+        todosArr = todosArr.filter(item => item.completed);
+        break;
+      case 'completed':
+        todosArr = todosArr.filter(item => !item.completed);
+        break;
+      default:
+        break;
+    }
+
+    const str = search.toLowerCase();
+
+    todosArr = todosArr.filter((item) => {
+      const title = item.title.toLowerCase();
+
+      return title.includes(str);
+    });
+
+    setTodos(todosArr);
+  };
+
+  useEffect(() => {
+    getTodosFromServer();
+  }, [filterSelect, selected, search]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                search={search}
+                selectMethod={setFilterSelect}
+                searchWord={setSearch}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {
+                todos.length === 0
+                  ? <Loader />
+                  : <TodoList todos={todos} selectMethod={(value) => setSelected(value)} selected={selected} />
+              }
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {
+        selected && (
+          <TodoModal
+            selectedTodo={selected}
+            selectMethod={setSelected}
+          />
+        )
+      }
     </>
   );
 };
