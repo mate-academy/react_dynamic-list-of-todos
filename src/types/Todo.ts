d39dc712eb61beff67be332c6928de2ebbbb3441
IlonaBorkovska <borkovska.io@gmail.com>
2023-07-19 12:27:40 +0300

    add task solution

--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,6 +1,15 @@
+import { User } from './User';
+
 export interface Todo {
   id: number;
   title: string;
   completed: boolean;
   userId: number;
+  user?: User | null;
+}
+
+export enum FilterBy {
+  COMPLETED = 'completed',
+  ACTIVE = 'active',
+  ALL = 'all',
 }
