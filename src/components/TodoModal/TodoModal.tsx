a651fad78c9070c7e77ba27c2543219a592be193
Eugene Kulish <genekulish13@gmail.com>
2023-07-22 13:09:00 +0300

    fix emails

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -83,7 +83,7 @@ export const TodoModal: React.FC<Props> = ({
 
                 {' by '}
 
-                <a href="mailto:Sincere@april.biz">
+                <a href={`mailto:${user?.email}`}>
                   {user?.name}
                 </a>
               </p>
