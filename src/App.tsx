6e6206b3026da57c4b4e1f8ca81369bfbef951d6
Valentyna <vabo1108@gmail.com>
2023-07-26 17:23:16 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,69 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos, getUser } from './api';
+import { User } from './types/User';
+
+const preparedTodos = (
+  todos: Todo[],
+  query: string,
+  filteredBy: string,
+) => {
+  let copyTodos = [...todos];
+  const normalizeQuary = query.trim().toLowerCase();
+
+  if (query) {
+    copyTodos = copyTodos.filter(todo => todo.title.toLowerCase().includes(normalizeQuary));
+  }
+
+  if (filteredBy) {
+    copyTodos = copyTodos.filter(todo => {
+      switch (filteredBy) {
+        case 'all':
+          return true;
+
+        case 'active':
+          return todo.completed === false;
+
+        case 'completed':
+          return todo.completed === true;
+
+        default:
+          return false;
+      }
+    });
+  }
+
+  return copyTodos;
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredBy, setFilteredBy] = useState('all');
+  const [isActive, setIsActive] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [query, setQuery] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const visibleTodos = preparedTodos(todos, query, filteredBy);
+
+  useEffect(() => {
+    setLoaded(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoaded(false));
+  }, []);
+
+  useEffect(() => {
+    if (!selectedTodo) {
+      return;
+    }
+
+    getUser(selectedTodo?.userId).then(setSelectedUser);
+  }, [selectedTodo]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +78,40 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                filteredBy={filteredBy}
+                setFilteredBy={setFilteredBy}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loaded ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos}
+                  selectedTodo={selectedTodo}
+                  setSelectedTodo={setSelectedTodo}
+                  isActive={isActive}
+                  setIsActive={setIsActive}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {isActive && (
+        <TodoModal
+          isActive={isActive}
+          setIsActive={setIsActive}
+          selectedTodo={selectedTodo}
+          setSelectedTodo={setSelectedTodo}
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import classNames from 'classnames';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: Todo | null,
+  setSelectedTodo: (todo: Todo | null) => void,
+  isActive: boolean,
+  setIsActive: (value: boolean) => void,
+  selectedUser: User | null,
+  setSelectedUser: (user: User | null) => void,
+};
+export const TodoModal: React.FC<Props> = ({
+  selectedTodo,
+  setSelectedTodo,
+  isActive,
+  setIsActive,
+  selectedUser,
+  setSelectedUser,
+}) => {
+  function handleModalCloser() {
+    setIsActive(false);
+    setSelectedTodo(null);
+    setSelectedUser(null);
+  }
+
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div
+      className={classNames('modal', {
+        'is-active': isActive,
+      })}
+      data-cy="modal"
+    >
       <div className="modal-background" />
 
-      {true ? (
+      {!selectedUser ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +43,8 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #
+              {selectedTodo?.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +52,32 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={handleModalCloser}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong className={classNames(
+                { 'has-text-danger': !selectedTodo?.completed },
+                { 'has-text-success': selectedTodo?.completed },
+              )}
+              >
+                {selectedTodo?.completed ? (
+                  'Done'
+                ) : (
+                  'Planned'
+                )}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${selectedUser?.email}`}>
+                {selectedUser?.name}
               </a>
             </p>
           </div>
