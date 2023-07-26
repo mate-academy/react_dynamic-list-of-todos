b5888724266db9f6307689d78d11a80fb47bcc65
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-07-27 00:31:48 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,94 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { Todo } from './types/Todo';
+import { getTodos, getUser } from './api';
 import { Loader } from './components/Loader';
+import { TodoModal } from './components/TodoModal';
+import { User } from './types/User';
+
+type FilterArgs = {
+  searchField: string;
+  sortField: 'completed' | 'active' | '';
+};
+
+function filteredTodos(todos: Todo[], { searchField, sortField }: FilterArgs) {
+  let visibleFilteredTodos = [...todos];
+
+  if (searchField) {
+    const searchFieldNormalize = searchField.toLowerCase().trim();
+
+    visibleFilteredTodos = visibleFilteredTodos.filter((todo) => {
+      return todo.title.toLowerCase().trim().includes(searchFieldNormalize);
+    });
+  }
+
+  if (sortField) {
+    visibleFilteredTodos = visibleFilteredTodos.filter((todo) => {
+      switch (sortField) {
+        case 'active':
+          return !todo.completed;
+        case 'completed':
+          return todo.completed;
+        default:
+          return todo;
+      }
+    });
+  }
+
+  return visibleFilteredTodos;
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [visibleTodo, setVisibleTodo] = useState<Todo | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [visibleUser, setVisibleUser] = useState<User | null>(null);
+  const [isLoadingUser, setIsLoadingUser] = useState(false);
+  const [searchField, setSearchField] = useState('');
+  const [sortField, setSortField] = useState<'completed' | 'active' | ''>('');
+
+  console.log(visibleUser, isLoadingUser);
+
+  const getUserById = (userId: number) => {
+    setIsLoadingUser(true);
+    getUser(userId)
+      .then((user) => setVisibleUser(user))
+      .finally(() => setIsLoadingUser(false));
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then((todo) => setTodos(todo))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const updateTodo = (todo: Todo) => {
+    setVisibleTodo(todo);
+  };
+
+  const updateSearchField = (str: string) => {
+    setSearchField(str);
+  };
+
+  const updateSortField = (str: 'completed' | 'active' | '') => {
+    setSortField(str);
+  };
+
+  const visibleTodos = filteredTodos(todos, { searchField, sortField });
+
   return (
     <>
       <div className="section">
@@ -17,18 +97,39 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                updateSearchField={updateSearchField}
+                searchField={searchField}
+                updateSortField={updateSortField}
+                sortField={sortField}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos}
+                  toggleModal={toggleModal}
+                  isModalOpen={isModalOpen}
+                  updateTodo={updateTodo}
+                  getUserById={getUserById}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalOpen && (
+        <TodoModal
+          toggleModal={toggleModal}
+          todo={visibleTodo}
+          user={visibleUser}
+          isLoadingUser={isLoadingUser}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,64 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/jsx-curly-newline */
+import React from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  updateSearchField: (str: string) => void;
+  updateSortField: (str: 'completed' | 'active' | '') => void;
+  searchField: string;
+  sortField: 'completed' | 'active' | '';
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({
+  updateSearchField,
+  updateSortField,
+  searchField,
+  sortField,
+}) => {
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={(event) =>
+              updateSortField(event.target.value as 'completed' | 'active' | '')
+            }
+            value={sortField}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchField}
+          onChange={(event) => updateSearchField(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          {searchField && (
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => updateSearchField('')}
+            />
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
