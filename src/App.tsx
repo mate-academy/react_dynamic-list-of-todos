30e89958490c8a563a2619b80565a4ae0616ac13
Yana <yanaklymenko.od@gmail.com>
2023-07-26 19:59:36 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { Todo } from './types/Todo';
+import { SelectType } from './types/Enum';
+import { getTodos } from './api';
+import './App.scss';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
@@ -9,6 +13,45 @@ import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [filter, setFilter] = useState('');
+  const [selectedType, setSelectedType] = useState('All');
+
+  function getVisibleTodos(todos: Todo[]): void {
+    const normalizeFilter = filter.trim().toLowerCase();
+    let visibleTodos = [...todos];
+
+    if (normalizeFilter) {
+      visibleTodos = visibleTodos.filter((todo) => todo.title.toLowerCase().includes(normalizeFilter));
+    }
+
+    switch (selectedType) {
+      case SelectType.Active:
+        visibleTodos = visibleTodos.filter((todo) => !todo.completed);
+        break;
+      case SelectType.Completed:
+        visibleTodos = visibleTodos.filter((todo) => todo.completed);
+        break;
+
+      default:
+        break;
+    }
+
+    setTodoList(visibleTodos);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(getVisibleTodos)
+      .catch(() => setError('Try again later'))
+      .finally(() => setLoading(false));
+  }, [filter, selectedType]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +60,30 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setFilter={setFilter}
+                filter={filter}
+                setSelectedType={setSelectedType}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              {!loading && todoList && !error && (
+                <TodoList
+                  todoList={todoList}
+                  setSelectedTodo={setSelectedTodo}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal setSelectedTodo={setSelectedTodo} selectedTodo={selectedTodo} />
+      )}
     </>
   );
 };
---
+++ src/types/Enum.ts
@@ -0,0 +1,5 @@
+export enum SelectType {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
