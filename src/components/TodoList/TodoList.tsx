78f5a70103a5b126b480ab58f7ebd4080c68a862
Iryna <irinabolbat98@gmail.com>
2023-07-27 09:11:52 +0300

    remove memo

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,67 +8,65 @@ type Props = {
   selectedTodo: Todo | null,
 };
 
-export const TodoList: React.FC<Props> = React.memo(
-  ({
-    todos,
-    onSelect,
-    selectedTodo,
-  }) => (
-    <table className="table is-narrow is-fullwidth">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>
-            <span className="icon">
-              <i className="fas fa-check" />
-            </span>
-          </th>
-          <th>Title</th>
-          <th> </th>
+export const TodoList: React.FC<Props> = ({
+  todos,
+  onSelect,
+  selectedTodo,
+}) => (
+  <table className="table is-narrow is-fullwidth">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>
+          <span className="icon">
+            <i className="fas fa-check" />
+          </span>
+        </th>
+        <th>Title</th>
+        <th> </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {todos.map(todo => (
+        <tr data-cy="todo" className="" key={todo.id}>
+          <td className="is-vcentered">{todo.id}</td>
+
+          <td className="is-vcentered">
+            {todo.completed && (
+              <span className="icon" data-cy="iconCompleted">
+                <i className="fas fa-check" />
+              </span>
+            )}
+          </td>
+
+          <td className="is-vcentered is-expanded">
+            <p className={classNames({
+              'has-text-danger': !todo.completed,
+              'has-text-success': todo.completed,
+            })}
+            >
+              {todo.title}
+            </p>
+          </td>
+          <td className="has-text-right is-vcentered">
+            <button
+              data-cy="selectButton"
+              className="button"
+              type="button"
+              onClick={() => onSelect(todo)}
+            >
+              <span className="icon">
+                <i className={classNames('far', {
+                  'fa-eye': !selectedTodo,
+                  'fa-eye-slash': selectedTodo?.id === todo.id,
+                })}
+                />
+              </span>
+            </button>
+          </td>
         </tr>
-      </thead>
-
-      <tbody>
-        {todos.map(todo => (
-          <tr data-cy="todo" className="" key={todo.id}>
-            <td className="is-vcentered">{todo.id}</td>
-
-            <td className="is-vcentered">
-              {todo.completed && (
-                <span className="icon" data-cy="iconCompleted">
-                  <i className="fas fa-check" />
-                </span>
-              )}
-            </td>
-
-            <td className="is-vcentered is-expanded">
-              <p className={classNames({
-                'has-text-danger': !todo.completed,
-                'has-text-success': todo.completed,
-              })}
-              >
-                {todo.title}
-              </p>
-            </td>
-            <td className="has-text-right is-vcentered">
-              <button
-                data-cy="selectButton"
-                className="button"
-                type="button"
-                onClick={() => onSelect(todo)}
-              >
-                <span className="icon">
-                  <i className={classNames('far', {
-                    'fa-eye': !selectedTodo,
-                    'fa-eye-slash': selectedTodo?.id === todo.id,
-                  })}
-                  />
-                </span>
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  ),
+      ))}
+    </tbody>
+  </table>
 );
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -11,73 +11,71 @@ type Props = {
   onSelectedTodo: (todo: Todo | null) => void,
 };
 
-export const TodoModal: React.FC<Props> = React.memo(
-  ({
-    loading,
-    onLoading,
-    selectedTodo,
-    onSelectedTodo,
-  }) => {
-    const [user, setUser] = useState<User | null>(null);
+export const TodoModal: React.FC<Props> = ({
+  loading,
+  onLoading,
+  selectedTodo,
+  onSelectedTodo,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
 
-    useEffect(() => {
-      onLoading(true);
+  useEffect(() => {
+    onLoading(true);
 
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      getUser(selectedTodo!.userId)
-        .then(setUser)
-        .finally(() => onLoading(false));
-    }, [selectedTodo]);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    getUser(selectedTodo!.userId)
+      .then(setUser)
+      .finally(() => onLoading(false));
+  }, [selectedTodo]);
 
-    const isLoading = loading && (!selectedTodo || !user);
+  const isLoading = loading && (!selectedTodo || !user);
 
-    return (
-      <div className="modal is-active" data-cy="modal">
-        <div className="modal-background" />
+  return (
+    <div className="modal is-active" data-cy="modal">
+      <div className="modal-background" />
 
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <div
-                className="modal-card-title has-text-weight-medium"
-                data-cy="modal-header"
-              >
-                {`Todo #${selectedTodo?.id}`}
-              </div>
-
-              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-              <button
-                type="button"
-                className="delete"
-                data-cy="modal-close"
-                onClick={() => onSelectedTodo(null)}
-              />
-            </header>
-
-            <div className="modal-card-body">
-              <p className="block" data-cy="modal-title">
-                {selectedTodo?.title}
-              </p>
-
-              <p className="block" data-cy="modal-user">
-                {selectedTodo?.completed ? (
-                  <strong className="has-text-success">Done</strong>
-                ) : (
-                  <strong className="has-text-danger">Planned</strong>
-                )}
-
-                {' by '}
-
-                <a href={`mailto:${user?.email}`}>
-                  {user?.name}
-                </a>
-              </p>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${selectedTodo?.id}`}
             </div>
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => onSelectedTodo(null)}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {selectedTodo?.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {selectedTodo?.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+
+              {' by '}
+
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
+              </a>
+            </p>
           </div>
-        )}
-      </div>
-    );
-  },
-);
+        </div>
+      )}
+    </div>
+  );
+};
