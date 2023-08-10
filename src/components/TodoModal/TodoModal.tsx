e04547b8c59193699c93a17ac0964cf48db26203
Diana Tuz <dianatuz12@gmail.com>
2023-08-10 20:51:06 +0300

    done

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -29,51 +29,54 @@ export const TodoModal: React.FC<Props> = ({ todo, openModal, user }) => {
   }, []);
 
   return (
-    (isLoading && user) ? (
-      <Loader />
-    ) : (
-      <div
-        className="modal is-active"
-        data-cy="modal"
-      >
-        <div className="modal-background" />
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              {`Todo #${id}`}
+    <div
+      className="modal is-active"
+      data-cy="modal"
+    >
+      <div className="modal-background" />
+      {isLoading && <Loader />}
+
+      {
+        !isLoading && (
+
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => openModal(false)}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {completed
+                  ? <strong className="has-text-success">Done</strong>
+                  : <strong className="has-text-danger">Planned</strong>}
+                {' by '}
+
+                <a href={`mailto:${user.email}`}>
+                  {user.name}
+                </a>
+
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-              onClick={() => openModal(false)}
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              {title}
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {completed
-                ? <strong className="has-text-success">Done</strong>
-                : <strong className="has-text-danger">Planned</strong>}
-              {' by '}
-
-              <a href={`mailto:${user.email}`}>
-                {user.name}
-              </a>
-
-            </p>
           </div>
-        </div>
-      </div>
-    )
+        )
+      }
+    </div>
   );
 };
