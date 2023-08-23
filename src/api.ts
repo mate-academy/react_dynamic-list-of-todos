e83c79d36138ac28a9ded85d9982ee45152f03cb
Denis Kononenko <deniskononenko88@gmail.com>
2023-08-23 23:40:24 +0300

    add small fix

--- src/api.ts
+++ src/api.ts
@@ -17,7 +17,7 @@ function get<T>(url: string): Promise<T> {
   const fullURL = BASE_URL + url + '.json';
 
   // we add some delay to see how the loader works
-  return wait(500)
+  return wait(300)
     .then(() => fetch(fullURL))
     .then(res => res.json());
 }
