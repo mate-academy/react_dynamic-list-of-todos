a30073fdb5b3b69a00614a40e195de901529fca7
haduigon <haduigon@gmail.com>
2023-08-27 12:15:08 +0300

    added loader in ModalTodo

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,6 +1,7 @@
 // eslint-disable
+/* eslint-disable */
 import React, { useContext, useEffect, useState } from 'react';
-// import { Loader } from '../Loader';
+import { Loader } from '../Loader';
 import { getUser } from '../../api';
 import { StateContext, ACTIONS, DispatchContext } from '../ToDoContext';
 import { User } from '../../types/User';
@@ -10,14 +11,18 @@ export const TodoModal: React.FC = () => {
   const dispatch = useContext(DispatchContext);
   const { selectedTodo } = useContext(StateContext);
   const [userState, setUserState] = useState({} as User);
+  const [doWeNeedLoader, setDoWeNeedLaoder] = useState(false);
 
   function closeTodoModal() {
     dispatch({ type: ACTIONS.SET_TODO, payload: {} as Todo });
   }
 
   useEffect(() => {
+    setDoWeNeedLaoder(true);
     getUser(selectedTodo.id)
-      .then((res) => setUserState(res));
+      .then((res) => setUserState(res))
+      .catch((err) => console.log(err))
+      .finally(() => setDoWeNeedLaoder(false));
 
     return () => {
       setUserState({} as User);
@@ -28,45 +33,45 @@ export const TodoModal: React.FC = () => {
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {/* {true ? (
+      {doWeNeedLoader ? (
         <Loader />
-      ) : ( */}
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #2
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              Todo #2
+            </div>
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={closeTodoModal}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {selectedTodo.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {/* <strong className="has-text-success">Done</strong> */}
+              <strong className="has-text-danger">Planned</strong>
+
+              {' by '}
+
+              <a href="mailto:Sincere@april.biz">
+                {userState.name}
+              </a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            type="button"
-            className="delete"
-            data-cy="modal-close"
-            onClick={closeTodoModal}
-          />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            {selectedTodo.title}
-          </p>
-
-          <p className="block" data-cy="modal-user">
-            {/* <strong className="has-text-success">Done</strong> */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {' by '}
-
-            <a href="mailto:Sincere@april.biz">
-              {userState.name}
-            </a>
-          </p>
         </div>
-      </div>
-      {/* )} */}
+      )}
     </div>
   );
 };
