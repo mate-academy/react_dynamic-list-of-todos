a5da76295b0f2ecccf3a0c242f44b7e1ea1e9e3a
Volodymyr Shostak <shostak_1@proton.me>
2023-09-04 21:21:35 +0300

    fix

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import './Loader.scss';
-import { useTodos } from '../Context';
 
 export const Loader: React.FC = () => {
-  const { loading, loadingModal } = useTodos();
-
   return (
-    (loading || !loadingModal) ? (
-      <div className="Loader" data-cy="loader">
-        <div className="Loader__content" />
-      </div>
-    ) : null
+    <div className="Loader" data-cy="loader">
+      <div className="Loader__content" />
+    </div>
   );
 };
