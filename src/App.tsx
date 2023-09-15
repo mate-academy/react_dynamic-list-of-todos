a2f938a315a1863c2d5fc9bf31224f0ed8be6208
Andrew <deyneka2003@gmail.com>
2023-09-15 14:53:55 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,64 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
+import { Options } from './types/Options';
+import { getTodos } from './api';
+
+function getFilteredTodos(todos: Todo[], filter: Filter) {
+  // let filteredTodos = [...todos];
+
+  const filteredTodos: Todo[] = todos.filter(todo => {
+    switch (filter.option) {
+      case Options.active:
+        return !todo.completed;
+
+      case Options.completed:
+        return todo.completed;
+
+      case Options.all:
+      default:
+        return filteredTodos;
+    }
+  });
+
+  if (filter.query) {
+    filteredTodos.filter(({ title }) => title.includes(filter.query.trim()));
+  }
+
+  return filteredTodos;
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filter, setFilter] = useState<Filter>({ option: Options.all, query: '' });
+
+  const filteredTodos = getFilteredTodos(todos, filter);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const handleFilter = (currentFilter: Filter) => {
+    setFilter(currentFilter);
+  };
+
+  const handleShowModal = (todo: Todo) => {
+    setSelectedTodo(todo);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedTodo(null);
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +73,30 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter handleFilter={handleFilter} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  handleShowModal={handleShowModal}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {showModal && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          handleCloseModal={handleCloseModal}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,67 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useEffect, useState } from 'react';
+import { Filter } from '../../types/Filter';
+import { Options } from '../../types/Options';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  handleFilter: (filter: Filter) => void,
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({ handleFilter }) => {
+  const [query, setQuery] = useState('');
+  const [option, setOption] = useState<Options>(Options.all);
+
+  useEffect(() => {
+    handleFilter({ query, option });
+  }, [query, option]);
+
+  const handleChangeFilterOption
+    = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setOption(event.target.value as Options);
+    };
+
+  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery('');
+    setQuery(event.target.value);
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={option}
+            onChange={handleChangeFilterOption}
+          >
+            <option value={Options.all}>All</option>
+            <option value={Options.active}>Active</option>
+            <option value={Options.completed}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={handleChangeQuery}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+          />
+        </span>
+      </p>
+    </form>
+  );
+};
