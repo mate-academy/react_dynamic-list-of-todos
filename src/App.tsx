ad1f5a9fae20b5a49a80967c42cb1d35136a3e56
Roman Yatsykovych <55942779+yar0man@users.noreply.github.com>
2023-09-18 20:51:32 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,58 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { FilterTodos } from './types/FilterTodos';
+import { getTodos } from './api';
+
+const getFilteredTodos = (
+  todos: Todo[],
+  query: string,
+  selectedFilter: string,
+) => {
+  let todosCopy = [...todos];
+
+  if (query) {
+    const normalizedQuery = query.toLowerCase().trim();
+
+    todosCopy = todosCopy.filter(({ title }) => (
+      title.toLowerCase().includes(normalizedQuery)
+    ));
+  }
+
+  if (selectedFilter) {
+    todosCopy = todosCopy.filter(({ completed }) => {
+      switch (selectedFilter) {
+        case FilterTodos.Active:
+          return !completed;
+        case FilterTodos.Completed:
+          return completed;
+        default:
+          return todosCopy;
+      }
+    });
+  }
+
+  return todosCopy;
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+  const [selectedFilter, setSelectedFilter] = useState<FilterTodos>(FilterTodos.All);
+
+  useEffect(() => {
+    getTodos().then((todosFromSerwer) => {
+      setTodos(todosFromSerwer);
+    }).finally(() => setIsLoading(false));
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    return getFilteredTodos(todos, query, selectedFilter);
+  }, [todos, query, selectedFilter]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +67,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                onQueryChange={setQuery}
+                selectedFilter={selectedFilter}
+                onSelectedFilter={setSelectedFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              )
+                : (
+                  <TodoList
+                    todos={filteredTodos}
+                    selectedTodo={selectedTodo}
+                    onSelectedTodo={setSelectedTodo}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onSelectedTodo={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { FilterTodos } from '../../types/FilterTodos';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  query: string,
+  onQueryChange: (val: string) => void,
+  selectedFilter: string,
+  onSelectedFilter: (val: FilterTodos) => void,
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<Props> = ({
+  query,
+  onQueryChange,
+  selectedFilter,
+  onSelectedFilter,
+}) => {
+  const resetAllFilters = () => {
+    onQueryChange('');
+    onSelectedFilter(FilterTodos.All);
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={selectedFilter}
+            onChange={(event) => {
+              onSelectedFilter(event?.target.value as FilterTodos);
+            }}
+          >
+            {(Object.values(FilterTodos))
+              .map((value) => (
+                <option
+                  value={value}
+                  key={value}
+                >
+                  {value}
+                </option>
+              ))}
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={(event) => onQueryChange(event.target.value)}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={resetAllFilters}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
