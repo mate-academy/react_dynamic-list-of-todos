e33dc3a9d12551fb0263d7cd5adb2d7ec23e2b60
Viktoriia Roik <viktoriaroik2103@gmail.com>
2023-09-18 22:30:44 +0300

    added functionality for filtering list-of-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,50 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { SortingStatus } from './types/sortingStatus';
+import { getTodos } from './api';
+
+function getFilteredTodos(todos: Todo[], sortingStatus: string, query: string) {
+  const searchingQuery = query.trim().toLowerCase();
+  const todoWithQuery = todos.filter(todo => (
+    todo.title.toLowerCase().includes(searchingQuery)
+  ));
+
+  return todoWithQuery.filter(todo => {
+    switch (sortingStatus) {
+      case SortingStatus.Active:
+        return !todo.completed;
+
+      case SortingStatus.Completed:
+        return todo.completed;
+
+      default:
+        return true;
+    }
+  });
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+  const [sortingStatus, setSortingStatus] = useState<SortingStatus>(SortingStatus.All);
+  const [query, setQuery] = useState('');
+
+  const filteredTodos = useMemo(() => {
+    return getFilteredTodos(todos, sortingStatus, query);
+  }, [todos, sortingStatus, query]);
+
+  const selectedTodo = filteredTodos.find(todo => todo.id === selectedTodoId) || null;
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(setTodos)
+      .then(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +59,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                onQuery={setQuery}
+                onStatus={setSortingStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && (
+                <Loader />
+              )}
+
+              {!isLoading && filteredTodos.length > 0 && ((
+                <TodoList
+                  todos={filteredTodos}
+                  onSelect={setSelectedTodoId}
+                  selectedTodo={selectedTodoId}
+                />
+              )
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onSelect={(todoId: null) => setSelectedTodoId(todoId)}
+        />
+      )}
     </>
   );
 };
