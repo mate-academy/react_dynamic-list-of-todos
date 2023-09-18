58189616a69e245b4ded5a5de4a998090fa26a8f
Daryna Pidlutska <daryna.pidlutska@gmail.com>
2023-09-18 18:03:04 +0200

    add task solution

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+  selectedTodo: Todo | null;
+  onSelectTodo: (todo: Todo) => void;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  selectedTodo,
+  onSelectTodo,
+}) => {
+  const { id, title, completed } = todo;
+  const isTodoSelected = selectedTodo === todo;
+
+  return (
+    <tr
+      data-cy="todo"
+      className={cn({ 'has-background-info-light': isTodoSelected })}
+    >
+      <td className="is-vcentered">{id}</td>
+      <td className="is-vcentered">
+        {completed && (
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+      <td className="is-vcentered is-expanded">
+        <p className={cn({
+          'has-text-success': completed,
+          'has-text-danger': !completed,
+        })}
+        >
+          {title}
+        </p>
+      </td>
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={() => onSelectTodo(todo)}
+        >
+          <span className="icon">
+            <i className={cn(
+              'far',
+              {
+                'fa-eye': !isTodoSelected,
+                'fa-eye-slash': isTodoSelected,
+              },
+            )}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: Todo | null;
+  onSelectTodo: (todo: Todo | null) => void;
+};
+
+export const TodoModal: React.FC<Props> = ({ selectedTodo, onSelectTodo }) => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (selectedTodo) {
+      getUser(selectedTodo.userId)
+        .then(setSelectedUser)
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [selectedTodo?.userId]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {loading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +40,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +48,28 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              aria-label="modal-close"
+              onClick={() => onSelectTodo(null)}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {selectedTodo?.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${selectedUser?.email}`}>
+                {selectedUser?.name}
               </a>
             </p>
           </div>
