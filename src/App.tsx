11d4d0e9d1fa5bf3e370ef665029827ef3422855
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-09-19 12:46:23 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,56 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { Status } from './types/Status';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedStatus, setSelectedStatus] = useState(Status.All);
+  const [query, setQuery] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        throw new Error('Please try again later');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  function filterTodos(
+    items: Todo[],
+    status: Status,
+    queryValue: string,
+  ) {
+    let filtered = [...items];
+
+    if (queryValue) {
+      filtered = filtered.filter(item => {
+        return item.title.toLowerCase().includes(queryValue?.toLowerCase());
+      });
+    }
+
+    switch (status) {
+      case Status.Active:
+        return filtered.filter(item => !item.completed);
+
+      case Status.Completed:
+        return filtered.filter(item => item.completed);
+
+      case Status.All:
+      default:
+        return filtered;
+    }
+  }
+
+  const filteredTodos = useMemo(() => {
+    return filterTodos(todos, selectedStatus, query);
+  }, [todos, selectedStatus, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +64,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setSelectedStatus={setSelectedStatus}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && (<Loader />)}
+
+              <TodoList
+                todos={filteredTodos}
+                selectedTodo={selectedTodo}
+                setSelectedTodo={setSelectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          setSelectedTodo={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
