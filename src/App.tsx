bf064b45f97a43fd66823913f11c189694207230
Palina Apanovich <palina.apanovich@gmail.com>
2023-09-19 20:07:53 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,66 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterByTitle, setFilterByTitle] = useState<string>('');
+  const [filter, setFilter] = useState<string>('');
+
+  const selectTodos = (todo: Todo) => {
+    const findTodo = todos.find(value => value === todo) as Todo;
+
+    setSelectedTodo(findTodo);
+  };
+
+  const handleModalClose = () => {
+    setSelectedTodo(null);
+  };
+
+  const handleFilter = (status: string) => {
+    if (status === 'all') {
+      setFilter('all');
+    } else if (status === 'completed') {
+      setFilter('completed');
+    } else if (status === 'active') {
+      setFilter('active');
+    }
+  };
+
+  const handleTitleFilter = (title: string) => {
+    setFilterByTitle(title.toLowerCase());
+  };
+
+  const visibleTodos = todos.filter(todo => {
+    if (filter === 'all') {
+      return todo.title.toLowerCase().includes(filterByTitle);
+    }
+
+    if (filter === 'completed') {
+      return todo.completed && todo.title.toLowerCase().includes(filterByTitle);
+    }
+
+    if (filter === 'active') {
+      return !todo.completed && todo.title.toLowerCase().includes(filterByTitle);
+    }
+
+    return todo.title.toLowerCase().includes(filterByTitle);
+  });
+
+  const handleClearFilter = () => {
+    setFilterByTitle('');
+    setFilter('all');
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos().then(setTodos).finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +75,26 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onFilter={handleFilter}
+                onTitleFilter={handleTitleFilter}
+                onClearFilter={handleClearFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              <TodoList
+                todos={visibleTodos}
+                handleSelectedTodo={selectTodos}
+                selectedTodo={selectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo !== null && <TodoModal todo={selectedTodo} handleModalClose={handleModalClose} />}
     </>
   );
 };
