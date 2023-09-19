f29c36d57c72d7fc2ed2dd5c6d8f966165158f78
Sergey <serg9205@gmail.com>
2023-09-19 12:23:31 +0300

    first version before refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,73 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 import { Loader } from './components/Loader';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedFilter, setSelectedFilter] = useState('all');
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [isSelectedTodo, setIsSelectedTodo] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<null | Todo>(null);
+  const [selectedTodoUserId, setSelectedTodoUserId] = useState(0);
+  const [showButtonHide, setShowButtonHide] = useState(false);
+
+  const visibleTodos = todos.filter(({ title }) => title.toLowerCase().includes(query.toLowerCase()));
+
+  // сделать фильтрацию отдельно от сервера
+  useEffect(() => {
+    if (selectedFilter === 'all') {
+      getTodos()
+        .then(setTodos)
+        .finally(() => setLoading(false));
+    } else if (selectedFilter === 'completed') {
+      getTodos()
+        .then(prevTodos => setTodos(prevTodos.filter(({ completed }) => completed)))
+        .finally(() => setLoading(false));
+    } else {
+      getTodos()
+        .then(prevTodos => setTodos(prevTodos.filter(({ completed }) => !completed)))
+        .finally(() => setLoading(false));
+    }
+  }, [selectedFilter]);
+
+  const handleSelectedFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedFilter(event.target.value);
+  };
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSelectButton = (value: boolean) => {
+    setIsSelectedTodo(value);
+  };
+
+  const handleSelectedTodoUserId = (userId: number) => {
+    setSelectedTodoUserId(userId);
+  };
+
+  const handleSelectTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleShowSelectedTodoButton = (value: boolean) => {
+    setShowButtonHide(value);
+  };
+
+  const resetForm = () => {
+    setQuery('');
+    setSelectedFilter('all');
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +76,41 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                handleSelectedFilter={handleSelectedFilter}
+                selectedFilter={selectedFilter}
+                query={query}
+                handleQueryChange={handleQueryChange}
+                resetForm={resetForm}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && (
+                <Loader />
+              )}
+              {!loading && (
+                <TodoList
+                  todos={visibleTodos}
+                  handleSelectButton={handleSelectButton}
+                  handleSelectedTodoUserId={handleSelectedTodoUserId}
+                  handleSelectTodo={handleSelectTodo}
+                  handleShowSelectedTodoButton={handleShowSelectedTodoButton}
+                  showButtonHide={showButtonHide}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {isSelectedTodo && showButtonHide && (
+        <TodoModal
+          selectedTodoUserId={selectedTodoUserId}
+          selectedTodo={selectedTodo}
+          handleShowSelectedTodoButton={handleShowSelectedTodoButton}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodoUserId: number,
+  selectedTodo: Todo | null,
+  handleShowSelectedTodoButton: (value: boolean) => void,
+};
+
+export const TodoModal: React.FC<Props> = ({
+  selectedTodoUserId,
+  selectedTodo,
+  handleShowSelectedTodoButton,
+}) => {
+  const [selectedTodoUser, setSelectedTodoUser] = useState<null | User>(null);
+  const [closeButtonOn, setCloseButtonOn] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getUser(selectedTodoUserId)
+      .then(setSelectedTodoUser)
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div
+      className={classnames('modal', {
+        'is-active': closeButtonOn,
+      })}
+      data-cy="modal"
+    >
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+      {loading
+        ? (
+          <Loader />
+        ) : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${selectedTodo?.id}`}
+              </div>
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => {
+                  setCloseButtonOn(!closeButtonOn);
+                  handleShowSelectedTodoButton(false);
+                }}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {selectedTodo?.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {selectedTodo?.completed
+                  ? (
+                    <strong className="has-text-success">Done</strong>
+                  ) : (
+                    <strong className="has-text-danger">Planned</strong>
+                  )}
+
+                {' by '}
+
+                <a href={`mailto:${selectedTodoUser?.email}`}>
+                  {selectedTodoUser?.name}
+                </a>
+              </p>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
