d7afb68c4e20a1bec4af91679e1ea5169bdb1f78
Tamara Chernetska <cty2802@gmail.com>
2023-09-19 16:43:54 +0300

    created component TodoItem

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+  onHandleModal: (todo: Todo) => void;
+  selectedTodo: Todo | undefined,
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  onHandleModal = () => {},
+  selectedTodo,
+}) => {
+  return (
+    <tr
+      data-cy="todo"
+      className=""
+      key={todo.id}
+    >
+      <td
+        className="is-vcentered"
+      >
+        {todo.id}
+      </td>
+      <td className="is-vcentered">
+        {todo.completed && (
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+      <td className="is-vcentered is-expanded">
+        <p
+          className={classNames(
+            'has-text-success',
+            { 'has-text-danger': !todo.completed },
+          )}
+        >
+          {todo.title}
+        </p>
+      </td>
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={() => {
+            onHandleModal(todo);
+          }}
+        >
+          <span className="icon">
+            <i className={classNames({
+              'far fa-eye': selectedTodo !== todo,
+              'far fa-eye-slash': selectedTodo === todo,
+            })}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem';
 
 type Props = {
   todos: Todo[];
@@ -32,52 +32,11 @@ export const TodoList: React.FC<Props> = ({
 
       <tbody>
         {todos.map(todo => (
-          <tr
-            data-cy="todo"
-            className=""
-            key={todo.id}
-          >
-            <td
-              className="is-vcentered"
-            >
-              {todo.id}
-            </td>
-            <td className="is-vcentered">
-              {todo.completed && (
-                <span className="icon" data-cy="iconCompleted">
-                  <i className="fas fa-check" />
-                </span>
-              )}
-            </td>
-            <td className="is-vcentered is-expanded">
-              <p
-                className={classNames(
-                  'has-text-success',
-                  { 'has-text-danger': !todo.completed },
-                )}
-              >
-                {todo.title}
-              </p>
-            </td>
-            <td className="has-text-right is-vcentered">
-              <button
-                data-cy="selectButton"
-                className="button"
-                type="button"
-                onClick={() => {
-                  onHandleModal(todo);
-                }}
-              >
-                <span className="icon">
-                  <i className={classNames({
-                    'far fa-eye': selectedTodo !== todo,
-                    'far fa-eye-slash': selectedTodo === todo,
-                  })}
-                  />
-                </span>
-              </button>
-            </td>
-          </tr>
+          <TodoItem
+            todo={todo}
+            onHandleModal={onHandleModal}
+            selectedTodo={selectedTodo}
+          />
         ))}
       </tbody>
     </table>
