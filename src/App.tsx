40e934b17679cb2bfebe05314e7158ece6fc6cde
patryk177m <patrykmajewski177@gmail.com>
2023-09-21 13:15:07 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
+import { Filter, filterTodos } from './components/utils';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { Loader } from './components/Loader';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [filterBy, setFilterBy] = useState<Filter>('all');
+  const [query, setQuery] = useState<string>('');
+  const [isHide, setIsHide] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      const data = await getTodos();
+
+      setTodos(data);
+      setIsLoading(false);
+    };
+
+    loadTodos();
+  }, []);
+
+  const filteredTodos = filterTodos(todos, filterBy, query);
+
   return (
     <>
       <div className="section">
@@ -17,18 +39,46 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterBy={filterBy}
+                setFilterBy={setFilterBy}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {
+                isLoading
+                  ? <Loader />
+                  : todos
+                  && (
+                    <TodoList
+                      todos={filteredTodos}
+                      setIsHide={setIsHide}
+                      selectedTodo={selectedTodo as Todo}
+                      setSelectedTodo={setSelectedTodo}
+                      setIsLoading={setIsLoading}
+                    />
+                  )
+              }
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {
+        isHide
+     || (
+       <TodoModal
+         isLoading={isLoading}
+         setIsLoading={setIsLoading}
+         setIsHide={setIsHide}
+         selectedTodo={selectedTodo as Todo}
+         setSelectedTodo={setSelectedTodo}
+       />
+     )
+      }
     </>
   );
 };
