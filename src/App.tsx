303722cfeb2de1e5d25150dbff4d6f899afdeec2
Nikita Orliak <nikitonorl@gmail.com>
2023-09-26 23:48:22 +0200

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,26 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [sortedTodos, setSortedTodos] = useState<Todo[] | null>(null);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    setLoader(true);
+
+    getTodos()
+      .then((response) => {
+        setTodos(response);
+        setSortedTodos(response);
+      })
+      .finally(() => setLoader(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +35,26 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter todos={todos} setTodos={setSortedTodos} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loader && (
+                <Loader />
+              )}
+              <TodoList todos={sortedTodos} selectedTodo={selectedTodo} setSelectedTodo={setSelectedTodo} />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {
+        selectedTodo && (
+          <TodoModal
+            todo={selectedTodo}
+            selectTodo={setSelectedTodo}
+          />
+        )
+      }
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,127 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
+import { Query } from '../../types/Query';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  todos: Todo[] | null;
+  setTodos: (todos: Todo[]) => void;
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+const getSortedTodos = (
+  todos: Todo[] | null,
+  query?: Query,
+  text?: string,
+): Todo[] => {
+  if (!todos) {
+    return [];
+  }
+
+  let newTodos = [...todos];
+
+  if (query) {
+    switch (query) {
+      case 'active': {
+        newTodos = newTodos.filter(todo => !todo.completed);
+
+        break;
+      }
+
+      case 'completed': {
+        newTodos = newTodos.filter(todo => todo.completed);
+
+        break;
+      }
+
+      default:
+        newTodos = newTodos.sort(
+          (todo1, todo2) => todo1.id - todo2.id,
+        );
+
+        break;
+    }
+  }
+
+  if (text) {
+    newTodos = newTodos
+      .filter(todo => todo.title.toLowerCase().includes(text.toLowerCase()));
+  }
+
+  return newTodos;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  todos,
+  setTodos = () => { },
+}) => {
+  const [input, setInput] = useState('');
+  const [query, setQuery] = useState<Query>('all');
+
+  useEffect(() => {
+    const newTodos = getSortedTodos(todos, query, input);
+
+    setTodos(newTodos);
+  }, [query, input]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const clearInput = () => {
+    setInput('');
+  };
+
+  return (
+    <form
+      className="field has-addons"
+      onSubmit={event => event.preventDefault()}
+    >
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={query}
+            onChange={event => setQuery(event.target.value as Query)}
+          >
+            <option value="all">
+              All
+            </option>
+            <option value="active">
+              Active
+            </option>
+            <option value="completed">
+              Completed
+            </option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={input}
+          onChange={handleChange}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {
+          !!input.length && (
+            <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={clearInput}
+              />
+            </span>
+          )
+        }
+      </p>
+    </form>
+  );
+};
