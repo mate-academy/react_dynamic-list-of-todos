5c5f40d1254897e21aa2168cfca1a6564aaf89ca
Emil Owczarek <owczarekemil@gmail.com>
2023-09-27 01:03:37 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,49 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos /* getUser */ } from './api';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [filter, setFilter] = useState<Filter>('all');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+
+  const visibleTodos = () => {
+    const visible = [...todos].filter((todo) => {
+      if (filter === 'active' && todo.completed) {
+        return false;
+      }
+
+      if (filter === 'completed' && !todo.completed) {
+        return false;
+      }
+
+      if (!todo.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return false;
+      }
+
+      return true;
+    });
+
+    return visible;
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then((todo) => {
+        setTodos(todo);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,47 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setFilter={setFilter}
+                setSearchTerm={setSearchTerm}
+                searchTerm={searchTerm}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos()}
+                  setModal={setModal}
+                  setSelectedTodo={setSelectedTodo}
+                  selectedTodoId={selectedTodoId}
+                  setSelectedTodoId={setSelectedTodoId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {modal && (
+        <TodoModal
+          setModal={(isVisible: boolean) => {
+            setModal(isVisible);
+            if (!isVisible) {
+              setSelectedTodoId(null);
+            }
+          }}
+          todo={
+            selectedTodo || {
+              id: 0,
+              title: '',
+              completed: false,
+              userId: 0,
+            }
+          }
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,72 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { useState } from 'react';
+import { Filter } from '../../types/Filter';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type TodoFilterProps = {
+  setFilter: (filter: Filter) => void;
+  searchTerm: string;
+  setSearchTerm: (searchTerm: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+export const TodoFilter: React.FC<TodoFilterProps> = ({
+  setFilter,
+  setSearchTerm,
+  searchTerm,
+}) => {
+  const [isSearchEmpty, setIsSearchEmpty] = useState(true);
+  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedFilter: Filter = event.target.value as Filter;
+
+    setFilter(selectedFilter);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchTerm = event.target.value;
+
+    setSearchTerm(newSearchTerm);
+    setIsSearchEmpty(newSearchTerm === '');
+  };
+
+  const handleClearSearch = () => {
+    setSearchTerm('');
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select data-cy="statusSelect" onChange={handleFilterChange}>
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearchChange}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {!isSearchEmpty && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleClearSearch}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
