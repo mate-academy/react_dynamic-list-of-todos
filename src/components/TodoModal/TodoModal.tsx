58cd587fbcaa68a4b58a46cac80c79b101796505
Denis Hurskiy <denis.gurskiy@gmail.com>
2023-09-30 00:56:47 +0300

    list-of-todos

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectTodo?: Todo,
+  setSelectTodo: (elem: Todo | undefined) => void,
+};
+
+export const TodoModal: React.FC<Props> = ({
+  selectTodo = null,
+  setSelectTodo,
+}) => {
+  const [user, setUser] = useState<User>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (selectTodo?.userId) {
+      getUser(selectTodo?.userId)
+        .then(setUser)
+        .finally(() => setLoading(false));
+    }
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
+      {
+        loading
+          ? (
+            <Loader />
+          ) : (
+            <div className="modal-card">
+              <header className="modal-card-head">
+                <div
+                  className="modal-card-title has-text-weight-medium"
+                  data-cy="modal-header"
+                >
+                  {`Todo #${selectTodo?.id}`}
+                </div>
+
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  type="button"
+                  className="delete"
+                  data-cy="modal-close"
+                  onClick={() => setSelectTodo(undefined)}
+                />
+              </header>
+
+              <div className="modal-card-body">
+                <p className="block" data-cy="modal-title">
+                  {selectTodo?.title}
+                </p>
+
+                <p className="block" data-cy="modal-user">
+                  {selectTodo?.completed
+                    ? <strong className="has-text-success">Done</strong>
+                    : <strong className="has-text-danger">Planned</strong>}
+
+                  {' by '}
+
+                  <a href={`mailto:${user?.email}`}>
+                    {user?.name}
+                  </a>
+                </p>
+              </div>
             </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
-              </a>
-            </p>
-          </div>
-        </div>
-      )}
+          )
+      }
     </div>
   );
 };
