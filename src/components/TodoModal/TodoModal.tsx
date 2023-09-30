3767c3f957be3cbe23c7cbac237b7664931def86
Denis Hurskiy <denis.gurskiy@gmail.com>
2023-09-30 15:37:05 +0300

    fixed

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -15,6 +15,7 @@ export const TodoModal: React.FC<Props> = ({
 }) => {
   const [user, setUser] = useState<User>();
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   useEffect(() => {
     setLoading(true);
@@ -22,6 +23,7 @@ export const TodoModal: React.FC<Props> = ({
     if (selectTodo?.userId) {
       getUser(selectTodo?.userId)
         .then(setUser)
+        .catch(() => setErrorMessage('Try again later!'))
         .finally(() => setLoading(false));
     }
   }, []);
@@ -30,49 +32,75 @@ export const TodoModal: React.FC<Props> = ({
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {
-        loading
-          ? (
-            <Loader />
-          ) : (
-            <div className="modal-card">
-              <header className="modal-card-head">
-                <div
-                  className="modal-card-title has-text-weight-medium"
-                  data-cy="modal-header"
-                >
-                  {`Todo #${selectTodo?.id}`}
-                </div>
+      {loading && (
+        <Loader />
+      )}
 
-                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                <button
-                  type="button"
-                  className="delete"
-                  data-cy="modal-close"
-                  onClick={() => setSelectTodo(undefined)}
-                />
-              </header>
-
-              <div className="modal-card-body">
-                <p className="block" data-cy="modal-title">
-                  {selectTodo?.title}
-                </p>
-
-                <p className="block" data-cy="modal-user">
-                  {selectTodo?.completed
-                    ? <strong className="has-text-success">Done</strong>
-                    : <strong className="has-text-danger">Planned</strong>}
-
-                  {' by '}
-
-                  <a href={`mailto:${user?.email}`}>
-                    {user?.name}
-                  </a>
-                </p>
-              </div>
+      {errorMessage.length && (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              Error
             </div>
-          )
-      }
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => setSelectTodo(undefined)}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {errorMessage}
+            </p>
+          </div>
+        </div>
+      )}
+
+      {!loading && errorMessage.length === 0 && (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${selectTodo?.id}`}
+            </div>
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => setSelectTodo(undefined)}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {selectTodo?.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {selectTodo?.completed
+                ? <strong className="has-text-success">Done</strong>
+                : <strong className="has-text-danger">Planned</strong>}
+
+              {' by '}
+
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
