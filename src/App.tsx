3fe860cba3a82f3b7a34a7c037e7409795403f5d
Volodymyr <prostonadobobb@gmail.com>
2023-10-12 13:32:04 +0300

    add recomendation

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,6 @@ export const App: React.FC = () => {
     getTodos().then(setTodos).finally(() => setLoading(false));
   }, []);
 
-  console.log(selectedTodo);
-
   return (
     <>
       <div className="section">
