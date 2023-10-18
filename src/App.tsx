710b5910572eb1ca509169ec97fa12f07893a850
Maksym Shevelin <shevmax01@gmail.com>
2023-10-18 16:10:10 +0300

    done but i dk why so many test are failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,68 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos, getUser } from './api';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [selectedUserId, setSelectedUserId] = useState<number>(0);
+
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const [status, setStatus] = useState('all');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+
+  const selectedTodo = useMemo(() => {
+    return todos.find(todo => todo.id === selectedTodoId);
+  }, [todos, selectedTodoId]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then(data => {
+        setTodos(data);
+        setFilteredTodos(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    let currentTodos = todos;
+
+    switch (status) {
+      case 'completed':
+        currentTodos = todos.filter(todo => todo.completed);
+        break;
+      case 'active':
+        currentTodos = todos.filter(todo => !todo.completed);
+        break;
+      default:
+        currentTodos = todos;
+        break;
+    }
+
+    if (searchQuery.trim() !== '') {
+      currentTodos = currentTodos.filter(todo => todo.title.toLowerCase()
+        .includes(searchQuery.toLowerCase()));
+    }
+
+    setFilteredTodos(currentTodos);
+
+    getUser(selectedUserId)
+      .then(setSelectedUser);
+  }, [status, todos, searchQuery, selectedUserId]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +77,38 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={status}
+                setStatus={setStatus}
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodoId={selectedTodoId}
+                  setSelectedTodoId={setSelectedTodoId}
+                  setSelectedUserId={setSelectedUserId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          setSelectedTodoId={setSelectedTodoId}
+          user={selectedUser}
+          todo={selectedTodo}
+        />
+      )}
+
     </>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,12 @@ import { Todo } from './types/Todo';
 import { User } from './types/User';
 
 // eslint-disable-next-line max-len
-const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
+// const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
+const BASE_URL = 'http://localhost:3000/api/';
 
 // This function creates a promise
 // that is resolved after a given delay
-function wait(delay: number): Promise<void> {
+export function wait(delay: number): Promise<void> {
   return new Promise(resolve => {
     setTimeout(resolve, delay);
   });
