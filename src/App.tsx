2463abf4a12a8eda8a7da70fe5411e83e7e4e9a2
Denys <kolbasin1990@gmail.com>
2023-10-24 01:24:01 +0300

    react_dynamic-list-of-todos_v-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState<string>('');
+  const [status, setStatus] = useState<string>('all');
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filteredTodos = todos.filter((todo) => {
+    return (
+      (status === 'all' || (status === 'completed' && todo.completed) || (status === 'active' && !todo.completed)) &&
+      todo.title.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
+
   return (
     <>
       <div className="section">
@@ -17,18 +40,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                status={status}
+                setStatus={setStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+
+              {!loading && filteredTodos.length > 0 && (
+                <TodoList
+                  todos={filteredTodos}
+                  onSelect={setSelectedTodo}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          userId={selectedTodo.userId}
+          selectedTodo={selectedTodo}
+          key={selectedTodo.id}
+          onClose={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
