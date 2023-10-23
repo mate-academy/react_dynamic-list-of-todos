b5cb80c3f9792b389ec171daeea519541933c9bb
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-10-23 10:01:28 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
+enum Filters {
+  All = 'All',
+  Active = 'Active',
+  Completed = 'Completed',
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
+  const [appliedFilter, setAppliedFilter] = useState(Filters.All);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [userId, setUserId] = useState(0);
+  const [checkId, setCheckId] = useState(0);
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then((todo) => {
+        setTodos(todo);
+        setFilteredTodos(todo);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +42,44 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={todos}
+                appliedFilter={appliedFilter}
+                filteredTodos={filteredTodos}
+                setFilteredTodos={setFilteredTodos}
+                setAppliedFilter={setAppliedFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && (
+                <Loader />
+              )}
+              {!loading && todos.length > 0 && (
+                <TodoList
+                  checkId={checkId}
+                  setCheckId={setCheckId}
+                  setUserId={setUserId}
+                  filteredTodos={filteredTodos}
+                  isCheck={isCheck}
+                  setIsCheck={setIsCheck}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal
+        setCheckId={setCheckId}
+        isCheck={isCheck}
+        setIsCheck={setIsCheck}
+        filteredTodos={filteredTodos}
+        checkId={checkId}
+        userId={userId}
+        loading={loading}
+        setLoading={setLoading}
+      />
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,120 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { useState, useEffect, useCallback } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+enum Filters {
+  All = 'All',
+  Active = 'Active',
+  Completed = 'Completed',
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+type Props = {
+  filteredTodos: Todo[];
+  todos: Todo[];
+  appliedFilter: Filters;
+  setFilteredTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setAppliedFilter: React.Dispatch<React.SetStateAction<Filters>>;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  todos,
+  appliedFilter,
+  filteredTodos,
+  setFilteredTodos,
+  setAppliedFilter,
+}) => {
+  const [search, setSearch] = useState('');
+  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedFilter = event.target.value as Filters;
+
+    setAppliedFilter(selectedFilter);
+  };
+
+  const setFilteredTodosCallback = useCallback((updatedFilteredTodos) => {
+    setFilteredTodos(updatedFilteredTodos);
+  }, [setFilteredTodos]);
+
+  useEffect(() => {
+    let updatedFilteredTodos = todos;
+
+    if (appliedFilter === Filters.Active) {
+      updatedFilteredTodos = todos.filter((todo) => !todo.completed);
+    } else if (appliedFilter === Filters.Completed) {
+      updatedFilteredTodos = todos.filter((todo) => todo.completed);
+    }
+
+    if (search) {
+      updatedFilteredTodos = updatedFilteredTodos.filter(
+        (todo) => todo.title.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setFilteredTodosCallback(updatedFilteredTodos);
+  }, [appliedFilter, todos, search, setFilteredTodosCallback]);
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearch = event.target.value;
+
+    setSearch(newSearch);
+    let updatedFilteredTodos = todos;
+
+    if (appliedFilter === Filters.Active) {
+      updatedFilteredTodos = todos.filter((todo) => !todo.completed);
+    } else if (appliedFilter === Filters.Completed) {
+      updatedFilteredTodos = todos.filter((todo) => todo.completed);
+    }
+
+    if (newSearch) {
+      updatedFilteredTodos = updatedFilteredTodos.filter(
+        (todo) => todo.title.toLowerCase().includes(newSearch.toLowerCase()),
+      );
+    }
+
+    setFilteredTodos(updatedFilteredTodos);
+  };
+
+  const handleDelete = () => {
+    setSearch('');
+    setFilteredTodos(filteredTodos);
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={handleFilterChange}
+          >
+            <option value={Filters.All}>All</option>
+            <option value={Filters.Active}>Active</option>
+            <option value={Filters.Completed}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={search}
+          onChange={handleChangeInput}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={handleDelete}
+          />
+        </span>
+      </p>
+    </form>
+  );
+};
