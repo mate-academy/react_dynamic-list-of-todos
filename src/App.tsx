e62f5818bbb775656aa91de32503e918bb11141f
Anton Bilous <toxanteloxran@gmail.com>
2023-10-31 17:14:25 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+// import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterType, setFilterType] = useState('all');
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    getTodos().then((data) => (
+      setTodos(data)
+    ));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +29,18 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter input={input} setInput={setInput} setFilterType={setFilterType} />
             </div>
 
             <div className="block">
               <Loader />
-              <TodoList />
+              <TodoList input={input} todos={todos} filterType={filterType} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,22 +1,67 @@
 import React from 'react';
+import { Todo } from '../../types/Todo';
 
-export const TodoList: React.FC = () => (
-  <table className="table is-narrow is-fullwidth">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>
-          <span className="icon">
-            <i className="fas fa-check" />
-          </span>
-        </th>
-        <th>Title</th>
-        <th> </th>
-      </tr>
-    </thead>
+interface T {
+  todos: Todo[];
+  filterType: string;
+  input: string;
+}
 
-    <tbody>
-      <tr data-cy="todo" className="">
+export const TodoList: React.FC<T> = ({ input, todos, filterType }) => {
+  const filterTodos = (array: Todo[], type: string) => {
+    switch (type) {
+      case 'active':
+        return array.filter(todo => !todo.completed);
+
+      case 'completed':
+        return array.filter(todo => todo.completed);
+
+      default:
+        return array;
+    }
+  };
+
+  return (
+    <table className="table is-narrow is-fullwidth">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>
+            <span className="icon">
+              <i className="fas fa-check" />
+            </span>
+          </th>
+          <th>Title</th>
+          <th> </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {filterTodos(todos, filterType).map(todo => todo.title.includes(input)
+        && (
+          <tr data-cy="todo" className="">
+            <td className="is-vcentered">{todo.id}</td>
+            <td className="is-vcentered">
+              {todo.completed && (
+                <span className="icon" data-cy="iconCompleted">
+                  <i className="fas fa-check" />
+                </span>
+              )}
+            </td>
+            <td className="is-vcentered is-expanded">
+              <p className={`has-text-${todo.completed ? 'success' : 'danger'}`}>{todo.title}</p>
+            </td>
+            <td className="has-text-right is-vcentered">
+              <button data-cy="selectButton" className="button" type="button">
+                <span className="icon">
+                  <i className="far fa-eye" />
+                </span>
+              </button>
+            </td>
+          </tr>
+        ))}
+
+        {/* <tr data-cy="todo" className="">
         <td className="is-vcentered">1</td>
         <td className="is-vcentered" />
         <td className="is-vcentered is-expanded">
@@ -94,7 +139,8 @@ export const TodoList: React.FC = () => (
             </span>
           </button>
         </td>
-      </tr>
-    </tbody>
-  </table>
-);
+      </tr> */}
+      </tbody>
+    </table>
+  );
+};
