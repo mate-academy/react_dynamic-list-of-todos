3684df1589ac43fef92318b5b1dd72df865320c3
Nazar <bidenkonazar@gmail.com>
2023-11-02 00:39:31 +0200

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,69 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+
+import { getTodos } from './api';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { Todo } from './types/Todo';
+
+const getFilteredTodos = (
+  param: string,
+  query: string,
+  todos: Todo[],
+) => {
+  let filteredTodos = [...todos];
+
+  if (param === 'active') {
+    filteredTodos = filteredTodos.filter(todo => !todo.completed);
+  } else if (param === 'completed') {
+    filteredTodos = filteredTodos.filter(todo => todo.completed);
+  }
+
+  if (query) {
+    filteredTodos = filteredTodos
+      .filter(todo => todo.title.toLocaleLowerCase()
+        .includes(query.toLocaleLowerCase()));
+  }
+
+  return filteredTodos;
+};
+
 export const App: React.FC = () => {
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  // const [errorMessage, setErrorMessage] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filter, setFilter] = useState('All');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then(todos => setTodosFromServer(todos))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  // const onSelectTodo = (todo: Todo | null) => {
+  //   setSelectedTodo(todo);
+  // };
+
+  // const onSetQuery = (newQuery: string) => {
+  //   setQuery(newQuery.trim());
+  // };
+
+  // const onSetFilter = (newFilter: string) => {
+  //   setFilter(newFilter);
+  // };
+
+  const filteredTodo = getFilteredTodos(filter, query, todosFromServer);
+
   return (
     <>
       <div className="section">
@@ -17,18 +72,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setNewQuery={newQuery => setQuery(newQuery.toLowerCase())}
+                setNewFilter={newFilter => setFilter(newFilter)}
+                query={query}
+                filter={filter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading
+                  && <Loader />}
+              {!isLoading
+                && (
+                  <TodoList
+                    todos={filteredTodo}
+                    selectedTodo={selectedTodo}
+                    selectNewTodo={todo => setSelectedTodo(todo)}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo
+        && (
+          <TodoModal
+            selectedTodo={selectedTodo}
+            onSelectTodo={todo => setSelectedTodo(todo)}
+          />
+        )}
     </>
   );
 };
