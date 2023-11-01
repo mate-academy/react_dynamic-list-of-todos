989f62981cf1dbbe47c8eb4d77493aee8f71faf9
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-11-01 23:11:35 +0200

    solution react_dynamic_list_of_todos task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { TodoModal } from './components/TodoModal';
+
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [select, setSelect] = useState('all');
+  const [isTodoSelected, setIsTodoSelected] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos);
+  }, []);
+
+  const normalizeQuery = query.toLowerCase().trim();
+
+  let filteredTodos = todos.filter((todo) => (
+    (todo.title.toLowerCase().includes(normalizeQuery))
+  ));
+
+  switch (select) {
+    case 'all':
+      filteredTodos = filteredTodos.filter(todo => todo);
+      break;
+    case 'active':
+      filteredTodos = filteredTodos.filter(todo => todo.completed === false);
+      break;
+    case 'completed':
+      filteredTodos = filteredTodos.filter(todo => todo.completed === true);
+      break;
+    default:
+      return null;
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +50,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setQuery={setQuery}
+                query={query}
+                select={select}
+                setSelect={setSelect}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0 ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  isTodoSelected={isTodoSelected}
+                  setIsTodoSelected={setIsTodoSelected}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {isTodoSelected && (
+        <TodoModal
+          isTodoSelected={isTodoSelected}
+          setIsTodoSelected={setIsTodoSelected}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,49 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import cn from 'classnames';
+
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+import { getUser } from '../../api';
+
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  isTodoSelected: Todo,
+  setIsTodoSelected: (value: Todo | null) => void,
+};
+
+export const TodoModal: React.FC<Props> = ({
+  isTodoSelected,
+  setIsTodoSelected = () => { },
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUser(isTodoSelected.userId)
+      .then(setUser);
+  }, [isTodoSelected.userId]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
-        <Loader />
-      ) : (
+      {user ? (
         <div className="modal-card">
           <header className="modal-card-head">
             <div
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${isTodoSelected.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
+              data-cy="modal-close"
               type="button"
               className="delete"
-              data-cy="modal-close"
+              onClick={() => setIsTodoSelected(null)}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {isTodoSelected.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong className={cn({
+                'has-text-danger': !isTodoSelected.completed,
+                'has-text-success': isTodoSelected.completed,
+              })}
+              >
+                {!isTodoSelected.completed ? (
+                  'Planned'
+                ) : (
+                  'Done'
+                )}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
               </a>
             </p>
           </div>
         </div>
+      ) : (
+        <Loader />
       )}
+
     </div>
   );
 };
