73bc92c8080ff8e7d64f0b645206cfbde1374ed2
Vovchuk <kulivackijvoloda@gmail.com>
2023-11-12 21:29:33 +0200

    new commit

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -16,77 +16,59 @@ export const TodoModal: React.FC<Props> = (
   },
 ) => {
   const [user, setUser] = useState<User | null>(null);
-  const [isUserLoading, setIsUserLoading] = useState(true);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsUserLoading(false);
-    }, 500);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [setIsUserLoading]);
-
-  useEffect(() => {
-    if (selectedTodo && selectedTodo.userId) {
-      getUser(selectedTodo.userId)
-        .then(setUser)
-        .catch((error) => {
-          throw new Error(error);
-        });
-    }
+    getUser(selectedTodo.userId)
+      .then(setUser);
   }, [selectedTodo]);
 
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {isUserLoading ? (
+      {!user ? (
         <Loader />
       ) : (
-        <div>
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <div
-                className="modal-card-title has-text-weight-medium"
-                data-cy="modal-header"
-              >
-                <span>
-                  {`Todo #${selectedTodo.id}`}
-                </span>
-              </div>
-
-              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-              <button
-                type="button"
-                className="delete"
-                data-cy="modal-close"
-                onClick={handleCloseModal}
-              />
-            </header>
-
-            <div className="modal-card-body">
-              <p className="block" data-cy="modal-title">
-                {selectedTodo.title}
-              </p>
-
-              <p className="block" data-cy="modal-user">
-                {selectedTodo.completed === true
-                  ? <strong className="has-text-success">Done</strong>
-                  : <strong className="has-text-danger">Planned</strong>}
-
-                {' by '}
-
-                {user ? (
-                  <a href={`mailto:${user.email}`} data-cy="todo">
-                    {user.name}
-                  </a>
-                ) : (
-                  <Loader />
-                )}
-              </p>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              <span>
+                {`Todo #${selectedTodo.id}`}
+              </span>
             </div>
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={handleCloseModal}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {selectedTodo.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {selectedTodo.completed
+                ? <strong className="has-text-success">Done</strong>
+                : <strong className="has-text-danger">Planned</strong>}
+
+              {' by '}
+
+              {user ? (
+                <a href={`mailto:${user.email}`} data-cy="todo">
+                  {user.name}
+                </a>
+              ) : (
+                <Loader />
+              )}
+            </p>
           </div>
         </div>
       )}
