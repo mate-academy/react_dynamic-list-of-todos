196172ef85d09c68f2cd416acb0be6745c631fd3
FurArt <iiionerart@gmail.com>
2023-11-17 12:10:01 +0200

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+// import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import {
+  getTodos,
+  // getUser
+} from './api';
+import { Todo } from './types/Todo';
+// import { User } from './types/User';
+
+// function getUserById(userId, usersArray) {
+//   return usersArray.find(user => user.id === userId)
+//     || null;
+// }
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [users, setUsers] = useState<User[] | null[]>([null]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Отримуємо список todo
+      getTodos()
+        .then((todosData) => {
+          setTodos(todosData);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +51,14 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? <Loader /> : <TodoList todos={todos} />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
+
   );
 };
--- src/components/TodoInfo/Todoinfo.tsx
+++ src/components/TodoInfo/Todoinfo.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import { Todo } from '../../types/Todo';
+import { TodoModal } from '../TodoModal';
 
 interface TodoInfoProps {
   todo: Todo;
@@ -8,49 +9,61 @@ interface TodoInfoProps {
 
 export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
   const { id, title, completed } = todo;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleSelectButton = () => {
+    setIsModalOpen(true);
+  };
 
   return (
-    <tr
-      key={id}
-      data-cy="todo"
-      className={classnames({
-        'has-background-info-light': completed,
-      })}
-    >
-      <td className="is-vcentered">{id}</td>
-      <td className="is-vcentered">
-        {completed && (
-          <span className="icon">
-            <i className="fas fa-check" />
-          </span>
-        )}
-      </td>
-      <td className="is-vcentered is-expanded">
-        <p
-          className={classnames({
-            'has-text-success': completed,
-            'has-text-danger': !completed,
-          })}
-        >
-          {title}
-        </p>
-      </td>
-      {/* Кнопка око */}
-      <td className="has-text-right is-vcentered">
-        <button
-          data-cy="selectButton"
-          className="button"
-          type="button"
-        >
-          <span className="icon">
-            <i className={classnames({
-              'far fa-eye-slash': completed,
-              'far fa-eye': !completed,
+    <>
+      {isModalOpen && (
+        <TodoModal
+          todo={todo}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+      <tr
+        key={id}
+        data-cy="todo"
+        className={classnames({
+          'has-background-info-light': completed,
+        })}
+      >
+        <td className="is-vcentered">{id}</td>
+        <td className="is-vcentered">
+          {completed && (
+            <span className="icon">
+              <i className="fas fa-check" />
+            </span>
+          )}
+        </td>
+        <td className="is-vcentered is-expanded">
+          <p
+            className={classnames({
+              'has-text-success': completed,
+              'has-text-danger': !completed,
             })}
-            />
-          </span>
-        </button>
-      </td>
-    </tr>
-  )
+          >
+            {title}
+          </p>
+        </td>
+        {/* Кнопка око */}
+        <td className="has-text-right is-vcentered">
+          <button
+            onClick={handleSelectButton}
+            data-cy="selectButton"
+            className="button"
+            type="button"
+
+          >
+            <span className="icon">
+              <i className="far fa-eye" />
+            </span>
+          </button>
+        </td>
+      </tr>
+    </>
+
+  );
 };
