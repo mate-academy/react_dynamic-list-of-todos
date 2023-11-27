8788a2ea08487c32cf93dcde37a9b1e22b5b7cd3
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2023-11-27 18:53:14 +0200

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 /* eslint-disable max-len */
-import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { useEffect, useMemo, useState } from 'react';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+// import { Status } from './types/Filtered';
+// import { User } from './types/User';
+// import { TodoModal } from './components/TodoModal';
+// import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  // const [users, setUsers] = useState<User[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState('');
+  // const [filterStatus, setFilterStatus] = useState<Status>(Status.All);
+
+  // const visibleTodos = useMemo(() => {
+  //   switch (filterStatus) {
+  //     case Status.Active:
+  //       return todos.filter((todo) => todo.status !== Status.Completed);
+  //     case Status.Completed:
+  //       return todos.filter((todo) => todo.status === Status.Completed);
+  //     default:
+  //       return todos;
+  //   }
+  // }, [todos, filterStatus]);
+
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+      });
+  }, []);
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const filteredItems = useMemo(() => todos.filter(todo => todo
+    .title.toLowerCase().includes(title.toLowerCase())), [title, todos]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +50,22 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                title={title}
+                setTitle={setTitle}
+                handleChangeInput={handleChangeInput}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {/* <Loader /> */}
+              <TodoList filteredItems={filteredItems} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
   );
 };
