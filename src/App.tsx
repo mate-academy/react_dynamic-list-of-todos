a9f2788d49ff1935cf99a8fe0d9795c3a2315a48
Yurii Hlushenko <yauglushenko@gmail.com>
2023-12-13 12:56:01 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,67 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+function getPreparedTodos(todos: Todo[], filter: string,
+  query: string): Todo[] {
+  const preparedTodos = todos.filter(todo => {
+    switch (filter) {
+      case 'completed':
+        return todo.completed;
+
+      case 'active':
+        return !todo.completed;
+
+      default:
+        return true;
+    }
+  });
+
+  if (query) {
+    return preparedTodos.filter(
+      todo => todo.title.toLowerCase().includes(query.toLowerCase()),
+    );
+  }
+
+  return preparedTodos;
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [viewTodoId, setViewTodoId] = useState<number | null>(null);
+  const [filter, setFilter] = useState('all');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const saveTodoId = (id: number | null) => {
+    setViewTodoId(id);
+  };
+
+  const onChange = (e: string) => {
+    setFilter(e);
+  };
+
+  const onQueryChange = (e: string) => {
+    setQuery(e);
+  };
+
+  const visibleTodos = getPreparedTodos(todos, filter, query);
+
+  const viewTodo = todos.find(todo => todo.id === viewTodoId);
+
   return (
     <>
       <div className="section">
@@ -17,18 +70,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setOption={onChange}
+                setQuery={onQueryChange}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              <TodoList
+                todos={visibleTodos}
+                viewTodoId={viewTodoId}
+                onEyeClick={saveTodoId}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {viewTodoId && (
+        <TodoModal
+          viewTodo={viewTodo || null}
+          onClose={saveTodoId}
+        />
+      )}
     </>
   );
 };
