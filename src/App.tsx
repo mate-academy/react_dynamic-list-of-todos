10c2d5a604a6e14b781c746f5c8b967f4929c528
Fluebubble <Trots.anatoly@gmail.com>
2023-12-13 20:42:09 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,80 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { FilterBy } from './types/FilterBy';
+
+const prepareTodos = (todos: Todo[], filter = FilterBy.all, query: string) => {
+  let todosToPrepare = [...todos];
+
+  if (filter) {
+    switch (filter) {
+      case FilterBy.active:
+        todosToPrepare = todosToPrepare.filter(todo => !todo.completed);
+        break;
+      case FilterBy.completed:
+        todosToPrepare = todosToPrepare.filter(todo => todo.completed);
+        break;
+      case FilterBy.all:
+        todosToPrepare = [...todos];
+        break;
+      default:
+        break;
+    }
+  }
+
+  if (query) {
+    const lowerQuery = query.toLowerCase();
+
+    todosToPrepare = todosToPrepare.filter(todo => todo.title
+      .toLowerCase()
+      .includes(lowerQuery));
+  }
+
+  return todosToPrepare;
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [areTodosLoading, setAreTodosLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState<FilterBy>(FilterBy.all);
+
+  const handleTodoSelect = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleCloseTodo = () => {
+    setSelectedTodo(null);
+  };
+
+  useEffect(() => {
+    getTodos().then(result => {
+      setAreTodosLoading(false);
+      setTodos(result);
+    });
+  }, []);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleClearQuery = () => {
+    setSearchQuery('');
+  };
+
+  const handleFilterSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(event.target.value as FilterBy);
+  };
+
+  let todosToRender;
+
+  if (todos) {
+    todosToRender = prepareTodos(todos, filter, searchQuery);
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +89,42 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                searchQuery={searchQuery}
+                filter={filter}
+                onQueryChange={handleQueryChange}
+                handleClearQuery={handleClearQuery}
+                onFilterSelect={handleFilterSelect}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {areTodosLoading && <Loader />}
+              {
+                !!todosToRender
+                && (
+                  <TodoList
+                    todos={todosToRender}
+                    onTodoSelect={handleTodoSelect}
+                    selectedTodo={selectedTodo}
+                  />
+                )
+              }
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {
+        selectedTodo
+        && (
+          <TodoModal
+            selectedTodo={selectedTodo}
+            handleCloseTodo={handleCloseTodo}
+          />
+        )
+      }
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,29 @@
-export const TodoFilter = () => (
+import React from 'react';
+import { FilterBy } from '../../types/FilterBy';
+
+interface Props {
+  searchQuery: string,
+  filter: FilterBy
+  onQueryChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  handleClearQuery: () => void
+  onFilterSelect: (event: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  searchQuery,
+  filter,
+  onQueryChange,
+  handleClearQuery,
+  onFilterSelect,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          onChange={event => onFilterSelect(event)}
+          defaultValue={filter}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,6 +37,8 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={searchQuery}
+        onChange={event => onQueryChange(event)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
@@ -23,11 +46,16 @@ export const TodoFilter = () => (
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
-        />
+        {!!searchQuery.length
+          && (
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleClearQuery}
+              aria-label="Clear Search"
+            />
+          )}
       </span>
     </p>
   </form>
