2cd1c5a25717e8b1ce1d26d3d8bdc47f3276f9b0
Oleksii Mantserov <a.v.mantserov@gmail.com>
2023-12-13 18:47:19 +0200

    little fix

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -36,7 +36,7 @@ export const TodoModal: React.FC<Props> = ({ todo, onClosing }) => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              {`Todo ${todo.id}`}
+              {`Todo #${todo.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
