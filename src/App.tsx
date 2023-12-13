465c14b596262e9628eb1588537bbfba1accf4b9
Yevhen Kolosov <kolosov214a@gmail.com>
2023-12-13 17:14:23 +0200

    added task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,56 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
+const baseTodo: Todo = {
+  id: 0,
+  title: '',
+  completed: false,
+  userId: 0,
+};
+
+const filterTodos = (sourceTodos: Todo[], filterBy: string, currentQuery: string) => {
+  let filteredTodos: Todo[];
+
+  switch (filterBy) {
+    case 'active':
+      filteredTodos = sourceTodos.filter(todo => !todo.completed);
+      break;
+
+    case 'completed':
+      filteredTodos = sourceTodos.filter(todo => todo.completed);
+      break;
+
+    default:
+      filteredTodos = [...sourceTodos];
+      break;
+  }
+
+  if (currentQuery) {
+    filteredTodos = filteredTodos
+      .filter(todo => todo.title.toLowerCase().includes(currentQuery.toLowerCase()));
+  }
+
+  return filteredTodos;
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([baseTodo]);
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+  const [isModalLoading, setIsModalLoading] = useState(false);
+  const [isListLoading, setIsListLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [filterByStatus, setFilterByStatus] = useState('all');
+
+  useEffect(() => {
+    setIsListLoading(true);
+    getTodos().then(setTodos).finally(() => setIsListLoading(false));
+  }, []);
+
+  const filteredTodos = filterTodos(todos, filterByStatus, query);
+
   return (
     <>
       <div className="section">
@@ -17,18 +65,39 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                filterByStatus={filterByStatus}
+                setFilterByStatus={setFilterByStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isListLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodoId={selectedTodoId}
+                  setSelectedTodoId={setSelectedTodoId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {(selectedTodoId !== 0) && (
+        <TodoModal
+          todos={todos}
+          selectedTodoId={selectedTodoId}
+          isModalLoading={isModalLoading}
+          setIsModalLoading={setIsModalLoading}
+          onClose={() => {
+            setSelectedTodoId(0);
+          }}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {
+  Dispatch,
+  SetStateAction,
+  useMemo,
+  useState,
+} from 'react';
+import cn from 'classnames';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todos: Todo[],
+  isModalLoading: boolean;
+  selectedTodoId: number;
+  setIsModalLoading: Dispatch<SetStateAction<boolean>>;
+  onClose: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  todos,
+  selectedTodoId,
+  isModalLoading,
+  setIsModalLoading,
+  onClose,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const selectedTodo: Todo = todos.find(
+    todo => todo.id === selectedTodoId,
+  ) || todos[0];
+
+  useMemo(() => {
+    setIsModalLoading(true);
+    getUser(selectedTodo.userId)
+      .then(setUser)
+      .finally(() => setIsModalLoading(false));
+  }, [selectedTodo, setIsModalLoading]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {isModalLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +51,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodo.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +59,29 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={onClose}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={cn({
+                  'has-text-danger': !selectedTodo.completed,
+                  'has-text-success': selectedTodo.completed,
+                })}
+              >
+                {selectedTodo.completed ? 'Done' : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
               </a>
             </p>
           </div>
