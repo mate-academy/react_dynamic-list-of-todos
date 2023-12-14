fd2c64a334f979811de6808b4ba5867da12ab4ad
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-12-14 22:52:19 +0200

    added fix to TodoFilter component

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { SortBy } from '../../types/types';
 
 type Props = {
@@ -23,61 +23,45 @@ const mapGetSortBy = (sortBy: string): SortBy => {
   }
 };
 
-export const TodoFilter: React.FC<Props> = ({ getQuery, query, getSortBy }) => {
-  const [showDeleteBtn, setShowDeleteBtn] = useState(false);
+export const TodoFilter: React.FC<Props> = ({ getQuery, query, getSortBy }) => (
+  <form className="field has-addons">
+    <p className="control">
+      <span className="select">
+        <select
+          onChange={e => getSortBy(mapGetSortBy(e.target.value))}
+          data-cy="statusSelect"
+        >
+          <option value="all">All</option>
+          <option value="active">Active</option>
+          <option value="completed">Completed</option>
+        </select>
+      </span>
+    </p>
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
+    <p className="control is-expanded has-icons-left has-icons-right">
+      <input
+        data-cy="searchInput"
+        type="text"
+        className="input"
+        placeholder="Search..."
+        value={query}
+        onChange={e => getQuery(e.target.value)}
+      />
+      <span className="icon is-left">
+        <i className="fas fa-magnifying-glass" />
+      </span>
 
-    setShowDeleteBtn(true);
-    getQuery(value);
-  };
-
-  const handleOndelete = () => {
-    setShowDeleteBtn(false);
-    getQuery('');
-  };
-
-  return (
-    <form className="field has-addons">
-      <p className="control">
-        <span className="select">
-          <select
-            onChange={e => getSortBy(mapGetSortBy(e.target.value))}
-            data-cy="statusSelect"
-          >
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
-          </select>
+      {query && (
+        <span className="icon is-right btn-icon">
+          <button
+            data-cy="clearSearchButton"
+            aria-label="clear search"
+            type="button"
+            className="delete"
+            onClick={() => getQuery('')}
+          />
         </span>
-      </p>
-
-      <p className="control is-expanded has-icons-left has-icons-right">
-        <input
-          data-cy="searchInput"
-          type="text"
-          className="input"
-          placeholder="Search..."
-          value={query}
-          onChange={handleSearch}
-        />
-        <span className="icon is-left">
-          <i className="fas fa-magnifying-glass" />
-        </span>
-
-        {showDeleteBtn && (
-          <span className="icon is-right btn-icon">
-            <button
-              data-cy="clearSearchButton"
-              aria-label="clear search"
-              type="button"
-              className="delete"
-              onClick={handleOndelete}
-            />
-          </span>
-        )}
-      </p>
-    </form>
-  );
-};
+      )}
+    </p>
+  </form>
+);
