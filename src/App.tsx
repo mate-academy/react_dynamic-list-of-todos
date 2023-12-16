40d33239ba88209961f833e2fe4070f424c36ada
SerhiiUnhurian <sergeyunguryan322@gmail.com>
2023-12-16 22:18:26 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,58 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { Filter } from './types/filterBy';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [todoChosenId, setTodoChosenId] = useState<number | null>(null);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState(Filter.All);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filterTodos: Todo[] = useMemo(() => {
+    let filteredTodos = [...todos];
+
+    if (query) {
+      filteredTodos = filteredTodos.filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+    }
+
+    switch (filter) {
+      case Filter.Active:
+        return filteredTodos.filter(todo => !todo.completed);
+
+      case Filter.Completed:
+        return filteredTodos.filter(todo => todo.completed);
+
+      default:
+        return filteredTodos;
+    }
+  }, [query, filter, todos]);
+
+  const saveTodoId = (todoId: number | null) => {
+    setTodoChosenId(todoId);
+  };
+
+  const onQueryChange = (newQuery: string) => {
+    setQuery(newQuery);
+  };
+
+  // const onChange = (event: string) => {
+  //   setFilter(event);
+  // };
+
+  const chosenTodo = todos.find(todo => todo.id === todoChosenId);
+
   return (
     <>
       <div className="section">
@@ -17,18 +67,30 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={onQueryChange}
+                setOption={setFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              <TodoList
+                todos={filterTodos}
+                onEyeClick={saveTodoId}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {todoChosenId && (
+        <TodoModal
+          viewTodo={chosenTodo || null}
+          onClose={saveTodoId}
+        />
+      )}
     </>
   );
 };
