90dac95e5ebd5a302ba6f6cfda4fd3d1822129cf
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2023-12-19 20:25:13 +0100

    added filter func

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-
 /* eslint-disable max-len */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-/*  eslint-disable */
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 // import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 import { Todo } from './types/Todo';
-import { getTodos} from './api';
+import { getTodos } from './api';
+
+function filterTodos(todos: Todo[], todosActivityFilter: string, todosQuery: string) {
+  let resultTodos = [...todos];
+  const query = todosQuery.toLowerCase();
+
+  switch (todosActivityFilter) {
+    case 'Completed':
+      resultTodos = resultTodos.filter(todo => todo.completed
+      && todo.title.toLowerCase().includes(query));
+      break;
+    case 'Active':
+      resultTodos = resultTodos.filter(todo => !todo.completed
+      && todo.title.toLowerCase().includes(query));
+      break;
+    default:
+      resultTodos = resultTodos.filter(todo => todo.title.toLowerCase().includes(query));
+      break;
+  }
+
+  return resultTodos;
+}
 
 export const App: React.FC = () => {
-
   const [todos, setTodos] = useState<Todo[]>([]);
   const [todosActivityFilter, setTodosActivityFilter] = useState('All');
   const [todosQuery, setTodosQuery] = useState('');
   const [todoShownId, setTodoShownId] = useState(-1);
   const [loadingDone, setLoadingDone] = useState(false);
+
   useEffect(() => {
     const loadTodos = async () => {
       const data = await getTodos();
-    }
+
       setTodos(data);
+      setLoadingDone(true);
     };
 
     loadTodos();
   }, []);
-
 
   return (
     <>
