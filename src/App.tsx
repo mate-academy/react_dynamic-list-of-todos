8d9f22a635f9c86f2f1ed0e1bfc9276025f4b954
Marcin Ligmann <marcinligmann2@gmail.com>
2023-12-19 16:34:17 +0100

    added all todos to TodoList component with rigth color

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,28 +7,40 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [loading] = useState<boolean>(false);
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos);
+  }, []);
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
+            {/* {todos?.map(el => <p>{el.title}</p>)} */}
 
             <div className="block">
               <TodoFilter />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading
+                && <Loader />}
+              <TodoList todos={todos} />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {loading
+        && <TodoModal />}
     </>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
 
-export const TodoList: React.FC = () => (
+type Props = {
+  todos: Todo[] | null;
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
   <table className="table is-narrow is-fullwidth">
     <thead>
       <tr>
@@ -16,20 +22,46 @@ export const TodoList: React.FC = () => (
     </thead>
 
     <tbody>
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
+
+      {todos?.map(task => (
+        <tr data-cy="todo" className="">
+          <td className="is-vcentered">{task.id}</td>
+          {task.completed
+            ? (
+              <td className="is-vcentered">
+                <span className="icon" data-cy="iconCompleted">
+                  <i className="fas fa-check" />
+                </span>
+              </td>
+            )
+            : (<td className="is-vcentered" />)}
+          <td className="is-vcentered is-expanded">
+            <p className={classNames({
+              'has-text-danger': !task.completed,
+              'has-text-success': task.completed,
+            })}
+            >
+              {task.title}
+            </p>
+          </td>
+          <td className="has-text-right is-vcentered">
+            <button data-cy="selectButton" className="button" type="button">
+              <span className="icon">
+                <i className="far fa-eye" />
+              </span>
+            </button>
+          </td>
+        </tr>
+      ))}
+
+      {/* <td className="is-vcentered">
+        <span className="icon" data-cy="iconCompleted">
+          <i className="fas fa-check" />
+        </span>
+      </td> */}
+
+      {/* <td className="is-vcentered" /> */}
+
       <tr data-cy="todo" className="has-background-info-light">
         <td className="is-vcentered">2</td>
         <td className="is-vcentered" />
