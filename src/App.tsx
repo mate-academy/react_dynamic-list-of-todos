de44ec86789340760b07db8a3d84a1c156c444e0
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-21 16:43:56 +0100

    filter test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+// import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 import { getTodos } from './api';
 import { Todo } from './types/Todo';
+import { FilterCase } from './types/Filter';
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [filterBy, setFilterBy] = useState<FilterCase>(FilterCase.All);
 
   useEffect(() => {
     getTodos().then(setTodos);
   }, []);
+
+  const filteredTodos: Todo[] = useMemo(() => {
+    const copyTodos = [...todos];
+
+    if (query) {
+      copyTodos.filter((todo) => todo.title.includes(
+        query.trim().toLowerCase() || query.trim().toUpperCase(),
+      ));
+    }
+
+    switch (filterBy) {
+      case FilterCase.Active:
+        return copyTodos.filter((todo) => !todo.completed);
+
+      case FilterCase.Completed:
+        return copyTodos.filter((todo) => todo.completed);
+
+      default:
+        return copyTodos;
+    }
+  }, [query, filterBy, todos]);
 
   return (
     <>
@@ -23,12 +48,16 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                setFilterCase={setFilterBy}
+              />
             </div>
 
             <div className="block">
               <Loader />
-              <TodoList todos={todos} />
+              <TodoList todos={filteredTodos} />
             </div>
           </div>
         </div>
