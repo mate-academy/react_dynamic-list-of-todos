41f885768a0131e81d1f5c9525d65d302db55d42
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-21 15:47:50 +0100

    display correct Todos list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -22,13 +28,12 @@ export const App: React.FC = () => {
 
             <div className="block">
               <Loader />
-              <TodoList />
+              <TodoList todos={todos} />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
   );
 };
