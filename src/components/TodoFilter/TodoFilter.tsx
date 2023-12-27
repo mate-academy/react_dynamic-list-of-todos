ea781e75ebc8b2ad2a424886747a48fac0c7343b
Marek Maciejewski <marekmaciejewski1982@gmail.com>
2023-12-27 17:18:57 +0100

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,34 +1,66 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { useSignals } from '@preact/signals-react/runtime';
+import { FilterValues, FilterValuesType, KeyboardKeys } from '../../types';
+import { filterValue, searchQuery } from '../../signals';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+export const TodoFilter: React.FC = () => {
+  useSignals();
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button
-          data-cy="clearSearchButton"
-          type="button"
-          className="delete"
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    searchQuery.value = e.target.value;
+  };
+
+  const handleFilterValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    filterValue.value = e.target.value as FilterValuesType;
+  };
+
+  const handleResetQuery = () => {
+    searchQuery.value = '';
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select data-cy="statusSelect" onChange={handleFilterValue}>
+            <option value={FilterValues.all}>
+              All
+            </option>
+            <option value={FilterValues.active}>
+              Active
+            </option>
+            <option value={FilterValues.completed}>
+              Complete
+            </option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchQuery.value}
+          onChange={handleInput}
+          onKeyDown={e => e.key === KeyboardKeys.Enter && e.preventDefault()}
         />
-      </span>
-    </p>
-  </form>
-);
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {!!searchQuery.value.length && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleResetQuery}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import { useSignals } from '@preact/signals-react/runtime';
+import { effect, signal } from '@preact/signals-react';
+import { getUser } from '../../api';
 import { Loader } from '../Loader';
+import { selectedTodo, user } from '../../signals';
+
+const modalLoading = signal<boolean>(true);
+
+effect(() => {
+  if (selectedTodo.value) {
+    getUser(selectedTodo.value.userId)
+      .then(t => {
+        user.value = t;
+      })
+      .then(() => {
+        modalLoading.value = false;
+      });
+  }
+});
 
 export const TodoModal: React.FC = () => {
+  useSignals();
+
+  const handleCloseModal = () => {
+    selectedTodo.value = null;
+    user.value = null;
+    modalLoading.value = true;
+  };
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {modalLoading.value ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,7 +40,7 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodo.value?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -23,22 +48,24 @@ export const TodoModal: React.FC = () => {
               type="button"
               className="delete"
               data-cy="modal-close"
+              onClick={handleCloseModal}
             />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo.value?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {selectedTodo.value?.completed
+                ? <strong className="has-text-success">Done</strong>
+                : <strong className="has-text-danger">Planned</strong>}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">
-                Leanne Graham
+              <a href={`mailto:${user.value?.email}`}>
+                {user.value?.name}
               </a>
             </p>
           </div>
