8e2400da6d3c30f0965c2b593099eaeb2f1c4f8b
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2024-01-29 16:58:37 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,71 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { TodoFilter } from './components/TodoFilter'; // форма запиту
 import { TodoList } from './components/TodoList';
-import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
+
+interface FilterProps {
+  query: string;
+  filterBy: Filter;
+}
+
+function getFilteredTodos(
+  todos: Todo[],
+  { filterBy, query }: FilterProps,
+) {
+  let filteredTodos = [...todos];
+  const readyQuery = query.trim().toLowerCase();
+
+  switch (filterBy) {
+    case Filter.Active:
+      filteredTodos = todos.filter(todo => !todo.completed);
+      break;
+
+    case Filter.Completed:
+      filteredTodos = todos.filter(todo => todo.completed);
+      break;
+
+    default:
+      break;
+  }
+
+  if (readyQuery) {
+    return filteredTodos.filter(todo => todo.title.toLowerCase().includes(readyQuery));
+  }
+
+  return filteredTodos;
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [filterBy, setFilterBy] = useState(Filter.All);
+
+  const filteredTodos = getFilteredTodos(todos, { query, filterBy });
+
+  const handleQueryChange = (q: string) => {
+    if (q.trim() !== '' || q === '') {
+      setQuery(q);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +74,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={handleQueryChange}
+                filterOption={setFilterBy}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && (
+                <Loader />
+              )}
+
+              {!loading && (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  onSelect={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          setSelectedTodo={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
