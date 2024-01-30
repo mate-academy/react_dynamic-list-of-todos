ad183a49239de03ba474dc5731f9af09ac07b4f5
Vlad Gerula <vlad.gerula@gmail.com>
2024-01-30 23:43:19 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
+// eslint-disable-next-line import/no-cycle
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+export interface Filter {
+  title: string,
+  status: string;
+}
+
+function getFilteredTodos(todos:Todo[], filter: Filter) {
+  return todos.filter(todo => todo.title.toLowerCase().includes(filter.title.toLowerCase())
+  && (filter.status === 'all' || (filter.status === 'active' && !todo.completed) || (filter.status === 'completed' && todo.completed)));
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [sort, setSort] = useState<Filter>({ status: 'all', title: '' });
+  const [loading, setLoading] = useState(false);
+  const [onSelected, setSelected] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos().then(setTodos).finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +40,20 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter onFilter={setSort} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading
+                ? <Loader />
+                : (<TodoList todos={getFilteredTodos(todos, sort)} selectedTodoId={onSelected?.id} onSelect={(todo) => setSelected(todo)} />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {onSelected && <TodoModal selectedTodo={onSelected} onClose={() => setSelected(null)} />}
     </>
   );
 };
