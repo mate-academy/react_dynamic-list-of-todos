0c1b5f26ba7ddc1e0683967a7e053701d5c4847c
Vasilliy Yarosh <vasilliy.yarosh@gmail.com>
2024-01-31 17:46:01 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +9,25 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState<string>('');
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  }, []);
+
+  const filteredTodos = todos.filter(todo => {
+    // const statusFilter = query === 'all' || !query;
+    const textFilter = todo.title.toLowerCase().includes(query.toLowerCase());
+
+    return textFilter;
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +36,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onFilterChange={setQuery}
+                onClearSearch={() => setQuery('')}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {filteredTodos.length > 0 ? (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  onTodoSelected={setSelectedTodo}
+                />
+              ) : (
+                <Loader />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onClose={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,4 +1,5 @@
 export interface Todo {
+  status: string;
   id: number;
   title: string;
   completed: boolean;
