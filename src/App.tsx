c1ed2daabc6c60ac016fb4c562e83347ed5d3ca7
Olexandr Rapin <alexrapin04@gmail.com>
2024-02-08 11:19:52 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { FilterParams } from './types/FilterParams';
+import { getFilteredTodos } from './components/getFilteredTodos/getFilteredTodos';
+import { Status } from './types/Status';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterParams, setFilterParams] = useState<FilterParams>({
+    query: '',
+    status: Status.All,
+  });
+  const filteredTodos = getFilteredTodos(todos, filterParams);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => new Error('Try again later'))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +38,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterParams={filterParams}
+                setFilterParams={setFilterParams}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+
+              {!loading && (
+                <TodoList
+                  todos={filteredTodos}
+                  onSelect={setSelectedTodo}
+                  selectedTodoId={selectedTodo?.id}
+                />
+              )}
+
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          onSelect={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 import { Todo } from './types/Todo';
 import { User } from './types/User';
 
@@ -13,7 +14,6 @@ function wait(delay: number): Promise<void> {
 }
 
 function get<T>(url: string): Promise<T> {
-  // eslint-disable-next-line prefer-template
   const fullURL = BASE_URL + url + '.json';
 
   // we add some delay to see how the loader works
