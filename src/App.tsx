59e24f3526f03121954697e2ea54db3a4a2da725
Eater228 <543145yura@gmail.com>
2024-03-11 15:47:08 +0200

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Status } from './types/Status';
+import { getPreparedTodos } from './untils/getPreparedTodos';
 
 export const App: React.FC = () => {
+  const [posts, setPosts] = useState<Todo[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState(Status.All);
+  const [modalInfo, setModalInfo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(elemt => {
+        setPosts(elemt);
+      })
+      .catch(() => setLoading(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const visableTodos = useMemo(() => {
+    return getPreparedTodos(posts, filter, query);
+  }, [posts, filter, query]);
+
+  console.log(modalInfo);
+
   return (
     <>
       <div className="section">
@@ -17,18 +42,26 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filter={filter}
+                query={query}
+                setFilter={setFilter}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              <TodoList
+                posts={visableTodos}
+                modalInfo={modalInfo}
+                setModal={setModalInfo}
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {modalInfo && <TodoModal modalInfo={modalInfo} setModal={setModalInfo} />}
     </>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
 
-export const TodoList: React.FC = () => (
+type Porps = {
+  posts: Todo[];
+  modalInfo: Todo | null;
+  setModal: (modal: Todo) => void;
+};
+
+export const TodoList: React.FC<Porps> = ({ posts, modalInfo, setModal }) => (
   <table className="table is-narrow is-fullwidth">
     <thead>
       <tr>
@@ -16,85 +24,130 @@ export const TodoList: React.FC = () => (
     </thead>
 
     <tbody>
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
-      <tr data-cy="todo" className="has-background-info-light">
-        <td className="is-vcentered">2</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">quis ut nam facilis et officia qui</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye-slash" />
-            </span>
-          </button>
-        </td>
-      </tr>
-
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
-
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">6</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">
-            qui ullam ratione quibusdam voluptatem quia omnis
-          </p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
-
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">8</td>
-        <td className="is-vcentered">
-          <span className="icon" data-cy="iconCompleted">
-            <i className="fas fa-check" />
-          </span>
-        </td>
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-success">quo adipisci enim quam ut ab</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
+      {posts.map(post => (
+        <tr
+          data-cy="todo"
+          className={classNames({
+            'has-background-info-light': modalInfo?.id === post.id,
+          })}
+          key={post.id}
+        >
+          <td className="is-vcentered">{post.id}</td>
+          <td className="is-vcentered">
+            {post.completed && (
+              <span className="icon" data-cy="iconCompleted">
+                <i className="fas fa-check" />
+              </span>
+            )}
+          </td>
+          <td className="is-vcentered is-expanded">
+            <p
+              className={classNames(
+                post.completed ? 'has-text-success ' : 'has-text-danger',
+              )}
+            >
+              {post.title}
+            </p>
+          </td>
+          <td className="has-text-right is-vcentered">
+            <button
+              data-cy="selectButton"
+              className="button"
+              type="button"
+              onClick={() => setModal(post)}
+            >
+              <span className="icon">
+                <i
+                  className={classNames(
+                    'far',
+                    modalInfo?.id === post.id ? 'fa-eye-slash' : 'fa-eye',
+                  )}
+                />
+              </span>
+            </button>
+          </td>
+        </tr>
+      ))}
     </tbody>
   </table>
 );
+
+// <tr data-cy="todo" className="">
+//         <td className="is-vcentered">1</td>
+//         <td className="is-vcentered" />
+//         <td className="is-vcentered is-expanded">
+//           <p className="has-text-danger">delectus aut autem</p>
+//         </td>
+//         <td className="has-text-right is-vcentered">
+//           <button data-cy="selectButton" className="button" type="button">
+//             <span className="icon">
+//               <i className="far fa-eye" />
+//             </span>
+//           </button>
+//         </td>
+//       </tr>
+//       <tr data-cy="todo" className="has-background-info-light">
+//         <td className="is-vcentered">2</td>
+//         <td className="is-vcentered" />
+//         <td className="is-vcentered is-expanded">
+//           <p className="has-text-danger">quis ut nam facilis et officia qui</p>
+//         </td>
+//         <td className="has-text-right is-vcentered">
+//           <button data-cy="selectButton" className="button" type="button">
+//             <span className="icon">
+//               <i className="far fa-eye-slash" />
+//             </span>
+//           </button>
+//         </td>
+//       </tr>
+
+//       <tr data-cy="todo" className="">
+//         <td className="is-vcentered">1</td>
+//         <td className="is-vcentered" />
+//         <td className="is-vcentered is-expanded">
+//           <p className="has-text-danger">delectus aut autem</p>
+//         </td>
+//         <td className="has-text-right is-vcentered">
+//           <button data-cy="selectButton" className="button" type="button">
+//             <span className="icon">
+//               <i className="far fa-eye" />
+//             </span>
+//           </button>
+//         </td>
+//       </tr>
+
+//       <tr data-cy="todo" className="">
+//         <td className="is-vcentered">6</td>
+//         <td className="is-vcentered" />
+//         <td className="is-vcentered is-expanded">
+//           <p className="has-text-danger">
+//             qui ullam ratione quibusdam voluptatem quia omnis
+//           </p>
+//         </td>
+//         <td className="has-text-right is-vcentered">
+//           <button data-cy="selectButton" className="button" type="button">
+//             <span className="icon">
+//               <i className="far fa-eye" />
+//             </span>
+//           </button>
+//         </td>
+//       </tr>
+
+//       <tr data-cy="todo" className="">
+//         <td className="is-vcentered">8</td>
+//         <td className="is-vcentered">
+//           <span className="icon" data-cy="iconCompleted">
+//             <i className="fas fa-check" />
+//           </span>
+//         </td>
+//         <td className="is-vcentered is-expanded">
+//           <p className="has-text-success">quo adipisci enim quam ut ab</p>
+//         </td>
+//         <td className="has-text-right is-vcentered">
+//           <button data-cy="selectButton" className="button" type="button">
+//             <span className="icon">
+//               <i className="far fa-eye" />
+//             </span>
+//           </button>
+//         </td>
+//       </tr>
