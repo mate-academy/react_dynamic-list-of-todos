803b1a2f2ba3a1349b553e300a3b927c4f695047
Yehor <eee.houmich@gmail.com>
2024-03-17 09:30:00 +0200

    Solved

---
+++ src/utils/getFilteredTodos.tsx
@@ -0,0 +1,27 @@
+import { Status } from '../services/filter-types';
+import { Todo } from '../types/Todo';
+
+export function getFilteredTodos(
+  todos: Todo[],
+  titles: string,
+  filter: Status,
+) {
+  const visibleTodos = [...todos];
+
+  return visibleTodos
+    .filter(todo =>
+      todo.title.toLowerCase().includes(titles.trim().toLowerCase()),
+    )
+    .filter(todo => {
+      switch (filter) {
+        case Status.Active:
+          return !todo.completed;
+
+        case Status.Completed:
+          return todo.completed;
+
+        default:
+          return visibleTodos;
+      }
+    });
+}
