233d7b22dfcfc83c08be067535bd3c84e98a6a87
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-03-18 16:16:43 +0000

    Added task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,52 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { Status } from './types/Status';
+
+function filterTodos(todos: Todo[], filter: string, query: string) {
+  let filteredTodos;
+
+  switch (filter) {
+    case Status.All:
+      filteredTodos = [...todos];
+      break;
+    case Status.Active:
+      filteredTodos = todos.filter(todo => !todo.completed);
+      break;
+
+    case Status.Completed:
+      filteredTodos = todos.filter(todo => todo.completed);
+      break;
+
+    default:
+      filteredTodos = [...todos];
+  }
+
+  return filteredTodos.filter(todo =>
+    todo.title.toLowerCase().includes(query.toLowerCase()),
+  );
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filter, setFilter] = useState(Status.All);
+  const [query, setQuery] = useState('');
+
+  const filteredTodos = useMemo(() => {
+    return filterTodos(todos, filter, query);
+  }, [todos, filter, query]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filter={filter}
+                setFilter={setFilter}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              {!isLoading && todos.length > 0 && (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo?.id}
+                  onSelectTodo={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal todo={selectedTodo} onClose={() => setSelectedTodo(null)} />
+      )}
     </>
   );
 };
