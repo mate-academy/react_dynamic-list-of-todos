349353a260168cb9e70e091f1e4d1a9c503b147d
Roman Babii <ggdiscone@gmail.com>
2024-03-19 12:35:10 +0200

    implemented dynamic list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Status } from './components/enums/Status';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+function filterTodos(todos: Todo[], filter: Status, query: string) {
+  let filteredTodos = [...todos];
+
+  if (query) {
+    filteredTodos = filteredTodos.filter(({ title }) =>
+      title.toLowerCase().includes(query.toLowerCase()),
+    );
+  }
+
+  switch (filter) {
+    case Status.active:
+      return filteredTodos.filter(todo => !todo.completed);
+
+    case Status.complete:
+      return filteredTodos.filter(todo => todo.completed);
+
+    default:
+      return filteredTodos;
+  }
+}
+
 export const App: React.FC = () => {
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Status>(Status.all);
+  const [query, setQuery] = useState('');
+  const [openedTodo, setOpenedTodo] = useState<Todo | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const visibleTodos = filterTodos(todosFromServer, filter, query);
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then(setTodosFromServer)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +55,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setFilter={setFilter}
+                setQuery={setQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos}
+                  setOpenedTodo={setOpenedTodo}
+                  openedTodo={openedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {openedTodo && (
+        <TodoModal todo={openedTodo} setOpenedTodo={setOpenedTodo} />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,25 @@
-export const TodoFilter = () => (
+import React from 'react';
+import { Status } from '../enums/Status';
+
+type Props = {
+  setFilter: (filter: Status) => void;
+  setQuery: (query: string) => void;
+  query: string;
+};
+
+export const TodoFilter: React.FC<Props> = ({ setFilter, setQuery, query }) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          onChange={event => setFilter(event.target.value as Status)}
+        >
+          {Object.values(Status).map(status => (
+            <option key={status} value={status}>
+              {status.charAt(0).toUpperCase() + status.slice(1)}
+            </option>
+          ))}
         </select>
       </span>
     </p>
@@ -16,15 +30,23 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={event => setQuery(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
+      {query && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => setQuery('')}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
