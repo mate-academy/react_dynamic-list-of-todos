ab189d5b5328c3883c061246e9fdf17fa5ef78db
Roman Babii <ggdiscone@gmail.com>
2024-03-19 13:06:30 +0200

    refactored

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -7,46 +7,51 @@ type Props = {
   query: string;
 };
 
-export const TodoFilter: React.FC<Props> = ({ setFilter, setQuery, query }) => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select
-          data-cy="statusSelect"
-          onChange={event => setFilter(event.target.value as Status)}
-        >
-          {Object.values(Status).map(status => (
-            <option key={status} value={status}>
-              {status.charAt(0).toUpperCase() + status.slice(1)}
-            </option>
-          ))}
-        </select>
-      </span>
-    </p>
+export const TodoFilter: React.FC<Props> = ({ setFilter, setQuery, query }) => {
+  const getStatus = (status: string) =>
+    status.charAt(0).toUpperCase() + status.slice(1);
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
-
-      {query && (
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-            onClick={() => setQuery('')}
-          />
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={event => setFilter(event.target.value as Status)}
+          >
+            {Object.values(Status).map(status => (
+              <option key={status} value={status}>
+                {getStatus(status)}
+              </option>
+            ))}
+          </select>
         </span>
-      )}
-    </p>
-  </form>
-);
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => setQuery('')}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
