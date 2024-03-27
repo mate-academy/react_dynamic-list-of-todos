2a2d02904e830e372d1d5ad4892d5b7a4f6ef1e4
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-03-27 21:52:55 +0200

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,23 @@
-export const TodoFilter = () => (
+interface Props {
+  setFilterType: (filterType: string) => void;
+  filterText: string;
+  setFilterText: (filterText: string) => void;
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  filterText,
+  setFilterText,
+  setFilterType,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+            setFilterType(event.target.value)
+          }
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,6 +31,10 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={filterText}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setFilterText(event.target.value);
+        }}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
@@ -23,7 +42,14 @@ export const TodoFilter = () => (
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        {filterText.length > 0 && (
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => setFilterText('')}
+          />
+        )}
       </span>
     </p>
   </form>
