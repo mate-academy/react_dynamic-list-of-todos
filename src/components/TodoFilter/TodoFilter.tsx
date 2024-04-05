c96fdabe9ddc055d999185f149b77dfdbc5a5d96
Mykhailo <mykhailoevo@gmail.com>
2024-04-05 14:22:01 +0300

    solution correct

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -38,17 +38,17 @@ export const TodoFilter: React.FC<Props> = ({
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        {query && (
+      {query && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           <button
             data-cy="clearSearchButton"
             type="button"
             className="delete"
             onClick={() => setQuery('')}
           />
-        )}
-      </span>
+        </span>
+      )}
     </p>
   </form>
 );
