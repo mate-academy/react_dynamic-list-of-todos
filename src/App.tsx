4dc0bdfa4099883ea525f5caed336f62dc13162d
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-04-06 14:31:24 +0200

    partly done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,47 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { Filter } from './types/Filters';
 
 export const App: React.FC = () => {
+  const [todo, setTodo] = useState<Todo | null>();
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [modalHeader, setModalHeader] = useState(false);
+  const [visibleTodos, setVisibleTodos] = useState<Filter>('all');
+  const [searchInputValue, setSearchInput] = useState('');
+  const [isloading, setIsloading] = useState(true);
+
+  const filteredTodos = (objects: Todo[], filter: Filter, word: string) => {
+    switch (filter) {
+      case 'active':
+        return objects.filter(
+          t => t.completed === false && t.title.includes(word),
+        );
+
+      case 'completed':
+        return objects.filter(
+          t => t.completed === true && t.title.includes(word),
+        );
+
+      default:
+        return objects.filter(t => t.title.includes(word));
+    }
+  };
+
+  const newTodos = filteredTodos(todos, visibleTodos, searchInputValue);
+
+  useEffect(() => {
+    setIsloading(true);
+    getTodos()
+      .then(setTodos)
+      .catch(error => {
+        throw new Error(`${error} ups... can't load todos`);
+      })
+      .finally(() => setIsloading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +56,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                searchInputValue={searchInputValue}
+                setSearchInput={setSearchInput}
+                setVisibleTodos={setVisibleTodos}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isloading && <Loader />}
+              {!isloading && todos.length > 0 && (
+                <TodoList
+                  todos={newTodos}
+                  setModalHeader={setModalHeader}
+                  setTodo={setTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modalHeader && (
+        <TodoModal
+          todo={todo}
+          modalHeader={modalHeader}
+          setModalHeader={setModalHeader}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo | null | undefined;
+  modalHeader: boolean;
+  setModalHeader: (boolean: boolean) => void;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  todo,
+
+  setModalHeader,
+}) => {
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!todo ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,21 +26,30 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{todo.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              onClick={() => setModalHeader(false)}
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {todo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
