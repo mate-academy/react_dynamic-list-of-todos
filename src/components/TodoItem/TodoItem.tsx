b58768a6c4f6c147d8eeb7377a3a9a8c09a734b3
Ярина Пугач <puhachyaryna@gmail.com>
2024-04-12 18:22:53 +0200

    dynamic list of todos\

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
+
+type Props = {
+  todo: Todo;
+  selectedTodoId: number | null;
+  onSelect: (id: number | null) => void;
+};
+export const TodoItem: FC<Props> = ({ todo, selectedTodoId, onSelect }) => (
+  <tr data-cy="todo" className="">
+    <td className="is-vcentered">{todo.id}</td>
+    {todo.completed ? (
+      <td className="is-vcentered">
+        <span className="icon" data-cy="iconCompleted">
+          <i className="fas fa-check" />
+        </span>
+      </td>
+    ) : (
+      <td className="is-vcentered" />
+    )}
+    <td className="is-vcentered is-expanded">
+      <p
+        className={classNames(
+          { 'has-text-danger': !todo.completed },
+          { 'has-text-success': todo.completed },
+        )}
+      >
+        {todo.title}
+      </p>
+    </td>
+    <td className="has-text-right is-vcentered">
+      <button
+        data-cy="selectButton"
+        className="button"
+        type="button"
+        onClick={() => onSelect(todo.id)}
+      >
+        <span className="icon">
+          {selectedTodoId && selectedTodoId === todo.id ? (
+            <i className="far fa-eye-slash" />
+          ) : (
+            <i className="far fa-eye" />
+          )}
+        </span>
+      </button>
+    </td>
+  </tr>
+);
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo;
+  onClose: () => void;
+};
+export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUser(todo.userId).then(u => {
+      setSelectedUser(u);
+    });
+  }, [todo]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!selectedUser ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +30,34 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{todo.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onClose}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {todo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${selectedUser.email}`}>{selectedUser.name}</a>
             </p>
           </div>
         </div>
@@ -41,3 +65,10 @@ export const TodoModal: React.FC = () => {
     </div>
   );
 };
+// function useMemo(arg0: () => any, arg1: any[]) {
+//   throw new Error('Function not implemented.');
+// }
+
+// function useEffect(arg0: () => void, arg1: never[]) {
+//   throw new Error('Function not implemented.');
+// }
