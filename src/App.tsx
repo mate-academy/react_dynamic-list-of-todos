25583d273afc307931bb3bbdc19374c759a54ff3
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-04-26 09:13:08 +0200

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
-export const App: React.FC = () => {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    const filtered = todos.filter(todo =>
+      todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredTodos(filtered);
+  }, [searchTerm, todos]);
+
+  useEffect(() => {
+    getTodos()
+      .then(data => {
+        setTodos(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        throw new Error('Fetch not completed', error);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (filter === 'all') {
+      setFilteredTodos(todos);
+    } else {
+      setFilteredTodos(todos.filter(todo => {
+        return filter === 'active' ? !todo.completed : todo.completed;
+      }));
+    }
+  }, [todos, filter]);
+
+  const handleShowTodoModal = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleCloseTodoModal = () => {
+    setSelectedTodo(null);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +64,20 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter filter={filter} setFilter={setFilter} setSearchTerm={setSearchTerm} searchTerm={searchTerm}/>
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              <TodoList todos={filteredTodos} showModal={handleShowTodoModal} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && <TodoModal todo={selectedTodo} close={handleCloseTodoModal}/>}
     </>
   );
 };
+
+export default App;
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,43 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+interface UserDetails {
+  name: string;
+  email: string;
+}
+
+interface Props {
+  todo: Todo;
+  close: () => void;
+}
+
+export const TodoModal: React.FC<Props> = ({ todo, close }) => {
+  const [userDetails, setUserDetails] = useState<UserDetails>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://mate-academy.github.io/react_dynamic-list-of-todos/api/users/${todo.userId}.json`,
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch user details');
+        }
+
+        const userData = await response.json();
+
+        setUserDetails(userData);
+        setLoading(false);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error fetching userdetails:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchUserDetails();
+  }, [todo.userId]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
-
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
-            </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-            </p>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <div
+            className="modal-card-title has-text-weight-medium"
+            data-cy="modal-header"
+          >
+            Todo {todo.id}
           </div>
+          <button
+            type="button"
+            className="delete"
+            data-cy="modal-close"
+            onClick={close}
+          />
+        </header>
+        <div className="modal-card-body">
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <p className="block" data-cy="modal-title">
+                {todo.title}
+              </p>
+              {userDetails && (
+                <p className="block" data-cy="modal-user">
+                  <strong className="has-text-danger">Planned</strong> by{' '}
+                  <a href={`mailto:${userDetails.email}`}>{userDetails.name}</a>
+                </p>
+              )}
+            </>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
