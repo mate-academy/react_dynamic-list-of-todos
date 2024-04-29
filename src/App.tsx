911ea5e2748af42581285c7ef575732207861477
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-04-29 11:23:36 +0200

    Solution proposed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,111 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
-import { TodoFilter } from './components/TodoFilter';
+import { StatusFilter, TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+  const [statusFilter, setStatusFilter] = useState<StatusFilter>('all');
+  const [query, setQuery] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getTodos()
+      .then(data => {
+        setTodos(data);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    if (!todos) {
+      return null;
+    }
+
+    let newFilteredTodos;
+
+    const includesQuery = (todo: Todo) =>
+      query
+        ? todo.title.toLowerCase().includes(query.trim().toLowerCase())
+        : true;
+
+    switch (statusFilter) {
+      case 'active':
+        newFilteredTodos = todos.filter(t => !t.completed && includesQuery(t));
+        break;
+      case 'completed':
+        newFilteredTodos = todos.filter(t => t.completed && includesQuery(t));
+        break;
+      case 'all':
+      default:
+        newFilteredTodos = todos.filter(t => includesQuery(t));
+    }
+
+    return newFilteredTodos;
+  }, [todos, statusFilter, query]);
+
+  const selectedTodo = useMemo(() => {
+    if (filteredTodos && selectedTodoId) {
+      return filteredTodos.find(t => t.id === selectedTodoId);
+    }
+
+    return null;
+  }, [selectedTodoId, filteredTodos]);
+
+  const handleSelectTodo = (id: number | null) => setSelectedTodoId(id);
+  const handleCloseModal = () => setSelectedTodoId(null);
+  const handleStatusFilterChange = (status: StatusFilter) =>
+    setStatusFilter(status);
+  const handleSearchQueryChange = (newQuery: string) => setQuery(newQuery);
+
   return (
     <>
-      <div className="section">
-        <div className="container">
-          <div className="box">
-            <h1 className="title">Todos:</h1>
-
-            <div className="block">
-              <TodoFilter />
-            </div>
-
-            <div className="block">
-              <Loader />
-              <TodoList />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="section">
+            <div className="container">
+              <div className="box">
+                <h1 className="title">Todos:</h1>
+                <div className="block">
+                  <TodoFilter
+                    query={query}
+                    onSearch={handleSearchQueryChange}
+                    onSelectFilter={handleStatusFilterChange}
+                  />
+                </div>
+                <div className="block">
+                  {filteredTodos ? (
+                    <TodoList
+                      todos={filteredTodos}
+                      selectedTodoId={selectedTodoId}
+                      onSelect={handleSelectTodo}
+                    />
+                  ) : isError ? (
+                    <p>Something gone wrong</p>
+                  ) : (
+                    <Loader />
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-      <TodoModal />
+          {selectedTodo && (
+            <TodoModal todo={selectedTodo} close={handleCloseModal} />
+          )}
+        </>
+      )}
     </>
   );
 };
