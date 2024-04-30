f6b0548740176c744d580de9e147e42792a8b939
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-04-30 19:51:22 +0300

    initial state moved

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,16 @@ import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { initialTodo, initialUser } from './initialState/initialState';
+import { getTodos, getUser } from './api';
+import { getSortedTodos } from './helpers/getSortedTodos';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 import { Todo } from './types/Todo';
-import { getTodos, getUser } from './api';
-import { getSortedTodos } from './helpers/getSortedTodos';
 
 export const App: React.FC = () => {
-  const initialTodo = {
-    id: -1,
-    title: '',
-    completed: false,
-    userId: -1,
-  };
-  const initialUser = { id: -1, name: '', email: '', phone: '' };
-
   const [todoList, setTodoList] = useState<Todo[]>([]);
   const [loading, setLoading] = useState(false);
   const [loadingModal, setLoadingModal] = useState(false);
