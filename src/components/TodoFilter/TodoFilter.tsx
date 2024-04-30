59532f658d9770b349df4c8f0836d5595b0ea16c
Kirill <ovoo1339@gmail.com>
2024-04-30 13:25:59 +0300

    work

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,95 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  todos: Todo[];
+  setFilterTodos: (b: Todo[]) => void;
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter = ({ todos, setFilterTodos }: Props) => {
+  const [value, setValue] = useState('');
+  const [select, setSelect] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>(todos);
+
+  useEffect(() => {
+    let updatedTodos = todos;
+
+    switch (select) {
+      case 'active':
+        updatedTodos = todos.filter(todo => !todo.completed);
+        break;
+      case 'completed':
+        updatedTodos = todos.filter(todo => todo.completed);
+        break;
+      default:
+        break;
+    }
+
+    setFilteredTodos(updatedTodos);
+    setFilterTodos(updatedTodos);
+  }, [select, todos, setFilterTodos]);
+
+  useEffect(() => {
+    let updatedFilteredTodos = filteredTodos;
+
+    if (value.trim() !== '') {
+      updatedFilteredTodos = filteredTodos.filter(todo =>
+        todo.title.toLowerCase().includes(value.toLowerCase()),
+      );
+    }
+
+    setFilterTodos(updatedFilteredTodos);
+  }, [value, filteredTodos, setFilterTodos]);
+
+  const handleClearSearch = () => {
+    setValue('');
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={select}
+            onChange={event => setSelect(event.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={value}
+          onChange={event => setValue(event.target.value)}
+          onKeyDown={event => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+            }
+          }}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {value && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleClearSearch}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
