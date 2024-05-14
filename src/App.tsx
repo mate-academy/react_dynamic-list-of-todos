360785b7346c5ac09301d5dcb9d28ce6ec10ffb4
Prytula Olexandr <liga5872@gmail.com>
2024-05-14 14:20:19 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,27 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { FilteredTodos } from './utilities/FilteredTodos';
+
+export type FilterTodoBy = 'all' | 'active' | 'completed';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterBy, setFilterBy] = useState<FilterTodoBy>('all');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filteredTodos = FilteredTodos(todos, query, filterBy);
+
   return (
     <>
       <div className="section">
@@ -17,18 +36,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onFilter={setFilterBy}
+                onQueryChange={setQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  onSelected={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          setSelectedTodo={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
---
+++ src/utilities/FilteredTodos.tsx
@@ -0,0 +1,36 @@
+import { FilterTodoBy } from '../App';
+import { Todo } from '../types/Todo';
+
+export const FilteredTodos = (
+  todos: Todo[],
+  query: string,
+  filterBy: FilterTodoBy,
+) => {
+  let filteredTodos = [...todos];
+
+  if (filterBy) {
+    switch (filterBy) {
+      case 'active':
+        filteredTodos = filteredTodos.filter(todo => !todo.completed);
+        break;
+      case 'completed':
+        filteredTodos = filteredTodos.filter(todo => todo.completed);
+        break;
+      case 'all':
+      default:
+        break;
+    }
+  }
+
+  const normalizedQuery = query.trim().toLowerCase();
+
+  if (normalizedQuery) {
+    filteredTodos = filteredTodos.filter(todo => {
+      const normalizedTodoTitle = todo.title.trim().toLowerCase();
+
+      return normalizedTodoTitle.includes(normalizedQuery);
+    });
+  }
+
+  return filteredTodos;
+};
