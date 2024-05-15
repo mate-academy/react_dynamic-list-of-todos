69bb1bb5f89ca0838910d74d0c84b84fed7d72d0
Andrii <andriykostiuk1@gmail.com>
2024-05-15 19:39:32 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,58 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { SelectStatus } from './types/SelectStatus';
+import { FilterParams } from './types/FilterParams';
+
+const getPrepearedTodos = (todos: Todo[], filterParsms: FilterParams) => {
+  let prepearedTodos = [...todos];
+
+  if (filterParsms.query.trim() || filterParsms.status) {
+    prepearedTodos = prepearedTodos.filter(todo => {
+      const matchingTitle = todo.title
+        .toLowerCase()
+        .includes(filterParsms.query.trim().toLowerCase());
+
+      let matchingStatus: boolean;
+
+      switch (filterParsms.status) {
+        case SelectStatus.ACTIVE:
+          matchingStatus = !todo.completed;
+          break;
+        case SelectStatus.COMPLETED:
+          matchingStatus = todo.completed;
+          break;
+        default:
+          matchingStatus = true;
+      }
+
+      return matchingTitle && matchingStatus;
+    });
+  }
+
+  return prepearedTodos;
+};
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filterParsms, setFilterParams] = useState<FilterParams>({
+    query: '',
+    status: SelectStatus.ALL,
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(todosFromServer => setTodos(todosFromServer))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const prepearedTodos = getPrepearedTodos(todos, filterParsms);
+
   return (
     <>
       <div className="section">
@@ -17,18 +67,30 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterParsms={filterParsms}
+                setFilterParams={setFilterParams}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={prepearedTodos}
+                  selectTodo={setSelectedTodo}
+                  selectedTodoId={selectedTodo?.id}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal todo={selectedTodo} close={() => setSelectedTodo(null)} />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,72 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React from 'react';
+import { SelectStatus } from '../../types/SelectStatus';
+import { FilterParams } from '../../types/FilterParams';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+interface Props {
+  filterParsms: FilterParams;
+  setFilterParams: React.Dispatch<React.SetStateAction<FilterParams>>;
+}
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({
+  filterParsms,
+  setFilterParams,
+}) => {
+  const handlerSetStatusSelect = (evt: React.ChangeEvent<HTMLSelectElement>) =>
+    setFilterParams(params => ({
+      ...params,
+      status: evt.target.value as SelectStatus,
+    }));
+
+  const handlerSetQuery = (evt: React.ChangeEvent<HTMLInputElement>) =>
+    setFilterParams(params => ({ ...params, query: evt.target.value }));
+
+  const handlerResetQuery = () =>
+    setFilterParams(params => ({
+      ...params,
+      query: '',
+    }));
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            onChange={handlerSetStatusSelect}
+            defaultValue={SelectStatus.ALL}
+          >
+            <option value={SelectStatus.ALL}>All</option>
+            <option value={SelectStatus.ACTIVE}>Active</option>
+            <option value={SelectStatus.COMPLETED}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={filterParsms.query}
+          onChange={handlerSetQuery}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {filterParsms.query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              aria-label="clear search input"
+              onClick={handlerResetQuery}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
