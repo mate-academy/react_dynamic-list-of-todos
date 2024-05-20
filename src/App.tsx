97f21279d6f2040086e1c3b549f96325afcc79e3
Esther Guimarães <estherguimaraesl@gmail.com>
2024-05-20 18:18:28 -0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { getTodos } from './api';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [selectedList, setSelectedList] = useState<string>('all');
+  const [filterText, setFilterText] = useState('');
+  const [filteredList, setFilteredList] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getTodos().then(todos => {
+      setTodoList(todos);
+      setFilteredList(todos);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    const selectedTable = todoList.filter(todo => {
+      const matchesFilterText = todo.title
+        .toLowerCase()
+        .includes(filterText.toLowerCase());
+
+      switch (selectedList) {
+        case 'active':
+          return !todo.completed && matchesFilterText;
+        case 'completed':
+          return todo.completed && matchesFilterText;
+        default:
+          return matchesFilterText;
+      }
+    });
+    setFilteredList(selectedTable);
+  }, [selectedList, todoList, filterText]);
+
+  const handleFilterChange = (newValue: string) => {
+    setFilterText(newValue);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +54,18 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onFilterChange={handleFilterChange}
+                onSelectedChange={setSelectedList}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? <Loader /> : <TodoList todoList={filteredList} />}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
     </>
   );
 };
