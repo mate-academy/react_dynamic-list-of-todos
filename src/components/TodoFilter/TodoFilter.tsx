4fdd1f92c0563a42cd053e0a5dab0fce8830d4e9
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2024-06-11 16:37:36 +0100

    deploy fix

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -43,7 +43,6 @@ export const TodoFilter: React.FC<Props> = ({
         </span>
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           {query && (
             <button
               data-cy="clearSearchButton"
