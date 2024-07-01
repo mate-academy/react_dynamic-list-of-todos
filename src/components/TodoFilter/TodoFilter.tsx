aa43342ae44c361dd928892efc10aac630c9ade7
kshvetsova <katshvetsova@gmail.com>
2024-07-01 23:11:53 +0300

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,26 @@
-export const TodoFilter = () => (
+import { SelectField } from '../../types/SelectField';
+
+type Props = {
+  selectValue: SelectField;
+  setSelectValue: (value: SelectField) => void;
+  query: string;
+  setQuery: (v: string) => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  selectValue,
+  setSelectValue,
+  query,
+  setQuery,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={selectValue}
+          onChange={({ target }) => setSelectValue(target.value as SelectField)}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,14 +34,22 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        {query && (
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => setQuery('')}
+          />
+        )}
       </span>
     </p>
   </form>
