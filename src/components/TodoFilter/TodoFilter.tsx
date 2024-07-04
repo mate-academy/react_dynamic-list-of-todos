12f2dc3de58f74e18ad56e82ce0a13704c59a183
Oleksii Knihin <alexyo2626@gmail.com>
2024-07-04 22:49:46 +0300

    add solution dynamic todos list

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,26 @@
-export const TodoFilter = () => (
+import { Completed } from "../../types/Filters";
+
+interface Props {
+  value: string;
+  onQuery: (text: string) => void;
+  onCompleted: (completed: Completed) => void;
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  value,
+  onQuery,
+  onCompleted,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          onChange={(e) => onCompleted(e.target.value as Completed)}
+        >
+          <option value={Completed.All}>All</option>
+          <option value={Completed.Active}>Active</option>
+          <option value={Completed.Completed}>Completed</option>
         </select>
       </span>
     </p>
@@ -16,14 +31,23 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={value}
+        onChange={(e) => onQuery(e.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+      <span className="icon is-right" style={{ pointerEvents: "all" }}>
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        {value && (
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => onQuery("")}
+          />
+        )}
       </span>
     </p>
   </form>
