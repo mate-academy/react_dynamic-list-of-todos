9c255d7f3cd6a64eade37ecc82d6b4946cbf8ff1
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-07-04 20:00:46 +0300

    made dynamic list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,69 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Completed, Filters } from './types/Filters';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [modal, setModal] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filtersParams, setFiltersParams] = useState<Filters>({
+    completedType: Completed.All,
+    searchByText: '',
+  });
+
+  const handleCloseModal = () => {
+    setSelectedTodo(null);
+  };
+
+  const handleOpenModal = (todo: Todo) => {
+    setSelectedTodo(todo);
+    setModal(true);
+  };
+
+  const changeFilters = (key: keyof Filters, value: string | Completed) => {
+    setFiltersParams(prevParams => {
+      return {
+        ...prevParams,
+        [key]: value,
+      };
+    });
+  };
+
+  const filterTodos = useMemo(() => {
+    let newTodos = [...todos];
+
+    if (filtersParams.completedType) {
+      newTodos = newTodos.filter(item => {
+        switch (filtersParams.completedType) {
+          case Completed.Active:
+            return item.completed === false;
+          case Completed.Completed:
+            return item.completed;
+          case Completed.All:
+          default:
+            return true;
+        }
+      });
+    }
+
+    if (filtersParams.searchByText) {
+      newTodos = newTodos.filter(item => {
+        return item.title
+          .toLowerCase()
+          .includes(filtersParams.searchByText.toLowerCase());
+      });
+    }
+
+    return newTodos;
+  }, [todos, filtersParams]);
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +78,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filtersParams={filtersParams}
+                onSetParam={changeFilters}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0 ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filterTodos}
+                  onTodo={handleOpenModal}
+                  currTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modal && (
+        <TodoModal
+          onModal={setModal}
+          todo={selectedTodo}
+          closeTodo={handleCloseModal}
+        />
+      )}
     </>
   );
 };
