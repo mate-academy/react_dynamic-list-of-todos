0ec1e19da72bf677eabedb8ff12ca6dba4c6967d
Ruslan <gnatisin.ruslan1988@gmail.com>
2024-07-05 00:56:59 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,73 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Filters, Select } from './types';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [filters, setFilters] = useState<Filters>({
+    search: '',
+    select: Select.All,
+  });
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const handleOpen = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleClose = () => {
+    setSelectedTodo(null);
+  };
+
+  const handleFilters = (key: keyof Filters, value: string | Select) => {
+    setFilters(currentState => ({
+      ...currentState,
+      [key]: value,
+    }));
+  };
+
+  const filteredTodos = useMemo(() => {
+    let filtered = [...todos];
+
+    switch (filters.select) {
+      case Select.Active:
+        filtered = filtered.filter(todo => !todo.completed);
+        break;
+
+      case Select.Completed:
+        filtered = filtered.filter(todo => todo.completed);
+        break;
+
+      case Select.All:
+        break;
+      default:
+        break;
+    }
+
+    if (filters.search) {
+      const search = filters.search.toLowerCase();
+
+      filtered = filtered.filter(todo => {
+        const title = todo.title.toLowerCase();
+
+        return title.includes(search);
+      });
+    }
+
+    return filtered;
+  }, [todos, filters]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +82,24 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter onFilter={handleFilters} filters={filters} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  onOpen={handleOpen}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && <TodoModal onClose={handleClose} todo={selectedTodo} />};
     </>
   );
 };
