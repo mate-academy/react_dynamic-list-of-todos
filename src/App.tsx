ab3fa58b59a2dce4514273b0a50e637bcc09ed48
Vlad <vladkygot@gmail.com>
2024-07-19 11:21:48 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,57 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [status, setStatus] = useState<string>('all');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [selectTodo, setSelectTodo] = useState<Todo | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const handleStatusChange = (newStatus: string) => {
+    setStatus(newStatus);
+  };
+
+  const handleSearchChange = (newSearchTerm: string) => {
+    setSearchTerm(newSearchTerm);
+  };
+
+  const onDeleteSearchChange = () => {
+    setSearchTerm('');
+  };
+
+  const handleClose = () => {
+    setSelectTodo(null);
+  };
+
+  const handleOpen = (todo: Todo) => {
+    setSelectTodo(todo);
+  };
+
+  const filteredTodos = useMemo(() => {
+    return todos.filter(todo => {
+      const matchesStatus =
+        status === 'all' ||
+        (status === 'active' && !todo.completed) ||
+        (status === 'completed' && todo.completed);
+      const matchesSearch = todo.title
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+
+      return matchesStatus && matchesSearch;
+    });
+  }, [todos, status, searchTerm]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +66,30 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={status}
+                searchTerm={searchTerm}
+                onStatusChange={handleStatusChange}
+                onSearchChange={handleSearchChange}
+                onDeleteSearchChange={onDeleteSearchChange}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  onOpen={handleOpen}
+                  selectedTodoId={selectTodo?.id || null}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectTodo && <TodoModal todo={selectTodo} onClose={handleClose} />}
     </>
   );
 };
