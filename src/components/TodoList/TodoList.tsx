2a411595496e5474d16310e752f4f572b0d3f486
Den <clixqqk@gmail.com>
2024-08-02 12:48:57 +0300

    deve

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -77,8 +77,9 @@ export const TodoList: React.FC<Props> = ({
               </td>
               <td className="is-vcentered is-expanded">
                 <p
-                  className={`${todo.completed ? 'has-text-success' : 'has-text-danger'
-                    }`}
+                  className={`${
+                    todo.completed ? 'has-text-success' : 'has-text-danger'
+                  }`}
                 >
                   {todo.title}
                 </p>
