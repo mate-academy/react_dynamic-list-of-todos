5330fa536b6653b67e763818452043c91d4e45b5
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-08-21 12:52:20 +0300

    task complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,49 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [isLoader, setIsLoader] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [methodFilter, setMethodFilter] = useState('all');
+  const [value, setValue] = useState('');
+
+  const [activeTodo, setActiveTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setIsLoader(true);
+
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+        setFilteredTodos(todosFromServer);
+      })
+      .finally(() => setIsLoader(false));
+  }, []);
+
+  useEffect(() => {
+    let filtered = todos;
+
+    if (methodFilter === 'active') {
+      filtered = todos.filter(todo => !todo.completed);
+    }
+
+    if (methodFilter === 'completed') {
+      filtered = todos.filter(todo => todo.completed);
+    }
+
+    if (value) {
+      filtered = filtered.filter(todo =>
+        todo.title.toLocaleLowerCase().includes(value.toLowerCase()),
+      );
+    }
+
+    setFilteredTodos(filtered);
+  }, [methodFilter, todos, value]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                method={methodFilter}
+                setMethod={setMethodFilter}
+                value={value}
+                setValue={setValue}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoader ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={activeTodo}
+                  onSelectedTodo={setActiveTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
+      {activeTodo && (
+        <TodoModal
+          selectedTodo={activeTodo}
+          onCloseSelectedTodo={() => setActiveTodo(null)}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,71 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  method: string;
+  setMethod: (method: string) => void;
+  value: string;
+  setValue: (str: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({
+  method,
+  setMethod,
+  value,
+  setValue,
+}) => {
+  const handleOnSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setMethod(event.target.value);
+  };
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleCleanInput = () => {
+    setValue('');
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={method}
+            onChange={handleOnSelect}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={value}
+          onChange={handleChangeInput}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {value.length > 0 && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleCleanInput}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
