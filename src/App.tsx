d2f44f6d8966a87d81d1d810aef2a150ac9b3c70
mar kol <example@e.com>
2024-08-22 16:41:48 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,45 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+export const enum SortMethod {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
 
 export const App: React.FC = () => {
+  const [isLoader, setIsLoader] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [methodFilter, setMethodFilter] = useState(SortMethod.All);
+  const [value, setValue] = useState('');
+  const [activeTodo, setActiveTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setIsLoader(true);
+
+    getTodos()
+      .then(todosFromServer => {
+        setTodos(todosFromServer);
+      })
+      .finally(() => setIsLoader(false));
+  }, []);
+
+  const filteredTodos = todos
+    .filter(todo => {
+      switch (methodFilter) {
+        case SortMethod.Active:
+          return !todo.completed;
+        case SortMethod.Completed:
+          return todo.completed;
+        default:
+          return true;
+      }
+    })
+    .filter(todo => todo.title.toLowerCase().includes(value.toLowerCase()));
+
   return (
     <>
       <div className="section">
@@ -17,18 +54,35 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                method={methodFilter}
+                setMethod={setMethodFilter}
+                value={value}
+                setValue={setValue}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoader ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={activeTodo}
+                  onSelectedTodo={setActiveTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {activeTodo && (
+        <TodoModal
+          selectedTodo={activeTodo}
+          onCloseSelectedTodo={() => setActiveTodo(null)}
+        />
+      )}
     </>
   );
 };
