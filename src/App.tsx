6f9372572feda61c28b4bb739989b1edc4bc4ff4
Oleksandr Cherpak <cherpak.oleksandr.wsei@gmail.com>
2024-08-25 19:00:51 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,50 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loaded, setLoaded] = useState(false);
+  const [selectValue, setSelectValue] = useState('all');
+  const [filtredTodos, setFiltredTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo>();
+
+  const onModalClick = () => {
+    setSelectedTodo(undefined);
+  };
+
+  useEffect(() => {
+    setLoaded(false);
+    getTodos()
+      .then(todosFromApi => setTodos(todosFromApi))
+      .finally(() => setLoaded(true));
+  }, []);
+
+  useEffect(() => {
+    let localFiltredTodos: Todo[] = [];
+
+    if (selectValue === 'active') {
+      localFiltredTodos = todos.filter(todo => !todo.completed);
+    }
+
+    if (selectValue === 'completed') {
+      localFiltredTodos = todos.filter(todo => todo.completed);
+    }
+
+    if (selectValue === 'all') {
+      localFiltredTodos = [...todos];
+    }
+
+    localFiltredTodos = localFiltredTodos.filter(todo =>
+      todo.title.toLowerCase().includes(query.toLowerCase()),
+    );
+
+    setFiltredTodos(localFiltredTodos);
+  }, [todos, selectValue, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +59,29 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selectValue={selectValue}
+                onSelect={setSelectValue}
+                onQueryChange={setQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!loaded && <Loader />}
+              <TodoList
+                todos={filtredTodos}
+                setSelectedTodo={setSelectedTodo}
+                selectedTodo={selectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal selectedTodo={selectedTodo} onModalClick={onModalClick} />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: Todo;
+  onModalClick: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({ selectedTodo, onModalClick }) => {
+  const [currentUser, setCurrentUser] = useState<User>();
+
+  useEffect(() => {
+    getUser(selectedTodo.userId).then(user => setCurrentUser(user));
+  }, [selectedTodo]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!currentUser ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +29,35 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodo.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onModalClick}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {selectedTodo.completed && (
+                <strong className="has-text-success">Done</strong>
+              )}
+
+              {!selectedTodo.completed && (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href="mailto:Sincere@april.biz">{currentUser.name}</a>
             </p>
           </div>
         </div>
