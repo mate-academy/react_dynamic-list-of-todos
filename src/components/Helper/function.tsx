a81f368cdcbfbce87eaaec900d15eb060b4539cd
Yurii <uaremko945@gmail.com>
2024-08-26 14:38:48 +0300

    Solution

---
+++ src/components/Helper/function.tsx
@@ -0,0 +1,39 @@
+import { Todo } from '../../types/Todo';
+
+export enum Statuses {
+  All = 'All',
+  active = 'active',
+  completed = 'completed',
+}
+
+export const filterTodos = (
+  todos: Todo[],
+  selectedFilter: Statuses,
+  searchTerm: string,
+): Todo[] => {
+  let filtered = todos;
+
+  switch (selectedFilter) {
+    case Statuses.All:
+      break;
+
+    case Statuses.active:
+      filtered = todos.filter(todo => !todo.completed);
+      break;
+
+    case Statuses.completed:
+      filtered = todos.filter(todo => todo.completed);
+      break;
+
+    default:
+      break;
+  }
+
+  if (searchTerm) {
+    filtered = filtered.filter(todo =>
+      todo.title.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+  }
+
+  return filtered;
+};
