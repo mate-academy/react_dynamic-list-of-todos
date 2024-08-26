94091c6a78f78c9a2c4aa801e7e76bd42ac7acef
Anuta Kuzmych <a.kusmych@gmail.com>
2024-08-26 13:19:12 +0300

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,29 +1,55 @@
-export const TodoFilter = () => (
+import React from 'react';
+import { Filter } from '../../types/EnumFilter';
+
+type Props = {
+  status: string;
+  query: string;
+  onStatusChanged: (status: string) => void;
+  onQueryChanged: (query: string) => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  status,
+  query,
+  onStatusChanged,
+  onQueryChanged,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          value={status}
+          onChange={event => onStatusChanged(event.target.value)}
+        >
+          <option value={Filter.all}>All</option>
+          <option value={Filter.active}>Active</option>
+          <option value={Filter.completed}>Completed</option>
         </select>
       </span>
     </p>
-
     <p className="control is-expanded has-icons-left has-icons-right">
       <input
         data-cy="searchInput"
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={event => onQueryChanged(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        {query && (
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => onQueryChanged('')}
+          />
+        )}
       </span>
     </p>
   </form>
