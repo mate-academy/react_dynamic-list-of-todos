50c6f82b2cda89f8b3ef24f5d280f5a82ec49180
Ivan Opar <vanya.opar@gmail.com>
2024-08-29 21:05:25 +0300

    add task solution

---
+++ src/components/Todo/TodoCard.tsx
@@ -0,0 +1,69 @@
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import React from 'react';
+
+type Props = {
+  selectedTodo: Todo | null;
+  setSelectedTodo: (todo: Todo) => void;
+  setIsModalActive: (isModalActive: boolean) => void;
+  isModalActive: boolean;
+  todo: Todo;
+};
+
+export const TodoCard: React.FC<Props> = ({
+  selectedTodo,
+  setSelectedTodo,
+  setIsModalActive,
+  isModalActive,
+  todo,
+}) => {
+  return (
+    <tr
+      data-cy="todo"
+      className={classNames(
+        selectedTodo?.id === todo?.id && isModalActive
+          ? 'has-background-info-light'
+          : '',
+      )}
+    >
+      <td className="is-vcentered">{todo?.id}</td>
+      <td className="is-vcentered">
+        {todo?.completed && (
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+      <td className="is-vcentered is-expanded">
+        <p
+          className={classNames(
+            todo?.completed ? 'has-text-success' : 'has-text-danger',
+          )}
+        >
+          {todo?.title}
+        </p>
+      </td>
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={() => {
+            setSelectedTodo(todo);
+            setIsModalActive(true);
+          }}
+        >
+          <span className="icon">
+            <i
+              className={classNames(
+                selectedTodo?.id === todo?.id && isModalActive
+                  ? 'far fa-eye-slash'
+                  : 'far fa-eye',
+              )}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
