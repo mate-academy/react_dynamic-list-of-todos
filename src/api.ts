382f71853e33fb4afc7cc42ed7854d7c1163906f
Ivan Opar <vanya.opar@gmail.com>
2024-08-30 10:22:57 +0300

    add task solution

--- src/api.ts
+++ src/api.ts
@@ -13,7 +13,7 @@ function wait(delay: number): Promise<void> {
 function get<T>(url: string): Promise<T> {
   const fullURL = BASE_URL + url + '.json';
 
-  return wait(300)
+  return wait(2000)
     .then(() => fetch(fullURL))
     .then(res => res.json());
 }
