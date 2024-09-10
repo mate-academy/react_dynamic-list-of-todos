6279820b0d1dae79837464c266e4e301a8ed98ff
Yulii Zilnyk <yuliizilnyk@gmail.com>
2024-09-10 12:13:17 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,45 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { TodoStatus } from './types/TodoStatus';
+import { getFilteredTodos } from './utils/getFilteredTodos';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filterBy, setFilterBy] = useState<TodoStatus>(TodoStatus.all);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const handleSelect = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const handleClose = () => {
+    setSelectedTodo(null);
+  };
+
+  const handleQueryReset = () => {
+    setQuery('');
+  };
+
+  const filteredTodos = useMemo(() => {
+    const filtered = getFilteredTodos(todos, filterBy);
+
+    return filtered.filter(todo =>
+      todo.title.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [filterBy, query, todos]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +54,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterBy={filterBy}
+                query={query}
+                onFilterChange={event =>
+                  setFilterBy(event.target.value as TodoStatus)
+                }
+                onQueryChange={event => setQuery(event.target.value)}
+                onClearQuery={handleQueryReset}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              {!isLoading && todos.length > 0 && (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  onSelectedTodo={handleSelect}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal selectedTodo={selectedTodo} onClose={handleClose} />
+      )}
     </>
   );
 };
