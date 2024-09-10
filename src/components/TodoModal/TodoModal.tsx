d07e5cb7a9fc5df526afa66bba72ae298a634659
Julia Churkina <curkinau980@gmail.com>
2024-09-10 18:53:53 +0200

    add task solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Loader } from '../Loader';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  currentTodo: Todo;
+  user: User | null;
+  userId: number;
+  canselSelectedTodo: () => void;
+  cardLoading: boolean;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  currentTodo,
+  cardLoading,
+  user,
+  canselSelectedTodo = () => {},
+}) => {
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {cardLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +31,36 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${currentTodo.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              onClick={canselSelectedTodo}
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {currentTodo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={classNames('has-text-success', {
+                  'has-text-danger': currentTodo.completed === false,
+                })}
+              >
+                {currentTodo.completed === true ? 'Done' : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href="mailto:Sincere@april.biz">{user?.name}</a>
             </p>
           </div>
         </div>
