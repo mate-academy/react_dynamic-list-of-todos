ee70a0e1e70454d5b1f87d875ae4d13b0f604ef6
Seva Podolskiy <s_podolskiy@live.com>
2024-09-16 13:11:49 -0400

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,77 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { getTodos } from './api';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { Todo } from './types/Todo';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selTodo, setSelTodo] = useState<Todo | null>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [todosLoaded, setTodosLoaded] = useState(false);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(res => {
+        setTodos(res);
+        setTodosLoaded(true);
+      })
+      .catch(() => {
+        setTodosLoaded(true);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selTodo) {
+      setShowModal(true);
+    }
+  }, [selTodo]);
+
+  useEffect(() => {
+    getTodos()
+      .then(res => {
+        let tempTodos = [...res];
+
+        switch (filter) {
+          case 'active':
+            tempTodos = tempTodos.filter(todo => !todo.completed);
+            break;
+          case 'completed':
+            tempTodos = tempTodos.filter(todo => todo.completed);
+            break;
+
+          case 'all':
+          default:
+            break;
+        }
+
+        tempTodos = tempTodos.filter(todo =>
+          todo.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
+        );
+
+        setTodos(tempTodos);
+        setTodosLoaded(true);
+      })
+      .catch(() => {
+        setTodos([]);
+        setTodosLoaded(true);
+      });
+  }, [query, filter]);
+
+  const handleModalClose = () => {
+    setSelTodo(null);
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +80,25 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter onSelect={setFilter} onQuery={setQuery} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!todosLoaded ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={todos}
+                  selectedId={selTodo ? selTodo.id : null}
+                  onViewTodo={setSelTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {showModal && <TodoModal todo={selTodo} onClose={handleModalClose} />}
     </>
   );
 };
