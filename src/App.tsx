6c0708b1074127731c0d3cbeba3c2ff6534191ee
Pavlo Pukalo <misterkatlet@gmail.com>
2024-10-02 16:09:31 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,49 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+function getPreparedTodos(
+  initTodos: Todo[],
+  status: string,
+  query: string,
+): Todo[] {
+  let todos = [...initTodos];
+
+  if (status === 'completed') {
+    todos = todos.filter(todo => todo.completed);
+  }
+
+  if (status === 'active') {
+    todos = todos.filter(todo => !todo.completed);
+  }
+
+  if (query) {
+    todos = todos.filter(todo =>
+      todo.title.toLowerCase().includes(query.toLowerCase()),
+    );
+  }
+
+  return todos;
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const [status, setStatus] = useState('all');
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const visibleTodos = getPreparedTodos(todos, status, query);
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={status}
+                onSelect={setStatus}
+                query={query}
+                onQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={visibleTodos}
+                  // onModal={setIsModalActive}
+                  onSelectTodo={setTodo}
+                  selectedTodo={todo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {todo && <TodoModal selectedTodo={todo} onSelectTodo={setTodo} />}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import classNames from 'classnames';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  onSelectTodo: (val: Todo | null) => void;
+  selectedTodo: Todo | null;
+};
+
+export const TodoModal: React.FC<Props> = ({ onSelectTodo, selectedTodo }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<null | User>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (selectedTodo === null) {
+      return;
+    }
+
+    getUser(selectedTodo.userId)
+      .then(setUser)
+      .finally(() => setIsLoading(false));
+  }, [selectedTodo, onSelectTodo]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +38,37 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => onSelectTodo(null)}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={classNames({
+                  'has-text-danger': !selectedTodo?.completed,
+                  'has-text-success': selectedTodo?.completed,
+                })}
+              >
+                {selectedTodo?.completed ? 'Done' : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
             </p>
           </div>
         </div>
