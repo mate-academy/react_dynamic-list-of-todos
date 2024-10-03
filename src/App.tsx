0cdcf40deb4e7a6368aae6fe097165a11a9ab6fa
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2024-10-03 14:25:23 +0200

    Solution done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,54 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos, getUser } from './api';
+import { User } from './types/User';
+import { Filter } from './types/Options';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loadingTodo, setLoadingTodo] = useState(true);
+  const [loadingUser, setLoadingUser] = useState(true);
+
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const [selectedOption, setSelectedOption] = useState<Filter>(Filter.all);
+  const [query, setQuery] = useState<string>('');
+
+  function handleSelectedOption(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedOption(event.target.value as Filter);
+  }
+
+  function handleQueryChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+  }
+
+  function showSelectedTodo(todo: Todo) {
+    setSelectedTodo(todo);
+
+    getUser(todo.userId)
+      .then(setSelectedUser)
+      .finally(() => setLoadingUser(false));
+  }
+
+  function closeSelectedTodo() {
+    setSelectedTodo(null);
+    setSelectedUser(null);
+  }
+
+  function resetFilterParams() {
+    setQuery('');
+    setSelectedOption(Filter.all);
+  }
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoadingTodo(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +63,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                inputValue={query}
+                handleSelectedOption={handleSelectedOption}
+                handleQueryChange={handleQueryChange}
+                resetFilterParams={resetFilterParams}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loadingTodo && <Loader />}
+              <TodoList
+                todos={todos}
+                showSelectedTodo={showSelectedTodo}
+                selectedOption={selectedOption}
+                query={query}
+                selectedTodo={selectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          closeTodo={closeSelectedTodo}
+          user={selectedUser}
+          loadingUser={loadingUser}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,21 +1,37 @@
-export const TodoFilter = () => (
+import { Filter } from '../../types/Options';
+
+interface Props {
+  inputValue: string;
+  handleSelectedOption: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  handleQueryChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  resetFilterParams: () => void;
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  inputValue,
+  handleSelectedOption,
+  handleQueryChange,
+  resetFilterParams,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select data-cy="statusSelect" onChange={handleSelectedOption}>
+          <option value="all">{Filter.all}</option>
+          <option value="active">{Filter.active}</option>
+          <option value="completed">{Filter.completed}</option>
         </select>
       </span>
     </p>
 
     <p className="control is-expanded has-icons-left has-icons-right">
       <input
+        value={inputValue}
         data-cy="searchInput"
         type="text"
         className="input"
         placeholder="Search..."
+        onChange={handleQueryChange}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
@@ -23,7 +39,14 @@ export const TodoFilter = () => (
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        {inputValue && (
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={resetFilterParams}
+          />
+        )}
       </span>
     </p>
   </form>
