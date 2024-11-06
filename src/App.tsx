b967a0e74297cae18ba7868acaae81e5e15358bc
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-11-06 17:54:18 +0200

    got some problem with loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,75 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { Filter } from './types/enumFilter';
 
 export const App: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const [loader, setLoader] = useState(true);
+  const [flag, setFlag] = useState(Filter.All);
+  const [modalLoad, setModalLoad] = useState(true);
+
+  useEffect(() => {
+    getTodos()
+      .then(fetchedTodos => {
+        setTodos(fetchedTodos);
+      })
+      .finally(() => setLoader(false));
+  }, []);
+
+  const getFilteredByFlag = (filterFlag: Filter) => {
+    switch (filterFlag) {
+      case Filter.All:
+        setFlag(Filter.All);
+        getTodos().then(setTodos);
+        break;
+      case Filter.Active:
+        setFlag(Filter.Active);
+        getTodos()
+          .then(fitchedTodos => fitchedTodos.filter(t => !t.completed))
+          .then(setTodos);
+        break;
+      case Filter.Completed:
+        getTodos()
+          .then(fetchTodos => fetchTodos.filter(t => t.completed))
+          .then(setTodos);
+        break;
+      default:
+        getTodos().then(setTodos);
+        break;
+    }
+  };
+
+  const getFilteredByValue = (value: string) => {
+    if (value.trim() !== '') {
+      setTodos(
+        todos.filter(t => t.title.toLowerCase().includes(value.toLowerCase())),
+      );
+    } else {
+      getFilteredByFlag(flag);
+    }
+  };
+
+  const getUserAndTodo = (todoUserId: number) => {
+    setModalLoad(true);
+
+    setTodo(todos.find(t => t.userId === todoUserId) || null);
+
+    getUser(todoUserId)
+      .then(setUser)
+      .finally(() => setModalLoad(false));
+  };
+
+  const handleCloseModal = () => {
+    setUser(null);
+    setTodo(null);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +83,31 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterByFlag={getFilteredByFlag}
+                filteredByValue={getFilteredByValue}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loader ? (
+                <Loader />
+              ) : (
+                <TodoList todos={todos} getUserAndTodo={getUserAndTodo} />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {user && todo && (
+        <TodoModal
+          user={user}
+          todo={todo}
+          loader={modalLoad}
+          getClose={handleCloseModal}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,74 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { useState } from 'react';
+import { Filter } from '../../types/enumFilter';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  filterByFlag: (value: Filter) => void;
+  filteredByValue: (value: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({
+  filterByFlag,
+  filteredByValue,
+}) => {
+  const [showButton, setShowButton] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleFilterByFlag = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    filterByFlag(e.target.value as Filter);
+  };
+
+  const handleFilterByValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    setShowButton(true);
+    filteredByValue(inputValue);
+
+    if (e.target.value === '') {
+      setShowButton(false);
+    }
+  };
+
+  const handleClearValue = () => {
+    setInputValue('');
+    setShowButton(false);
+    filteredByValue('');
+  };
+
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select data-cy="statusSelect" onChange={handleFilterByFlag}>
+            <option value={Filter.All}>All</option>
+            <option value={Filter.Active}>Active</option>
+            <option value={Filter.Completed}>Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          onChange={handleFilterByValue}
+          value={inputValue}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {showButton && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => handleClearValue()}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
