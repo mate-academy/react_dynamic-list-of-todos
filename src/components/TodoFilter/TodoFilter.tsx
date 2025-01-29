fb519c95de641679121f9d0f985db1086b31811b
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2025-01-29 18:46:04 +0200

    dynamic-list-of-todos

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,28 @@
-export const TodoFilter = () => (
+import React from 'react';
+
+interface Props {
+  query: string;
+  setQuery: (value: string) => void;
+  status: 'all' | 'completed' | 'active';
+  setStatus: (value: 'all' | 'completed' | 'active') => void;
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  query,
+  setQuery,
+  status,
+  setStatus,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={status}
+          onChange={e =>
+            setStatus(e.target.value as 'all' | 'completed' | 'active')
+          }
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,15 +36,22 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={e => setQuery(e.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
-
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
+      {query && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => setQuery('')}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
