d60b341623f14d78cb59463a292c1028578a82bd
AndreaBoiko <andriy.boyko35@gmail.com>
2025-02-06 02:25:33 +0100

    hell

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,80 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getUser, getTodos } from './api';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [selectTodo, setSelectTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState<string>('');
+  const [filterBy, setFilterBy] = useState<string>('all');
+
+  useEffect(() => {
+    getTodos().then(todosFromServer => {
+      let filteredTodos = [...todosFromServer];
+
+      if (query) {
+        filteredTodos = filteredTodos.filter(todo =>
+          todo.title.toLowerCase().includes(query.toLowerCase()),
+        );
+      }
+
+      if (filterBy === 'completed') {
+        filteredTodos = filteredTodos.filter(todo => todo.completed);
+      } else if (filterBy === 'active') {
+        filteredTodos = filteredTodos.filter(todo => !todo.completed);
+      }
+
+      setTodos(filteredTodos);
+    });
+  }, [query, filterBy]);
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModel() {
+    setModalIsOpen(false);
+    setUser(null);
+    setSelectTodo(null);
+  }
+
+  function getUserById(id: number) {
+    if (id) {
+      getUser(id).then(setUser);
+    } else {
+      setUser(null);
+    }
+  }
+
+  function getTodoId(todo: Todo) {
+    if (todo) {
+      setSelectTodo(todo);
+    } else {
+      setSelectTodo(null);
+    }
+  }
+
+  function getQuery(text: string) {
+    if (text) {
+      setQuery(text);
+    } else {
+      setQuery('');
+    }
+  }
+
+  function getFilter(text: string) {
+    if (text) {
+      setFilterBy(text);
+    } else {
+      setFilterBy('');
+    }
+  }
+
   return (
     <>
       <div className="section">
@@ -17,18 +89,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                getQuery={getQuery}
+                getFilter={getFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos ? (
+                <TodoList
+                  todos={todos}
+                  selectedTodo={selectTodo}
+                  openModal={openModal}
+                  getUserId={getUserById}
+                  getTodoId={getTodoId}
+                />
+              ) : (
+                <Loader />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modalIsOpen && (
+        <TodoModal user={user} todo={selectTodo} onClose={closeModel} />
+      )}
     </>
   );
 };
