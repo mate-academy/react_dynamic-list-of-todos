8c7946593e71e1513aa41ee7fe5e86df4bd27c38
renatoveludo <matiasveludo@gmail.com>
2025-02-26 19:44:46 -0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+
+function getTodos(): Promise<Todo[]> {
+  return fetch(
+    'https://mate-academy.github.io/react_dynamic-list-of-todos/api/todos.json',
+  ).then(response => response.json());
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [loading, setLoading] = React.useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      getTodos()
+        .then(setTodos)
+        .finally(() => {
+          setLoading(false);
+        });
+    }, 1000);
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +36,13 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter todos={todos} />
             </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
+            <div className="block">{loading && <Loader />}</div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
     </>
   );
 };
