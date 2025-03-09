591063c36ccf28958b55644071b915e411aa282e
Yehor Heiko <yehorheiko.developer@gmail.com>
2025-03-09 18:46:44 +0200

    add task solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,43 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
+import classNames from 'classnames';
 
-export const TodoModal: React.FC = () => {
+type TodoModalProps = {
+  todo: Todo;
+  onClose: () => void;
+};
+
+const TodoModal: React.FC<TodoModalProps> = ({ todo, onClose }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoadingModal, setIsLoadingModal] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoadingModal(true);
+    getUser(todo.userId)
+      .then((fetchedUser: User) => {
+        setUser(fetchedUser);
+      })
+      .finally(() => {
+        setIsLoadingModal(false);
+      });
+  }, [todo.userId]);
+
+  if (isLoadingModal) {
+    return (
+      <div data-cy="modal" className="modal is-active">
+        {' '}
+        {/* Add data-cy for modal */}
+        <Loader data-cy="modal-loader" />
+      </div>
+    );
+  }
+
   return (
     <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
-
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
-            </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-            </p>
+      {' '}
+      {/* Modal container */}
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <div
+            className="modal-card-title has-text-weight-medium"
+            data-cy="modal-header"
+          >
+            Todo #{todo.id}
           </div>
+          <button
+            type="button"
+            className="delete"
+            data-cy="modal-close"
+            onClick={onClose}
+          />
+        </header>
+
+        <div className="modal-card-body">
+          <p className="block" data-cy="modal-title">
+            {todo.title}
+          </p>
+
+          <p className="block" data-cy="modal-user">
+            <strong
+              className={classNames({
+                'has-text-danger': !todo.completed,
+                'has-text-success': todo.completed,
+              })}
+            >
+              {todo.completed ? 'Done' : 'Planned'}
+            </strong>
+
+            {' by '}
+            <a href={`mailto:${user?.email || ''}`} data-cy="user-email">
+              {user?.name}
+            </a>
+          </p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
+
+export default TodoModal;
