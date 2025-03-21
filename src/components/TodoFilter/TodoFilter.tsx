c3afdccfb6a69d7b7461f4f079daf8ab504c095b
Volodymyr Bardyuzha <bardyuzha@gmail.com>
2025-03-20 22:04:12 -0500

    added solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,22 @@
-export const TodoFilter = () => (
+import { FC } from 'react';
+
+type Props = {
+  handleChangeType: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  handleChangeQuery: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleClearQuery: () => void;
+  query: string;
+};
+
+export const TodoFilter: FC<Props> = ({
+  handleChangeType,
+  handleChangeQuery,
+  handleClearQuery,
+  query,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select data-cy="statusSelect" onChange={handleChangeType}>
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,14 +30,22 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        onChange={handleChangeQuery}
+        value={query}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        {query && (
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={handleClearQuery}
+          />
+        )}
       </span>
     </p>
   </form>
