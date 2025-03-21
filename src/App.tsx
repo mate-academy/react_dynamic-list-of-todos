08b9a2e586da221da3f36a9ae4139bf9669c8f2e
Volodymyr Bardyuzha <bardyuzha@gmail.com>
2025-03-20 22:10:46 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
