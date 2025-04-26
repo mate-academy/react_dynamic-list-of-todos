9f73affba72910ba95ff20043576b711263c8158
Wallacee <wallaceks1995@gmail.com>
2025-04-26 18:37:41 -0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,93 @@
-/* eslint-disable max-len */
-import React from 'react';
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+import React, { Component } from 'react';
+import { getTodos, getUserDetails, wait } from './api/api';
+import { Todo } from './api/types';
+import TodoList from './components/TodoList';
+import Loader from './components/Loader';
+import TodoFilter from './components/TodoFilter';
+import TodoModal from './components/TodoModal';
 
-import { TodoList } from './components/TodoList';
-import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
+interface AppState {
+  todos: Todo[];
+  visibleTodos: Todo[];
+  selectedTodo: Todo | null;
+  isLoading: boolean;
+  query: string;
+  filterBy: 'all' | 'completed' | 'active';
+}
 
-export const App: React.FC = () => {
-  return (
-    <>
-      <div className="section">
-        <div className="container">
-          <div className="box">
-            <h1 className="title">Todos:</h1>
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      todos: [],
+      visibleTodos: [],
+      selectedTodo: null,
+      isLoading: false,
+      query: '',
+      filterBy: 'all',
+    };
+  }
 
-            <div className="block">
-              <TodoFilter />
-            </div>
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    await wait(1000); // Simulate server delay
+    const todos = await getTodos();
+    this.setState({
+      todos,
+      visibleTodos: todos,
+      isLoading: false,
+    });
+  }
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
-          </div>
-        </div>
+  handleQueryChange = (query: string) => {
+    const filteredTodos = this.state.todos.filter(todo =>
+      todo.title.toLowerCase().includes(query.toLowerCase())
+    );
+    this.setState({ query, visibleTodos: filteredTodos });
+  };
+
+  clearQuery = () => {
+    this.setState({ query: '', visibleTodos: this.state.todos });
+  };
+
+  handleFilterChange = (filterBy: 'all' | 'completed' | 'active') => {
+    const filteredTodos = this.state.todos.filter(todo => {
+      if (filterBy === 'completed') return todo.completed;
+      if (filterBy === 'active') return !todo.completed;
+      return true;
+    });
+    this.setState({ filterBy, visibleTodos: filteredTodos });
+  };
+
+  openModal = async (todo: Todo) => {
+    this.setState({ isLoading: true });
+    const user = await getUserDetails(todo.userId);
+    this.setState({ selectedTodo: { ...todo, user }, isLoading: false });
+  };
+
+  closeModal = () => {
+    this.setState({ selectedTodo: null });
+  };
+
+  render() {
+    const { visibleTodos, isLoading, selectedTodo, query, filterBy } = this.state;
+
+    return (
+      <div>
+        <h1>Todo List</h1>
+        {isLoading && <Loader />}
+        <TodoFilter
+          query={query}
+          filterBy={filterBy}
+          onQueryChange={this.handleQueryChange}
+          onFilterChange={this.handleFilterChange}
+          onClearQuery={this.clearQuery}
+        />
+        <TodoList todos={visibleTodos} onShow={this.openModal} />
+        {selectedTodo && <TodoModal todo={selectedTodo} onClose={this.closeModal} />}
       </div>
+    );
+  }
+}
 
-      <TodoModal />
-    </>
-  );
-};
+export default App;
