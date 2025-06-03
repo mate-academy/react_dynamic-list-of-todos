448dce49289f8fc73e5f38c7e37949af56448cc3
Roman Koltunov <roman.koltunov.1999@gmail.com>
2025-06-03 13:03:07 +0300

    add task solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,55 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
+type Props = {
+  searchValue: string;
+  onSearchChange: (value: string) => void;
+  filterStatus: 'all' | 'active' | 'completed';
+  onFilterChange: (value: 'all' | 'active' | 'completed') => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  searchValue,
+  onSearchChange,
+  filterStatus,
+  onFilterChange,
+}) => (
+  <form className="field has-addons" onSubmit={e => e.preventDefault()}>
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={filterStatus}
+          onChange={e =>
+            onFilterChange(e.target.value as 'all' | 'active' | 'completed')
+          }
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
         </select>
       </span>
     </p>
-
     <p className="control is-expanded has-icons-left has-icons-right">
       <input
         data-cy="searchInput"
         type="text"
         className="input"
         placeholder="Search..."
+        value={searchValue}
+        onChange={e => onSearchChange(e.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
-
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
+      {searchValue && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            aria-label="Clear search"
+            onClick={() => onSearchChange('')}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
