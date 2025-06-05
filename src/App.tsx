c0fef84f532ff013244d8ea789881ac8bbe18aea
Yura <yurii.st11@gmail.com>
2025-06-05 17:04:26 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,24 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getTodos().then(todosFromServ => {
+      setTodos(todosFromServ);
+      setFilteredTodos(todosFromServ);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +33,28 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter todos={todos} onSetTodos={setFilteredTodos} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  onModalVisible={setIsModalVisible}
+                  isModalVisible={isModalVisible}
+                  todos={filteredTodos}
+                  onTodoSelect={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalVisible && (
+        <TodoModal onModalVisible={setIsModalVisible} todo={selectedTodo} />
+      )}
     </>
   );
 };
