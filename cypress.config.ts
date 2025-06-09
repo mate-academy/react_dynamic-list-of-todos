67d59e31f32b1c00d18a01987e65129c6c87835e
Dennis Paixão <dennispaixaoads@gmail.com>
2025-06-09 12:24:51 -0300

    solution

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,8 +6,8 @@ module.exports = defineConfig({
     specPattern: 'cypress/integration/**/*.spec.{js,ts,jsx,tsx}',
   },
   video: true,
-  viewportHeight: 1920,
-  viewportWidth: 1080,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   screenshotOnRunFailure: true,
   reporter: 'mochawesome',
   reporterOptions: {
