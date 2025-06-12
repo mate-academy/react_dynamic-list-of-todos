8573bfa53c03c03fa4cc682b506678364a4794c3
Anori <anorifrell@gmail.com>
2025-06-12 23:30:35 +0300

    add-todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,59 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Status } from './components/modifiers/Status';
+
+interface FilterParams {
+  status: string;
+  query: string;
+}
+
+function getFilteredTodos(todos: Todo[], { status, query }: FilterParams) {
+  let filteredTodos = [...todos];
+  const niceQuery = query.toLowerCase().trim();
+
+  switch (status) {
+    case Status.ACTIVE:
+      filteredTodos = todos.filter(todo => !todo.completed);
+      break;
+
+    case Status.COMPLETE:
+      filteredTodos = todos.filter(todo => todo.completed);
+      break;
+
+    default:
+      break;
+  }
+
+  if (niceQuery) {
+    return filteredTodos.filter(todo =>
+      todo.title.toLowerCase().includes(niceQuery),
+    );
+  }
+
+  return filteredTodos;
+}
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [query, setQuery] = useState('');
+  const [status, setStatus] = useState(Status.ALL);
+
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  const filteredTodos = getFilteredTodos(todos, { status, query });
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +68,29 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setStatus={setStatus}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              {!loading && !!todos.length && (
+                <TodoList
+                  todos={filteredTodos}
+                  setSelected={setSelectedTodo}
+                  selected={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal setSelected={setSelectedTodo} selected={selectedTodo} />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,24 @@
-export const TodoFilter = () => (
+import { Status } from '../modifiers/Status';
+
+type Props = {
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  setStatus: React.Dispatch<React.SetStateAction<Status>>;
+};
+
+export const TodoFilter: React.FC<Props> = ({ query, setQuery, setStatus }) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          onChange={event => {
+            setStatus(event?.target.value as Status);
+          }}
+        >
+          <option value={Status.ALL}>All</option>
+          <option value={Status.ACTIVE}>Active</option>
+          <option value={Status.COMPLETE}>Completed</option>
         </select>
       </span>
     </p>
@@ -16,15 +29,28 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={query}
+        onChange={event => {
+          setQuery(event.target.value);
+        }}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
+      {query && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => {
+              setQuery('');
+            }}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
