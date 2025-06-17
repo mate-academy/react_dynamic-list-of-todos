1ce5c990f2db4dbe4b28569bf6328a79f657ae65
tobias <tobias.harasim@seznam.cz>
2025-06-17 12:01:22 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,32 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { getTodos } from './api';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import type { Todo } from './types/Todo';
+
+
+import { useState, useEffect } from 'react'
 
 export const App: React.FC = () => {
+  const [allTodos, setAllTodos] = useState<Todo[]>([]);
+  const [filtered, setFiltered] = useState<Todo[]>([]);
+  const [modal, setModal] = useState<string>('')
+  const [modalTodo, setModalTodo] = useState<Todo | null>(null)
+
+
+  useEffect(() => {
+    (async () => {
+      const todosFromServer = await getTodos();
+      setAllTodos(todosFromServer);
+      setFiltered(todosFromServer);
+    })();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +36,22 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter todos={allTodos} setTodos={setFiltered} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {filtered.length > 0 ? (<TodoList todos={filtered} setModal={setModal} setModalTodo={setModalTodo} modalTodo={modalTodo}/>) : (
+                <Loader/>
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modal.length === 0 ? null : (
+        <TodoModal openedModal={modalTodo} id={modal} setModal={setModal} setOpenedModal={setModalTodo}/>
+      )}
+
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import type { User } from '../../types/User';
+import type { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type modalProps = {
+  id: string;
+  setModal: (item: string) => void;
+  openedModal: Todo | null
+  setOpenedModal: (item: Todo | null) => void
+};
+
+export const TodoModal: React.FC<modalProps> = ({ id, setModal, openedModal, setOpenedModal }: modalProps) => {
+  const [user, setUser] = React.useState<User | null>(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const userData = await getUser(Number(id));
+      setUser(userData);
+    };
+
+    loadUser();
+  }, [id]);
+
+  console.log(openedModal)
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {!user ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +38,25 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{openedModal?.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button type="button" className="delete" data-cy="modal-close" onClick={() => {setModal(''); setOpenedModal(null)}}/>
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {openedModal?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong className={`has-text-${openedModal?.completed === true ? 'success' : 'danger'}`}>{openedModal?.completed === true ? 'Done' : 'Planned'}</strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${user.email}`}>{user.name}</a>
             </p>
           </div>
         </div>
