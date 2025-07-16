688b9118ac4c55885b898de6ebc2868153669a4f
andreinik1 <125960028+andreinik1@users.noreply.github.com>
2025-07-16 04:07:43 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,75 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isComplete, setIsComplete] = useState<string | boolean>('all');
+  const [query, setQuery] = useState<string>('');
+  const [isShownPostInfo, setIsShownPostInfo] = useState<boolean>(false);
+  const [selectedUser, setSelectedUser] = useState<User>({} as User);
+  const [selectedTodo, setSelectedTodo] = useState<Todo>({} as Todo);
+
+  const handleInputChange = (e: string): void => {
+    setQuery(e);
+  };
+
+  const handleShowPostInfo = (todo: Todo): void => {
+    getUser(todo.userId).then(user => {
+      setSelectedUser(user);
+      setSelectedTodo(todo);
+    });
+  };
+
+  const filteredTodos = todos.filter(todo =>
+    todo.title.toLowerCase().includes(query.toLowerCase()),
+  );
+
+  useEffect(() => {
+    if (!query) {
+      return setTodos(todos);
+    }
+  }, [query, filteredTodos, todos]);
+
+  useEffect(() => {
+    getTodos().then(todosFrom => {
+      setTodos(todosFrom);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isComplete === 'all') {
+      getTodos().then(todosFrom => {
+        setTodos(todosFrom);
+      });
+    } else if (isComplete === true) {
+      getTodos().then(todosFrom => {
+        setTodos(todosFrom.filter(todo => todo.completed));
+      });
+    } else if (isComplete === false) {
+      getTodos().then(todosFrom => {
+        setTodos(todosFrom.filter(todo => !todo.completed));
+      });
+    }
+  }, [isComplete]);
+
+  const filterTodos = (param: string) => {
+    switch (param) {
+      case 'all':
+        setIsComplete('all');
+        break;
+      case 'completed':
+        setIsComplete(true);
+        break;
+      case 'active':
+        setIsComplete(false);
+        break;
+    }
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +84,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                filterTodos={filterTodos}
+                handleInputChange={handleInputChange}
+                query={query}
+              />
             </div>
 
             <div className="block">
               <Loader />
-              <TodoList />
+              <TodoList
+                todos={filteredTodos}
+                handleShowPostInfo={handleShowPostInfo}
+                setIsShownPostInfo={setIsShownPostInfo}
+                isShownPostInfo={isShownPostInfo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isShownPostInfo && (
+        <TodoModal
+          setIsShownPostInfo={setIsShownPostInfo}
+          selectedUser={selectedUser}
+          selectedTodo={selectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  setIsShownPostInfo: (isShown: boolean) => void;
+  selectedUser: User;
+  selectedTodo: Todo;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  setIsShownPostInfo,
+  selectedUser,
+  selectedTodo,
+}) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +37,39 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{selectedTodo.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => {
+                setIsShownPostInfo(false);
+              }}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              <strong
+                className={`${
+                  selectedTodo.completed
+                    ? 'has-text-success'
+                    : 'has-text-danger'
+                }`}
+              >
+                {selectedTodo.completed ? 'Done' : 'Planned'}
+              </strong>
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${selectedUser.email}`}>{selectedUser.name}</a>
             </p>
           </div>
         </div>
