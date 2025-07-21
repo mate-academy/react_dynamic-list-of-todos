a7b9fa524421e714ddbabc103e7ae942acf0f1cb
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2025-07-21 20:13:03 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,40 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [category, setCategory] = useState('all');
+  const [filtered, setFiltered] = useState('');
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(todos => {
+        let filteredTodos = todos;
+
+        if (category === 'active') {
+          filteredTodos = filteredTodos.filter(todo => !todo.completed);
+        } else if (category === 'completed') {
+          filteredTodos = filteredTodos.filter(todo => todo.completed);
+        }
+
+        if (filtered.trim()) {
+          filteredTodos = filteredTodos.filter(todo =>
+            todo.title.toLowerCase().includes(filtered.toLowerCase()),
+          );
+        }
+
+        setTodoList(filteredTodos);
+      })
+      .finally(() => setLoading(false));
+  }, [category, filtered]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +49,29 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selected={category}
+                onSelected={setCategory}
+                filter={filtered}
+                onFilter={setFiltered}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              <TodoList
+                todos={todoList}
+                onSelectedTodo={setSelectedTodo}
+                selectTodo={selectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal todo={selectedTodo} onTodo={setSelectedTodo} />
+      )}
     </>
   );
 };
