ea507f037fbb567b78e449c983c078ec197a3e64
panasovskyi <ie.panasovskyi@gmail.com>
2025-08-16 15:00:19 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,54 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [sortField, setSortField] = useState('all');
+  const [query, setQuery] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  type Params = {
+    sf: string;
+    q: string;
+  };
+
+  const preparedTodos = (
+    items: Todo[],
+    { sf, q }: Params,
+  ): Todo[] => {
+    let prepTodos = [...items];
+
+    if (sf === 'active') {
+      prepTodos = prepTodos.filter(todo => !todo.completed);
+    }
+
+    if (sortField === 'completed') {
+      prepTodos = prepTodos.filter(todo => todo.completed);
+    }
+
+    if (q) {
+      const normalizedQuery = q.trim().toLowerCase();
+
+      prepTodos = prepTodos.filter(todo =>
+        todo.title.toLowerCase().includes(normalizedQuery),
+      );
+    }
+
+    return prepTodos;
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +63,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                sortField={sortField}
+                setSortField={setSortField}
+                query={query}
+                setQuery={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+
+              <TodoList
+                todos={preparedTodos(todos, { sortField, query })}
+                setSelectedTodo={setSelectedTodo}
+                selectedTodo={selectedTodo}
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          setSelectedTodo={setSelectedTodo}
+          selectedTodo={selectedTodo}
+        />
+      )}
     </>
   );
 };
