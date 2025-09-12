5953b57857c51956655862bb98483b47bcc206a8
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-09-12 12:00:15 -0500

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,38 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { FilterType } from './types/FilterType';
 
 export const App: React.FC = () => {
+  const [loadingTodos, setLoadingTodos] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [searchFilter, setSearchFilter] = useState('');
+  const [selectedTodoId, setSelectedTodoId] = useState(0);
+  const [statusFilter, setStatusFilter] = useState<FilterType>('all');
+
+  useEffect(() => {
+    setLoadingTodos(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoadingTodos(false));
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    return todos
+      .filter(todo => {
+        if (statusFilter === 'active') return !todo.completed;
+        if (statusFilter === 'completed') return todo.completed;
+        return true;
+      })
+      .filter(todo =>
+        todo.title.toLowerCase().includes(searchFilter.toLowerCase()),
+      );
+  }, [todos, statusFilter, searchFilter]);
+
+  const selectedTodo = todos.find(todo => todo.id === selectedTodoId);
+
   return (
     <>
       <div className="section">
@@ -17,18 +46,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                statusFilter={statusFilter}
+                setStatusFilter={setStatusFilter}
+                searchFilter={searchFilter}
+                setSearchFilter={setSearchFilter}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loadingTodos && <Loader />}
+
+              <TodoList
+                todos={visibleTodos}
+                setSelectedTodoId={setSelectedTodoId}
+                selectedTodoId={selectedTodoId}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          setSelectedTodoId={setSelectedTodoId}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,58 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { FilterType } from '../../types/FilterType';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  statusFilter: FilterType;
+  setStatusFilter: (value: FilterType) => void;
+  searchFilter: string;
+  setSearchFilter: (value: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({
+  statusFilter,
+  setStatusFilter,
+  searchFilter,
+  setSearchFilter,
+}) => {
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={statusFilter}
+            onChange={e => setStatusFilter(e.target.value as FilterType)}
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={searchFilter}
+          onChange={e => setSearchFilter(e.target.value)}
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {searchFilter && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => setSearchFilter('')}
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
