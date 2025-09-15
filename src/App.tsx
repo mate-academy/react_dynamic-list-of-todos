5c0bbba247e57c7b043b161865df7c2e15225dc0
Michał Szwindowski <github@nedi.me>
2025-09-15 11:01:49 +0200

    Solution #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +7,66 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { getTodos, getUser } from './api';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loadingTodos, setLoadingTodos] = useState(false);
+
+  const [status, setStatus] = useState<'all' | 'active' | 'completed'>('all');
+  const [query, setQuery] = useState('');
+
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [loadingUser, setLoadingUser] = useState(false);
+
+  // load todos
+  useEffect(() => {
+    setLoadingTodos(true);
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoadingTodos(false));
+  }, []);
+
+  // set selectedTodo when selectedId changes
+  useEffect(() => {
+    if (selectedId) {
+      const todo = todos.find(t => t.id === selectedId) || null;
+
+      setSelectedTodo(todo);
+
+      if (todo) {
+        setLoadingUser(true);
+        getUser(todo.userId)
+          .then(setUser)
+          .finally(() => setLoadingUser(false));
+      }
+    } else {
+      setSelectedTodo(null);
+      setUser(null);
+    }
+  }, [selectedId, todos]);
+
+  // filtering
+  const filteredTodos = todos.filter(todo => {
+    if (status === 'completed' && !todo.completed) {
+      return false;
+    }
+
+    if (status === 'active' && todo.completed) {
+      return false;
+    }
+
+    if (query && !todo.title.toLowerCase().includes(query.toLowerCase())) {
+      return false;
+    }
+
+    return true;
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +75,37 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={status}
+                onStatusChange={setStatus}
+                query={query}
+                onQueryChange={setQuery}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loadingTodos ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedId={selectedId}
+                  onSelect={id => setSelectedId(selectedId === id ? null : id)}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          user={user}
+          loading={loadingUser}
+          onClose={() => setSelectedId(null)}
+        />
+      )}
     </>
   );
 };
