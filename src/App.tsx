a79a885c63d7fa23c916917a7de5b765095a5458
Diana Berezan <berezandiana24@gmail.com>
2025-09-20 11:58:46 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,39 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
+  const [todoes, setTodoes] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState<Filter>('all');
+
+  const filterProps = { filter, query, setFilter, setQuery };
+  const modalProps = { selectedTodo, setSelectedTodo };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodoes)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const visibleTodoes = todoes.filter(todo => {
+    const matchesQuery = todo.title
+      .toLowerCase()
+      .includes(query.toLocaleLowerCase());
+
+    const matchesFilter =
+      filter === 'all' ||
+      (filter === 'active' && !todo.completed) ||
+      (filter === 'completed' && todo.completed);
+
+    return matchesQuery && matchesFilter;
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,19 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter {...filterProps} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+
+              <TodoList todoes={visibleTodoes} {...modalProps} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal todo={selectedTodo} onClose={() => setSelectedTodo(null)} />
     </>
   );
 };
