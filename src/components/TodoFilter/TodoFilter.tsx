19991a57c2f2174ba2a80d45c870c87883f39aaa
Viktoriia <vikapitsan4@gmail.com>
2025-10-27 00:57:50 +0200

    feat: initial commit

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,26 @@
-export const TodoFilter = () => (
+import React from 'react';
+import { FilterStatus } from '../../types/FilterStatus';
+interface TodoFilterProps {
+  searchQuery: string;
+  onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  selectedFilter: FilterStatus;
+  onFilterChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export const TodoFilter: React.FC<TodoFilterProps> = ({
+  searchQuery,
+  onSearchChange,
+  selectedFilter,
+  onFilterChange,
+}) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          value={selectedFilter}
+          onChange={onFilterChange}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -13,6 +31,8 @@ export const TodoFilter = () => (
     <p className="control is-expanded has-icons-left has-icons-right">
       <input
         data-cy="searchInput"
+        value={searchQuery}
+        onChange={onSearchChange}
         type="text"
         className="input"
         placeholder="Search..."
@@ -21,10 +41,21 @@ export const TodoFilter = () => (
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
+      {searchQuery && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() =>
+              onSearchChange({
+                target: { value: '' },
+              } as React.ChangeEvent<HTMLInputElement>)
+            }
+          />
+        </span>
+      )}
     </p>
   </form>
 );
