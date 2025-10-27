a00658a90df81762571b2b296367933e71c563b8
Oleh Pshyk <pshykol@gmail.com>
2025-10-27 10:34:43 +0200

    refactored parts with conditional applying of CSS classes to classnames library

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Loader } from '../Loader';
 import { Todo } from '../../types/Todo';
 import { User } from '../../types/User';
+import cn from 'classnames';
 
 type Props = {
   todo: Todo;
@@ -47,9 +48,10 @@ export const TodoModal: React.FC<Props> = ({
 
             <p className="block" data-cy="modal-user">
               <strong
-                className={
-                  todo.completed ? 'has-text-success' : 'has-text-danger'
-                }
+                className={cn({
+                  'has-text-success': todo.completed,
+                  'has-text-danger': !todo.completed,
+                })}
               >
                 {todo.completed ? 'Done' : 'Planned'}
               </strong>
