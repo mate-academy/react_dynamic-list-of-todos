6c34a7bb5e5d2fe4cf9c2d6233ee336a73b1eb20
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-02-05 12:16:35 +0200

    Add dynamic Todo list rendering and server data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,20 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    getTodos().then(todosFromServer => {
+      setLoading(false);
+      setTodos(todosFromServer);
+    });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,8 +33,8 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              <TodoList todos={todos} />
             </div>
           </div>
         </div>
