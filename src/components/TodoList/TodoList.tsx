2cdc149d934008849098b51af653d2e55c4061b3
Ivan Maksymiv <maksymiv.ivan2000@gmail.com>
2026-02-19 19:15:30 +0200

    src

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
 
 type Props = {
   todos: Todo[];
@@ -35,7 +36,9 @@ export const TodoList: React.FC<Props> = ({
             <tr
               key={todo.id}
               data-cy="todo"
-              className={isSelected ? 'has-background-info-light' : ''}
+              className={classNames({
+                'has-background-info-light': isSelected,
+              })}
             >
               <td className="is-vcentered">{todo.id}</td>
 
@@ -49,9 +52,10 @@ export const TodoList: React.FC<Props> = ({
 
               <td className="is-vcentered is-expanded">
                 <p
-                  className={
-                    todo.completed ? 'has-text-success' : 'has-text-danger'
-                  }
+                  className={classNames({
+                    'has-text-success': todo.completed,
+                    'has-text-danger': !todo.completed,
+                  })}
                 >
                   {todo.title}
                 </p>
@@ -66,7 +70,10 @@ export const TodoList: React.FC<Props> = ({
                 >
                   <span className="icon">
                     <i
-                      className={isSelected ? 'far fa-eye-slash' : 'far fa-eye'}
+                      className={classNames('far', {
+                        'fa-eye-slash': isSelected,
+                        'fa-eye': !isSelected,
+                      })}
                     />
                   </span>
                 </button>
