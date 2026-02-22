8171bb7f74fe20a3b99418fd25d21a9888930b17
Alexandr <AlexandrKsenz@gmail.com>
2026-02-22 21:15:47 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,74 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { Filters } from './types/Filters';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [filters, setFilters] = useState<Filters>({
+    filter: 'all',
+    search: '',
+  });
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      try {
+        const todosFromServer = await getTodos();
+
+        setTodos(todosFromServer);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadTodos();
+  }, []);
+
+  const openTodo = async (selectedTodo: Todo) => {
+    try {
+      setLoading(true);
+      setTodo(todo);
+
+      const userFromServer = await getUser(selectedTodo.userId);
+
+      setUser(userFromServer);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const closeTodo = () => {
+    setTodo(null);
+    setUser(null);
+  };
+
+  const filteredTodos = () => {
+    let copyTodos = [...todos];
+
+    switch (filters.filter) {
+      case 'active':
+        copyTodos = copyTodos.filter(item => !item.completed);
+        break;
+      case 'completed':
+        copyTodos = copyTodos.filter(item => item.completed);
+        break;
+      default:
+        break;
+    }
+
+    copyTodos = copyTodos.filter(item => item.title.includes(filters.search));
+
+    return copyTodos;
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +83,25 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter filters={filters} setFilters={setFilters} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0 ? (
+                <Loader />
+              ) : (
+                <TodoList todos={filteredTodos()} openTodo={openTodo} />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      <TodoModal
+        todo={todo}
+        user={user}
+        loading={loading}
+        closeTodo={closeTodo}
+      />
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,67 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import { Filter, Filters } from '../../types/Filters';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  filters: Filters;
+  setFilters: React.Dispatch<React.SetStateAction<Filters>>;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({ filters, setFilters }) => {
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={filters.filter}
+            onChange={event =>
+              setFilters({
+                ...filters,
+                filter: event.target.value as Filter,
+              })
+            }
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={filters.search}
+          onChange={event =>
+            setFilters({
+              ...filters,
+              search: event.target.value as Filter,
+            })
+          }
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {filters.search.length > 0 && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() =>
+                setFilters({
+                  ...filters,
+                  search: '',
+                })
+              }
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
