3cc41d994a5d2e81a609a62066110113405964a6
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-03-16 02:47:58 +0200

    Added solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,76 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import debounce from 'lodash.debounce';
+import React from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  handleStatusFilterChange: (value: string) => void;
+  handleFilterChange: (value: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
+export const TodoFilter = React.memo(
+  ({ handleStatusFilterChange, handleFilterChange }: Props) => {
+    const [selectFilter, setSelectFilter] = React.useState('all');
+    const [query, setQuery] = React.useState('');
+
+    const debouncedFilterChange = React.useMemo(
+      () =>
+        debounce((value: string) => {
+          handleFilterChange(value);
+        }, 300),
+      [handleFilterChange],
+    );
+
+    const handleQueryChange = (value: string) => {
+      setQuery(value);
+      debouncedFilterChange(value);
+    };
+
+    return (
+      <form className="field has-addons">
+        <p className="control">
+          <span className="select">
+            <select
+              data-cy="statusSelect"
+              value={selectFilter}
+              onChange={e => {
+                setSelectFilter(e.target.value);
+                handleStatusFilterChange(e.target.value);
+              }}
+            >
+              <option value="all">All</option>
+              <option value="active">Active</option>
+              <option value="completed">Completed</option>
+            </select>
+          </span>
+        </p>
+
+        <p className="control is-expanded has-icons-left has-icons-right">
+          <input
+            data-cy="searchInput"
+            type="text"
+            className="input"
+            placeholder="Search..."
+            value={query}
+            onChange={(event) => handleQueryChange(event.target.value)}
+          />
+          <span className="icon is-left">
+            <i className="fas fa-magnifying-glass" />
+          </span>
+
+          {query && (
+            <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="clearSearchButton"
+                type="button"
+                className="delete"
+                onClick={() => handleQueryChange('')}
+              />
+            </span>
+          )}
+        </p>
+      </form>
+    );
+  },
 );
+
+TodoFilter.displayName = 'TodoFilter';
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Loader } from '../Loader';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: Todo | null;
+  onModalClose?: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  selectedTodo,
+  onModalClose,
+}: Props) => {
+  const [isModalLoading, setIsModalLoading] = React.useState(false);
+  const [user, setUser] = React.useState<User | null>(null);
+
+  useEffect(() => {
+    if (!selectedTodo) {
+      return;
+    }
+
+    setIsModalLoading(true);
+
+    getUser(selectedTodo.userId)
+      .then(setUser)
+      .finally(() => {
+        setIsModalLoading(false);
+      });
+  }, [selectedTodo]);
+
+  const isModalActive = selectedTodo !== null;
+
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div
+      className={classNames('modal', { 'is-active': isModalActive })}
+      data-cy="modal"
+    >
       <div className="modal-background" />
 
-      {true ? (
+      {isModalLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +49,33 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              {`Todo #${selectedTodo?.id}`}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onModalClose}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedTodo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {selectedTodo?.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
             </p>
           </div>
         </div>
