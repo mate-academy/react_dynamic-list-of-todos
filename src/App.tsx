4af9a27dc3f19ab10cd3a1c41b3f8d8075d00e77
Mykyta Brenhach <nikassafronovs@gmail.com>
2026-05-11 13:49:55 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable max-len */
-import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,44 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { useEffect, useState } from 'react';
+import { getTodos } from './service/todos';
+// import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [status, setStatus] = useState<'all' | 'active' | 'completed'>('all');
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filteredTodos = todos
+    .filter(todo => {
+      if (status === 'all') {
+        return true;
+      }
+
+      if (status === 'active') {
+        return !todo.completed;
+      }
+
+      if (status === 'completed') {
+        return todo.completed;
+      }
+
+      return true;
+    })
+    .filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+
   return (
     <>
       <div className="section">
@@ -17,18 +52,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                status={status}
+                onQueryChange={setQuery}
+                onStatusChange={setStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              <TodoList
+                todos={filteredTodos}
+                selectedTodosId={selectedTodo?.id}
+                onSelect={todo => setSelectedTodo(todo)}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          onClose={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,3 @@
-import { Todo } from './types/Todo';
-import { User } from './types/User';
-
 // eslint-disable-next-line operator-linebreak
 const BASE_URL =
   'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
@@ -13,7 +10,7 @@ function wait(delay: number): Promise<void> {
   });
 }
 
-function get<T>(url: string): Promise<T> {
+export function getData<T>(url: string): Promise<T> {
   // eslint-disable-next-line prefer-template
   const fullURL = BASE_URL + url + '.json';
 
@@ -22,7 +19,3 @@ function get<T>(url: string): Promise<T> {
     .then(() => fetch(fullURL))
     .then(res => res.json());
 }
-
-export const getTodos = () => get<Todo[]>('/todos');
-
-export const getUser = (userId: number) => get<User>(`/users/${userId}`);
