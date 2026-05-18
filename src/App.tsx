e307b4be1e6559e8767e3462754c058adbe1c651
Roman Lysunets <rlysunets@gmail.com>
2026-05-18 15:37:11 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,67 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { getTodos } from './api';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+
+type Filter = 'all' | 'active' | 'completed';
+
+const getFilteredTodos = (todos: Todo[], filter: Filter, search: string) => {
+  let result = todos;
+
+  switch (filter) {
+    case 'active':
+      result = result.filter(todo => !todo.completed);
+      break;
+    case 'completed':
+      result = result.filter(todo => todo.completed);
+      break;
+    default:
+      break;
+  }
+
+  const prepearedSearch = search.trim().toLowerCase();
+
+  if (prepearedSearch) {
+    result = result.filter(todo =>
+      todo.title.toLowerCase().includes(prepearedSearch),
+    );
+  }
+
+  return result;
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filter, setFilter] = useState<Filter>('all');
+  const [search, setSearch] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then(data => setTodos(data))
+      .catch(error => alert(error))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const fiteredTodos = getFilteredTodos(todos, filter, search);
+
+  const openModal = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const selectedTodoModal =
+    todos.find(todo => todo.id === selectedTodo?.id) || null;
+
   return (
     <>
       <div className="section">
@@ -17,18 +70,34 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setFilter={setFilter}
+                search={search}
+                setSearch={setSearch}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={fiteredTodos}
+                  openModal={openModal}
+                  selectedTodo={selectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo !== null && (
+        <TodoModal
+          todo={selectedTodoModal!}
+          setSelectedTodo={setSelectedTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,15 @@
+/* eslint-disable max-len */
 import React from 'react';
+import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
 
-export const TodoList: React.FC = () => (
+type TodoListProps = {
+  todos: Todo[];
+  selectedTodo: Todo | null;
+  openModal: (todo: Todo) => void;
+};
+
+export const TodoList = ({ todos, openModal, selectedTodo }: TodoListProps) => (
   <table className="table is-narrow is-fullwidth">
     <thead>
       <tr>
@@ -16,21 +25,48 @@ export const TodoList: React.FC = () => (
     </thead>
 
     <tbody>
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
-      <tr data-cy="todo" className="has-background-info-light">
+      {todos.map(todo => {
+        return (
+          <tr data-cy="todo" className="" key={todo.id}>
+            <td className="is-vcentered">{todo.id}</td>
+            <td className="is-vcentered">
+              {todo.completed && (
+                <span className="icon" data-cy="iconCompleted">
+                  <i className="fas fa-check"></i>
+                </span>
+              )}
+            </td>
+            <td className="is-vcentered is-expanded">
+              <p
+                className={classNames({
+                  'has-text-success': todo.completed,
+                  'has-text-danger': !todo.completed,
+                })}
+              >
+                {todo.title}
+              </p>
+            </td>
+            <td className="has-text-right is-vcentered">
+              <button
+                data-cy="selectButton"
+                className="button"
+                type="button"
+                onClick={() => openModal(todo)}
+              >
+                <span className="icon">
+                  {selectedTodo !== null ? (
+                    <i className="far fa-eye-slash" />
+                  ) : (
+                    <i className="far fa-eye" />
+                  )}
+                </span>
+              </button>
+            </td>
+          </tr>
+        );
+      })}
+
+      {/* <tr data-cy="todo" className="has-background-info-light">
         <td className="is-vcentered">2</td>
         <td className="is-vcentered" />
         <td className="is-vcentered is-expanded">
@@ -43,9 +79,9 @@ export const TodoList: React.FC = () => (
             </span>
           </button>
         </td>
-      </tr>
+      </tr> */}
 
-      <tr data-cy="todo" className="">
+      {/* <tr data-cy="todo" className="">
         <td className="is-vcentered">1</td>
         <td className="is-vcentered" />
         <td className="is-vcentered is-expanded">
@@ -94,7 +130,7 @@ export const TodoList: React.FC = () => (
             </span>
           </button>
         </td>
-      </tr>
+      </tr> */}
     </tbody>
   </table>
 );
