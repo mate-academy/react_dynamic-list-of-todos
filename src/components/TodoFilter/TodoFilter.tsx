f615d6afffca2ff5b5c898261e369bc2df771685
Dazavrrr <damelasha@gmail.com>
2026-05-20 17:54:10 +0300

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,11 +1,37 @@
-export const TodoFilter = () => (
+import { Dispatch, SetStateAction } from 'react';
+
+export enum CompletedFilter {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
+
+type TodoFilterProps = {
+  search: string;
+  completed: CompletedFilter;
+  onSearchChange: Dispatch<SetStateAction<string>>;
+  onCompletedChange: Dispatch<SetStateAction<CompletedFilter>>;
+};
+
+export const TodoFilter = ({
+  search,
+  completed,
+  onSearchChange,
+  onCompletedChange,
+}: TodoFilterProps) => (
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
+        <select
+          data-cy="statusSelect"
+          value={completed}
+          onChange={event => {
+            onCompletedChange(event.target.value as CompletedFilter);
+          }}
+        >
+          <option value={CompletedFilter.All}>All</option>
+          <option value={CompletedFilter.Active}>Active</option>
+          <option value={CompletedFilter.Completed}>Completed</option>
         </select>
       </span>
     </p>
@@ -16,15 +42,24 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        value={search}
+        onChange={event => onSearchChange(event.target.value)}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
+      {search !== '' && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={() => onSearchChange('')}
+          />
+        </span>
+      )}
     </p>
   </form>
 );
