9624b08a02422a22e2c6da730f3e52d0df1dad74
Vitalii <vitalilkrutoy234@gmail.com>
2026-05-24 16:32:30 +0300

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,92 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
-import { TodoFilter } from './components/TodoFilter';
+import { CompletedFilter, TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+
+const getFilteredTodos = (
+  todos: Todo[],
+  filters: { search: string; completed: CompletedFilter },
+) => {
+  let filteredTodos = todos;
+  const preparedSearch = filters.search.trim().toLowerCase();
+
+  if (preparedSearch !== '') {
+    filteredTodos = filteredTodos.filter(todo =>
+      todo.title.toLowerCase().includes(preparedSearch),
+    );
+  }
+
+  if (filters.completed !== CompletedFilter.All) {
+    filteredTodos = filteredTodos.filter(todo => {
+      switch (filters.completed) {
+        case CompletedFilter.Active:
+          return !todo.completed;
+        case CompletedFilter.Completed:
+          return todo.completed;
+        default:
+          return true;
+      }
+    });
+  }
+
+  return filteredTodos;
+};
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [search, setSearch] = useState('');
+  const [completedFilter, setCompletedFilter] = useState(CompletedFilter.All);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(error => alert(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filteredTodos = getFilteredTodos(todos, {
+    search,
+    completed: completedFilter,
+  });
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
-
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                search={search}
+                completed={completedFilter}
+                onSearchChange={setSearch}
+                onCompletedChange={setCompletedFilter}
+              />
             </div>
-
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodo={selectedTodo}
+                  onSelectTodo={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal todo={selectedTodo} onClose={() => setSelectedTodo(null)} />
+      )}
     </>
   );
 };
