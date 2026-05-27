f0f00426267af4164fbb891616e398d37f93cf57
Oleksandr <kozyrev.sania@gmail.com>
2026-05-27 14:08:33 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,44 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [isData, setIsData] = useState(false);
+  const [todos, setTodo] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState('all');
+  const [selectedId, setSelectedId] = useState(0);
+
+  const visibleTodos = todos
+    .filter(todo => {
+      if (filter === 'active') {
+        return todo.completed === false;
+      }
+
+      if (filter === 'completed') {
+        return todo.completed === true;
+      }
+
+      return true;
+    })
+    .filter(todo => {
+      const lowerTitle = todo.title.toLocaleLowerCase();
+      const lowerQuery = query.toLocaleLowerCase();
+
+      return lowerTitle.includes(lowerQuery);
+    });
+
+  useEffect(() => {
+    getTodos()
+      .then(response => {
+        setTodo(response);
+      })
+      .catch(e => e)
+      .finally(() => setIsData(true));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +53,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={value => setQuery(value)}
+                filter={value => setFilter(value)}
+                reject={() => setQuery('')}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!isData && <Loader />}
+              {isData && (
+                <TodoList
+                  todos={visibleTodos}
+                  selectedUser={selectedId}
+                  changeSelectUser={(id: number) => {
+                    setSelectedId(id);
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedId !== 0 && (
+        <TodoModal
+          selectId={selectedId}
+          todos={todos}
+          reject={() => setSelectedId(0)}
+        />
+      )}
     </>
   );
 };
