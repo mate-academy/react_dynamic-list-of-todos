61582f829a99e71a642a022782d0d11efe35fe72
Anna Maltseva <aa.maltseva03@gmail.com>
2026-07-14 11:10:17 +0300

    added solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +8,60 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { getTodos, getUser } from './api';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoadingTodos, setIsLoadingTodos] = useState(true);
+
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [isLoadingUser, setIsLoadingUser] = useState(false);
+
+  const [query, setQuery] = useState('');
+  const [status, setStatus] = useState('all');
+
+  useEffect(() => {
+    setIsLoadingTodos(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setIsLoadingTodos(false));
+  }, []);
+
+  const openTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+    setSelectedUser(null);
+    setIsLoadingUser(true);
+
+    getUser(todo.userId)
+      .then(setSelectedUser)
+      .finally(() => setIsLoadingUser(false));
+  };
+
+  const closeTodo = () => {
+    setSelectedTodo(null);
+    setSelectedUser(null);
+  };
+
+  const visibleTodos = useMemo(() => {
+    return todos
+      .filter(todo => {
+        if (status === 'active') {
+          return !todo.completed;
+        }
+
+        if (status === 'completed') {
+          return todo.completed;
+        }
+
+        return true;
+      })
+      .filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+  }, [todos, status, query]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +70,38 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                status={status}
+                onQueryChange={setQuery}
+                onStatusChange={setStatus}
+                onClear={() => setQuery('')}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoadingTodos && <Loader />}
+
+              {!isLoadingTodos && (
+                <TodoList
+                  todos={visibleTodos}
+                  selectedTodo={selectedTodo}
+                  onSelect={openTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          user={selectedUser}
+          loading={isLoadingUser}
+          onClose={closeTodo}
+        />
+      )}
     </>
   );
 };
