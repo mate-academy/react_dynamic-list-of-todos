e396f55ce20476161782736b2300d0a2eca3220c
Maksym Savchenko <maks.20050817@gmail.com>
2026-07-17 20:52:08 +0200

    ready to check

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,39 +1,72 @@
 import React from 'react';
+import { ModalData } from '../../App';
 import { Loader } from '../Loader';
 
-export const TodoModal: React.FC = () => {
+interface Props {
+  isActive: boolean;
+  isLoading: boolean;
+  hasError: boolean;
+  modalData: ModalData;
+  onClose: () => void;
+}
+
+export const TodoModal: React.FC<Props> = ({
+  isActive,
+  isLoading,
+  hasError,
+  modalData,
+  onClose,
+}) => {
+  if (!isActive) {
+    return null;
+  }
+
   return (
     <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+      <div className="modal-background" role="presentation" onClick={onClose} />
 
-      {true ? (
-        <Loader />
-      ) : (
+      {isLoading && <Loader />}
+
+      {hasError && !isLoading && (
+        <p className="has-text-danger">Failed to load user</p>
+      )}
+
+      {modalData !== null && !hasError && (
         <div className="modal-card">
           <header className="modal-card-head">
             <div
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{modalData.todo.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onClose}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {modalData.todo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {modalData.todo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${modalData.user.email}`}>
+                {modalData.user.name}
+              </a>
             </p>
           </div>
         </div>
