ecd8732cf9349c72835cce7ad62ea272167f073b
LimeRider <cgakaviy@gmail.com>
2026-08-18 15:05:18 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import { TodoList } from './components/TodoList';
-import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodoFilter } from './components/TodoFilter/TodoFilter';
+import { TodoModal } from './components/TodoModal/TodoModal';
+import { Loader } from './components/Loader/Loader';
 import { Todo } from './types/Todo';
 import { Status } from './types/Status';
 import { getFilteredTodos } from './components/utils/getFilteredTodos';
