20eedb178b7e439de705f4fdbc34a963d5467e7b
Yelyzaveta Romanovska <lizarom2608@gmail.com>
2026-09-13 19:43:34 +0200

    (tests fail)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,28 +7,94 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
+  const [filterBy, setFilterBy] = useState<Filter>('all');
+  const [query, setQuery] = useState<string>('');
+  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<null | number>(null);
+  const [firstRendering, setFirstRendering] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(todos => {
+        let list: Todo[] = [];
+
+        switch (filterBy) {
+          case 'active':
+            list = todos.filter(todo => !todo.completed);
+            break;
+          case 'completed':
+            list = todos.filter(todo => todo.completed);
+            break;
+          case 'all':
+          default:
+            list = todos;
+        }
+
+        if (query) {
+          list = list.filter(todo =>
+            todo.title.toLowerCase().includes(query.toLowerCase()),
+          );
+        }
+
+        setVisibleTodos(list);
+        setFirstRendering(false);
+      })
+      .catch(setError);
+  }, [filterBy, query]);
+
+  const getTodoById = useCallback(
+    (id: number): Todo | null => {
+      return visibleTodos.find(todo => todo.id === id) || null;
+    },
+    [visibleTodos],
+  );
+
   return (
-    <>
-      <div className="section">
-        <div className="container">
-          <div className="box">
-            <h1 className="title">Todos:</h1>
+    error || (
+      <>
+        <div className="section">
+          <div className="container">
+            <div className="box">
+              <h1 className="title">Todos:</h1>
 
-            <div className="block">
-              <TodoFilter />
-            </div>
+              <div className="block">
+                <TodoFilter
+                  onFilter={setFilterBy}
+                  onInput={setQuery}
+                  value={query}
+                />
+              </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
+              <div className="block">
+                {firstRendering ? (
+                  <Loader />
+                ) : (
+                  <TodoList
+                    todos={visibleTodos}
+                    selected={selectedTodo}
+                    onSelect={setSelectedTodo}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <TodoModal />
-    </>
+        {selectedTodo && (
+          <TodoModal
+            todo={getTodoById(selectedTodo)}
+            onClose={setSelectedTodo}
+            onError={setError}
+            errorMessage={error}
+          />
+        )}
+      </>
+    )
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
-  return (
+type Props = {
+  todo: Todo | null;
+  onClose: (selected: null) => void;
+  onError: (message: string) => void;
+  errorMessage: string;
+};
+
+export const TodoModal: React.FC<Props> = React.memo(function TodoModal({
+  todo,
+  onClose,
+  onError,
+  errorMessage,
+}) {
+  const [user, setUser] = useState<User>();
+  const [rendering, setRendering] = useState(true);
+
+  useEffect(() => {
+    if (todo) {
+      getUser(todo.id)
+        .then(author => {
+          setRendering(false);
+          setUser(author);
+        })
+        .catch(onError);
+    } else {
+      setRendering(false);
+    }
+  }, [todo, onError]);
+
+  return errorMessage || todo === null ? (
+    'Error'
+  ) : (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {rendering ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,29 +48,37 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{todo?.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => onClose(null)}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo?.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {todo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
             </p>
           </div>
         </div>
       )}
     </div>
   );
-};
+});
